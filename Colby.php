30088875f3bffdf3e34f5a82f107908af9dc38eb
matt <matt@52b490d9-f4c4-4f3c-888f-01cccff8b323>
2012-01-23 07:42:33 +0000

    Version 0.0.7 -> 0.0.8
    added a sample configuration file
    
    git-svn-id: http://svn.mattifesto.com/colby/trunk@9 52b490d9-f4c4-4f3c-888f-01cccff8b323

--- Colby.php
+++ Colby.php
@@ -3,7 +3,7 @@
 //
 // colby
 //
-// version 0.0.7
+// version 0.0.8
 //
 
 error_reporting(E_ALL | E_STRICT);
---
+++ setup/colby-configuration.php
@@ -0,0 +1,12 @@
+<?php
+
+define('COLBY_SITE_PATH', __DIR__);
+define('COLBY_SITE_URL', 'http://example.com/');
+
+define('COLBY_FACEBOOK_APP_ID', '012345678901234');
+define('COLBY_FACEBOOK_APP_SECRET', '0123456789abcdef01234567890abcde');
+
+define('COLBY_MYSQL_HOST', 'mysql.example.com');
+define('COLBY_MYSQL_USER', 'user');
+define('COLBY_MYSQL_PASSWORD', 'password');
+define('COLBY_MYSQL_DATABASE', 'example');
