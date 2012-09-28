b1be416939763ecc59f8293395362f6680b9da83
Matt Calkins <matt@mattifesto.com>
2012-09-27 18:46:14 -0700

    Version 2.6.2.002
    Check for odd website state that would prevent a good installation.
    
    Add data entry fields for configuration information.

--- setup/index.php
+++ setup/index.php
@@ -25,24 +25,76 @@ class ColbySetup
     }
 }
 
+$indexFilename = COLBY_SITE_DIR . '/index.php';
+$configurationFilename = COLBY_SITE_DIR . '/colby-configuration.php';
+$htaccessFilename = COLBY_SITE_DIR . '/.htaccess';
+
 ColbySetup::begin();
 
 ?>
+
+<style>
+input[type=text]
+{
+    width: 400px;
+    padding: 2px;
+}
+
+dd
+{
+    margin: 5px 0px 15px;
+}
+</style>
 
 <h1>Colby Setup</h1>
 
 <?php
 
-if (is_file(COLBY_SITE_DIR . '/index.php'))
+if (is_file($indexFilename))
 {
     echo '<p>This site has already been set up.';
 
     goto done;
 }
 
+if (is_file($htaccessFilename))
+{
+    echo '<p style="color: red;">.htaccess already exists but index.php doesn\'t<br>manual setup required';
+
+    goto done;
+}
+
+if (is_file($configurationFilename))
+{
+    echo '<p style="color: red;">colby-configuration.php already exists but index.php doesn\'t<br>manual setup required';
+
+    goto done;
+}
+
 ?>
 
-<pre><?php echo var_export($_SERVER, true); ?></pre>
+<dl>
+    <dt>Site URL</dt>
+    <dd><input type="text" id="colby-site-url" value="http://<?php echo $_SERVER['SERVER_NAME']; ?>"></dd>
+    <dt>Site Name</dt>
+    <dd><input type="text" id="colby-site-name" value=""></dd>
+    <dt>Site Administrator Email Address</dt>
+    <dd><input type="text" id="colby-site-administrator" value=""></dd>
+    <dt>Facebook App ID</dt>
+    <dd><input type="text" id="colby-facebook-app-id" value=""></dd>
+    <dt>Facebook App Secret</dt>
+    <dd><input type="text" id="colby-facebook-app-secret" value=""></dd>
+    <dt>MySQL Host</dt>
+    <dd><input type="text" id="colby-mysql-host" value=""></dd>
+    <dt>MySQL Database</dt>
+    <dd><input type="text" id="colby-mysql-database" value=""></dd>
+    <dt>MySQL User</dt>
+    <dd><input type="text" id="colby-mysql-user" value=""></dd>
+    <dt>MySQL Password</dt>
+    <dd><input type="text" id="colby-mysql-password" value=""></dd>
+    <dt>Developer Information</dt>
+    <dd><input type="checkbox" id="colby-site-is-being-debugged"> site is being debugged</dd>
+</dl>
 
 <?php
 
