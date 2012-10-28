567f41373018fe71d675a105de7c925f8fda574b
Matt Calkins <matt@mattifesto.com>
2012-10-28 01:16:54 -0700

    Version 2.7.1.009
    modified:   css/equalize.css
    -   removed deprecated browser specific settings
    -   updated form element styles
    
    modified:   handlers/handle,edit-generic-blog-post,.php
    -   added editing form with minimal functionality
        still doesn't work for real

--- handlers/handle,edit-generic-blog-post,.php
+++ handlers/handle,edit-generic-blog-post,.php
@@ -21,9 +21,47 @@ ColbyPage::beginAdmin($args);
 
 ?>
 
+<style>
+input,
+textarea
+{
+    display: block;
+    width: 100%;
+    padding: 4px;
+    margin-top: 5px;
+    border: 1px solid LightGray;
+}
+
+fieldset
+{
+    width: 600px;
+    border: none;
+}
+
+fieldset > div + div
+{
+    margin-top: 10px;
+}
+</style>
+
 <h1>Generic Blog Post Editor</h1>
 
 <p>fileId: <?php echo $fileId; ?>
+
+<fieldset>
+    <div>Title <input type="text" onkeypress="handleKeyPressed(this);"></div>
+    <div>Content <textarea style="height: 400px;" onkeypress="handleKeyPressed(this);"></textarea></div>
+    <div><input type="date"></div>
+</fieldset>
+
+<script>
+"use strict";
+
+function handleKeyPressed(sender)
+{
+    sender.style.backgroundColor = 'LightYellow';
+}
+</script>
 
 <?php
 
