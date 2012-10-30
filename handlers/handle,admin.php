625785cdb963c775745325725da252d687dd11df
Matt Calkins <matt@mattifesto.com>
2012-10-29 19:17:16 -0700

    Version 2.7.1.013
    Added official admin area that will use handle filenaming patterns to find sub-areas. Renamed blog admin handler to match new pattern.

---
+++ handlers/handle,admin.php
@@ -0,0 +1,29 @@
+<?php
+
+ColbyPage::requireVerifiedUser();
+
+$args = new stdClass();
+$args->title = 'Site Administration';
+$args->description = 'Edit the settings and content of this website.';
+
+ColbyPage::beginAdmin($args);
+
+?>
+
+<h1>Site Administration</h1>
+
+<?php
+
+$adminSections = glob(COLBY_SITE_DIRECTORY . '/colby/handlers/handle,admin~*.php');
+$adminSections = $adminSections + glob(COLBY_SITE_DIRECTORY . '/handlers/handle,admin~*.php');
+
+$adminSections = preg_grep('/handle,admin~[^~]*.php$/', $adminSections);
+
+foreach ($adminSections as $adminSection)
+{
+    preg_match('/handle,admin~(.*).php$/', $adminSection, $matches);
+
+    echo "<p><a href=\"/admin/{$matches[1]}/\">{$matches[1]}</a>\n";
+}
+
+ColbyPage::end();
