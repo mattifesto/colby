be0ccde6ae81c2192af576978099f33bf3407ae5
Matt Calkins <matt@mattifesto.com>
2012-11-07 23:51:19 -0800

    Change $rootObject to $data.

--- handlers/handle,admin,blog,d74e2f3d347395acdb627e7c57516c3c4c94e988,ajax,update.php
+++ handlers/handle,admin,blog,d74e2f3d347395acdb627e7c57516c3c4c94e988,ajax,update.php
@@ -13,29 +13,29 @@ $response->message = 'incomplete';
 
 $archive = ColbyArchive::open($_POST['archive-id']);
 
-$rootObject = $archive->rootObject();
+$data = $archive->rootObject();
 
 if (!$archive->attributes()->created)
 {
-    $rootObject->type = 'd74e2f3d347395acdb627e7c57516c3c4c94e988';
+    $data->type = 'd74e2f3d347395acdb627e7c57516c3c4c94e988';
 }
 
-$rootObject->title = $_POST['title'];
-$rootObject->titleHTML = ColbyConvert::textToHTML($rootObject->title);
-$rootObject->stub = ColbyConvert::textToStub($_POST['title']);
-$rootObject->subhead = $_POST['subhead'];
-$rootObject->subheadHTML = ColbyConvert::textToHTML($rootObject->subhead);
-$rootObject->content = $_POST['content'];
-$rootObject->contentHTML = ColbyConvert::textToFormattedContent($rootObject->content);
-$rootObject->published = NULL;
+$data->title = $_POST['title'];
+$data->titleHTML = ColbyConvert::textToHTML($data->title);
+$data->stub = ColbyConvert::textToStub($_POST['title']);
+$data->subhead = $_POST['subhead'];
+$data->subheadHTML = ColbyConvert::textToHTML($data->subhead);
+$data->content = $_POST['content'];
+$data->contentHTML = ColbyConvert::textToFormattedContent($data->content);
+$data->published = null;
 
 $archive->save();
 
 ColbyBlog::updateDatabaseWithPostArchive($archive);
 
-$response->stub = $rootObject->stub;
+$response->stub = $data->stub;
 $response->wasSuccessful = true;
-$response->message = 'Post Last Updated: ' . ColbyConvert::timestampToLocalUserTime(time());
+$response->message = 'Post last updated: ' . ColbyConvert::timestampToLocalUserTime(time());
 
 echo json_encode($response);
 
