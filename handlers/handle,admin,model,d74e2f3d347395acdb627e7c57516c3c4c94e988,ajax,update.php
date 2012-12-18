a5b6aa037bb5a5975497b1a854cd6f5b7a68ffe9
Matt Calkins <matt@mattifesto.com>
2012-12-17 19:10:23 -0800

    Version 3.0.0.010
    modified:   handlers/handle,admin,model,d74e2f3d347395acdb627e7c57516c3c4c94e988,ajax,update.php
    -   Changed s/pageModel/model/

--- handlers/handle,admin,model,d74e2f3d347395acdb627e7c57516c3c4c94e988,ajax,update.php
+++ handlers/handle,admin,model,d74e2f3d347395acdb627e7c57516c3c4c94e988,ajax,update.php
@@ -4,32 +4,32 @@ $response = ColbyOutputManager::beginVerifiedUserAjaxResponse();
 
 $archive = ColbyArchive::open($_POST['archive-id']);
 $data = $archive->data();
-$pageModel = ColbyPageModel::modelWithData($data);
+$model = ColbyPageModel::modelWithData($data);
 
-if (!$pageModel->viewId())
+if (!$model->viewId())
 {
-    $pageModel->setViewId($_POST['view-id']);
+    $model->setViewId($_POST['view-id']);
 }
 
-$pageModel->setTitle($_POST['title']);
+$model->setTitle($_POST['title']);
 
-$pageModel->setSubtitle($_POST['subtitle']);
+$model->setSubtitle($_POST['subtitle']);
 
-$pageModel->setPageStubData($_POST['preferred-page-stub'],
+$model->setPageStubData($_POST['preferred-page-stub'],
                             $_POST['stub-is-locked'],
                             $_POST['custom-page-stub-text']);
 
-$pageModel->setPublicationData($_POST['is-published'],
+$model->setPublicationData($_POST['is-published'],
                                $_POST['published-by'],
                                $_POST['publication-date']);
 
 $data->content = $_POST['content'];
 $data->contentHTML = ColbyConvert::textToFormattedContent($data->content);
 
-$pageModel->updateDatabase();
+$model->updateDatabase();
 $archive->save();
 
-$response->pageStub = $pageModel->pageStub();
+$response->pageStub = $model->pageStub();
 $response->wasSuccessful = true;
 //$response->message = var_export($_POST, true);
 $response->message = 'Post last updated: ' . ColbyConvert::timestampToLocalUserTime(time());
