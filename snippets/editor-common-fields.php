8a9e77070bef1de06e4fd06397d5b4a45ebab352
Matt Calkins <matt@mattifesto.com>
2012-12-26 16:34:48 -0800

    Version 3.1.0.022
    modified:   snippets/editor-common-fields.php
    -   Unindented many lines that were left indented for the previous checkin so the diff would be more meaningful. The git diff for this looks horrible, but there are no code changes, just unindenting.

--- snippets/editor-common-fields.php
+++ snippets/editor-common-fields.php
@@ -20,83 +20,83 @@ $javascriptPublicationDate = $archive->valueForKey('publicationDate') * 1000;
 
 ?>
 
-    <!-- common fields -->
+<!-- common fields -->
 
-    <input type="hidden" id="archive-id" value="<?php echo $archive->archiveId(); ?>">
-    <input type="hidden" id="view-id" value="<?php echo $archive->valueForKey('viewId'); ?>">
-    <input type="hidden" id="preferred-page-stub" value="<?php echo $archive->valueForKey('preferredPageStub'); ?>">
-    <input type="hidden" id="published-by" value="<?php echo $archive->valueForKey('publishedBy'); ?>">
-    <input type="hidden" id="publication-date" value="<?php echo $archive->valueForKey('publicationDate'); ?>">
+<input type="hidden" id="archive-id" value="<?php echo $archive->archiveId(); ?>">
+<input type="hidden" id="view-id" value="<?php echo $archive->valueForKey('viewId'); ?>">
+<input type="hidden" id="preferred-page-stub" value="<?php echo $archive->valueForKey('preferredPageStub'); ?>">
+<input type="hidden" id="published-by" value="<?php echo $archive->valueForKey('publishedBy'); ?>">
+<input type="hidden" id="publication-date" value="<?php echo $archive->valueForKey('publicationDate'); ?>">
 
-    <progress value="0"
-              style="width: 100px; float: right;"></progress>
+<progress value="0"
+          style="width: 100px; float: right;"></progress>
 
-    <div><label>Title
+<div><label>Title
+    <input type="text"
+           id="title"
+           value="<?php echo $archive->valueForKey('titleHTML'); ?>">
+</label></div>
+
+<div style="padding: 0px 50px; font-size: 0.75em;">
+    <style scoped="scoped">
+        .stub
+        {
+            font-family: "Courier New", monospace;
+        }
+
+        table.stubs
+        {
+            width: 100%;
+            margin-bottom: 5px;
+        }
+
+        table.stubs tr td:first-child
+        {
+            width: 100px;
+            text-align: right;
+        }
+    </style>
+    <table class="stubs"><tr>
+        <td>Preferred URL:</td>
+        <td id="preferred-stub-view" class="stub"><?php echo $archive->model->preferredStub(); ?></td>
+    </tr><tr>
+        <td>Actual URL:</td>
+        <td id="stub-view" class="stub"><?php echo $archive->model->stub(); ?></td>
+    </td></table>
+    <label style="float:right; margin-left: 20px;">
+        <input type="checkbox"
+               id="stub-is-locked"
+               <?php echo $stubIsLockedChecked; ?>> Lock Stub
+    </label>
+    <label>Custom Stub Text
         <input type="text"
-               id="title"
-               value="<?php echo $archive->valueForKey('titleHTML'); ?>">
-    </label></div>
+               id="custom-page-stub-text"
+               value="<?php echo $customPageStubTextHTML; ?>">
+    </label>
+</div>
 
-    <div style="padding: 0px 50px; font-size: 0.75em;">
-        <style scoped="scoped">
-            .stub
-            {
-                font-family: "Courier New", monospace;
-            }
+<div><label>Subtitle
+    <input type="text"
+           id="subtitle"
+           value="<?php echo $archive->valueForKey('subtitleHTML'); ?>">
+</label></div>
 
-            table.stubs
-            {
-                width: 100%;
-                margin-bottom: 5px;
-            }
-
-            table.stubs tr td:first-child
-            {
-                width: 100px;
-                text-align: right;
-            }
-        </style>
-        <table class="stubs"><tr>
-            <td>Preferred URL:</td>
-            <td id="preferred-stub-view" class="stub"><?php echo $archive->model->preferredStub(); ?></td>
-        </tr><tr>
-            <td>Actual URL:</td>
-            <td id="stub-view" class="stub"><?php echo $archive->model->stub(); ?></td>
-        </td></table>
-        <label style="float:right; margin-left: 20px;">
-            <input type="checkbox"
-                   id="stub-is-locked"
-                   <?php echo $stubIsLockedChecked; ?>> Lock Stub
-        </label>
-        <label>Custom Stub Text
-            <input type="text"
-                   id="custom-page-stub-text"
-                   value="<?php echo $customPageStubTextHTML; ?>">
-        </label>
-    </div>
-
-    <div><label>Subtitle
+<div>
+    <label style="float: right;">
+        <input type="checkbox"
+               id="is-published"
+               <?php echo $isPublishedChecked; ?>
+               onchange="handleIsPublishedChanged(this);">
+    Published</label>
+    <label>Publication Date:
         <input type="text"
-               id="subtitle"
-               value="<?php echo $archive->valueForKey('subtitleHTML'); ?>">
-    </label></div>
+               id="publication-date-text"
+               class="ignore"
+               onblur="handlePublicationDateBlurred(this);">
+    </label>
+</div>
 
-    <div>
-        <label style="float: right;">
-            <input type="checkbox"
-                   id="is-published"
-                   <?php echo $isPublishedChecked; ?>
-                   onchange="handleIsPublishedChanged(this);">
-        Published</label>
-        <label>Publication Date:
-            <input type="text"
-                   id="publication-date-text"
-                   class="ignore"
-                   onblur="handlePublicationDateBlurred(this);">
-        </label>
-    </div>
-
-    <!-- end common fields -->
+<!-- end common fields -->
 
 <div id="error-log"></div>
 
