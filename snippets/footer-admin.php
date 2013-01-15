b479ef488d63a01972a3ae0bc963aa4d1c82b29f
Matt Calkins <matt@mattifesto.com>
2013-01-14 18:54:28 -0800

    Version 3.004
    new file:   snippets/footer-admin.php
    -   Added a simple footer because the pages were ending flush with the bottom of the browser window.
    
    modified:   snippets/markaround-help.txt
    -   Updated text and formatting to be more clear and understandable.

---
+++ snippets/footer-admin.php
@@ -0,0 +1,7 @@
+        <div class="menubar" style="margin-top: 100px;">
+            <div style="font-weight: normal; text-align: center;">
+                Copyright &copy; 2012-2013 Mattifesto Design
+            </div>
+        </div>
+    </body>
+</html>
