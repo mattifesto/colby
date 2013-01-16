fb6b722690bd864916052983ed5f598397a16ab1
Matt Calkins <matt@mattifesto.com>
2013-01-16 14:48:31 -0800

    Version 3.007
    modified:   css/admin.css
    -   Gave pleasant margins to the `fieldset` element which center forms
    
    modified:   snippets/editor-common-fields.php
    -   Organized and simplified

--- snippets/editor-common-fields.php
+++ snippets/editor-common-fields.php
@@ -7,35 +7,28 @@
  * $modelId string
  */
 
-// mise en place
-
-$ajaxURL = COLBY_SITE_URL . "/admin/model/{$modelId}/ajax/update/";
-
-$customPageStubTextHTML = $archive->valueForKey('customPageStubTextHTML');
-$stubIsLockedChecked = $archive->valueForKey('stubIsLocked') ? ' checked="checked"' : '';
-
-$isPublishedChecked = $archive->valueForKey('isPublished') ? ' checked="checked"' : '';
-$currentUserId = ColbyUser::currentUserId();
-$javascriptPublicationDate = $archive->valueForKey('publicationDate') * 1000;
-
 ?>
 
-<!-- common fields -->
+<input type="hidden" id="archive-id"
+       value="<?php echo $archive->archiveId(); ?>">
+<input type="hidden" id="view-id"
+       value="<?php echo $archive->valueForKey('viewId'); ?>">
+<input type="hidden" id="preferred-page-stub"
+       value="<?php echo $archive->valueForKey('preferredPageStub'); ?>">
+<input type="hidden" id="published-by"
+       value="<?php echo $archive->valueForKey('publishedBy'); ?>">
+<input type="hidden" id="publication-date"
+        value="<?php echo $archive->valueForKey('publicationDate'); ?>">
 
-<input type="hidden" id="archive-id" value="<?php echo $archive->archiveId(); ?>">
-<input type="hidden" id="view-id" value="<?php echo $archive->valueForKey('viewId'); ?>">
-<input type="hidden" id="preferred-page-stub" value="<?php echo $archive->valueForKey('preferredPageStub'); ?>">
-<input type="hidden" id="published-by" value="<?php echo $archive->valueForKey('publishedBy'); ?>">
-<input type="hidden" id="publication-date" value="<?php echo $archive->valueForKey('publicationDate'); ?>">
+<progress value="0" style="width: 100px; float: right;"></progress>
 
-<progress value="0"
-          style="width: 100px; float: right;"></progress>
-
-<div><label>Title
-    <input type="text"
-           id="title"
-           value="<?php echo $archive->valueForKey('titleHTML'); ?>">
-</label></div>
+<div>
+    <label>Title
+        <input type="text"
+               id="title"
+               value="<?php echo $archive->valueForKey('titleHTML'); ?>">
+    </label>
+</div>
 
 <div style="padding: 0px 50px; font-size: 0.75em;">
     <style scoped="scoped">
@@ -56,6 +49,7 @@ $javascriptPublicationDate = $archive->valueForKey('publicationDate') * 1000;
             text-align: right;
         }
     </style>
+
     <table class="stubs"><tr>
         <td>Preferred URL:</td>
         <td id="preferred-stub-view" class="stub"><?php echo $archive->model->preferredStub(); ?></td>
@@ -63,51 +57,52 @@ $javascriptPublicationDate = $archive->valueForKey('publicationDate') * 1000;
         <td>Actual URL:</td>
         <td id="stub-view" class="stub"><?php echo $archive->model->stub(); ?></td>
     </td></table>
+
     <label style="float:right; margin-left: 20px;">
-        <input type="checkbox"
-               id="stub-is-locked"
-               <?php echo $stubIsLockedChecked; ?>> Lock Stub
+        <input type="checkbox" id="stub-is-locked"
+               <?php if ($archive->valueForKey('stubIsLocked')) echo 'checked'; ?>>
+        Locked
     </label>
-    <label>Custom Stub Text
-        <input type="text"
-               id="custom-page-stub-text"
-               value="<?php echo $customPageStubTextHTML; ?>">
+
+    <label>
+        Custom Stub Text
+        <input type="text" id="custom-page-stub-text"
+               value="<?php echo $archive->valueForKey('customPageStubTextHTML'); ?>">
     </label>
 </div>
 
-<div><label>Subtitle
-    <input type="text"
-           id="subtitle"
-           value="<?php echo $archive->valueForKey('subtitleHTML'); ?>">
-</label></div>
+<div>
+    <label>
+        Subtitle
+        <input type="text" id="subtitle"
+               value="<?php echo $archive->valueForKey('subtitleHTML'); ?>">
+    </label>
+</div>
 
 <div>
     <label style="float: right;">
-        <input type="checkbox"
-               id="is-published"
-               <?php echo $isPublishedChecked; ?>
+        <input type="checkbox" id="is-published"
+               <?php if ($archive->valueForKey('isPublished')) echo 'checked'; ?>
                onchange="ColbyPageEditor.handleIsPublishedChanged(this);">
-    Published</label>
-    <label>Publication Date:
-        <input type="text"
-               id="publication-date-text"
-               class="ignore"
+        Published
+    </label>
+
+    <label>
+        Publication Date:
+        <input type="text" id="publication-date-text" class="ignore"
                onblur="ColbyPageEditor.handlePublicationDateBlurred(this);">
     </label>
 </div>
-
-<!-- end common fields -->
 
 <div id="error-log"></div>
 
 <script>
 "use strict";
 
-var ajaxURL = '<?php echo $ajaxURL; ?>'
-var currentUserId = <?php echo $currentUserId; ?>;
+var ajaxURL = '<?php echo COLBY_SITE_URL . "/admin/model/{$modelId}/ajax/update/"; ?>'
+var currentUserId = <?php echo ColbyUser::currentUserId(); ?>;
 var groupStub = '<?php echo $archive->valueForKey('groupStub'); ?>';
-var publicationDate = <?php echo $javascriptPublicationDate; ?>;
-
+var publicationDate = <?php echo $archive->valueForKey('publicationDate') * 1000; ?>;
 </script>
 
 <script src="<?php echo COLBY_SITE_URL . '/colby/javascript/ColbyPageEditor.js'; ?>"></script>
