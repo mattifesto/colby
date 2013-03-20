49e4a5536a0f70ce928545f9b7253d5fe3cf2626
Matt Calkins <matt@mattifesto.com>
2013-03-19 20:29:31 -0700

    Version 20

--- version.php
+++ version.php
@@ -1,6 +1,6 @@
 <?php
 
-define('COLBY_VERSION_NUMBER', 19);
+define('COLBY_VERSION_NUMBER', 20);
 
 define('COLBY_DIRECTORY', __DIR__);
 define('COLBY_URL', COLBY_SITE_URL . '/' . basename(__DIR__));
