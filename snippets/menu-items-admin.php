1b5bb14f29bb1ce2bef6425aec0b4ba11c2d9249
Matt Calkins <matt@mattifesto.com>
2013-03-28 20:53:02 -0700

    Version 21.007
    modified:   snippets/menu-items-admin.php
    -   Renamed menu items to match new document paradigm.
    -   Reordered menu items.

--- snippets/menu-items-admin.php
+++ snippets/menu-items-admin.php
@@ -8,11 +8,11 @@ if (ColbyUser::current()->isOneOfThe('Administrators'))
 
     <li><a href="/admin/pages/">Pages</a></li>
     <li><a href="/admin/blog/">Blog</a></li>
-    
+
     <li><h1>Help</h1></li>
 
     <li><a href="/admin/help/markaround-syntax/">Markaround</a></li>
-    
+
     <?php
 }
 
@@ -22,11 +22,11 @@ if (ColbyUser::current()->isOneOfThe('Developers'))
 
     <li><h1>Developers</h1></li>
 
-    <li><a href="/developer/test/">Tests</a></li>
-    <li><a href="/developer/models/">Models</a></li>
-    <li><a href="/developer/views/">Views</a></li>
-    <li><a href="/developer/groups/">Groups</a></li>
     <li><a href="/developer/archives/">Archives</a></li>
+    <li><a href="/developer/groups/">Document Groups</a></li>
+    <li><a href="/developer/models/">Document Types</a></li>
+    <li><a href="/developer/views/">Document Views</a></li>
+    <li><a href="/developer/test/">Tests</a></li>
 
     <?php
 }
