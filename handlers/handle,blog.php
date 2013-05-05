84134404953cdb958506d252c6762b1fd62eb5d4
Matt Calkins <matt@mattifesto.com>
2013-05-04 22:34:44 -0700

    Version 23.009
    modified:   handlers/handle,blog.php
    -   Wrapped all of the content of the page in a `main` element.

--- handlers/handle,blog.php
+++ handlers/handle,blog.php
@@ -11,45 +11,46 @@ $page->begin();
 
 ?>
 
-<h1 style="text-align: center;">Blog</h1>
+<main>
+    <h1 style="text-align: center;">Blog</h1>
 
-<section style="width: 800px; margin: 50px auto 0px;">
-    <style>
-        article
-        {
-            margin-bottom: 20px;
-            overflow: hidden; /* contains floated thumbnail */
-        }
+    <section style="width: 800px; margin: 50px auto 0px;">
+        <style>
+            article
+            {
+                margin-bottom: 20px;
+                overflow: hidden; /* contains floated thumbnail */
+            }
 
-        article > h1
-        {
-            font-size: 1.5em;
-        }
+            article > h1
+            {
+                font-size: 1.5em;
+            }
 
-        article > h2
-        {
-            font-size: 1.0em;
-        }
+            article > h2
+            {
+                font-size: 1.0em;
+            }
 
-        div.img
-        {
-            width: 100px;
-            height: 100px;
-            margin-right: 10px;
-            float: left;
-            background-color: #fffff8;
-        }
+            div.img
+            {
+                width: 100px;
+                height: 100px;
+                margin-right: 10px;
+                float: left;
+                background-color: #fffff8;
+            }
 
-        img.thumbnail
-        {
-            max-width: 100px;
-            max-height: 100px;
-        }
-    </style>
+            img.thumbnail
+            {
+                max-width: 100px;
+                max-height: 100px;
+            }
+        </style>
 
-    <?php
+        <?php
 
-    $sql = <<<EOT
+        $sql = <<<EOT
 SELECT
     `stub`,
     `titleHTML`,
@@ -64,46 +65,47 @@ ORDER BY
     `published` DESC
 EOT;
 
-    $result = Colby::query($sql);
+        $result = Colby::query($sql);
 
-    if ($result->num_rows > 0)
-    {
-        while ($row = $result->fetch_object())
+        if ($result->num_rows > 0)
         {
-            $postURL = COLBY_SITE_URL . "/{$row->stub}/";
+            while ($row = $result->fetch_object())
+            {
+                $postURL = COLBY_SITE_URL . "/{$row->stub}/";
 
-            ?>
+                ?>
 
-            <article>
-                <div class="img">
-
-                    <?php
-
-                    if ($row->thumbnailURL)
-                    {
-                        ?>
-
-                        <img src="<?php echo $row->thumbnailURL; ?>" alt="" class="thumbnail">
+                <article>
+                    <div class="img">
 
                         <?php
-                    }
 
-                    ?>
+                        if ($row->thumbnailURL)
+                        {
+                            ?>
 
-                </div>
-                <h1><a href="<?php echo $postURL; ?>"><?php echo $row->titleHTML; ?></a></h1>
-                <h2><?php echo $row->subtitleHTML; ?></h2>
-            </article>
+                            <img src="<?php echo $row->thumbnailURL; ?>" alt="" class="thumbnail">
 
-            <?php
+                            <?php
+                        }
+
+                        ?>
+
+                    </div>
+                    <h1><a href="<?php echo $postURL; ?>"><?php echo $row->titleHTML; ?></a></h1>
+                    <h2><?php echo $row->subtitleHTML; ?></h2>
+                </article>
+
+                <?php
+            }
         }
-    }
 
-    $result->free();
+        $result->free();
 
-    ?>
+        ?>
 
-</section>
+    </section>
+</main>
 
 <?php
 
