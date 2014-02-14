29349c1f1a3348bbf56c2c35cb70aed54db161fa
Matt Calkins (MacBook) <matt@mattifesto.com>
2014-02-13 22:13:27 -0800

    Version 53.28
    modified:   javascript/CBSection.js
    -   Added code to create an interface to insert and remove sections for a page.

--- javascript/CBSection.js
+++ javascript/CBSection.js
@@ -1,29 +1,65 @@
-"using strict";
+"use strict";
 
 /**
  *
  */
-function CBSection(title)
+function CBSection(sectionID, title)
 {
-    var outerElement    = document.createElement("section");
+    var sectionElementID    = "s" + sectionID;
+    var outerElement        = document.createElement("section");
+    outerElement.id         = sectionElementID;
     this._outerElement  = outerElement;
 
 
     var header      = document.createElement("header");
     header.appendChild(document.createTextNode(title));
     this._header    = header;
-
     outerElement.appendChild(header);
+
+
+    var selectionControl    = new CBSelectionControl(" insert ");
+    this._selectionControl  = selectionControl;
+    header.appendChild(selectionControl.rootElement());
+
+    for (var ID in CBSectionDescriptors)
+    {
+        selectionControl.appendOption(ID, CBSectionDescriptors[ID].name);
+    }
+
+
+    var button  = document.createElement("button");
+    button.appendChild(document.createTextNode("Insert"));
+    header.appendChild(button);
+
+    var insertSection = function()
+    {
+        var newSectionTypeID = selectionControl.value();
+
+        CBPageEditor.insertNewSectionBefore(newSectionTypeID, sectionID);
+    }
+
+    button.addEventListener('click', insertSection, false);
+
+
+    var button  = document.createElement("button");
+    button.appendChild(document.createTextNode("Delete"));
+    header.appendChild(button);
+
+    var removeSection = function()
+    {
+        CBPageEditor.removeSection(sectionID);
+    }
+
+    button.addEventListener('click', removeSection, false);
 
 
     var innerElement    = document.createElement("div");
     this._innerElement  = innerElement;
-
     outerElement.appendChild(innerElement);
 }
 
 /**
- *
+ * @return void
  */
 CBSection.prototype.innerElement = function()
 {
@@ -31,7 +67,7 @@ CBSection.prototype.innerElement = function()
 }
 
 /**
- *
+ * @return void
  */
 CBSection.prototype.outerElement = function()
 {
