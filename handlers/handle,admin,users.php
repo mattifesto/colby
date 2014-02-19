de6b2026701cfc67dfa38c3b61447db152385c5b
Matt Calkins (MacBook) <matt@mattifesto.com>
2014-02-18 21:40:00 -0800

    Version 53.38
    modified:   handlers/handle,admin,users.php
    -   Update to use `CBHTMLOutput`.
    -   Update design of page.
    -   Implement new menu system.

--- handlers/handle,admin,users.php
+++ handlers/handle,admin,users.php
@@ -2,10 +2,13 @@
 
 $page = new ColbyOutputManager('admin-html-page');
 
-$page->titleHTML = 'Users';
-$page->descriptionHTML = 'Manage users.';
+include_once CBSystemDirectory . '/classes/CBHTMLOutput.php';
 
-$page->begin();
+
+CBHTMLOutput::setTitleHTML('Permissions');
+CBHTMLOutput::setDescriptionHTML('Manage user permissions.');
+CBHTMLOutput::begin();
+
 
 if (!ColbyUser::current()->isOneOfThe('Developers'))
 {
@@ -14,15 +17,27 @@ if (!ColbyUser::current()->isOneOfThe('Developers'))
     goto done;
 }
 
+
+include CBSystemDirectory . '/sections/admin-page-header.php';
+
+$selectedMenuItemID     = 'general';
+$selectedSubmenuItemID  = 'permissions';
+
+include CBSystemDirectory . '/sections/admin-page-menu.php';
+
+CBHTMLOutput::addJavaScriptURL(CBSystemURL . '/handlers/handle,admin,users.js');
+
 /**
  *
  */
 
-$sql = <<<END
-SELECT
-    USER() as `user`,
-    CURRENT_USER() as `currentUser`
-END;
+$sql = <<<EOT
+
+    SELECT
+        USER() as `user`,
+        CURRENT_USER() as `currentUser`
+
+EOT;
 
 $result = Colby::query($sql);
 
@@ -32,134 +47,173 @@ $result->free();
 
 ?>
 
-<table><thead>
-<tr><th>user()</th><th>current_user()</th></tr>
-</thead><tbody>
-<tr><td><?php echo $row->user; ?></td><td><?php echo $row->currentUser; ?></td></tr>
-</tbody></table>
-
-<?php
-
-/**
- *
- */
-
-$sql = <<<END
-SELECT
-    `TABLE_NAME` AS `tableName`
-FROM
-    information_schema.TABLES
-WHERE
-    `TABLE_SCHEMA` = DATABASE() AND
-    `TABLE_NAME` LIKE 'ColbyUsersWhoAre%'
-END;
-
-$result = Colby::query($sql);
-
-$friendlyNames = array();
-$tableNames = array();
-$columnNames = array();
-$joins = array();
-
-while ($row = $result->fetch_object())
-{
-    preg_match('/ColbyUsersWhoAre(.*)/', $row->tableName, $matches);
-
-    $friendlyName = $matches[1];
-    $friendlyNames[] = $friendlyName;
-    $tableNames[] = $row->tableName;
-    $columnNames[] = "`{$row->tableName}`.`added` AS `isIn{$friendlyName}`";
-    $joins[] = "LEFT JOIN `{$row->tableName}` ON `ColbyUsers`.`id` = `{$row->tableName}`.`userId`";
-}
-
-$result->free();
-
-/**
- *
- */
-
-$columnNamesClause = implode(",\n", $columnNames);
-$joinsClause = implode("\n", $joins);
-
-$sql = <<<END
-SELECT
-    `ColbyUsers`.`id`,
-    `ColbyUsers`.`facebookId`,
-    `ColbyUsers`.`facebookName`,
-    `ColbyUsers`.`hasBeenVerified`,
-    {$columnNamesClause}
-FROM
-    `ColbyUsers`
-{$joinsClause}
-ORDER BY
-    `ColbyUsers`.`facebookLastName`
-END;
-
-$result = Colby::query($sql);
-
-?>
-
-<table>
+<main>
     <style scoped>
-    td, th
-    {
-        padding: 2px 5px;
-    }
-    </style>
-    <thead>
-        <tr>
-            <th>Id</th>
-            <th>Name</th>
 
-            <?php
-
-            foreach ($friendlyNames as $friendlyName)
-            {
-                echo "<th>{$friendlyName}</th>\n";
-            }
-
-            ?>
-
-        </tr>
-    </thead>
-    <tbody>
-
-        <?php
-
-        while ($row = $result->fetch_object())
+        table.permissions-administration
         {
-            ?>
+            table-layout:   fixed;
+            margin:         0px auto;
+        }
 
+        table.permissions-administration td,
+        table.permissions-administration th
+        {
+            padding: 2px 5px;
+        }
+
+        table.database-user-information
+        {
+            width: 800px;
+            margin-bottom: 50px;
+        }
+
+        table.database-user-information td,
+        table.database-user-information th
+        {
+            width: 50%;
+        }
+
+        table.database-user-information th
+        {
+            text-align: right;
+        }
+
+    </style>
+
+    <table class="permissions-administration database-user-information">
+        <tbody>
             <tr>
-                <td><?php echo $row->id; ?></td>
-                <td><?php echo $row->facebookName; ?></td>
+                <th>MySQL USER()</th>
+                <td><?php echo $row->user; ?></td>
+            </tr>
+            <tr>
+                <th>MySQL CURRENT_USER()</th>
+                <td><?php echo $row->currentUser; ?></td>
+            </tr>
+        </tbody>
+    </table>
+
+
+    <?php
+
+    /**
+     *
+     */
+
+    $sql = <<<EOT
+
+        SELECT
+            `TABLE_NAME` AS `tableName`
+        FROM
+            information_schema.TABLES
+        WHERE
+            `TABLE_SCHEMA` = DATABASE() AND
+            `TABLE_NAME` LIKE 'ColbyUsersWhoAre%'
+
+EOT;
+
+    $result = Colby::query($sql);
+
+    $friendlyNames = array();
+    $tableNames = array();
+    $columnNames = array();
+    $joins = array();
+
+    while ($row = $result->fetch_object())
+    {
+        preg_match('/ColbyUsersWhoAre(.*)/', $row->tableName, $matches);
+
+        $friendlyName = $matches[1];
+        $friendlyNames[] = $friendlyName;
+        $tableNames[] = $row->tableName;
+        $columnNames[] = "`{$row->tableName}`.`added` AS `isIn{$friendlyName}`";
+        $joins[] = "LEFT JOIN `{$row->tableName}` ON `ColbyUsers`.`id` = `{$row->tableName}`.`userId`";
+    }
+
+    $result->free();
+
+    /**
+     *
+     */
+
+    $columnNamesClause = implode(",\n", $columnNames);
+    $joinsClause = implode("\n", $joins);
+
+    $sql = <<<EOT
+
+        SELECT
+            `ColbyUsers`.`id`,
+            `ColbyUsers`.`facebookId`,
+            `ColbyUsers`.`facebookName`,
+            `ColbyUsers`.`hasBeenVerified`,
+            {$columnNamesClause}
+        FROM
+            `ColbyUsers`
+        {$joinsClause}
+        ORDER BY
+            `ColbyUsers`.`facebookLastName`
+
+EOT;
+
+    $result = Colby::query($sql);
+
+    ?>
+
+    <table class="permissions-administration">
+        <thead>
+            <tr>
+                <th>Id</th>
+                <th>Name</th>
 
                 <?php
 
                 foreach ($friendlyNames as $friendlyName)
                 {
-                    $columnName = "isIn{$friendlyName}";
-
-                    echo '<td style="text-align: center;">';
-
-                    renderCheckbox($row->id, $friendlyName, $row->$columnName);
-
-                    echo '</td>';
+                    echo "<th>{$friendlyName}</th>\n";
                 }
 
                 ?>
 
             </tr>
+        </thead>
+        <tbody>
 
             <?php
-        }
 
-        ?>
+            while ($row = $result->fetch_object())
+            {
+                ?>
 
-    </tbody>
-</table>
+                <tr>
+                    <td><?php echo $row->id; ?></td>
+                    <td><?php echo $row->facebookName; ?></td>
 
-<script src="<?php echo COLBY_SYSTEM_URL, '/handlers/handle,admin,users.js'; ?>"></script>
+                    <?php
+
+                    foreach ($friendlyNames as $friendlyName)
+                    {
+                        $columnName = "isIn{$friendlyName}";
+
+                        echo '<td style="text-align: center;">';
+
+                        renderCheckbox($row->id, $friendlyName, $row->$columnName);
+
+                        echo '</td>';
+                    }
+
+                    ?>
+
+                </tr>
+
+                <?php
+            }
+
+            ?>
+
+        </tbody>
+    </table>
+</main>
 
 <?php
 
@@ -167,7 +221,9 @@ $result->free();
 
 done:
 
-$page->end();
+include CBSystemDirectory . '/sections/admin-page-footer.php';
+
+CBHTMLOutput::render();
 
 
 //
