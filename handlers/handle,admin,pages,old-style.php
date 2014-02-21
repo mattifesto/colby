7a6f78edb7f239efd5dcf71f25ef45fef74f5bc2
Matt Calkins (MacBook) <matt@mattifesto.com>
2014-02-20 17:06:55 -0800

    Version 53.47
    modified:   handlers/handle,admin,pages,old-style.php
    -   Use the constants from constants.php.
    -   Add the list of old-style blog posts to this page.

--- handlers/handle,admin,pages,old-style.php
+++ handlers/handle,admin,pages,old-style.php
@@ -1,6 +1,5 @@
 <?php
 
-define('CBOldStylePagesGroupId', 'a3f5d7ead80d4e6cb644ec158a13f3a89a9a0622');
 
 include_once CBSystemDirectory . '/classes/CBHTMLOutput.php';
 
@@ -40,8 +39,8 @@ include CBSystemDirectory . '/sections/admin-page-menu.php';
 
         <?php
 
-        $groupID    = CBOldStylePagesGroupId;
-        $result     = CBOldStylePagesAdmin::queryForPages();
+        $groupID    = COLBY_PAGES_DOCUMENT_GROUP_ID;
+        $result     = CBOldStylePagesAdmin::queryForPages(COLBY_PAGE_DOCUMENT_TYPE_ID, $groupID);
 
         while ($row = $result->fetch_object())
         {
@@ -91,6 +90,67 @@ include CBSystemDirectory . '/sections/admin-page-menu.php';
 
     ?>
 
+    <table class="document-list" style="margin-top: 100px;">
+        <thead><tr>
+            <th></th>
+            <th>Title</th>
+            <th>Published</th>
+        </tr></thead>
+        <tbody>
+
+        <?php
+
+        $groupID    = COLBY_BLOG_POSTS_DOCUMENT_GROUP_ID;
+        $result     = CBOldStylePagesAdmin::queryForPages(COLBY_BLOG_POST_DOCUMENT_TYPE_ID, $groupID);
+
+        while ($row = $result->fetch_object())
+        {
+            $editURL = COLBY_SITE_URL . "/admin/document/edit/" .
+                "?document-group-id={$groupID}" .
+                "&document-type-id={$row->documentTypeId}" .
+                "&archive-id={$row->archiveId}";
+
+            ?>
+
+            <tr>
+                <td><a href="<?php echo $editURL; ?>">edit</a></td>
+                <td><?php echo $row->titleHTML; ?></td>
+                <td><span class="time"
+                          data-timestamp="<?php echo $row->published ? $row->published * 1000 : ''; ?>">
+                    </span></td>
+            </tr>
+
+            <?php
+        }
+
+        $result->free();
+
+        ?>
+
+    </tbody></table>
+
+    <h2>Create a new blog post</h2>
+
+    <?php
+
+    $documentTypes = Colby::findDocumentTypes($groupID);
+
+    foreach ($documentTypes as $documentType)
+    {
+        $createNewPageURL = COLBY_SITE_URL .
+            '/admin/document/edit/' .
+            "?document-group-id={$groupID}" .
+            "&document-type-id={$documentType->id}";
+
+        ?>
+        <div><a href="<?php echo $createNewPageURL; ?>">
+            <?php echo $documentType->nameHTML; ?>
+        </a></div>
+        <?php
+    }
+
+    ?>
+
 </main>
 
 <?php
@@ -110,9 +170,10 @@ class CBOldStylePagesAdmin
     /**
      * @return mysqli_result
      */
-    public static function queryForPages()
+    public static function queryForPages($typeID, $groupID)
     {
-        $groupID = CBOldStylePagesGroupId;
+        $typeIDForSQL   = ColbyConvert::textToSQL($typeID);
+        $groupIDForSQL  = ColbyConvert::textToSQL($groupID);
 
         $sql = <<<EOT
 
@@ -124,7 +185,8 @@ class CBOldStylePagesAdmin
         FROM
             `ColbyPages`
         WHERE
-            `groupID` = UNHEX('{$groupID}')
+            `typeID`    = UNHEX('{$typeIDForSQL}')  AND
+            `groupID`   = UNHEX('{$groupIDForSQL}')
         ORDER BY
             ISNULL(`published`) DESC,
             `published` DESC
