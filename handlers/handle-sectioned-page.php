e5f1d179787578bc667a5ec9974eeff1378b8070
Matt Calkins (MacBook) <matt@mattifesto.com>
2014-02-26 21:40:10 -0800

    Version 54.23
    modified:   handlers/handle-sectioned-page.php
    -   Include the equalize section instead of manually including the equalize files.

--- handlers/handle-sectioned-page.php
+++ handlers/handle-sectioned-page.php
@@ -8,19 +8,19 @@ include_once CBSystemDirectory . '/classes/CBHTMLOutput.php';
 include_once CBSystemDirectory . '/classes/CBDataStore.php';
 
 
-CBHTMLOutput::addCSSURL(CBSystemURL . '/css/equalize.css');
-CBHTMLOutput::addJavaScriptURL(CBSystemURL . '/javascript/html5shiv.js');
-CBHTMLOutput::addJavaScriptURL(CBSystemURL . '/javascript/ColbyEqualize.js');
-
 if (defined('GOOGLE_UNIVERSAL_ANALYTICS_TRACKING_ID'))
 {
     CBHTMLOutput::addJavaScriptSnippet(CBSystemDirectory . '/javascript/snippet-google-universal-analytics.php');
 }
 
+
 include_once Colby::findFile('page-renderer-configuration.php');
 
 
 CBHTMLOutput::begin();
+
+
+include CBSystemDirectory . '/sections/equalize.php';
 
 
 $dataStore      = new CBDataStore($dataStoreID);
