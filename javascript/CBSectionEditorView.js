375515193702a06d73f20e248f58d36df2a70a51
Matt Calkins (MacBook) <matt@mattifesto.com>
2014-03-01 23:42:52 -0800

    Version 55.6
    modified:   javascript/CBSectionEditorView.css
    modified:   javascript/CBSectionEditorView.js
    -   Updated the styles to be less intrusive but more helpful.
    -   Sections now support a more traditional model of returning their root element for the caller to add to the DOM.

--- javascript/CBSectionEditorView.js
+++ javascript/CBSectionEditorView.js
@@ -12,14 +12,12 @@ function CBSectionEditorView(model, sectionListView)
     this._element.classList.add("CBSectionEditorView");
 
     var header              = document.createElement("header");
-    header.style.overflow   = "hidden";
     header.appendChild(document.createTextNode(title));
     this._element.appendChild(header);
 
 
     var deleteButton            = document.createElement("button");
     var deleteSectionCallback   = sectionListView.deleteSectionCallback(model);
-    deleteButton.style.float    = "right";
     deleteButton.addEventListener('click', deleteSectionCallback, false);
     deleteButton.appendChild(document.createTextNode("Delete"));
     header.appendChild(deleteButton);
@@ -28,11 +26,9 @@ function CBSectionEditorView(model, sectionListView)
     this._innerElement  = document.createElement("div");
     this._element.appendChild(this._innerElement);
 
-    var sectionEditorConstructor = CBPageEditor.sectionEditors[model.sectionTypeID];
-
-    // TODO: this is an odd model, the editor creates itself and then places itself in the tree. I'm not sure about this.
-
-    new sectionEditorConstructor(CBPageEditor.model, model, this._innerElement);
+    var sectionEditorConstructor    = CBPageEditor.sectionEditors[model.sectionTypeID];
+    var sectionEditor               = new sectionEditorConstructor(CBPageEditor.model, model);
+    this._innerElement.appendChild(sectionEditor.element());
 }
 
 /**
