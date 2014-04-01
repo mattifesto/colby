9b4a11151cb9a5ebcb7472b9e6869cc2bbff552c
Matt Calkins (MacBook) <matt@mattifesto.com>
2014-04-01 16:34:14 -0700

    Version 63.1
    modified:   handlers/handle,admin.php
    -   Updated to the most recent coding standards.

--- handlers/handle,admin.php
+++ handlers/handle,admin.php
@@ -2,24 +2,21 @@
 
 include(Colby::findHandler('handle-ensure-installation.php'));
 
-include_once CBSystemDirectory . '/classes/CBHTMLOutput.php';
-
-
 if (!ColbyUser::current()->isOneOfThe('Administrators'))
 {
     return include CBSystemDirectory . '/handlers/handle-authorization-failed.php';
 }
 
 
+include_once CBSystemDirectory . '/classes/CBHTMLOutput.php';
+
 CBHTMLOutput::begin();
 CBHTMLOutput::setTitleHTML('Status');
 CBHTMLOutput::setDescriptionHTML('The status of the website');
 
-include CBSystemDirectory . '/sections/equalize.php';
+include CBSystemDirectory . '/sections/admin-page-settings.php';
 
-CBHTMLOutput::addCSSURL('https://fonts.googleapis.com/css?family=Source+Sans+Pro:400');
 CBHTMLOutput::addCSSURL('https://fonts.googleapis.com/css?family=Source+Sans+Pro:600');
-CBHTMLOutput::addCSSURL('https://fonts.googleapis.com/css?family=Source+Sans+Pro:700');
 CBHTMLOutput::addCSSURL(CBSystemURL . '/handlers/handle,admin.css');
 
 
