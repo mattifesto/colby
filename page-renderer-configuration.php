5fce6be3cf15560ad45d740e73b18424aef9811f
Matt Calkins (MacBook) <matt@mattifesto.com>
2014-04-05 16:10:36 -0700

    Version 65.2
    modified:   page-renderer-configuration.php
    -   Removed unneeded whitespace.

--- page-renderer-configuration.php
+++ page-renderer-configuration.php
@@ -2,5 +2,4 @@
 
 include_once CBSystemDirectory . '/sections/CBStandardPageHeaderSection.php';
 include_once CBSystemDirectory . '/sections/CBStandardPageFooterSection.php';
-
 include_once CBSystemDirectory . '/sections/CBBackgroundSection.php';
