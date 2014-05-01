1488b90fd5e25e26f1035fb47c11b10fee55d834
Matt Calkins (MacBook) <matt@mattifesto.com>
2014-05-01 12:58:58 -0700

    Version 74.1
    modified:   sections/public-page-settings.php
    -   Updated the JavaScript files for `html5shiv` and `ColbyEqualize` to be placed in the head element so they are available to all HTML, CSS, and JavaScript entities that follow.

--- sections/public-page-settings.php
+++ sections/public-page-settings.php
@@ -7,8 +7,8 @@ CBHTMLOutput::addCSSURL($publicPageSettingsCSSURL);
 CBHTMLOutput::addCSSURL('https://fonts.googleapis.com/css?family=Source+Sans+Pro:400');
 CBHTMLOutput::addCSSURL('https://fonts.googleapis.com/css?family=Source+Sans+Pro:700');
 
-CBHTMLOutput::addJavaScriptURL(CBSystemURL . '/javascript/html5shiv.js');
-CBHTMLOutput::addJavaScriptURL(CBSystemURL . '/javascript/ColbyEqualize.js');
+CBHTMLOutput::addJavaScriptURL(CBSystemURL . '/javascript/html5shiv.js', CBHTMLOutput::JSInHeadElement);
+CBHTMLOutput::addJavaScriptURL(CBSystemURL . '/javascript/ColbyEqualize.js', CBHTMLOutput::JSInHeadElement);
 CBHTMLOutput::addJavaScriptURL(CBSystemURL . '/javascript/Colby.js');
 
 if (defined('GOOGLE_UNIVERSAL_ANALYTICS_TRACKING_ID'))
