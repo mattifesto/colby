eff8185a149a62cdeff07d6ad9ce91822251410a
Matt Calkins (MacBook) <matt@mattifesto.com>
2014-05-31 21:38:38 -0700

    Version 79.2
    modified:   handlers/handle,colby,logout.php
    -   This hanlder now works without a query string. If there's no query string the user is redirected to the home page after being logged out.

--- handlers/handle,colby,logout.php
+++ handlers/handle,colby,logout.php
@@ -2,6 +2,13 @@
 
 ColbyUser::logoutCurrentUser();
 
-$state = json_decode(urldecode($_GET['state']));
+if (isset($_GET['state']))
+{
+    $state = json_decode(urldecode($_GET['state']));
 
-header('Location: ' . $state->colby_redirect_uri);
+    header('Location: ' . $state->colby_redirect_uri);
+}
+else
+{
+    header('Location: ' . COLBY_SITE_URL);
+}
