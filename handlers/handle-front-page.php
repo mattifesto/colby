772b71771165e019cfc7f73efc0b648b7276acaf
Matt Calkins (MacBook) <matt@mattifesto.com>
2014-07-30 19:42:12 -0700

    Version 87.4
    modified:   handlers/handle-front-page.php
    -   Upgraded from `ColbyOutputManager` to `CBHTMLOutput`.

--- handlers/handle-front-page.php
+++ handlers/handle-front-page.php
@@ -1,16 +1,30 @@
 <?php
 
-$page = new ColbyOutputManager();
+CBHTMLOutput::begin();
+CBHTMLOutput::setTitleHTML('Front Page');
+CBHTMLOutput::setDescriptionHTML('This is the default front page.');
 
-$page->titleHTML = 'Front Page';
-$page->descriptionHTML = 'This is the front page.';
-
-$page->begin();
+include CBSystemDirectory . '/sections/public-page-settings.php';
 
 ?>
 
-<p>This is the front page.
+<main>
+    <style scoped>
+
+        main > p
+        {
+            line-height:    1.5;
+            margin:         100px auto;
+            text-align:     center;
+            width:          540px;
+        }
+
+    </style>
+
+    <p>This is the default front page. To replace it create the file<br><br><code>handlers/handle-front-page.php</code><br><br>in the main website directory. Alternatively, create a page in the admin area and set it as the front page. A front page created in the admin area has higher priority than a handler.
+
+</main>
 
 <?php
 
-$page->end();
+CBHTMLOutput::render();
