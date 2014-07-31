47e78c70df7ac681c0a09371373ae86d1e57d72d
Matt Calkins (MacBook) <matt@mattifesto.com>
2014-07-30 19:13:13 -0700

    Version 87.3
    modified:   handlers/handle-ensure-installation.php
    -   Upgraded from `ColbyOutputManager` to `CBHTMLOutput`.

--- handlers/handle-ensure-installation.php
+++ handlers/handle-ensure-installation.php
@@ -2,64 +2,78 @@
 
 /**
  * This handler is always included from another handler. If this handler
- * decides to output a page it should call `exit` to prevent the caller from
+ * decides to output a page it will call `exit` to prevent the caller from
  * generating a second page after this handler returns.
  *
  * This handler will always output an entire page or nothing at all.
  */
 
-$page = new ColbyOutputManager('simple-html-page');
+$databaseIsAvailable = COLBY_MYSQL_HOST && COLBY_MYSQL_DATABASE && COLBY_MYSQL_USER;
 
-$page->titleHTML = 'Installation';
-$page->descriptionHTML = 'This page performs the initial installation of a Colby website.';
-
-$page->begin();
-
-$databaseIsConfigured = COLBY_MYSQL_HOST && COLBY_MYSQL_DATABASE && COLBY_MYSQL_USER;
-
-if ($databaseIsConfigured)
+if ($databaseIsAvailable)
 {
     $sql = <<<EOT
-SELECT
-    COUNT(*) AS `count`
-FROM
-    `information_schema`.`ROUTINES`
-WHERE
-    `ROUTINE_SCHEMA` = DATABASE() AND
-    `ROUTINE_TYPE` = 'FUNCTION' AND
-    `ROUTINE_NAME` = 'ColbySchemaVersionNumber'
+
+        SELECT
+            COUNT(*) AS `count`
+        FROM
+            `information_schema`.`ROUTINES`
+        WHERE
+            `ROUTINE_SCHEMA` = DATABASE() AND
+            `ROUTINE_TYPE` = 'FUNCTION' AND
+            `ROUTINE_NAME` = 'ColbySchemaVersionNumber'
+
 EOT;
 
-    $result = Colby::query($sql);
-
-    $databaseIsInstalled = $result->fetch_object()->count;
+    $result                 = Colby::query($sql);
+    $databaseIsInstalled    = $result->fetch_object()->count;
 
     $result->free();
 
     if ($databaseIsInstalled)
     {
-        $page->discard();
-
         return;
     }
 }
 
+CBHTMLOutput::begin();
+CBHTMLOutput::setTitleHTML('Installation');
+CBHTMLOutput::setDescriptionHTML('This page performs the initial installation of a Colby website.');
+
+include CBSystemDirectory . '/sections/admin-page-settings.php';
+
 ?>
 
-<section class="widget"
-         style="text-align: center;">
+<main>
+    <style scoped>
+
+        main
+        {
+            text-align: center;
+        }
+
+        main > header
+        {
+            margin: 100px 0 50px;
+        }
+
+        main progress
+        {
+            width:  100px;
+            margin: 50px;
+        }
+
+    </style>
 
     <?php
 
-    if ($databaseIsConfigured)
+    if ($databaseIsAvailable)
     {
         ?>
 
         <header><h1>Database Installation Required</h1></header>
         <div>
-            <progress id="database-intallation-progress"
-                      value="0"
-                      style="width: 50px; margin: 50px;"></progress>
+            <progress id="database-intallation-progress" value="0"></progress>
             <div>
                 <button onclick="DatabaseInstaller.installDatabase(this);">Install Database</button>
             </div>
@@ -73,10 +87,11 @@ EOT;
 
         <header><h1>Database Configuration Required</h1></header>
         <div>
-            <div style="width: 400px; margin: 100px auto;" class="formatted-content">
-                <p style="text-align: center;">The database configuration section in
-                <p style="text-align: center;"><code>colby-configuration.php</code>
-                <p style="text-align: center;">has not been completed. Please complete this section of the configuration file before proceeding.
+            <div style="width: 400px; margin: 50px auto; line-height: 1.5;">
+                <p>The database configuration section in
+                <p style="margin: 20px 0;"><code>colby-configuration.php</code>
+                <p>has not been completed. Please complete this section of the
+                   configuration file before proceeding.
             </div>
             <div>
                 <button onclick="location.reload();">Check Configuration</button>
@@ -88,7 +103,7 @@ EOT;
 
     ?>
 
-</section>
+</main>
 
 <script>
 
@@ -128,12 +143,14 @@ DatabaseInstaller.installDatabase = function(sender)
     xhr.send(formData);
 
     document.getElementById('database-intallation-progress').removeAttribute('value');
-}
+};
 
 </script>
 
 <?php
 
-$page->end();
+include CBSystemDirectory . '/sections/admin-page-footer-2.php';
+
+CBHTMLOutput::render();
 
 exit;
