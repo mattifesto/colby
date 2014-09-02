b2c2c55fcd04ce0b284ab2817a34901a26f556ee
Matt Calkins (MacBook) <matt@mattifesto.com>
2014-09-01 20:17:07 -0700

    Versio 97.3 (improved authorization page)
    renamed:    snippets/authenticate.php -> handlers/handle-authorization-failed.php
    -   The content of `snippets/authenticate.php` was moved into this file because there was only one other use of the snippet. The idea behind the snippet was that sites could override it for a customer authentication experience. To that, a page should create a custom authentication page.

--- handlers/handle-authorization-failed.php
+++ handlers/handle-authorization-failed.php
@@ -1,14 +1,60 @@
 <?php
 
-include_once CBSystemDirectory . '/classes/CBHTMLOutput.php';
+$currentUserIsLoggedIn  = ColbyUser::current()->isLoggedIn();
+$titleHTML              = $currentUserIsLoggedIn ? 'Authorization Failed' : 'Please Log In';
 
 CBHTMLOutput::begin();
-CBHTMLOutput::setTitleHTML('Authorization Failed');
+CBHTMLOutput::setTitleHTML($titleHTML);
 CBHTMLOutput::setDescriptionHTML('You are not authorized to view this page.');
 
 include CBSystemDirectory . '/sections/admin-page-settings.php';
 
-include Colby::findSnippet('authenticate.php');
+?>
+
+<main>
+    <style scoped>
+
+        main
+        {
+            padding-top:  100px;
+            text-align: center;
+        }
+
+        div.CBLogInLink
+        {
+            margin-top: 20px;
+        }
+
+    </style>
+
+    <?php
+
+    if ($currentUserIsLoggedIn)
+    {
+        ?>
+
+        <p>You are not authorized to view this page.
+
+        <?php
+    }
+    else
+    {
+        ?>
+
+        <p>You must be logged in to view this page.
+
+        <div class="CBLogInLink">
+            <a href="<?php echo ColbyUser::loginURL(); ?>">Log in</a>
+        </div>
+
+        <?php
+    }
+
+    ?>
+
+</main>
+
+<?php
 
 include CBSystemDirectory . '/sections/admin-page-footer-2.php';
 
