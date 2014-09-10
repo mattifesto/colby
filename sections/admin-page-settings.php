ab822b5284bea447870bc86e7a6050ca5ad1540c
Matt Calkins (MacBook) <matt@mattifesto.com>
2014-09-10 16:20:37 -0700

    Verison 98.1 (update page-settings files)
    modified:   sections/admin-page-settings.php
    modified:   sections/public-page-settings.php
    -   Replace direct includes of equalization files with a single include of
        `sections/equalize.php`.

--- sections/admin-page-settings.php
+++ sections/admin-page-settings.php
@@ -1,10 +1,9 @@
 <?php
 
-CBHTMLOutput::addCSSURL(CBSystemURL . '/css/equalize.css');
+include CBSystemDirectory . '/sections/equalize.php';
+
 CBHTMLOutput::addCSSURL(CBSystemURL . '/sections/admin-page-settings.css');
 CBHTMLOutput::addCSSURL('https://fonts.googleapis.com/css?family=Source+Sans+Pro:400');
 CBHTMLOutput::addCSSURL('https://fonts.googleapis.com/css?family=Source+Sans+Pro:700');
 
-CBHTMLOutput::addJavaScriptURL(CBSystemURL . '/javascript/html5shiv.js');
-CBHTMLOutput::addJavaScriptURL(CBSystemURL . '/javascript/ColbyEqualize.js');
 CBHTMLOutput::addJavaScriptURL(CBSystemURL . '/javascript/Colby.js');
--- sections/public-page-settings.php
+++ sections/public-page-settings.php
@@ -1,14 +1,13 @@
 <?php
 
+include CBSystemDirectory . '/sections/equalize.php';
+
 $publicPageSettingsCSSURL = Colby::findFile('sections/public-page-settings.css', Colby::returnURL);
 
-CBHTMLOutput::addCSSURL(CBSystemURL . '/css/equalize.css');
 CBHTMLOutput::addCSSURL($publicPageSettingsCSSURL);
 CBHTMLOutput::addCSSURL('https://fonts.googleapis.com/css?family=Source+Sans+Pro:400');
 CBHTMLOutput::addCSSURL('https://fonts.googleapis.com/css?family=Source+Sans+Pro:700');
 
-CBHTMLOutput::addJavaScriptURL(CBSystemURL . '/javascript/html5shiv.js', CBHTMLOutput::JSInHeadElement);
-CBHTMLOutput::addJavaScriptURL(CBSystemURL . '/javascript/ColbyEqualize.js', CBHTMLOutput::JSInHeadElement);
 CBHTMLOutput::addJavaScriptURL(CBSystemURL . '/javascript/Colby.js');
 
 if (defined('GOOGLE_UNIVERSAL_ANALYTICS_TRACKING_ID'))
