b13cd68b725c8b5de2bb5aca5179359e14a5a89b
Matt Calkins (Server) <matt@mattifesto.com>
2015-02-14 19:55:26 -0800

    Version 121.3
    modified:   classes/CBImages.php
    modified:   handlers/handle,admin,pages,api,upload-image.php
    -   Moved the logic for uploading an original image from the handler into the CBImages class.
    -   The images are now saved in an image data store rather than the page data store. So if an image is shared between pages they will both use the same image.
    -   When an image is uploaded the images table is updated with the fact that image exists and its created and modified timestamps.

--- classes/CBImages.php
+++ classes/CBImages.php
@@ -25,4 +25,61 @@ EOT;
 
         Colby::query($SQL);
     }
+
+    /**
+     * @return void
+     */
+    private static function updateRow($ID, $timestamp) {
+        $timestampAsSQL = (int)$timestamp;
+        $IDAsSQL        = ColbyConvert::textToSQL($ID);
+        $IDAsSQL        = "UNHEX('{$IDAsSQL}')";
+
+        $SQL = <<<EOT
+
+            INSERT INTO `CBImages`
+                (`ID`, `created`, `modified`)
+            VALUES
+                ({$IDAsSQL}, {$timestampAsSQL}, {$timestampAsSQL})
+            ON DUPLICATE KEY UPDATE
+                `modified` = {$timestampAsSQL}
+
+EOT;
+
+        Colby::query($SQL);
+    }
+
+    /**
+     * @return void
+     */
+    public static function uploadImageForAjax() {
+        $response = new CBAjaxResponse();
+
+        Colby::query('START TRANSACTION');
+
+        try {
+            $timestamp  = isset($_POST['timestamp']) ? $_POST['timestamp'] : time();
+            $uploader   = ColbyImageUploader::uploaderForName('image');
+            $ID         = $uploader->sha1();
+            $dataStore  = new CBDataStore($ID);
+            $filename   = 'original' .  $uploader->canonicalExtension();
+            $filepath   = $dataStore->directory() . "/{$filename}";
+
+            self::updateRow($ID, $timestamp);
+            $dataStore->makeDirectory();
+            $uploader->moveToFilename($filepath);
+        } catch (Exception $exception) {
+            Colby::query('ROLLBACK');
+            throw $exception;
+        }
+
+        Colby::query('COMMIT');
+
+        $response->imageFilename    = $filename;
+        $response->imageURL         = $dataStore->URL() . "/{$filename}";
+        $response->imageSizeX       = $uploader->sizeX();
+        $response->imageSizeY       = $uploader->sizeY();
+        $response->wasSuccessful    = true;
+
+        $response->send();
+    }
 }
--- handlers/handle,admin,pages,api,upload-image.php
+++ handlers/handle,admin,pages,api,upload-image.php
@@ -5,29 +5,4 @@ if (!ColbyUser::current()->isOneOfThe('Administrators'))
     return include CBSystemDirectory . '/handlers/handle-authorization-failed-ajax.php';
 }
 
-$response = new CBAjaxResponse();
-
-/**
- *
- */
-
-$dataStoreID        = $_POST['dataStoreID'];
-$dataStore          = new CBDataStore($dataStoreID);
-$uploader           = ColbyImageUploader::uploaderForName('image');
-$filename           = $uploader->sha1() . '-original' .  $uploader->canonicalExtension();
-$absoluteFilename   = $dataStore->directory() . "/{$filename}";
-
-$uploader->moveToFilename($absoluteFilename);
-
-
-/**
- * Send the response
- */
-
-$response->imageFilename    = $filename;
-$response->imageURL         = $dataStore->URL() . "/{$filename}";
-$response->imageSizeX       = $uploader->sizeX();
-$response->imageSizeY       = $uploader->sizeY();
-$response->wasSuccessful    = true;
-
-$response->send();
+CBImages::uploadImageForAjax();
