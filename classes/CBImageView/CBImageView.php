18f37af3a4b4fab6902f903fea6bcde93149319f
Matt Calkins (MacBook Air) <matt@mattifesto.com>
2015-03-31 10:54:04 -0700

    Version 133.6
    modified:   classes/CBImageView/CBImageView.php
    -   Upgraded view.

--- classes/CBImageView/CBImageView.php
+++ classes/CBImageView/CBImageView.php
@@ -3,38 +3,6 @@
 final class CBImageView {
 
     /**
-     * @return instance type
-     */
-    public static function init() {
-        $view                                   = new self();
-        $view->alternativeTextView              = CBTextView::init();
-        $view->model                            = CBView::modelWithClassName(__CLASS__);
-        $view->model->actualHeight              = null;
-        $view->model->actualWidth               = null;
-        $view->model->alternativeTextViewModel  = $view->alternativeTextView->model;
-        $view->model->displayHeight             = null;
-        $view->model->displayWidth              = null;
-        $view->model->filename                  = null;
-        $view->model->maxHeight                 = null;
-        $view->model->maxWidth                  = null;
-        $view->model->URL                       = null;
-        $view->model->URLForHTML                = null;
-
-        return $view;
-    }
-
-    /**
-     * @return instance type
-     */
-    public static function initWithModel($model) {
-        $view                       = new self();
-        $view->model                = $model;
-        $view->alternativeTextView  = CBTextView::initWithModel($view->model->alternativeTextViewModel);
-
-        return $view;
-    }
-
-    /**
      * @return void
      */
     public static function includeEditorDependencies() {
@@ -46,13 +14,6 @@ final class CBImageView {
 
         $URL = CBSystemURL . '/classes/CBImageView/CBImageViewEditor.js';
         CBHTMLOutput::addJavaScriptURL($URL);
-    }
-
-    /**
-     * @return bool
-     */
-    public function hasImage() {
-        return self::modelHasImage($this->model);
     }
 
     /**
@@ -74,9 +35,7 @@ final class CBImageView {
     /**
      * @return void
      */
-    public function renderHTML() {
-
-        $model  = $this->model;
+    public static function renderModelAsHTML(stdClass $model = null) {
         $styles = array();
 
         if ($model->displayHeight || $model->displayWidth) {
@@ -113,7 +72,7 @@ final class CBImageView {
 
         ?>
 
-        <img alt="<?= $this->alternativeTextView->HTML() ?>"
+        <img alt="<?= $model->alternativeTextViewModel->HTML ?>"
              src="<?= $model->URLForHTML ?>"
              style="<?= $styles ?>">
 
