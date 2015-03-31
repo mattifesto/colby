ba9b174dc84a2e61157d0c3771e7e6efebd66080
Matt Calkins (MacBook Air) <matt@mattifesto.com>
2015-03-31 11:26:50 -0700

    Version 133.8
    modified:   classes/CBTextView/CBTextView.php
    -   Upgraded view.

--- classes/CBTextView/CBTextView.php
+++ classes/CBTextView/CBTextView.php
@@ -3,42 +3,6 @@
 final class CBTextView {
 
     /**
-     * @return instance type
-     */
-    public static function init() {
-        $view               = new self();
-        $view->model        = CBView::modelWithClassName(__CLASS__);
-        $view->model->text  = '';
-        $view->model->HTML  = '';
-
-        return $view;
-    }
-
-    /**
-     * 2014.10.14
-     *  The `contentType` property was added to the model so this method now
-     *  sets it if it to the default value if it doesn't already exists.
-     *  This value may not be correct for all existing models but it will
-     *  produce the current behavior regardless. The number of models existing
-     *  in the wild is very low.
-     *
-     * @return instance type
-     */
-    public static function initWithModel($model) {
-        $view           = new self();
-        $view->model    = $model;
-
-        return $view;
-    }
-
-    /**
-     * @return string
-     */
-    public function HTML() {
-        return $this->model->HTML;
-    }
-
-    /**
      * @return void
      */
     public static function includeEditorDependencies() {
@@ -61,40 +25,18 @@ final class CBTextView {
     /**
      * @return void
      */
-    public function renderHTML() {
-        echo "<span class=\"CBTextView\">{$this->model->HTML}</span>";
-    }
-
-    /**
-     * @return void
-     */
-    public function setText($text) {
-        $this->model->text  = (string)$text;
-        $this->model->HTML  = self::textToHTML($this->model->text);
+    public static function renderModelAsHTML(stdClass $model = null) {
+        echo "<span class=\"CBTextView\">{$model->HTML}</span>";
     }
 
     /**
      * @return stdClass
      */
-    public static function specToModel($spec) {
+    public static function specToModel(stdClass $spec = null) {
         $model          = CBView::modelWithClassName(__CLASS__);
         $model->text    = isset($spec->text) ? (string)$spec->text : '';
         $model->HTML    = self::textToHTML($model->text);
 
         return $model;
-    }
-
-    /**
-     * @return string
-     */
-    public function text() {
-        return $this->model->text;
-    }
-
-    /**
-     * @return string
-     */
-    public static function textToHTML($text) {
-        return ColbyConvert::textToHTML($text);
     }
 }
