f54017e432a09f95283b4d83d48ec5d919aa7dc9
Matt Calkins (MacBook Air) <matt@mattifesto.com>
2015-03-31 10:29:20 -0700

    Version 133.3
    modified:   classes/CBAdminPageFooterView/CBAdminPageFooterView.php
    -   Upgraded view.

--- classes/CBAdminPageFooterView/CBAdminPageFooterView.php
+++ classes/CBAdminPageFooterView/CBAdminPageFooterView.php
@@ -7,32 +7,9 @@
 final class CBAdminPageFooterView {
 
     /**
-     * @deprecated
-     *
-     * @return stdClass
-     */
-    public static function init() {
-        $view           = new self();
-        $view->model    = CBView::modelWithClassName(__CLASS__);
-
-        return $view;
-    }
-
-    /**
-     * @note functional programming
-     *
      * @return void
      */
     public static function renderModelAsHTML(stdClass $model = null) {
         include __DIR__ . '/CBAdminPageFooterViewHTML.php';
-    }
-
-    /**
-     * @deprecated
-     *
-     * @return void
-     */
-    public function renderHTML() {
-        self::renderModelAsHTML($this->model);
     }
 }
