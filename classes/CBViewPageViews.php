0c9b5d91686404c50a67585a08dfd494cb43b716
Matt Calkins (MacBook Air) <matt@mattifesto.com>
2015-04-27 17:56:51 -0700

    Version 144.dev.1
    modified:   classes/CBViewPageViews.php
    -   Upgraded comments and array syntax.

--- classes/CBViewPageViews.php
+++ classes/CBViewPageViews.php
@@ -1,16 +1,14 @@
 <?php
 
 /**
- * This class exists to provide the view page editor with the list of available
- * views for this website. Copy this file to the website's `classes` directory
- * to customize the views available for a website.
- */
+This class exists to provide the view page editor with the list of available views for this website. Copy this file to the website's `classes` directory to customize the views available for a website.
+*/
 class CBViewPageViews {
 
     /**
-     * array
-     */
+    @return array
+    */
     public static function availableViewClassNames() {
-        return array('CBBackgroundView', 'CBImageView', 'CBMarkaroundView');
+        return ['CBBackgroundView', 'CBImageView', 'CBMarkaroundView'];
     }
 }
