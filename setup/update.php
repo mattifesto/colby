50cc9140f708d21cd7ed9c1f9b35c51b4ddfd5ca
Matt Calkins (MacBook Air) <matt@mattifesto.com>
2015-05-14 09:37:46 -0700

    Improve the update script comments

--- setup/update.php
+++ setup/update.php
@@ -16,16 +16,9 @@
 /**
  * Library database installation scripts.
  *
- * These are included so that if a new table is added it only has to be added
- * to the installation script and an upgrade script does not also have to be
- * created. Installation scripts should do nothing if the database elements
- * for the library are already installed.
+ * Each library should a 'setup/install-database.php` file. This file should
+ * have a "create if not exists" attitude as it will be run both for
+ * installation and update. Updates should be added to the end of this file.
  */
 
 include CBSystemDirectory . '/setup/install-database.php';
-
-/**
- * This is a hand maintained upgrade list specific to this website. Scripts
- * should be removed and deprecated once all known installations have
- * been upgraded to keep the upgrade process fast and simple.
- */
