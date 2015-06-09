3f5692e642a0a6999cb35d4200fe6be3b935e318
Matt Calkins (MacBook Air) <matt@mattifesto.com>
2015-06-09 16:45:43 -0700

    Add the `CBImageLinkView` class

---
+++ classes/CBImageLinkView/CBImageLinkView.php
@@ -0,0 +1,91 @@
+<?php
+
+/**
+ *     This class is meant to a simple and universally useful view to display
+ * an image with an optional link. Part of the simplicity of this class is that
+ * it does not allow for image resizing. Image resizing usually requires
+ * domain specific knowledge and is not a great feature for a view that is
+ * meant to be useful everywhere.
+ *     Two features, aside from image uploading and display, have been included
+ * to make this class useful for the long term. The first is to allow an
+ * optional link which is needed in the majority of scenarios. The other is to
+ * allow the image to be specified as "retina" which will allow the view to be
+ * useful into the foreseeable future. Declaring the image as "retina" will use
+ * the same image but will render it at half size.
+ */
+final class CBImageLinkView {
+
+    /**
+     * @return null
+     */
+    public static function includeEditorDependencies() {
+        CBHTMLOutput::addCSSURL(self::URL('CBImageLinkViewEditor.css'));
+        CBHTMLOutput::addJavaScriptURL(self::URL('CBImageLinkViewEditorFactory.js'));
+    }
+
+    /**
+     * @return {string}|null
+     */
+    public static function modelToSearchText(stdClass $model) {
+        return $model->alt;
+    }
+
+    /**
+     * @return null
+     */
+    public static function renderModelAsHTML(stdClass $model) {
+        if ($model->HREF) {
+            echo "<a href=\"{$model->HREF}\">";
+        }
+
+        switch ($model->density) {
+            case '2x':
+                $height = ceil($model->height / 2);
+                $width  = ceil($model->width / 2);
+                break;
+
+            default:
+                $height = $model->height;
+                $width  = $model->width;
+                break;
+        }
+
+        $CSS = "width: {$width}px; height: {$height}px; flex: none; -ms-flex: none; -webkit-flex: none;";
+
+        echo "<img src=\"{$model->URLAsHTML}\" alt=\"{$model->altAsHTML}\" style=\"{$CSS}\">";
+
+        if ($model->HREF) {
+            echo '</a>';
+        }
+    }
+
+    /**
+     * Spec properties:
+     *  {string}    alt     The alternative text for the image
+     *  {string}    density The pixel density of the image ('1x'|'2x')
+     *  {int}       height  The height of the image in pixels
+     *  {string}    HREF    The link HREF
+     *  {string}    URL     The image URL
+     *  {int}       width   The width of the image in pixels
+     *
+     * @return {stdClass}
+     */
+    public static function specToModel(stdClass $spec) {
+        $model              = CBView::modelWithClassName(__CLASS__);
+        $model->alt         = isset($spec->alt) ? (string)$spec->alt : null;
+        $model->altAsHTML   = ColbyConvert::textToHTML($model->alt);
+        $model->density     = isset($spec->density) ? (string)$spec->density : '1x';
+        $model->height      = isset($spec->height) ? (int)$spec->height : null;
+        $model->HREF        = isset($spec->HREF) ? (string)$spec->HREF : null;
+        $model->HREFAsHTML  = ColbyConvert::textToHTML($model->HREF);
+        $model->URL         = isset($spec->URL) ? (string)$spec->URL : null;
+        $model->URLAsHTML   = ColbyConvert::textToHTML($model->URL);
+        $model->width       = isset($spec->width) ? (int)$spec->width : null;
+
+        return $model;
+    }
+
+    public static function URL($filename) {
+        return CBSystemURL . "/classes/CBImageLinkView/{$filename}";
+    }
+}
---
+++ classes/CBImageLinkView/CBImageLinkViewEditorFactory.js
@@ -0,0 +1,85 @@
+"use strict";
+
+var CBImageLinkViewEditorFactory = {
+
+    /**
+     * @param   {function}  handleSpecChanged
+     * @param   {Object}    spec
+     *
+     * @return  {Element}
+     */
+    createEditor : function(args) {
+        var element             = document.createElement("div");
+        element.className       = "CBImageLinkViewEditor";
+        var preview             = CBImageEditorFactory.createEditorPreview();
+        var dimensions          = document.createElement("div");
+        dimensions.className    = "dimensions";
+        dimensions.textContent  = CBImageLinkViewEditorFactory.specToDimensionsText(args.spec);
+        var handler             = CBImageLinkViewEditorFactory.handleImageUploaded.bind(undefined, {
+            dimensionsElement   : dimensions,
+            handleSpecChanged   : args.handleSpecChanged,
+            previewElement      : preview,
+            spec                : args.spec
+        });
+        var button              = CBImageEditorFactory.createEditorUploadButton({
+            handleImageUploaded : handler
+        });
+
+        if (args.spec.URL) {
+            preview.editorPreviewSetSrc(args.spec.URL);
+        }
+
+        element.appendChild(preview);
+        element.appendChild(dimensions);
+        element.appendChild(button);
+
+        return element;
+    },
+
+    /**
+     * @param   {Element}   dimensionsElement
+     * @param   {function}  handleSpecChanged
+     * @param   {Element}   previewElement
+     * @param   {Object}    spec
+     *
+     * @return undefined
+     */
+    handleImageUploaded : function(args, response) {
+        args.spec.height    = response.sizes.original.height;
+        args.spec.URL       = response.sizes.original.URL;
+        args.spec.width     = response.sizes.original.width;
+
+        args.previewElement.editorPreviewSetSrc(args.spec.URL);
+        args.handleSpecChanged.call();
+    },
+
+    /**
+     * @param   {Object}    spec
+     *
+     * @return  {string}
+     */
+    specToDimensionsText : function(spec) {
+        if (spec.URL && spec.height && spec.width) {
+            var originalDimensions = spec.width + ' × ' + spec.height;
+            var retinaDimensions;
+
+            if (spec.density === '2x') {
+                retinaDimensions = Math.ceil(spec.width / 2) + ' × ' + Math.ceil(spec.height / 2);
+                return retinaDimensions + ' (' + originalDimensions + ')';
+            } else {
+                return originalDimensions;
+            }
+        } else if (spec.URL) {
+            return 'unknown';
+        } else {
+            return 'no image';
+        }
+    },
+
+    /**
+     * @return {string}
+     */
+    widgetClassName : function() {
+        return "CBImageLinkViewEditorWidget";
+    }
+}
