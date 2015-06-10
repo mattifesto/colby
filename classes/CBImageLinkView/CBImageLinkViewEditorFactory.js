5b8d82f3969ba1b778122ff4f2b8b0bdf058260b
Matt Calkins (MacBook Air) <matt@mattifesto.com>
2015-06-09 19:34:29 -0700

    Add retina propery to CBImageLinkView editor

--- classes/CBImageLinkView/CBImageLinkViewEditorFactory.js
+++ classes/CBImageLinkView/CBImageLinkViewEditorFactory.js
@@ -3,6 +3,33 @@
 var CBImageLinkViewEditorFactory = {
 
     /**
+     * @param   {function}  args.handleValueChanged
+     * @param   {boolean}   args.initialValue
+     * @param   {string}    args.labelText
+     *
+     * @return  {Element}
+     */
+    createBooleanEditor : function(args) {
+        var element         = document.createElement("div");
+        element.className   = "CBImageLinkViewBooleanEditor";
+        var checkbox        = document.createElement("input");
+        checkbox.type       = "checkbox";
+        checkbox.checked    = !!args.initialValue;
+        var label           = document.createElement("label");
+
+        checkbox.addEventListener("change", CBImageLinkViewEditorFactory.handleCheckboxChanged.bind(undefined, {
+            checkboxElement     : checkbox,
+            handleValueChanged  : args.handleValueChanged
+        }));
+
+        label.appendChild(checkbox);
+        label.appendChild(document.createTextNode(args.labelText));
+        element.appendChild(label);
+
+        return element;
+    },
+
+    /**
      * @param   {function}  handleSpecChanged
      * @param   {Object}    spec
      *
@@ -15,7 +42,17 @@ var CBImageLinkViewEditorFactory = {
         var dimensions          = document.createElement("div");
         dimensions.className    = "dimensions";
         dimensions.textContent  = CBImageLinkViewEditorFactory.specToDimensionsText(args.spec);
-        var handler             = CBImageLinkViewEditorFactory.handleImageUploaded.bind(undefined, {
+        var handler             = CBImageLinkViewEditorFactory.handleRetinaChanged.bind(undefined, {
+            dimensionsElement   : dimensions,
+            handleSpecChanged   : args.handleSpecChanged,
+            spec                : args.spec
+        });
+        var retina              = CBImageLinkViewEditorFactory.createBooleanEditor({
+            handleValueChanged  : handler,
+            initialValue        : (args.spec.density === "2x"),
+            labelText           : "Retina"
+        });
+        handler                 = CBImageLinkViewEditorFactory.handleImageUploaded.bind(undefined, {
             dimensionsElement   : dimensions,
             handleSpecChanged   : args.handleSpecChanged,
             previewElement      : preview,
@@ -43,10 +80,21 @@ var CBImageLinkViewEditorFactory = {
         element.appendChild(preview);
         element.appendChild(dimensions);
         element.appendChild(button);
+        element.appendChild(retina);
         element.appendChild(alt);
         element.appendChild(HREF);
 
         return element;
+    },
+
+    /**
+     * @param   {Element}   checkboxElement
+     * @param   {function}  handleValueChanged
+     *
+     * @return  undefined
+     */
+    handleCheckboxChanged : function(args) {
+        args.handleValueChanged.call(undefined, args.checkboxElement.checked);
     },
 
     /**
@@ -55,14 +103,31 @@ var CBImageLinkViewEditorFactory = {
      * @param   {Element}   previewElement
      * @param   {Object}    spec
      *
-     * @return undefined
+     * @return  undefined
      */
     handleImageUploaded : function(args, response) {
-        args.spec.height    = response.sizes.original.height;
-        args.spec.URL       = response.sizes.original.URL;
-        args.spec.width     = response.sizes.original.width;
+        args.spec.height                    = response.sizes.original.height;
+        args.spec.URL                       = response.sizes.original.URL;
+        args.spec.width                     = response.sizes.original.width;
+        args.dimensionsElement.textContent  = CBImageLinkViewEditorFactory.specToDimensionsText(args.spec);
 
         args.previewElement.editorPreviewSetSrc(args.spec.URL);
+        args.handleSpecChanged.call();
+    },
+
+    /**
+     * @param   {Element}   args.dimensionsElement
+     * @param   {function}  args.handleSpecChanged
+     * @param   {Object}    args.spec
+     *
+     * @param   {boolean}   retina
+     *
+     * @return  undefined
+     */
+    handleRetinaChanged : function(args, retina) {
+        args.spec.density                   = retina ? "2x" : "1x";
+        args.dimensionsElement.textContent  = CBImageLinkViewEditorFactory.specToDimensionsText(args.spec);
+
         args.handleSpecChanged.call();
     },
 
