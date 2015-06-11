66b07868141278c7c20015e85ec59d52c3f148da
Matt Calkins (MacBook Air) <matt@mattifesto.com>
2015-06-10 18:40:55 -0700

    Add the CBViewPageViews::selectableViewClassNames function
    Also updated the comments.

--- classes/CBViewPageViews.php
+++ classes/CBViewPageViews.php
@@ -1,14 +1,31 @@
 <?php
 
 /**
-This class exists to provide the view page editor with the list of available views for this website. Copy this file to the website's `classes` directory to customize the views available for a website.
-*/
+ * This implementation is for the Laughing Elephant website.
+ *
+ * This class exists to provide the view page editor with the list of available
+ * views for this website. Copy this file to the website's `classes` directory
+ * to customize the views available for a website.
+ */
 final class CBViewPageViews {
 
     /**
-    @return array
-    */
+     * Returns all of the views that are supported by the editor.
+     *
+     * @return {array}
+     */
     public static function availableViewClassNames() {
+        return ['CBBackgroundView', 'CBImageView', 'CBMarkaroundView'];
+    }
+
+    /**
+     * Returns the subset of the views returned by `availableViewClassNames`
+     * that will be used in the editor view menu. View that still need to be
+     * supported but have been deprecated should not be included.
+     *
+     * @return {array}
+     */
+    public static function selectableViewClassNames() {
         return ['CBBackgroundView', 'CBImageView', 'CBMarkaroundView'];
     }
 }
