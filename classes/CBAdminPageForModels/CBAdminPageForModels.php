1bb592c623b2174cd24f1a6af61565571191f090
Matt Calkins (MacBook Air) <matt@mattifesto.com>
2015-06-19 11:24:06 -0700

    Add /admin/models/ page

---
+++ classes/CBAdminPageForModels/CBAdminPageForModels.php
@@ -0,0 +1,15 @@
+<?php
+
+/**
+ * This class is responsible for generating a page that will display lists of
+ * models that can be edited and lists of classes that can be created.
+ */
+final class CBAdminPageForModels {
+
+    /**
+     * @return null
+     */
+    public static function renderModelAsHTML() {
+        echo 'Hello, world!';
+    }
+}
