9f4b7f24647bc978d5634c21e57b4ec64f2a8cee
Matt Calkins (MacBook Air) <matt@mattifesto.com>
2015-06-21 14:09:51 -0700

    Implement workding fetch and save of model specs

--- classes/CBAdminPageForEditingModels/CBAdminPageForEditingModels.js
+++ classes/CBAdminPageForEditingModels/CBAdminPageForEditingModels.js
@@ -56,12 +56,136 @@ var CBAdminPageForEditingModels = {
     },
 
     /**
+     * @param   {Object}            info
+     * @param   {Object}            spec
+     * @param   {XMLHttpRequest}    xhr
+     *
+     * @return  undefined
+     */
+    handleSaveCompleted : function(args) {
+        args.info.saving    = false;
+        var response        = Colby.responseFromXMLHttpRequest(args.xhr);
+
+        if (response.wasSuccessful) {
+            args.info.countOfFailures   = undefined;
+            args.spec.version           = args.spec.version ? (args.spec.version + 1) : 1;
+
+            /**
+             * If the save request took so long that edits have been made
+             * and the save timeout has occurred, request another save
+             * immediately.
+             */
+
+            if (args.info.pending) {
+                args.info.pending = undefined;
+
+                CBAdminPageForEditingModels.save({
+                    info    : args.info,
+                    spec    : args.spec
+                });
+            }
+        } else {
+            Colby.displayResponse(response);
+        }
+    },
+
+    /**
+     * @param   {Object}            info
+     * @param   {Object}            spec
+     * @param   {XMLHttpRequest}    xhr
+     *
+     * @return  undefined
+     */
+    handleSaveFailed : function(args) {
+        args.info.saving            = false;
+        args.info.countOfFailures   = args.info.countOfFailures ? (args.info.countOfFailures + 1) : 1;
+
+        if (args.info.countOfFailures < 5) {
+
+            /**
+             * Behave is if `info.pending` is set which may be the case.
+             */
+
+            args.info.pending = false;
+
+            CBAdminPageForEditingModels.save({
+                info    : args.info,
+                spec    : args.spec
+            });
+        } else {
+            Colby.setPanelText("After multiple attempts it appers the server is not responding to save requests.");
+            Colby.showPanel();
+        }
+    },
+
+    /**
+     * @param   {Object} info
+     * @param   {Object} spec
+     *
+     * @return  undefined
+     */
+    handleSaveTimeout : function(args) {
+
+        /**
+         * The timer is finished so clear the timeoutID.
+         */
+
+        args.info.timeoutID = undefined;
+
+        /**
+         * If we're waiting for a save request to complete and another timeout
+         * has occurred set a flag (`pending`) that means another save request
+         * should be sent immediately when current one completes.
+         */
+
+        if (args.info.saving) {
+            args.info.pending = true;
+            return;
+        }
+
+        /**
+         * Save the spec.
+         */
+
+        CBAdminPageForEditingModels.save({
+            info    : args.info,
+            spec    : args.spec
+        });
+    },
+
+    /**
+     * @param   {Object} info
      * @param   {Object} spec
      *
      * @return  undefined
      */
     handleSpecChanged : function(args) {
-        console.log("Spec changed");
+
+        /**
+         * If a save is pending we're going to save again as soon as the current
+         * save request has completed so there's nothing else to do.
+         */
+
+        if (args.info.pending) {
+            return;
+        }
+
+        /**
+         * If there's a current timer running, stop it.
+         */
+
+        if (args.info.timeoutID) {
+            window.clearTimeout(args.info.timeoutID);
+        }
+
+        /**
+         * Create a new timer.
+         */
+
+        args.info.timeoutID = window.setTimeout(CBAdminPageForEditingModels.handleSaveTimeout.bind(undefined, {
+            info            : args.info,
+            spec            : args.spec
+        }), 2000);
     },
 
     /**
@@ -77,6 +201,7 @@ var CBAdminPageForEditingModels = {
         if (editorFactory && editorFactory.createEditor) {
             editor = editorFactory.createEditor({
                 handleSpecChanged   : CBAdminPageForEditingModels.handleSpecChanged.bind(undefined, {
+                    info            : {},
                     spec            : args.spec
                 }),
                 spec                : args.spec
@@ -88,6 +213,42 @@ var CBAdminPageForEditingModels = {
         }
 
         main.appendChild(editor);
+    },
+
+    /**
+     * @param   {Object} info
+     * @param   {Object} spec
+     *
+     * @return  undefined
+     */
+    save : function(args) {
+
+        /**
+         * A save is about to happen, so set the `saving` property.
+         */
+
+        args.info.saving = true;
+
+        /**
+         * Send a save request to the server.
+         */
+
+        var formData    = new FormData();
+        var xhr         = new XMLHttpRequest();
+        xhr.onload      = CBAdminPageForEditingModels.handleSaveCompleted.bind(undefined, {
+            info        : args.info,
+            spec        : args.spec,
+            xhr         : xhr
+        });
+        xhr.onerror     = CBAdminPageForEditingModels.handleSaveFailed.bind(undefined, {
+            info        : args.info,
+            spec        : args.spec,
+            xhr         : xhr
+        });
+
+        formData.append("specAsJSON", JSON.stringify(args.spec));
+        xhr.open("POST", "/api/?class=CBModels&function=save");
+        xhr.send(formData);
     }
 };
 
