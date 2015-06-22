aa4f41ac07170f69bb9b01bb2b56b84b0effc20f
Matt Calkins (MacBook Air) <matt@mattifesto.com>
2015-06-21 18:53:35 -0700

    Add model list display to CBAdminPageForModels

--- classes/CBAdminPageForModels/CBAdminPageForModels.js
+++ classes/CBAdminPageForModels/CBAdminPageForModels.js
@@ -14,6 +14,8 @@ var CBAdminPageForModels = {
         button.textContent  = "New";
         var select          = document.createElement("select");
         var title           = document.createElement("h1");
+        var models          = document.createElement("div");
+        models.className    = "models";
 
         button.addEventListener("click", CBAdminPageForModels.handleNewClicked.bind(undefined, {
             selectElement : select
@@ -27,6 +29,7 @@ var CBAdminPageForModels = {
         });
 
         var handleClassChanged = CBAdminPageForModels.handleClassChanged.bind(undefined, {
+            modelsElement   : models,
             selectElement   : select,
             titleElement    : title
         });
@@ -37,6 +40,7 @@ var CBAdminPageForModels = {
         menu.appendChild(button);
         element.appendChild(menu);
         element.appendChild(title);
+        element.appendChild(models);
 
         handleClassChanged.call();
 
@@ -44,6 +48,7 @@ var CBAdminPageForModels = {
     },
 
     /**
+     * @param   {Element}   modelsElement
      * @param   {Element}   selectElement
      * @param   {Element}   titleElement
      *
@@ -51,6 +56,73 @@ var CBAdminPageForModels = {
      */
     handleClassChanged : function(args) {
         args.titleElement.textContent = CBEditableClasses[args.selectElement.value].title;
+
+        var className   = CBEditableClasses[args.selectElement.value].className;
+        var formData    = new FormData();
+        var xhr         = new XMLHttpRequest();
+        xhr.onload      = CBAdminPageForModels.handleFetchListCompleted.bind(undefined, {
+            modelsElement   : args.modelsElement,
+            xhr             : xhr
+        });
+        xhr.onerror     = CBAdminPageForModels.handleFetchListFailed.bind(undefined, {
+            modelsElement   : args.modelsElement,
+            xhr             : xhr
+        });
+
+        formData.append("className",    className);
+        formData.append("pageNumber",   1);
+        xhr.open("POST", "/api/?class=CBAdminPageForModels&function=fetchModelList");
+        xhr.send(formData);
+    },
+
+    /**
+     * @param   {Element}           modelsElement
+     * @param   {XMLHttpRequest}    xhr
+     *
+     * @return  undefined
+     */
+    handleFetchListCompleted : function(args) {
+        var response = Colby.responseFromXMLHttpRequest(args.xhr);
+
+        if (response.wasSuccessful) {
+            var child;
+
+            if (child = args.modelsElement.firstChild) {
+                args.modelsElement.removeChild(child);
+            }
+
+            var table = document.createElement("table");
+
+            response.models.forEach(function(model) {
+                var tr              = document.createElement("tr");
+                var edit            = document.createElement("td");
+                var a               = document.createElement("a");
+                a.href              = "/admin/models/edit/?ID=" + model.ID;
+                a.textContent       = "edit";
+                var title           = document.createElement("td");
+                title.textContent   = model.title;
+
+                edit.appendChild(a);
+                tr.appendChild(edit);
+                tr.appendChild(title);
+                table.appendChild(tr);
+            });
+
+            args.modelsElement.appendChild(table);
+        } else {
+            Colby.displayResponse(response);
+        }
+    },
+
+    /**
+     * @param   {Element}           modelsElement
+     * @param   {XMLHttpRequest}    xhr
+     *
+     * @return  undefined
+     */
+    handleFetchListFailed : function(args) {
+        // TODO: implement
+        alert('An error occurred while trying to retreive the model list.');
     },
 
     /**
