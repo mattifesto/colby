df84b3c68ba7a5de4ea4c867bf6e958330889150
Matt Calkins (MacBook Air) <matt@mattifesto.com>
2015-06-24 15:43:10 -0700

    Add CBClassMenuItem

---
+++ classes/CBClassMenuItem/CBClassMenuItem.php
@@ -0,0 +1,50 @@
+<?php
+
+/**
+ * This class represents a single menu item in a menu of class names such as a
+ * menu of class names of views in the page editor or a menu of class names of
+ * editable model classes in the model editor.
+ *
+ * Whenever you are creating a menu of class names, this is the class to use to
+ * represent a single menu item.
+ */
+final class CBClassMenuItem {
+
+    /**
+     * @return [{string}]
+     */
+    public static function editorURLsForCSS() {
+        return [
+            CBClassMenuItem::URL('CBClassNameMenuItemEditor.css')
+        ];
+    }
+
+    /**
+     * @return [{string}]
+     */
+    public static function editorURLsForJavaScript() {
+        return [
+            CBSystemURL . '/javascript/CBStringEditorFactory.js',
+            CBClassMenuItem::URL('CBClassNameMenuItemEditorFactory.js')
+        ];
+    }
+
+    /**
+     * @return {stdClass}
+     */
+    public static function specToModel(stdClass $spec) {
+        $model                  = CBModels::modelWithClassName(__CLASS__);
+        $model->itemClassName   = isset($spec->itemClassName) ? (string)$spec->itemClassName : '';
+        $model->group           = isset($spec->group) ? (string)$spec->group : '';
+        $model->title           = isset($spec->title) ? (string)$spec->title : '';
+
+        return $model;
+    }
+
+    /**
+     * @return {string}
+     */
+    public static function URL($filename) {
+        return CBSystemURL . "/classes/CBClassMenuItem/{$filename}";
+    }
+}
