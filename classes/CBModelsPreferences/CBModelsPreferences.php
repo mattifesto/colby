b39031144332319162b7dbefc0f9c64877904e43
Matt Calkins (MacBook Air) <matt@mattifesto.com>
2015-06-24 15:43:52 -0700

    Add CBModelsPreferences

---
+++ classes/CBModelsPreferences/CBModelsPreferences.php
@@ -0,0 +1,63 @@
+<?php
+
+final class CBModelsPreferences {
+
+    const ID = '69b3958b95e87cca628fc2b9cd70f420faf33a0a';
+
+    /**
+     * @return [{string}]
+     */
+    public static function editorURLsForCSS() {
+        return [
+            CBModelsPreferences::URL('CBModelsPreferencesEditor.css')
+        ];
+    }
+
+    /**
+     * @return [{string}]
+     */
+    public static function editorURLsForJavaScript() {
+        return [
+            CBSystemURL . '/javascript/CBStringEditorFactory.js',
+            CBModelsPreferences::URL('CBModelsPreferencesEditorFactory.js')
+        ];
+    }
+
+    /**
+     * @return null
+     */
+    public static function install() {
+        $spec = CBModels::fetchSpecByID(CBModelsPreferences::ID);
+
+        if ($spec === false) {
+            $spec               = CBModels::modelWithClassName(__CLASS__, [
+                'ID'            => CBModelsPreferences::ID
+            ]);
+
+            $menuItem                   = CBModels::modelWithClassName('CBClassMenuItem');
+            $menuItem->itemClassName    = 'CBMenu';
+            $menuItem->group            = 'Administrators';
+            $menuItem->title            = 'Menus';
+            $spec->classMenuItems       = [$menuItem];
+
+            CBModels::save([$spec]);
+        }
+    }
+
+    /**
+     * @return {stdClass}
+     */
+    public static function specToModel(stdClass $spec) {
+        $model                  = CBModels::modelWithClassName(__CLASS__);
+        $model->classMenuItems  = array_map('CBClassMenuItem::specToModel', $spec->classMenuItems);
+
+        return $model;
+    }
+
+    /**
+     * @return {string}
+     */
+    public static function URL($filename) {
+        return CBSystemURL . "/classes/CBModelsPreferences/{$filename}";
+    }
+}
