ad82f59c46199eaf5898bf9847efe7f56cc8b97c
Matt Calkins (MacBook Air) <matt@mattifesto.com>
2015-06-27 21:00:23 -0700

    Improve CBModelsPreferences UI

--- classes/CBModelsPreferences/CBModelsPreferencesEditorFactory.js
+++ classes/CBModelsPreferences/CBModelsPreferencesEditorFactory.js
@@ -11,17 +11,46 @@ var CBModelsPreferencesEditorFactory = {
     createEditor : function(args) {
         var element         = document.createElement("div");
         element.className   = "CBModelsPreferencesEditor";
+        var h1              = document.createElement("h1");
+        h1.textContent      = "Models Preferences";
+        var container       = document.createElement("div");
+
+        element.appendChild(h1);
 
         if (!args.spec.classMenuItems) {
             args.spec.classMenuItems = [];
         }
 
-        element.appendChild(CBSpecArrayEditorFactory.createEditor({
+        var classMenuItemsEditor = CBSpecArrayEditorFactory.createEditor({
             array           : args.spec.classMenuItems,
             classNames      : ["CBClassMenuItem"],
             handleChanged   : args.handleSpecChanged
+        });
+
+        element.appendChild(CBModelsPreferencesEditorFactory.createSection({
+            contentElement  : classMenuItemsEditor,
+            title           : 'Class Menu Items'
         }));
 
         return element;
+    },
+
+    /**
+     * @param   {Element}   contentElement
+     * @param   {string}    title
+     *
+     * @return  {Element}
+     */
+    createSection : function(args) {
+        var section     = document.createElement("section");
+        var h1          = document.createElement("h1");
+        h1.textContent  = args.title || '';
+        var container   = document.createElement("div");
+
+        container.appendChild(args.contentElement);
+        section.appendChild(h1);
+        section.appendChild(container);
+
+        return section;
     }
 };
