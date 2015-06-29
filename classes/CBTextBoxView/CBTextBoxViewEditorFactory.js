1e66a3ad88a0d8380b25480a321163bb9b719dc6
Matt Calkins (MacBook Air) <matt@mattifesto.com>
2015-06-29 11:19:20 -0700

    Add theme, height, and width properties to CBTextBoxViewEditor UI

--- classes/CBTextBoxView/CBTextBoxViewEditorFactory.js
+++ classes/CBTextBoxView/CBTextBoxViewEditorFactory.js
@@ -11,11 +11,36 @@ var CBTextBoxViewEditorFactory = {
     createEditor : function(args) {
         var element         = document.createElement("div");
         element.className   = "CBTextBoxViewEditor";
+        var settings        = document.createElement("div");
+        settings.className  = "settings";
+
+        settings.appendChild(CBStringEditorFactory.createSelectEditor({
+            handleSpecChanged   : args.handleSpecChanged,
+            labelText           : "Theme",
+            propertyName        : "themeID",
+            spec                : args.spec
+        }));
+
+        settings.appendChild(CBStringEditorFactory.createSingleLineEditor({
+            handleSpecChanged   : args.handleSpecChanged,
+            labelText           : "Width",
+            propertyName        : "width",
+            spec                : args.spec
+        }));
+
+        settings.appendChild(CBStringEditorFactory.createSingleLineEditor({
+            handleSpecChanged   : args.handleSpecChanged,
+            labelText           : "Height",
+            propertyName        : "height",
+            spec                : args.spec
+        }));
+
+        element.appendChild(settings);
 
         element.appendChild(CBStringEditorFactory.createSingleLineEditor({
             handleSpecChanged   : args.handleSpecChanged,
             labelText           : "Title",
-            propertyName        : "title",
+            propertyName        : "titleAsMarkaround",
             spec                : args.spec
         }));
 
