d29e4108f667396a62781883e6e37f32cc955dfb
Matt Calkins (MacBook Air) <matt@mattifesto.com>
2015-07-04 17:51:11 -0700

    Add "Add Style" button to CBTextBoxViewThemeEditor

--- classes/CBTextBoxViewTheme/CBTextBoxViewThemeEditorFactory.js
+++ classes/CBTextBoxViewTheme/CBTextBoxViewThemeEditorFactory.js
@@ -9,8 +9,10 @@ var CBTextBoxViewThemeEditorFactory = {
      * @return  {Element}
      */
     createEditor : function(args) {
-        var element         = document.createElement("div");
-        element.className   = "CBTextBoxViewThemeEditor";
+        var element                 = document.createElement("div");
+        element.className           = "CBTextBoxViewThemeEditor";
+        var button                  = document.createElement("button");
+        button.textContent          = "Add Style";
 
         element.appendChild(CBStringEditorFactory.createSingleLineEditor({
             handleSpecChanged   : args.handleSpecChanged,
@@ -19,19 +21,30 @@ var CBTextBoxViewThemeEditorFactory = {
             spec                : args.spec
         }));
 
-        var CSSClass                = "T" + args.spec.ID;
-        var classIndicator          = document.createElement("div");
-        classIndicator.textContent  = "Theme CSS Class: " + CSSClass;
-
-        element.appendChild(classIndicator);
-
         element.appendChild(CBStringEditorFactory.createMultiLineEditor({
-            handleSpecChanged   : args.handleSpecChanged,
-            labelText           : "Styles",
-            propertyName        : "styles",
+            handleSpecChanged       : args.handleSpecChanged,
+            labelText               : "Styles",
+            propertyName            : "styles",
+            propertyUpdatedEvent    : args.spec.ID,
+            spec                    : args.spec
+        }));
+
+        button.addEventListener("click", CBTextBoxViewThemeEditorFactory.handleAddStyle.bind(undefined, {
             spec                : args.spec
         }));
 
+        element.appendChild(button);
+
         return element;
+    },
+
+    /**
+     * @param   {Object}    spec
+     *
+     * @return  undefined
+     */
+    handleAddStyle : function(args) {
+        args.spec.styles = args.spec.styles.replace(/[\s]+$/, "\n\n.T" + args.spec.ID + " {\n\n}\n");
+        document.dispatchEvent(new Event(args.spec.ID));
     }
 };
