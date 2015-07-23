b77ac05a0b4ff38a29c89709dc1bbce4814d3ccf
Matt Calkins (MacBook Air) <matt@mattifesto.com>
2015-07-23 12:32:12 -0700

    Remove CBSectionEditorView

--- handlers/handle,admin,pages,edit.php
+++ handlers/handle,admin,pages,edit.php
@@ -66,8 +66,6 @@ CBHTMLOutput::addJavaScriptURL(CBSystemURL . '/javascript/CBPublicationControl.j
  * should be removed from this file.
  */
 
-CBHTMLOutput::addJavaScriptURL(CBSystemURL . '/javascript/CBSectionEditorView.js');
-CBHTMLOutput::addCSSURL(       CBSystemURL . '/javascript/CBSectionEditorView.css');
 CBHTMLOutput::addJavaScriptURL(CBSystemURL . '/javascript/CBViewEditorChromeFactory.js');
 CBHTMLOutput::addJavaScriptURL(CBSystemURL . '/javascript/CBViewEditorWidgetFactory.js');
 CBHTMLOutput::addJavaScriptURL(CBSystemURL . '/javascript/CBViewMenu.js');
