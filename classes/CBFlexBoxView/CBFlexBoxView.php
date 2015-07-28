4045943d785be6389a421c71a9abc7718aae8c82
Matt Calkins (MacBook Air) <matt@mattifesto.com>
2015-07-28 15:18:56 -0700

    Add CBFlexBoxView

---
+++ classes/CBFlexBoxView/CBFlexBoxView.php
@@ -0,0 +1,253 @@
+<?php
+
+final class CBFlexBoxView {
+
+    /**
+     * @return [{string}]
+     */
+    public static function editorURLsForCSS() {
+        return [
+            CBSystemURL . '/javascript/CBImageEditorFactory.css',
+            CBSystemURL . '/javascript/CBSpecArrayEditor.css',
+            CBFlexBoxView::URL('CBFlexBoxViewEditor.css')
+        ];
+    }
+
+    /**
+     * @return [{string}]
+     */
+    public static function editorURLsForJavaScript() {
+        return [
+            CBSystemURL . '/javascript/CBImageEditorFactory.js',
+            CBSystemURL . '/javascript/CBSpecArrayEditorFactory.js',
+            CBSystemURL . '/javascript/CBStringEditorFactory.js',
+            CBFlexBoxView::URL('CBFlexBoxViewEditorFactory.js')
+        ];
+    }
+
+    /**
+     * @return null
+     */
+    public static function renderModelAsHTML(stdClass $model) {
+        $styles = [];
+
+        if ($model->backgroundColor !== null) {
+            $styles[] = "background-color: {$model->backgroundColor};";
+        }
+
+        $styles[]   = "background-position: {$model->backgroundPositionX} {$model->backgroundPositionY};";
+        $styles[]   = "background-repeat: {$model->backgroundRepeatX} {$model->backgroundRepeatY};";
+
+        if ($model->width !== null) {
+            $styles[] = "width: {$model->width}px;";
+        }
+
+        if ($model->height !== null) {
+            $styles[] = "height: {$model->height}px;";
+        }
+
+        if (!empty($model->imageURL)) {
+            $styles[] = "background-image: url({$model->imageURL});";
+        }
+
+        $flexItemAlign  = CBIE10Flexbox::alignSelfToFlexItemAlign($model->flexAlignSelf);
+        $styles[]       = "align-self: {$model->flexAlignSelf};";
+        $styles[]       = "-ms-flex-item-align: {$flexItemAlign};";
+        $styles[]       = "-webkit-align-self: {$model->flexAlignSelf};";
+
+        $flexAlign  = CBIE10Flexbox::alignItemsToFlexAlign($model->flexAlignItems);
+        $styles[]   = "align-items: {$model->flexAlignItems};";
+        $styles[]   = "-ms-flex-align: {$flexAlign};";
+        $styles[]   = "-webkit-align-items: {$model->flexAlignItems};";
+
+        $styles[]   = "flex-direction: {$model->flexDirection};";
+        $styles[]   = "-ms-flex-direction: {$model->flexDirection};";
+        $styles[]   = "-webkit-flex-direction: {$model->flexDirection};";
+
+        $flexPack   = CBIE10Flexbox::justifyContentToFlexPack($model->flexJustifyContent);
+        $styles[]   = "justify-content: {$model->flexJustifyContent};";
+        $styles[]   = "-ms-flex-pack: {$flexPack};";
+        $styles[]   = "-webkit-justify-content: {$model->flexJustifyContent};";
+
+        $styles     = implode(' ', $styles);
+
+        CBHTMLOutput::addCSSURL(CBFlexBoxView::URL('CBFlexBoxView.css'));
+
+        echo "<{$model->type} class=\"CBFlexBoxView\" style=\"{$styles}\">";
+
+        array_walk($model->subviews, 'CBView::renderModelAsHTML');
+
+        echo "</{$model->type}>";
+    }
+
+    /**
+     * @return {stdClass}
+     */
+    public static function specToModel(stdClass $spec) {
+        $model                  = CBModels::modelWithClassName(__CLASS__);
+        $model->backgroundColor = isset($spec->backgroundColor) ?
+                                  CBFlexBoxView::textToCSSValue($spec->backgroundColor) : null;
+        $model->height          = isset($spec->height) ? CBFlexBoxView::valueToPixelExtent($spec->height) : null;
+        $model->imageURL        = isset($spec->imageURL) ? CBFlexBoxView::URLToCSS($spec->imageURL) : '';
+        $model->subviews        = isset($spec->subviews) ? array_map('CBView::specToModel', $spec->subviews) : [];
+        $model->width           = isset($spec->width) ? CBFlexBoxView::valueToPixelExtent($spec->width) : null;
+        $type                   = isset($spec->type) ? trim($spec->type) : '';
+
+        switch ($type) {
+            case 'article':
+                $model->type = 'article';
+                break;
+            case 'main':
+                $model->type = 'main';
+                break;
+            default:
+                $model->type = 'div';
+        }
+
+        $backgroundPositionX    = isset($spec->backgroundPositionX) ? trim($spec->backgroundPositionX) : '';
+
+        switch ($backgroundPositionX) {
+            case 'left':
+            case 'right':
+                $model->backgroundPositionX = $backgroundPositionX;
+                break;
+            default:
+                $model->backgroundPositionX = 'center';
+        }
+
+        $backgroundPositionY    = isset($spec->backgroundPositionY) ? trim($spec->backgroundPositionY) : '';
+
+        switch ($backgroundPositionY) {
+            case 'center':
+            case 'bottom':
+                $model->backgroundPositionY = $backgroundPositionY;
+                break;
+            default:
+                $model->backgroundPositionY = 'top';
+        }
+
+        $backgroundRepeatX      = isset($spec->backgroundRepeatX) ? trim($spec->backgroundRepeatX) : '';
+
+        switch ($backgroundRepeatX) {
+            case 'repeat':
+                $model->backgroundRepeatX = $backgroundRepeatX;
+                break;
+            default:
+                $model->backgroundRepeatX = 'no-repeat';
+        }
+
+        $backgroundRepeatY      = isset($spec->backgroundRepeatY) ? trim($spec->backgroundRepeatY) : '';
+
+        switch ($backgroundRepeatY) {
+            case 'repeat':
+                $model->backgroundRepeatY = $backgroundRepeatY;
+                break;
+            default:
+                $model->backgroundRepeatY = 'no-repeat';
+        }
+
+        $flexAlignItems         = isset($spec->flexAlignItems) ? trim($spec->flexAlignItems) : '';
+
+        switch ($flexAlignItems) {
+            case 'flex-start':
+            case 'flex-end':
+            case 'center':
+            case 'baseline':
+                $model->flexAlignItems = $flexAlignItems;
+                break;
+            default:
+                $model->flexAlignItems = 'stretch';
+        }
+
+        $flexAlignSelf          = isset($spec->flexAlignSelf) ? trim($spec->flexAlignSelf) : '';
+
+        switch ($flexAlignSelf) {
+            case 'flex-start':
+            case 'flex-end':
+            case 'center':
+            case 'baseline':
+            case 'stretch':
+                $model->flexAlignSelf = $flexAlignSelf;
+                break;
+            default:
+                $model->flexAlignSelf = 'auto';
+        }
+
+        $flexDirection          = isset($spec->flexDirection) ? trim($spec->flexDirection) : '';
+
+        switch ($flexDirection) {
+            case 'row-reverse':
+            case 'column':
+            case 'column-reverse':
+                $model->flexDirection = $flexDirection;
+                break;
+            default:
+                $model->flexDirection = 'row';
+        }
+
+        $flexJustifyContent     = isset($spec->flexJustifyContent) ? trim($spec->flexJustifyContent) : '';
+
+        switch ($flexJustifyContent) {
+            case 'flex-end':
+            case 'center':
+            case 'space-between':
+            case 'space-around':
+                $model->flexJustifyContent = $flexJustifyContent;
+                break;
+            default:
+                $model->flexJustifyContent = 'flex-start';
+        }
+
+        return $model;
+    }
+
+    /**
+     * @return {string} | null
+     */
+    public static function textToCSSValue($text) {
+        $value = trim(str_replace(['<', '>', '"', '\'', ';'], '', $text));
+        return empty($value) ? null : $value;
+    }
+
+    /**
+     * @return {string}
+     */
+    public static function URL($filename) {
+        return CBSystemURL . "/classes/CBFlexBoxView/{$filename}";
+    }
+
+    /**
+     * This function detects the following characters in a URL:
+     *
+     *          '  "  <  >  &  (  )
+     *
+     * If the URL contains one or more of these characters it is considered
+     * invalid for the purposes of this view due to the way it will be embedded
+     * in the style property of the element and the encertainties on how those
+     * characters should or even can be escaped propertly.  Otherise the URL is
+     * trimmed and returned.
+     *
+     * @return {string}
+     */
+    public static function URLToCSS($URL) {
+        if (preg_match('/[\'"<>&()]/', $URL)) {
+            return '';
+        } else {
+            return trim($URL);
+        }
+    }
+
+    /**
+     * @return {int}|{float}|null
+     */
+    public static function valueToPixelExtent($value) {
+        if (is_numeric($value)) {
+            $number = $value + 0;
+            if ($number > 0) {
+                return $number;
+            }
+        }
+
+        return null;
+    }
+}
---
+++ classes/CBFlexBoxView/CBFlexBoxViewEditorFactory.js
@@ -0,0 +1,322 @@
+"use strict";
+
+var CBFlexBoxViewEditorFactory = {
+
+    /**
+     * @param   {function}  handleSpecChanged
+     * @param   {Object}    spec
+     *
+     * @return  {Element}
+     */
+    createEditor : function(args) {
+        var row;
+        var element             = document.createElement("div");
+        element.className       = "CBFlexBoxViewEditor";
+        var container           = document.createElement("div");
+        container.className     = "container";
+        var preview             = CBImageEditorFactory.createImagePreviewElement();
+        preview.img.src         = args.spec.imageURL || "";
+        var options             = document.createElement("div");
+        options.className       = "options";
+        var flexbox             = document.createElement("h2");
+        flexbox.textContent     = "Flexbox";
+        var subviews            = document.createElement("div");
+        subviews.className      = "subviews";
+
+        container.appendChild(preview.element);
+
+        row                 = document.createElement("div");
+        row.className       = "row";
+
+        row.appendChild(CBStringEditorFactory.createSelectEditor({
+            data                : [
+                { textContent : "Div",      value : "" },
+                { textContent : "Article",  value : "article" },
+                { textContent : "Main",     value : "main" }
+            ],
+            handleSpecChanged   : args.handleSpecChanged,
+            labelText           : "Type",
+            propertyName        : "type",
+            spec                : args.spec
+        }));
+
+        row.appendChild(CBStringEditorFactory.createSingleLineEditor({
+            className           : "pixels",
+            handleSpecChanged   : args.handleSpecChanged,
+            labelText           : "Width",
+            propertyName        : "width",
+            spec                : args.spec
+        }));
+
+        row.appendChild(CBStringEditorFactory.createSingleLineEditor({
+            className           : "pixels",
+            handleSpecChanged   : args.handleSpecChanged,
+            labelText           : "Height",
+            propertyName        : "height",
+            spec                : args.spec
+        }));
+
+        options.appendChild(row);
+
+        /* background section */
+
+        var background          = document.createElement("h2");
+        background.textContent  = "Background";
+
+        options.appendChild(background);
+
+        row                 = document.createElement("div");
+        row.className       = "row";
+        var clear           = document.createElement("button");
+        clear.textContent   = "Clear Image";
+        var size            = preview.size;
+        var color           = CBStringEditorFactory.createSingleLineEditor({
+            handleSpecChanged   : args.handleSpecChanged,
+            labelText           : "Color",
+            propertyName        : "backgroundColor",
+            spec                : args.spec
+        });
+        var upload          = CBImageEditorFactory.createEditorUploadButton({
+            handleImageUploaded     : CBFlexBoxViewEditorFactory.handleImageUploaded.bind(undefined, {
+                handleSpecChanged   : args.handleSpecChanged,
+                previewImageElement : preview.img,
+                sizeElement         : size,
+                spec                : args.spec
+            })
+        });
+
+        clear.addEventListener("click", CBFlexBoxViewEditorFactory.handleClearImage.bind(undefined, {
+            handleSpecChanged   : args.handleSpecChanged,
+            previewImageElement : preview.img,
+            sizeElement         : size,
+            spec                : args.spec
+        }));
+
+        CBFlexBoxViewEditorFactory.displaySize({
+            sizeElement : size,
+            spec        : args.spec
+        });
+
+        CBFlexBoxViewEditorFactory.displayThumbnail({
+            previewImageElement : preview.img,
+            spec                : args.spec
+        });
+
+        row.appendChild(upload);
+        row.appendChild(clear);
+        row.appendChild(color);
+
+        row.appendChild(CBStringEditorFactory.createSelectEditor({
+            data                : [
+                { textContent : "Left",     value : "left" },
+                { textContent : "Center",   value : "" },
+                { textContent : "Right",    value : "right" }
+            ],
+            handleSpecChanged   : args.handleSpecChanged,
+            labelText           : "Position X",
+            propertyName        : "backgroundPositionX",
+            spec                : args.spec
+        }));
+
+        row.appendChild(CBStringEditorFactory.createSelectEditor({
+            data                : [
+                { textContent : "Top",      value : "" },
+                { textContent : "Center",   value : "center" },
+                { textContent : "Bottom",   value : "bottom" }
+            ],
+            handleSpecChanged   : args.handleSpecChanged,
+            labelText           : "Position Y",
+            propertyName        : "backgroundPositionY",
+            spec                : args.spec
+        }));
+
+        options.appendChild(row);
+
+        row                 = document.createElement("div");
+        row.className       = "row";
+
+        row.appendChild(CBStringEditorFactory.createSelectEditor({
+            data                : [
+                { textContent : "No",   value : "" },
+                { textContent : "Yes",  value : "repeat" }
+            ],
+            handleSpecChanged   : args.handleSpecChanged,
+            labelText           : "Repeat X",
+            propertyName        : "backgroundRepeatX",
+            spec                : args.spec
+        }));
+
+        row.appendChild(CBStringEditorFactory.createSelectEditor({
+            data                : [
+                { textContent : "No",   value : "" },
+                { textContent : "Yes",  value : "repeat" }
+            ],
+            handleSpecChanged   : args.handleSpecChanged,
+            labelText           : "Repeat Y",
+            propertyName        : "backgroundRepeatY",
+            spec                : args.spec
+        }));
+
+        options.appendChild(row);
+
+        /* flexbox section */
+
+        options.appendChild(flexbox);
+
+        row                 = document.createElement("div");
+        row.className       = "row";
+
+        row.appendChild(CBStringEditorFactory.createSelectEditor({
+            data                : [
+                { textContent : "Row",      value : "" },
+                { textContent : "Row (Reverse)",  value : "row-reverse" },
+                { textContent : "Column",  value : "column" },
+                { textContent : "Column (Reverse)",     value : "column-reverse" }
+            ],
+            handleSpecChanged   : args.handleSpecChanged,
+            labelText           : "Direction",
+            propertyName        : "flexDirection",
+            spec                : args.spec
+        }));
+
+        row.appendChild(CBStringEditorFactory.createSelectEditor({
+            data                : [
+                { textContent : "Flex Start",       value : "" },
+                { textContent : "Flex End",         value : "flex-end" },
+                { textContent : "Center",           value : "center" },
+                { textContent : "Space Between",    value : "space-between" },
+                { textContent : "Space Around",     value : "space-around" }
+            ],
+            handleSpecChanged   : args.handleSpecChanged,
+            labelText           : "Justify Content",
+            propertyName        : "flexJustifyContent",
+            spec                : args.spec
+        }));
+
+        row.appendChild(CBStringEditorFactory.createSelectEditor({
+            data                : [
+                { textContent : "Flex Start",       value : "flex-start" },
+                { textContent : "Flex End",         value : "flex-end" },
+                { textContent : "Center",           value : "center" },
+                { textContent : "Baseline",         value : "baseline" },
+                { textContent : "Stretch",          value : "" }
+            ],
+            handleSpecChanged   : args.handleSpecChanged,
+            labelText           : "Align Items",
+            propertyName        : "flexAlignItems",
+            spec                : args.spec
+        }));
+
+        row.appendChild(CBStringEditorFactory.createSelectEditor({
+            data                : [
+                { textContent : "Auto",     value : "" },
+                { textContent : "Start",    value : "flex-start" },
+                { textContent : "End",      value : "flex-end" },
+                { textContent : "Center",   value : "center" },
+                { textContent : "Baseline", value : "baseline" },
+                { textContent : "Stretch",  value : "stretch" }
+            ],
+            handleSpecChanged   : args.handleSpecChanged,
+            labelText           : "Align Self",
+            propertyName        : "flexAlignSelf",
+            spec                : args.spec
+        }));
+
+        options.appendChild(row);
+        container.appendChild(options);
+
+        /* subviews */
+
+        if (args.spec.subviews === undefined) {
+            args.spec.subviews = [];
+        }
+
+        subviews.appendChild(CBSpecArrayEditorFactory.createEditor({
+            array           : args.spec.subviews,
+            classNames      : CBPageEditorAvailableViewClassNames,
+            handleChanged   : args.handleSpecChanged
+        }));
+
+        element.appendChild(container);
+        element.appendChild(subviews);
+
+        return element;
+    },
+
+    /**
+     * @param   {Element}   sizeElement
+     * @param   {Object}    spec
+     *
+     * @return  undefined
+     */
+    displaySize : function(args) {
+        if (args.spec.imageHeight === undefined || args.spec.imageWidth === undefined) {
+            args.sizeElement.textContent = "no image";
+        } else {
+            args.sizeElement.textContent = args.spec.imageWidth + " × " + args.spec.imageHeight;
+        }
+    },
+
+    /**
+     * @param   {Element}   previewImageElement
+     * @param   {Object}    spec
+     *
+     * @return  undefined
+     */
+    displayThumbnail : function(args) {
+        if (args.spec.imageURL === undefined) {
+            args.previewImageElement.style.visibility   = "hidden";
+        } else {
+            args.previewImageElement.src                = args.spec.imageURL;
+            args.previewImageElement.style.visibility   = "visible";
+        }
+    },
+
+    /**
+     * @param   {function}  handleSpecChanged
+     * @param   {Element}   previewImageElement
+     * @param   {Element}   sizeElement
+     * @param   {Object}    spec
+     */
+    handleClearImage : function(args) {
+        args.spec.imageHeight           = undefined;
+        args.spec.imageURL              = undefined;
+        args.spec.imageWidth            = undefined;
+
+        CBFlexBoxViewEditorFactory.displaySize({
+            sizeElement : args.sizeElement,
+            spec        : args.spec
+        });
+
+        CBFlexBoxViewEditorFactory.displayThumbnail({
+            previewImageElement : args.previewImageElement,
+            spec                : args.spec
+        });
+
+        args.handleSpecChanged.call();
+    },
+
+    /**
+     * @param   {function}  handleSpecChanged
+     * @param   {Element}   previewImageElement
+     * @param   {Element}   sizeElement
+     * @param   {Object}    spec
+     */
+    handleImageUploaded : function(args, response) {
+        args.spec.imageHeight           = response.sizes.original.height;
+        args.spec.imageURL              = response.sizes.original.URL;
+        args.spec.imageWidth            = response.sizes.original.width;
+
+        CBFlexBoxViewEditorFactory.displaySize({
+            sizeElement : args.sizeElement,
+            spec        : args.spec
+        });
+
+        CBFlexBoxViewEditorFactory.displayThumbnail({
+            previewImageElement : args.previewImageElement,
+            spec                : args.spec
+        });
+
+        args.handleSpecChanged.call();
+    }
+};
