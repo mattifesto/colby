1408e8634dc722c826cd315c318bbeaa613d42ec
Matt Calkins (MacBook Air) <matt@mattifesto.com>
2015-08-29 19:57:18 -0700

    Update whitespace

--- handlers/handle,colby,javascript-error.php
+++ handlers/handle,colby,javascript-error.php
@@ -3,8 +3,7 @@
 $attributes = array();
 $hashes     = array();
 
-if (isset($_POST['message']))
-{
+if (isset($_POST['message'])) {
     $key                        = 'Message';
     $value                      = $_POST['message'];
     $attributes[$key]           = $value;
@@ -34,9 +33,7 @@ if (isset($_POST['message']))
     $attributes[$key]           = $value;
     $hash                       = sha1("{$key}: {$value}");
     $hashes[$key]               = $hash;
-}
-else
-{
+} else {
     $key                        = 'Message';
     $value                      = 'Colby: The standard error parameters were not specified.';
     $attributes[$key]           = $value;
@@ -56,13 +53,11 @@ $attributes[$key]           = $value;
 $hash                       = sha1("{$key}: {$value}");
 $hashes[$key]               = $hash;
 
-if (containsExcludedHash($hashes))
-{
+if (containsExcludedHash($hashes)) {
     exit;
 }
 
-if (class_exists('Swift_SmtpTransport'))
-{
+if (class_exists('Swift_SmtpTransport')) {
     $CSS = <<<EOT
 
         <style>
@@ -86,14 +81,12 @@ EOT;
 
     $html = "<table>{$CSS}";
 
-    foreach ($attributes as $key => $value)
-    {
+    foreach ($attributes as $key => $value) {
         $keyHTML    = ColbyConvert::textToHTML($key);
         $valueHTML  = ColbyConvert::textToHTML($value);
         $valueHTML  = "<div>$valueHTML</div>";
 
-        if (isset($hashes[$key]))
-        {
+        if (isset($hashes[$key])) {
             $hashHTML   = ColbyConvert::textToHTML($hashes[$key]);
             $valueHTML  = "{$valueHTML}<div><small>{$hashHTML}</small></div>";
         }
@@ -131,27 +124,21 @@ error_log('JavaScript error: ' . var_export($attributes, true));
 /**
  * @Return Boolean
  */
-function containsExcludedHash($hashes)
-{
+function containsExcludedHash($hashes) {
     $excludesFilename = Colby::findFile('setup/excludedJavaScriptErrors.txt');
 
-    if ($excludesFilename)
-    {
+    if ($excludesFilename) {
         $excludedHashes = array();
         $lines          = file($excludesFilename);
 
-        foreach ($lines as $line)
-        {
-            if (preg_match('/^([a-f0-9]{40})/', $line, $matches))
-            {
+        foreach ($lines as $line) {
+            if (preg_match('/^([a-f0-9]{40})/', $line, $matches)) {
                 $excludedHashes[] = $matches[1];
             }
         }
 
-        foreach ($hashes as $hash)
-        {
-            if (in_array($hash, $excludedHashes))
-            {
+        foreach ($hashes as $hash) {
+            if (in_array($hash, $excludedHashes)) {
                 return true;
             }
         }
