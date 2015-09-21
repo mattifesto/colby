992761611be070cf05174dc9c995c8e97cbaff18
Matt Calkins (MacBook Air) <matt@mattifesto.com>
2015-09-21 16:58:52 -0700

    Add CBPageSettingsForStaticPages

---
+++ classes/CBPageSettingsForStaticPages/CBPageSettingsForStaticPages.php
@@ -0,0 +1,40 @@
+<?php
+
+final class CBPageSettingsForStaticPages {
+
+    /**
+     * @return  null
+     */
+    public static function renderEndOfBodyContent() { ?>
+        <script src="<?= CBSystemURL ?>/javascript/Colby.js"></script>
+    <?php }
+
+    /**
+     * @return  null
+     */
+    public static function renderHeadContent() { ?>
+        <script src="<?= CBSystemURL ?>/javascript/html5shiv.js"></script>
+        <script src="<?= CBSystemURL ?>/javascript/ColbyEqualize.js"></script>
+        <link rel="stylesheet" href="<?= CBSystemURL ?>/css/equalize.css">
+        <style>html { color: hsl(0, 0%, 20%); min-width: 980px; }</style>
+    <?php }
+
+    /**
+     * @return  null
+     */
+    public static function renderStartOfBodyContent() {
+        $googleTagManagerID = CBSitePreferences::googleTagManagerID();
+
+        if ($googleTagManagerID !== '') { ?>
+            <!-- Google Tag Manager -->
+            <noscript><iframe src="//www.googletagmanager.com/ns.html?id=<?= $googleTagManagerID ?>"
+            height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
+            <script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+            new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+            j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+            '//www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+            })(window,document,'script','dataLayer','<?= $googleTagManagerID ?>');</script>
+            <!-- End Google Tag Manager -->
+        <?php }
+    }
+}
