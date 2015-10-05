d3297c99e4cc2287b2d638b56a3f91cfdbe1ddd4
Matt Calkins (MacBook Air) <matt@mattifesto.com>
2015-10-04 19:17:28 -0700

    Update 404 page

--- handlers/handle-default.php
+++ handlers/handle-default.php
@@ -2,49 +2,18 @@
 
 header($_SERVER['SERVER_PROTOCOL'] . ' 404 Not Found');
 
-include_once CBSystemDirectory . '/classes/CBHTMLOutput.php';
-
-
+CBHTMLOutput::begin();
 CBHTMLOutput::setTitleHTML('Page Not Found');
 CBHTMLOutput::setDescriptionHTML('The page you requested was not found.');
-CBHTMLOutput::begin();
-
-include Colby::findFile('sections/public-page-settings.php');
 
 ?>
 
-<main>
-    <style scoped>
+<main style="text-align: center; padding: 40px 10px;">
+    <p style="margin-bottom: 40px;">The page you requested was not found.
 
-        main
-        {
-            text-align: center;
-        }
-
-        main h1
-        {
-            margin: 50px 0;
-        }
-
-    </style>
-
-    <h1>The page you requested was not found.</h1>
-
-    <p>
-        <code><?php
-
-            // put spaces around the following three characters: / ? &
-            // to allow for nice line break behavior
-
-            echo CBSiteURL,
-                htmlspecialchars(
-                    preg_replace('/([\/\?&])/',
-                                 ' $1 ',
-                                 $_SERVER['REQUEST_URI']));
-
-
-        ?></code>
-
+    <div style="font-size: 1.5em;">
+        <?= CBSiteURL, htmlspecialchars( preg_replace('/([\/\?&])/', ' $1 ', $_SERVER['REQUEST_URI'])) ?>
+    </div>
 </main>
 
 <?php
