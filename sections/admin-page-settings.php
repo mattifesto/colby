491a46cda384a590268638821901659208ee93f3
Matt Calkins (MacBook Air) <matt@mattifesto.com>
2015-10-04 20:11:07 -0700

    Remove deprecated admin-page-settings

--- sections/admin-page-settings.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-include CBSystemDirectory . '/sections/equalize.php';
-
-CBHTMLOutput::addCSSURL(CBSystemURL . '/sections/admin-page-settings.css');
-CBHTMLOutput::addCSSURL('https://fonts.googleapis.com/css?family=Source+Sans+Pro:400');
-CBHTMLOutput::addCSSURL('https://fonts.googleapis.com/css?family=Source+Sans+Pro:700');
-
-CBHTMLOutput::addJavaScriptURL(CBSystemURL . '/javascript/Colby.js');
