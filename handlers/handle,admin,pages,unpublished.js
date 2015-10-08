518e5722fa9934113690099998c9ff218d20aef6
Matt Calkins (MacBook Air) <matt@mattifesto.com>
2015-10-08 14:30:32 -0700

    Update the unpublished pages admin page

--- handlers/handle,admin,pages,unpublished.js
+++ handlers/handle,admin,pages,unpublished.js
@@ -8,10 +8,63 @@ var CBUnpublishedPagesAdmin = {
     createElement : function() {
         var element = document.createElement("div");
         element.className = "CBUnpublishedPagesAdmin";
-        element.textContent = "foo";
+
+        CBUnpublishedPagesAdmin.fetchPages({
+            element : element
+        });
 
         return element;
-    }
+    },
+
+    /**
+     * @param {Element} element
+     *
+     * @return undefined
+     */
+    fetchPages : function(args) {
+        var xhr = new XMLHttpRequest();
+        xhr.onload = CBUnpublishedPagesAdmin.fetchPagesDidLoad.bind(undefined, {
+            element: args.element,
+            xhr : xhr
+        });
+        xhr.onerror = CBUnpublishedPagesAdmin.fetchPagesDidError.bind(undefined, {
+            xhr : xhr
+        });
+
+        xhr.open("POST", "/api/?class=CBViewPage&function=fetchUnpublishedPagesList");
+        xhr.send();
+    },
+
+    /**
+     * @return undefined
+     */
+    fetchPagesDidError : function(args) {
+
+    },
+
+    /**
+     * @param {Element} element
+     *
+     * @return undefined
+     */
+    fetchPagesDidLoad : function(args) {
+        var response = Colby.responseFromXMLHttpRequest(args.xhr);
+        var pages = response.pages.sort(function(left, right) {
+            if (left.created > right.created) {
+                return -1;
+            } else if (left.created < right.created) {
+                return 1;
+            } else {
+                return 0;
+            }
+        });
+        var list = CBPageList.createElement({
+            pages : pages
+        });
+
+        args.element.textContent = null;
+        args.element.appendChild(list);
+    },
 };
 
 document.addEventListener("DOMContentLoaded", function() {
--- handlers/handle,admin,pages,unpublished.php
+++ handlers/handle,admin,pages,unpublished.php
@@ -9,93 +9,17 @@ CBHTMLOutput::$classNameForSettings = 'CBPageSettingsForAdminPages';
 CBHTMLOutput::setTitleHTML('Unpublished Pages');
 CBHTMLOutput::setDescriptionHTML('Pages that haven\'t been published.');
 CBHTMLOutput::addCSSURL(CBSystemURL . '/handlers/handle,admin,pages.css');
-CBHTMLOutput::addJavaScriptURL(CBSystemURL . '/handlers/handle,admin,pages.js');
 CBHTMLOutput::addJavaScriptURL(CBSystemURL . '/handlers/handle,admin,pages,unpublished.js');
+CBHTMLOutput::addCSSURL(CBSystemURL . '/javascript/CBPageList.css');
+CBHTMLOutput::addJavaScriptURL(CBSystemURL . '/javascript/CBPageList.js');
 
 $selectedMenuItemID     = 'pages';
 $selectedSubmenuItemID  = 'unpublished';
 
 include CBSystemDirectory . '/sections/admin-page-menu.php';
 
-?>
-
-<main class="CBSystemFont">
-
-    <table class="list-of-pages">
-        <thead><tr>
-            <th class="actions-cell" style="width: 150px;"></th>
-            <th class="title-cell">Title</th>
-            <th class="publication-date-cell">Publication Date</th>
-        </tr></thead>
-        <tbody class="standard-admin-row-colors">
-
-        <?php
-
-        $result = CBPagesAdmin::queryForPages();
-
-        while ($row = $result->fetch_object())
-        {
-            $elementID = "id-{$row->dataStoreID}";
-
-            $editURL        = COLBY_SITE_URL . "/admin/pages/edit/?data-store-id={$row->dataStoreID}";
-            $editAction     = "location.href = '{$editURL}'";
-            $deleteAction   = "CBPagesAdmin.movePageWithDataStoreIDToTrash('{$row->dataStoreID}');";
-
-            ?>
-
-            <tr id="<?php echo $elementID; ?>">
-                <td class="actions-cell">
-                    <button onclick="<?php echo $editAction; ?>">Edit</button><!--
-                    --><button onclick="<?php echo $deleteAction; ?>">Move to Trash</button>
-                </td>
-                <td class="title-cell"><?php echo $row->titleHTML; ?></td>
-                <td class="publication-date-cell"><span class="time"
-                          data-timestamp="<?php echo $row->published ? $row->published * 1000 : ''; ?>">
-                    </span></td>
-            </tr>
-
-            <?php
-        }
-
-        ?>
-
-    </tbody></table>
-
-</main>
-
-<?php
+echo '<main></main>';
 
 include CBSystemDirectory . '/sections/admin-page-footer-2.php';
 
 CBHTMLOutput::render();
-
-
-/**
- *
- */
-class CBPagesAdmin
-{
-    /**
-     * @return mysqli_result
-     */
-    public static function queryForPages()
-    {
-        $sql = <<<EOT
-
-        SELECT
-            LOWER(HEX(`archiveID`)) AS `dataStoreID`,
-            `titleHTML`,
-            `published`
-        FROM
-            `ColbyPages`
-        WHERE
-            `className` = 'CBViewPage' AND
-            `published` IS NULL
-        ORDER BY
-            `ID` DESC
-
-EOT;
-
-        return Colby::query($sql);
-    }
-}
