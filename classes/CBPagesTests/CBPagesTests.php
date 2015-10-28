7fa986e2ac76918e13b09e4bab555b1960db130c
Matt Calkins (MacBook Air) <matt@mattifesto.com>
2015-10-28 14:02:11 -0700

    Add CBPagesTests

---
+++ classes/CBPagesTests/CBPagesTests.php
@@ -0,0 +1,27 @@
+<?php
+
+final class CBPagesTests {
+
+    /**
+     * @return null
+     */
+    public static function stringToDencodedURIPathTest() {
+        $tests = [
+            ['',                                    ''],
+            ['///////',                             ''],
+            ['///bob// //sam///',                   'bob/sam'],
+            ['bob/sam',                             'bob/sam'],
+            ['///Piñata // Örtega Smith /// //',    'pinata/ortega-smith']
+        ];
+
+        foreach ($tests as $test) {
+            $dencodedURIPath = CBPages::stringToDencodedURIPath($test[0]);
+
+            if ($dencodedURIPath !== $test[1]) {
+                throw new Exception("The parameter \"{$test[0]}\" returned " .
+                "the path \"{$dencodedURIPath}\" instead of the expected " .
+                "path \"{$test[1]}\"");
+            }
+        }
+    }
+}
