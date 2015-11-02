69417a63975c069f2111d6ae6341fdbb8b083b7f
Matt Calkins (MacBook Air) <matt@mattifesto.com>
2015-11-01 21:23:13 -0800

    Make preview work for page classes

--- handlers/handle,admin,pages,preview.php
+++ handlers/handle,admin,pages,preview.php
@@ -4,30 +4,32 @@ if (!ColbyUser::current()->isOneOfThe('Administrators')) {
     return include CBSystemDirectory . '/handlers/handle-authorization-failed.php';
 }
 
-$ID         = $_GET['ID'];
-$iteration  = isset($_GET['iteration']) ? $_GET['iteration'] : null;
-$IDForSQL   = CBHex160::toSQL($ID);
-$SQL        = <<<EOT
+$ID = $_GET['ID'];
+$iteration = isset($_GET['iteration']) ? $_GET['iteration'] : null;
+$IDAsSQL = CBHex160::toSQL($ID);
+$SQL = <<<EOT
 
-    SELECT
-        `className`,
-        `iteration`
-    FROM
-        `ColbyPages`
-    WHERE
-        `archiveID` = {$IDForSQL}
+    SELECT      `p`.`className`,
+                `p`.`iteration`,
+                `v`.`modelAsJSON` as `model`
+    FROM        `ColbyPages`        AS `p`
+    LEFT JOIN   `CBModels`          AS `m` ON `p`.`archiveID` = `m`.`ID`
+    LEFT JOIN   `CBModelVersions`   AS `v` ON `m`.`ID` = `v`.`ID` AND `m`.`version` = `v`.`version`
+    WHERE       `p`.`archiveID` = {$IDAsSQL}
 
 EOT;
 
-$result     = Colby::query($SQL);
-$row        = $result->fetch_object();
-$iteration  = $iteration ? $iteration : $row->iteration;
+$data = CBDB::SQLToObject($SQL);
 
-$result->free();
-
-if (!$row) {
+if ($data === false) {
     echo 'No page exists for the provided data store ID.';
     return 1;
+} else {
+    $data->model = json_decode($data->model);
 }
 
-call_user_func("{$row->className}::renderAsHTMLForID", $ID, $iteration);
+if ($data->model && is_callable($function = "{$data->className}::renderModelAsHTML")) {
+    call_user_func($function, $data->model);
+} else {
+    call_user_func("{$data->className}::renderAsHTMLForID", $ID, $data->iteration);
+}
