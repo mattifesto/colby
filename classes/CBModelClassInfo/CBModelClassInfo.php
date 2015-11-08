94ad428406fd5cff5c553232e5d5006a9c8a0fb2
Matt Calkins (MacBook Air) <matt@mattifesto.com>
2015-11-07 20:52:52 -0800

    Added CBModelClassInfo::classNameToInfo() and support for permissions

--- classes/CBModelClassInfo/CBModelClassInfo.php
+++ classes/CBModelClassInfo/CBModelClassInfo.php
@@ -3,16 +3,30 @@
 final class CBModelClassInfo {
 
     /**
+     * @param {string} $className
+     *
+     * @return {stdClass}
+     */
+    public static function classNameToInfo($className) {
+        if (is_callable($function = "{$className}::info")) {
+            return call_user_func($function);
+        } else {
+            return CBModelClassInfo::specToModel();
+        }
+    }
+
+    /**
      * @param {stdClass} $spec
      *
      * @return {stdClass}
      */
-    public static function specToModel(stdClass $spec) {
+    public static function specToModel(stdClass $spec = null) {
         $model = CBModels::modelWithClassName(__CLASS__);
         $model->pluralTitle = isset($spec->pluralTitle) ? trim($spec->pluralTitle) : '';
         $model->pluralTitleAsHTML = cbhtml($model->pluralTitle);
         $model->singularTitle = isset($spec->singularTitle) ? trim($spec->singularTitle) : '';
         $model->singularTitleAsHTML = cbhtml($model->singularTitle);
+        $model->userGroup = isset($spec->userGroup) ? trim($spec->userGroup) : 'Developers';
 
         return $model;
     }
