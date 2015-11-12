0f7e533addb9ae1cf8b404358555142af3aed472
Matt Calkins (MacBook Air) <matt@mattifesto.com>
2015-11-12 15:49:56 -0800

    Added CBUpgradesForVersion174

---
+++ classes/CBUpgradesForVersion174/CBUpgradesForVersion174.php
@@ -0,0 +1,28 @@
+<?php
+
+/**
+ * 2015.11.12
+ *
+ * The image uploader was allowing PSD files to be uploaded as images when it
+ * shouldn't have been. This update removes all existing PSD images and they
+ * are no longer allowed to be uploaded as images.
+ */
+class CBUpgradesForVersion174 {
+
+    /**
+     * @return null
+     */
+    public static function run() {
+        $SQL = <<<EOT
+
+            SELECT  LOWER(HEX(`ID`))
+            FROM    `CBImages`
+            WHERE   `extension` = 'psd'
+
+EOT;
+
+        $IDs = CBDB::SQLToArray($SQL);
+
+        array_walk($IDs, 'CBImages::deleteImageByID');
+    }
+}
