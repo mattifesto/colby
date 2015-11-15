f1474c53781e150f3b5e3405cc319149b5716644
Matt Calkins (MacBook Air) <matt@mattifesto.com>
2015-11-14 21:21:21 -0800

    Make CBThemedTextViewTheme use the CBTheme class functions

--- classes/CBThemedTextViewTheme/CBThemedTextViewTheme.php
+++ classes/CBThemedTextViewTheme/CBThemedTextViewTheme.php
@@ -6,37 +6,23 @@ final class CBThemedTextViewTheme {
      * @return [{string}]
      */
     public static function editorURLsForJavaScript() {
-        return [
-            CBSystemURL . '/javascript/CBResponsiveEditorFactory.js',
-            CBSystemURL . '/javascript/CBThemeEditorFactory.js',
+        return CBTheme::editorURLsForJavaScript([
             CBThemedTextViewTheme::URL('CBThemedTextViewThemeEditorFactory.js')
-        ];
+        ]);
     }
 
     /**
      * @return null
      */
     public static function modelsWillSave(array $tuples) {
-        array_walk($tuples, function($tuple) {
-            CBDataStore::makeDirectoryForID($tuple->model->ID);
-            $filepath       = CBDataStore::filepath([
-                'ID'        => $tuple->model->ID,
-                'filename'  => 'CBThemedTextViewTheme.css'
-            ]);
-            file_put_contents($filepath, $tuple->model->styles);
-        });
+        CBTheme::modelsWillSaveWithClassName($tuples, __CLASS__);
     }
 
     /**
      * @return {stdClass}
      */
     public static function specToModel(stdClass $spec) {
-        $model = CBModels::modelWithClassName(__CLASS__);
-        $template = isset($spec->styles) ? $spec->styles : '';
-        $title = isset($spec->title) ? trim($spec->title) : '';
-        $model->styles = CBThemedTextViewTheme::templateToStyles($template, $spec->ID, $title);
-
-        return $model;
+        return CBTheme::specToModelWithClassName($spec, __CLASS__);
     }
 
     /**
@@ -47,26 +33,6 @@ final class CBThemedTextViewTheme {
             'pluralTitle' => 'Themed Text View Themes',
             'singularTitle' => 'Themed Text View Theme'
         ]);
-    }
-
-    /**
-     * This function replaces the strings "view" or ".view" with the CSS
-     * class name for the CBThemedTextViewTheme. The string "\view" will not be
-     * replaced.
-     *
-     * @return {string}
-     */
-    private static function templateToStyles($template, $ID, $title) {
-        $keyword    = 'view';
-        $escape     = "\\\\{$keyword}";
-        $hash       = sha1($escape);
-        $selector   = ".T{$ID}";
-        $styles     = $template;
-        $styles     = preg_replace("/{$escape}/", $hash, $styles);
-        $styles     = preg_replace("/\\.?{$keyword}/", $selector, $styles);
-        $styles     = preg_replace("/{$hash}/", $keyword, $styles);
-        $styles     = "{$styles}\n\n/**\n * Styles for the \"{$title}\" CBThemedTextViewTheme\n */\n";
-        return $styles;
     }
 
     /**
