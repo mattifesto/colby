eadb9ac39aeec7749d07b1c492e9710928d7dce8
Matt Calkins (MacBook Air) <matt@mattifesto.com>
2015-11-16 16:56:06 -0800

    Add CBThemedMenuView class and editor

---
+++ classes/CBThemedMenuView/CBThemedMenuView.php
@@ -0,0 +1,168 @@
+<?php
+
+final class CBThemedMenuView {
+
+    /**
+     * @return [{string}]
+     */
+    public static function editorURLsForJavaScript() {
+        return [
+            CBSystemURL . '/javascript/CBResponsiveEditorFactory.js',
+            CBSystemURL . '/javascript/CBStringEditorFactory.js',
+            CBThemedMenuView::URL('CBThemedMenuViewEditorFactory.js')
+        ];
+    }
+
+    /**
+     * @return {stdClass}
+     */
+    public static function fetchMenuItemOptionsForAjax() {
+        $response = new CBAjaxResponse();
+        $menuID = $_POST['menuID'];
+
+        if (empty($menuID)) {
+            $menu = false;
+        } else {
+            $menu = CBModels::fetchModelByID($menuID);
+        }
+
+        if ($menu === false) {
+            $options = [];
+        } else {
+            $options = array_map(function ($menuItem) {
+                return (object)['value' => $menuItem->name, 'textContent' => $menuItem->text];
+            }, $menu->items);
+        }
+
+        $response->menuItemOptions = $options;
+        $response->wasSuccessful = true;
+        $response->send();
+    }
+
+    /**
+     * @return {stdClass}
+     */
+    public static function fetchMenuItemOptionsForAjaxPermissions() {
+        return (object)['group' => 'Administrators'];
+    }
+
+    /**
+     * @return {stdClass}
+     */
+    public static function fetchMenusForAjax() {
+        $response   = new CBAjaxResponse();
+        $SQL        = <<<EOT
+
+            SELECT      LOWER(HEX(`ID`)) AS `value`, `title` AS `textContent`
+            FROM        `CBModels`
+            WHERE       `className` = 'CBMenu'
+            ORDER BY    `title`
+
+EOT;
+
+        $menus = CBDB::SQLToObjects($SQL);
+        $response->menus = ($menus !== false) ? $menus : [];
+        $response->wasSuccessful = true;
+        $response->send();
+    }
+
+    /**
+     * @return {stdClass}
+     */
+    public static function fetchMenusForAjaxPermissions() {
+        return (object)['group' => 'Administrators'];
+    }
+
+    /**
+     * @return {stdClass}
+     */
+    public static function fetchThemesForAjax() {
+        $response   = new CBAjaxResponse();
+        $SQL        = <<<EOT
+
+            SELECT      LOWER(HEX(`ID`)) AS `value`, `title` AS `textContent`
+            FROM        `CBModels`
+            WHERE       `className` = 'CBThemedMenuViewTheme'
+            ORDER BY    `title`
+
+EOT;
+
+        $themes = CBDB::SQLToObjects($SQL);
+        $response->themes = ($themes !== false) ? $themes : [];
+        $response->wasSuccessful = true;
+        $response->send();
+    }
+
+    /**
+     * @return {stdClass}
+     */
+    public static function fetchThemesForAjaxPermissions() {
+        return (object)['group' => 'Administrators'];
+    }
+
+    /**
+     * @param {stdClass} $menuItem
+     *
+     * @return null
+     */
+    public static function renderMenuItem(stdClass $menuItem) { ?>
+        <li>
+            <a href="<?= $menuItem->URLAsHTML ?>"><?= $menuItem->textAsHTML ?></a>
+        </li>
+    <?php }
+
+    /**
+     * @param {stdClass} $model
+     *
+     * @return null
+     */
+    public static function renderModelAsHTML(stdClass $model) {
+        if (empty($model->menuID)) {
+            return;
+        }
+
+        $menu = CBModelCache::fetchModelByID($model->menuID);
+
+        if ($menu === false || empty($menu->items)) {
+            return;
+        }
+
+        if (empty($model->themeID)) {
+            $class = "CBThemedMenuView";
+        } else {
+            $class = "CBThemedMenuView T{$model->themeID}";
+        }
+
+        ?>
+
+        <div class="<?= $class ?>"><ul>
+            <?php array_walk($menu->items, 'CBThemedMenuView::renderMenuItem'); ?>
+        </ul></div>
+
+        <?php
+    }
+
+    /**
+     * @param {stdClass} $spec
+     *
+     * @return {stdClass}
+     */
+    public static function specToModel(stdClass $spec) {
+        $model = CBModels::modelWithClassName(__CLASS__);
+        $model->menuID = isset($spec->menuID) ? trim($spec->menuID) : '';
+        $model->selectedItemName = isset($spec->selectedItemName) ? trim($spec->selectedItemName) : '';
+        $model->themeID = isset($spec->themeID) ? trim($spec->themeID) : '';
+
+        return $model;
+    }
+
+    /**
+     * @param {string} $filename
+     *
+     * @return {string}
+     */
+    public static function URL($filename) {
+        $className = __CLASS__;
+        return CBSystemURL . "/classes/{$className}/{$filename}";
+    }
+}
---
+++ classes/CBThemedMenuView/CBThemedMenuViewEditorFactory.js
@@ -0,0 +1,211 @@
+"use strict";
+
+var CBThemedMenuViewEditorFactory = {
+
+    themes : [],
+    themesUpdated : "CBThemedMenuViewEditorThemesUpdated",
+
+    /**
+     * @param {function} handleSpecChanged
+     * @param {Object} spec
+     *
+     * @return {Element}
+     */
+    createEditor : function (args) {
+        var element = document.createElement("div");
+        element.className = "CBThemedMenuViewEditor";
+        var container = document.createElement("div");
+        container.className = "container";
+
+        // menuID
+
+        var menuIDEditor = CBStringEditorFactory.createSelectEditor2({
+            handleSpecChanged : args.handleSpecChanged,
+            labelTextContent : "Menu",
+            propertyName : "menuID",
+            spec : args.spec,
+        });
+
+        CBThemedMenuViewEditorFactory.fetchMenus({
+            updateMenuIDOptionsCallback : menuIDEditor.updateSelectEditorOptionsCallback,
+        });
+
+        container.appendChild(menuIDEditor.element);
+
+        // selectedItemName
+
+        var selectedItemNameEditor = CBStringEditorFactory.createSelectEditor2({
+            handleSpecChanged : args.handleSpecChanged,
+            labelTextContent : "Selected Item",
+            propertyName : "selectedItemName",
+            spec : args.spec,
+        });
+
+        var handleMenuIDChangedCallback = CBThemedMenuViewEditorFactory.fetchMenuItemOptions.bind(undefined, {
+            menuIDEditorSelectElement : menuIDEditor.selectElement,
+            updateMenuItemOptionsCallback : selectedItemNameEditor.updateSelectEditorOptionsCallback
+        });
+
+        menuIDEditor.selectElement.addEventListener("change", handleMenuIDChangedCallback);
+
+        handleMenuIDChangedCallback.call();
+
+        container.appendChild(selectedItemNameEditor.element);
+
+        // themeID
+
+        container.appendChild(CBThemedMenuViewEditorFactory.createThemeIDEditor({
+            handleSpecChanged   : args.handleSpecChanged,
+            labelText           : "Theme",
+            propertyName        : "themeID",
+            spec                : args.spec
+        }));
+
+        element.appendChild(container);
+
+        return element;
+    },
+
+    /**
+     * @param   {function}  handleSpecChanged
+     * @param   {string}    labelText
+     * @param   {string}    propertyName
+     * @param   {Object}    spec
+     * @return  {Element}
+     */
+    createThemeIDEditor : function(args) {
+        return CBStringEditorFactory.createSelectEditor({
+            data                : CBThemedMenuViewEditorFactory.themes,
+            dataUpdatedEvent    : CBThemedMenuViewEditorFactory.themesUpdated,
+            handleSpecChanged   : args.handleSpecChanged,
+            labelText           : args.labelText,
+            propertyName        : args.propertyName,
+            spec                : args.spec
+        });
+    },
+
+
+    /**
+     * @param Element args.menuIDEditorSelectElement
+     * @param function args.updateMenuItemOptionsCallback
+     *
+     * @return undefined
+     */
+    fetchMenuItemOptions : function (args) {
+        args.menuIDEditorSelectElement.disabled = true;
+        var formData = new FormData();
+
+        formData.append("menuID", args.menuIDEditorSelectElement.value);
+
+        var xhr = new XMLHttpRequest();
+        xhr.onerror = function () {
+            Colby.alert("The menu item options failed to load.");
+        };
+        xhr.onload = CBThemedMenuViewEditorFactory.fetchMenuItemOptionsDidLoad.bind(undefined, {
+            menuIDEditorSelectElement : args.menuIDEditorSelectElement,
+            updateMenuItemOptionsCallback : args.updateMenuItemOptionsCallback,
+            xhr : xhr,
+        });
+        xhr.open("POST", "/api/?class=CBThemedMenuView&function=fetchMenuItemOptions");
+        xhr.send(formData);
+    },
+
+    /**
+     * @param Element args.menuIDEditorSelectElement
+     * @param function args.updateMenuItemOptionsCallback
+     * @param XMLHttpRequest args.xhr
+     * @return undefined
+     */
+    fetchMenuItemOptionsDidLoad : function (args) {
+        args.menuIDEditorSelectElement.disabled = false;
+        var response = Colby.responseFromXMLHttpRequest(args.xhr);
+
+        if (response.wasSuccessful) {
+            args.updateMenuItemOptionsCallback(response.menuItemOptions);
+        } else {
+            Colby.displayResponse(response);
+        }
+    },
+
+    /**
+     * @param function args.updateMenuIDOptionsCallback
+     *
+     * @return undefined
+     */
+    fetchMenus : function (args) {
+        var xhr = new XMLHttpRequest();
+        xhr.onload = CBThemedMenuViewEditorFactory.fetchMenusDidLoad.bind(undefined, {
+            updateMenuIDOptionsCallback : args.updateMenuIDOptionsCallback,
+            xhr : xhr,
+        });
+        xhr.onerror = function () {
+            Colby.alert("The CBThemedMenuView menus failed to load.");
+        };
+
+        xhr.open("POST", "/api/?class=CBThemedMenuView&function=fetchMenus");
+        xhr.send();
+    },
+
+    /**
+     * @param function args.updateMenuIDOptionsCallback
+     * @param XMLHttpRequest xhr
+     *
+     * @return undefined
+     */
+    fetchMenusDidLoad : function(args) {
+        var response = Colby.responseFromXMLHttpRequest(args.xhr);
+
+        if (response.wasSuccessful) {
+            var menus = response.menus;
+
+            menus.push({ textContent : "None", value : ""});
+            args.updateMenuIDOptionsCallback(menus);
+        } else {
+            Colby.displayResponse(response);
+        }
+    },
+
+    /**
+     * @return undefined
+     */
+    fetchThemes : function() {
+        var xhr = new XMLHttpRequest();
+        xhr.onload = CBThemedMenuViewEditorFactory.fetchThemesDidLoad.bind(undefined, {
+            xhr : xhr
+        });
+        xhr.onerror = function() {
+            alert("The CBThemedMenuView themes failed to load.");
+        };
+
+        xhr.open("POST", "/api/?class=CBThemedMenuView&function=fetchThemes");
+        xhr.send();
+    },
+
+    /**
+     * @param XMLHttpRequest args.xhr
+     *
+     * @return undefined
+     */
+    fetchThemesDidLoad : function (args) {
+        var response = Colby.responseFromXMLHttpRequest(args.xhr);
+
+        if (response.wasSuccessful) {
+            var themes = CBThemedMenuViewEditorFactory.themes;
+            themes.length = 0;
+
+            themes.push({ textContent : "None", value : ""});
+
+            response.themes.forEach(function(theme) {
+                themes.push(theme);
+            });
+
+            document.dispatchEvent(new Event(CBThemedMenuViewEditorFactory.themesUpdated));
+        } else {
+            Colby.displayResponse(response);
+        }
+    },
+};
+
+document.addEventListener("DOMContentLoaded", function() {
+    CBThemedMenuViewEditorFactory.fetchThemes();
+});
