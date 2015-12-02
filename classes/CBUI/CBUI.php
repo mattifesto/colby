b7d39a92b74874aae6f128734e9c4db10e9e5161
Matt Calkins (MacBook Air) <matt@mattifesto.com>
2015-12-01 20:42:28 -0800

    Add CBUI

---
+++ classes/CBUI/CBUI.php
@@ -0,0 +1,18 @@
+<?php
+
+final class CBUI {
+
+    /**
+     * @return [string]
+     */
+    public static function editorURLsForCSS() {
+        return [CBSystemURL . '/javascript/CBUI.css'];
+    }
+
+    /**
+     * @return [string]
+     */
+    public static function editorURLsForJavaScript() {
+        return [CBSystemURL . '/javascript/CBUI.js'];
+    }
+}
