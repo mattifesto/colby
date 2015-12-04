cb32372c2446b567504703f8601c0a0acf29289e
Matt Calkins (MacBook Air) <matt@mattifesto.com>
2015-12-04 10:49:20 -0800

    Upgrade CBUI for class name inclusion

--- classes/CBUI/CBUI.php
+++ classes/CBUI/CBUI.php
@@ -3,16 +3,44 @@
 final class CBUI {
 
     /**
+     * @deprecated use requiredCSSURLs
+     *
      * @return [string]
      */
     public static function editorURLsForCSS() {
+        return CBUI::requiredCSSURLs();
+    }
+
+    /**
+     * @deprecated use requiredJavaScriptURLs
+     *
+     * @return [string]
+     */
+    public static function editorURLsForJavaScript() {
+        return CBUI::requiredJavaScriptURLs();
+    }
+
+    /**
+     * @return [string]
+     */
+    public static function requiredCSSURLs() {
         return [CBSystemURL . '/javascript/CBUI.css'];
     }
 
     /**
      * @return [string]
      */
-    public static function editorURLsForJavaScript() {
+    public static function requiredJavaScriptURLs() {
         return [CBSystemURL . '/javascript/CBUI.js'];
+    }
+
+    /**
+     * @param string $filename
+     *
+     * @return string
+     */
+    public static function URL($filename) {
+        $className = __CLASS__;
+        return CBSystemURL . "/classes/{$className}/{$filename}";
     }
 }
