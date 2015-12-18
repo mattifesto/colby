cc87f6e974a587119458a095b8f750ed4af56170
Matt Calkins (MacBook Air) <matt@mattifesto.com>
2015-12-17 19:55:07 -0800

    Update CBTestPage comments

--- classes/CBTestPage/CBTestPage.php
+++ classes/CBTestPage/CBTestPage.php
@@ -7,7 +7,7 @@
 final class CBTestPage {
 
     /**
-     * @param [{stdClass}] $tuples
+     * @param [stdClass] $tuples
      *
      * @return null
      */
@@ -17,20 +17,26 @@ final class CBTestPage {
     }
 
     /**
-     * @param [{hex160}] $IDs
+     * @param [hex160] $IDs
+     *
+     * @return null
      */
     public static function modelsWillDelete(array $IDs) {
         CBPages::deletePagesByID($IDs);
     }
 
     /**
-     * @return {string}
+     * @param stdClass $model
+     *
+     * @return string
      */
     public static function modelToSearchText(stdClass $model) {
         return "{$model->title} {$model->description}";
     }
 
     /**
+     * @param stdClass $model
+     *
      * @return null
      */
     public static function renderModelAsHTML(stdClass $model) {
@@ -44,7 +50,9 @@ final class CBTestPage {
     }
 
     /**
-     * @return {stdClass}
+     * @param stdClass $spec
+     *
+     * @return stdClass
      */
     public static function specToModel(stdClass $spec) {
         $model = CBPages::specToModel($spec);
