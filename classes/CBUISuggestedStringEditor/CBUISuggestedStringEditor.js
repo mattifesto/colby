0207b90c2d5ef9672256973bad03fbfb71b113e9
Matt Calkins (MacBook Air) <matt@mattifesto.com>
2015-12-20 19:13:33 -0800

    Add string editing to CBUISuggestedStringEditor

--- classes/CBUISuggestedStringEditor/CBUISuggestedStringEditor.js
+++ classes/CBUISuggestedStringEditor/CBUISuggestedStringEditor.js
@@ -27,6 +27,14 @@ var CBUISuggestedStringEditor = {
         arrowElement.className = "arrow";
         arrowElement.textContent = ">";
 
+        element.addEventListener("click", args.navigateCallback.bind(undefined, {
+            className : "CBUIStringValue",
+            labelText : args.labelText,
+            propertyName : args.propertyName,
+            spec : args.spec,
+            specChangedCallback : args.specChangedCallback,
+        }));
+
         var displayValueCallback = CBUISuggestedStringEditor.displayValue.bind(undefined, {
             getSuggestedStringCallback : args.getSuggestedStringCallback,
             propertyName : args.propertyName,
@@ -102,4 +110,99 @@ var CBUISuggestedStringEditor = {
         args.displayValueCallback.call();
         args.specChangedCallback.call();
     },
+};
+
+var CBUIStringValueEditor = {
+
+    /**
+     * @param string args.propertyName
+     * @param object args.spec
+     * @param function args.specChangedCallback
+     * @param object args.state
+     *
+     * @return undefined
+     */
+    acceptValue : function (args) {
+        args.spec[args.propertyName] = args.state.value;
+
+        args.specChangedCallback.call();
+
+        history.back();
+    },
+
+    /**
+     * @param args.navigateCallback
+     * @param args.spec
+     * @param args.specChangedCallback
+     *
+     * @return Element
+     */
+    createEditor : function (args) {
+        var item, section;
+        var targetLabelText = args.spec.labelText;
+        var targetPropertyName = args.spec.propertyName;
+        var targetSpec = args.spec.spec;
+        var targetSpecChangedCallback = args.spec.specChangedCallback;
+        var element = document.createElement("div");
+        element.className = "CBUIStringValueEditor";
+        var state = {
+            value : targetSpec[targetPropertyName],
+        };
+
+        var editor = CBUIStringEditor.createEditor({
+            labelText : targetLabelText,
+            propertyName : "value",
+            spec : state,
+            specChangedCallback : CBUIStringValueEditor.noop,
+        });
+
+        element.appendChild(CBUI.createHalfSpace());
+
+        section = CBUI.createSection();
+        item = CBUI.createSectionItem();
+        item.appendChild(editor.element);
+        section.appendChild(item);
+        element.appendChild(section);
+
+        element.appendChild(CBUI.createHalfSpace());
+
+        var container = document.createElement("div");
+        container.className = "container";
+
+        container.appendChild(CBUI.createButton({
+            buttonClickedCallback : history.back.bind(history),
+            text : "Cancel",
+        }).element);
+
+        var clearValueCallback = CBUIStringValueEditor.acceptValue.bind(undefined, {
+            propertyName : targetPropertyName,
+            spec : targetSpec,
+            specChangedCallback : targetSpecChangedCallback,
+            state : { value : undefined },
+        });
+
+        container.appendChild(CBUI.createButton({
+            buttonClickedCallback : clearValueCallback,
+            text : "Clear",
+        }).element);
+
+        var acceptValueCallback = CBUIStringValueEditor.acceptValue.bind(undefined, {
+            propertyName : targetPropertyName,
+            spec : targetSpec,
+            specChangedCallback : targetSpecChangedCallback,
+            state : state,
+        });
+
+        container.appendChild(CBUI.createButton({
+            buttonClickedCallback : acceptValueCallback,
+            text : "Accept",
+        }).element);
+
+        element.appendChild(container);
+        element.appendChild(CBUI.createHalfSpace());
+
+        return element;
+    },
+
+    noop : function () {},
 };
