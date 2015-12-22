f02c7d8876abcfe7349c1f5a730c97398c653054
Matt Calkins (MacBook Air) <matt@mattifesto.com>
2015-12-22 14:46:53 -0800

    Fix inclusion of CBUI

--- handlers/handle,admin,models,directory.php
+++ handlers/handle,admin,models,directory.php
@@ -8,7 +8,7 @@ CBHTMLOutput::$classNameForSettings = 'CBPageSettingsForAdminPages';
 CBHTMLOutput::begin();
 CBHTMLOutput::setTitleHTML('Model Directory');
 CBHTMLOutput::setDescriptionHTML('A list of model classes');
-CBHTMLOutput::addJavaScriptURL(CBSystemURL . '/javascript/CBUI.js');
+CBHTMLOutput::requireClassName('CBUI');
 CBHTMLOutput::addCSSURL(CBSystemURL . '/handlers/handle,admin,models,directory.css');
 
 $spec = (object)['selectedMenuItemName' => 'models'];
