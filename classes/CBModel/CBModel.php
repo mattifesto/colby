643bd393c12780e2ce9fa9e528f8a740c34d57c9
Matt Calkins (MacBook Air) <matt@mattifesto.com>
2016-01-07 13:32:11 -0800

    Add `transform` parameter to CBModel::value()

--- classes/CBModel/CBModel.php
+++ classes/CBModel/CBModel.php
@@ -6,12 +6,17 @@ final class CBModel {
      * @param stdClass $model
      * @param string $propertyName
      * @param mixed $default
+     * @param function $transform
      *
      * @return mixed
      */
-    public static function value(stdClass $model, $propertyName, $default = null) {
+    public static function value(stdClass $model, $propertyName, $default = null, callable $transform = null) {
         if (isset($model->{$propertyName})) {
-            return $model->{$propertyName};
+            if ($transform !== null) {
+                return call_user_func($transform, $model->{$propertyName});
+            } else {
+                return $model->{$propertyName};
+            }
         } else {
             return $default;
         }
