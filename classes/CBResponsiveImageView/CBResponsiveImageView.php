1f3938868b5fd793bb782b0d6f782f80cd24f2b6
Matt Calkins (MacBook Air) <matt@mattifesto.com>
2016-01-24 20:13:21 -0800

    Increment CBResponsiveImageView

--- classes/CBResponsiveImageView/CBResponsiveImageView.php
+++ classes/CBResponsiveImageView/CBResponsiveImageView.php
@@ -3,84 +3,16 @@
 final class CBResponsiveImageView {
 
     /**
-     * @param hex160? $model->image->ID
-     * @param int $model->contentWidth2x
-     *
-     * @return string?
-     */
-    public static function modelToThemeCSS(stdClass $model) {
-        $themeID = CBResponsiveImageView::modelToThemeID($model);
-        $class = "T{$themeID}";
-
-        return <<<EOT
-
-.{$class} {
-    background-color: red;
-}
-
-EOT;
-    }
-
-    /**
-     * @param hex160? $model->image->ID
-     * @param int $model->contentWidth2x
-     *
-     * @return string?
-     */
-    public static function modelToThemeFilepath(stdClass $model) {
-        if (empty($model->image->ID)) {
-            return null;
-        } else {
-            return CBDataStore::filepath([
-                'ID' => $model->image->ID,
-                'filename' => "CBResponsiveImageView{$model->contentWidth2x}.css"
-            ]);
-        }
-    }
-
-    /**
-     * @param hex160? $model->image->ID
-     * @param int $model->contentWidth2x
-     *
-     * @return hex160?
-     */
-    public static function modelToThemeID(stdClass $model) {
-        if (empty($model->image->ID)) {
-            return null;
-        } else {
-            return sha1("CBResponsiveImageView{$model->image->ID}{$model->contentWidth2x}");
-        }
-    }
-
-    /**
-     * @param hex160? $model->image->ID
-     * @param int $model->contentWidth2x
-     *
-     * @return string?
-     */
-    public static function modelToThemeURL(stdClass $model) {
-        if (empty($model->image->ID)) {
-            return null;
-        } else {
-            return CBDataStore::toURL([
-                'ID' => $model->image->ID,
-                'filename' => "CBResponsiveImageView{$model->contentWidth2x}.css"
-            ]);
-        }
-    }
-
-    /**
      * @param stdClass $model
      *
      * @return null
      */
     public static function renderModelAsHTML(stdClass $model) {
-        CBHTMLOutput::addCSSURL(CBResponsiveImageView::modelToThemeURL($model));
-
-        if ($themeID = CBResponsiveImageView::modelToThemeID($model)) {
-            $class = "T{$themeID}";
-        } else {
+        if (empty($model->themeID)) {
             $class = "";
+        } else {
+            CBHTMLOutput::addCSSURL(CBResponsiveImageView::themeIDToStyleSheetURL($model->themeID));
+            $class = "T{$model->themeID}";
         }
 
         ?><figure class="CBResponsiveImageView <?= $class ?>">CBResponsiveImageView</figure><?php
@@ -93,14 +25,93 @@ EOT;
      */
     public static function specToModel(stdClass $spec) {
         $model = (object)['className' => __CLASS__];
-        $model->image = CBModel::value($spec, 'image'); // CBImageStruct::clone
-        $model->contentWidth2x = CBModel::value($spec, 'contentWidth2x', null, function ($v) { return ($v > 0) ? max(640, (int)$v) : null; });
-
-        if ($filepath = CBResponsiveImageView::modelToThemeFilepath($model)) {
-            file_put_contents($filepath, CBResponsiveImageView::modelToThemeCSS($model));
-        }
+        $model->themeID = CBModel::value($spec, 'themeID');
 
         return $model;
+    }
+
+    /**
+     * @return string?
+     */
+    public static function specToThemeCSS(stdClass $spec, array $args) {
+        $themeID = null;
+        extract($args, EXTR_IF_EXISTS);
+
+        $class = "T{$themeID}";
+
+        return <<<EOT
+
+.{$class} {
+    background-color: red;
+}
+
+EOT;
+    }
+
+    /**
+     * return hex160?
+     */
+    public static function specToThemeID(stdClass $spec) {
+        if (!empty($spec->largeImage->ID) && !empty($spec->mediumImage->ID) && !empty($spec->smallImage->ID)) {
+            return sha1("{$spec->largeImage->ID}{$spec->mediumImage->ID}{$spec->smallImage->ID}");
+        } else {
+            return null;
+        }
+    }
+
+    /**
+     * @param hex160 $themeID
+     *
+     * @return string
+     */
+    public static function themeIDToStyleSheetFilepath($themeID) {
+        return CBDataStore::filepath([
+            'ID' => $themeID,
+            'filename' => 'CBResponsiveImageView.css',
+        ]);
+    }
+
+    /**
+     * @param hex160 $themeID
+     *
+     * @return string
+     */
+    public static function themeIDToStyleSheetURL($themeID) {
+        return CBDataStore::toURL([
+            'ID' => $themeID,
+            'filename' => 'CBResponsiveImageView.css',
+        ]);
+    }
+
+    /**
+     * @return null
+     */
+    public static function updateStylesForAjax() {
+        $response = new CBAjaxResponse();
+        $spec = json_decode($_POST['specAsJSON']);
+
+        if (!empty($spec->largeImage->ID) && !empty($spec->mediumImage->ID) && !empty($spec->smallImage->ID)) {
+            $themeID = sha1("{$spec->largeImage->ID}{$spec->mediumImage->ID}{$spec->smallImage->ID}");
+            $filepath = CBResponsiveImageView::themeIDToStyleSheetFilepath($themeID);
+
+            CBDataStore::makeDirectoryForID($themeID);
+
+            file_put_contents($filepath, CBResponsiveImageView::specToThemeCSS($spec, [
+                'themeID' => $themeID,
+            ]));
+
+            $response->themeID = $themeID;
+        }
+
+        $response->wasSuccessful = true;
+        $response->send();
+    }
+
+    /**
+     * @return null
+     */
+    public static function updateStylesForAjaxPermissions() {
+        return (object)['group' => 'Administrators'];
     }
 
     /**
--- classes/CBResponsiveImageViewEditor/CBResponsiveImageViewEditor.js
+++ classes/CBResponsiveImageViewEditor/CBResponsiveImageViewEditor.js
@@ -10,48 +10,15 @@ var CBResponsiveImageViewEditor = {
      * @return Element
      */
     createEditor : function(args) {
-        var section, item;
+        var section, item, imageView, imageSizeView;
         var element = document.createElement("div");
         element.className = "CBResponsiveImageViewEditor";
 
-        /* section */
-        section = CBUI.createSection();
-
-        /* image view */
-        item = CBUI.createSectionItem();
-        var imageView = CBUIImageView.create({
-            propertyName : "image",
+        var styleChangedCallback = CBResponsiveImageViewEditor.handleStylesChanged.bind(undefined, {
             spec : args.spec,
+            specChangedCallback : args.specChangedCallback,
+            status : {},
         });
-        item.appendChild(imageView.element);
-        section.appendChild(item);
-
-        /* image size view */
-        item = CBUI.createSectionItem();
-        var imageSizeView = CBUIImageSizeView.create({
-            propertyName : "image",
-            spec : args.spec,
-        });
-        item.appendChild(imageSizeView.element);
-        section.appendChild(item);
-
-        /* image uploader */
-        item = CBUI.createSectionItem();
-        item.appendChild(CBUIImageUploader.create({
-            propertyName : "image",
-            spec : args.spec,
-            specChangedCallback : CBResponsiveImageViewEditor.handleImageChanged.bind(undefined, {
-                propertyName : "image",
-                spec : args.spec,
-                specChangedCallback : args.specChangedCallback,
-                updateImageCallback : imageView.updateImageCallback,
-                updateImageSizeCallback : imageSizeView.updateImageSizeCallback,
-            }),
-        }).element);
-        section.appendChild(item);
-        element.appendChild(section);
-
-        element.appendChild(CBUI.createHalfSpace());
 
         /* section */
         section = CBUI.createSection();
@@ -65,27 +32,105 @@ var CBResponsiveImageViewEditor = {
             specChangedCallback : args.specChangedCallback,
         }).element);
         section.appendChild(item);
+        element.appendChild(section);
 
-        /* content width */
+        element.appendChild(CBUI.createHalfSpace());
+
+        /* large image section */
+        element.appendChild(CBUI.createSectionHeader({ text : "Large Image" }));
+
+        section = CBUI.createSection();
+
+        /* image view */
         item = CBUI.createSectionItem();
-        item.appendChild(CBUISelector.create({
-            labelText : "Content Width",
-            navigateCallback : args.navigateCallback,
-            propertyName : "contentWidth2x",
+        imageView = CBUIImageView.create({
+            propertyName : "largeImage",
             spec : args.spec,
-            specChangedCallback : args.specChangedCallback,
-            options : [
-                { title : "None", value : undefined },
-                { title : "320pt (640px)", value : 640 },
-                { title : "480pt (960px)", value : 960 },
-                { title : "640pt (1280px)", value : 1280 },
-                { title : "800pt (1600px)", value : 1600 },
-                { title : "960pt (1920px)", value : 1920 },
-                { title : "1280pt (2560px)", value : 2560 },
-                { title : "1600pt (3200px)", value : 3200 },
-                { title : "1920pt (3840px)", value : 3840 },
-                { title : "2560pt (5120px)", value : 5120 },
-            ],
+        });
+        item.appendChild(imageView.element);
+        section.appendChild(item);
+
+        /* image size view */
+        item = CBUI.createSectionItem();
+        imageSizeView = CBUIImageSizeView.create({
+            propertyName : "largeImage",
+            spec : args.spec,
+        });
+        item.appendChild(imageSizeView.element);
+        section.appendChild(item);
+
+        /* image uploader */
+        item = CBUI.createSectionItem();
+        item.appendChild(CBUIImageUploader.create({
+            propertyName : "largeImage",
+            spec : args.spec,
+            specChangedCallback : styleChangedCallback,
+        }).element);
+        section.appendChild(item);
+        element.appendChild(section);
+
+        /* medium image section */
+        element.appendChild(CBUI.createSectionHeader({ text : "Medium Image" }));
+
+        section = CBUI.createSection();
+
+        /* image view */
+        item = CBUI.createSectionItem();
+        imageView = CBUIImageView.create({
+            propertyName : "mediumImage",
+            spec : args.spec,
+        });
+        item.appendChild(imageView.element);
+        section.appendChild(item);
+
+        /* image size view */
+        item = CBUI.createSectionItem();
+        imageSizeView = CBUIImageSizeView.create({
+            propertyName : "mediumImage",
+            spec : args.spec,
+        });
+        item.appendChild(imageSizeView.element);
+        section.appendChild(item);
+
+        /* image uploader */
+        item = CBUI.createSectionItem();
+        item.appendChild(CBUIImageUploader.create({
+            propertyName : "mediumImage",
+            spec : args.spec,
+            specChangedCallback : styleChangedCallback,
+        }).element);
+        section.appendChild(item);
+        element.appendChild(section);
+
+        /* small image section */
+        element.appendChild(CBUI.createSectionHeader({ text : "Small Image" }));
+
+        section = CBUI.createSection();
+
+        /* image view */
+        item = CBUI.createSectionItem();
+        imageView = CBUIImageView.create({
+            propertyName : "smallImage",
+            spec : args.spec,
+        });
+        item.appendChild(imageView.element);
+        section.appendChild(item);
+
+        /* image size view */
+        item = CBUI.createSectionItem();
+        imageSizeView = CBUIImageSizeView.create({
+            propertyName : "smallImage",
+            spec : args.spec,
+        });
+        item.appendChild(imageSizeView.element);
+        section.appendChild(item);
+
+        /* image uploader */
+        item = CBUI.createSectionItem();
+        item.appendChild(CBUIImageUploader.create({
+            propertyName : "smallImage",
+            spec : args.spec,
+            specChangedCallback : styleChangedCallback,
         }).element);
         section.appendChild(item);
         element.appendChild(section);
@@ -94,18 +139,55 @@ var CBResponsiveImageViewEditor = {
     },
 
     /**
-     * @param string args.propertyName
      * @param object args.spec
      * @param function args.specChangedCallback
-     * @param function args.updateImageCallback
-     * @param function args.updateImageSizeCallback
+     * @param object args.status
      *
      * @return undefined
      */
-    handleImageChanged : function (args) {
-        var image = args.spec[args.propertyName];
-        args.updateImageCallback(image);
-        args.updateImageSizeCallback(image);
-        args.specChangedCallback();
+    handleStylesChanged : function (args) {
+        if (args.status.waiting === true) {
+            args.status.pending = true;
+            return;
+        } else if (args.status.pending === true) {
+            args.status.pending = undefined;
+        }
+
+        var data = new FormData();
+        data.append("specAsJSON", JSON.stringify(args.spec));
+
+        var xhr = new XMLHttpRequest();
+        xhr.onerror = Colby.displayXHRError.bind(undefined, { xhr : xhr });
+        xhr.onload = CBResponsiveImageViewEditor.handleUpdateStylesDidLoad.bind(undefined, args);
+        xhr.open("POST", "/api/?class=CBResponsiveImageView&function=updateStyles");
+        xhr.send(data);
+
+        args.status.waiting = true;
+        args.status.xhr = xhr;
+    },
+
+    /**
+     * @param object args.spec
+     * @param function args.specChangedCallback
+     * @param object args.status
+     *
+     * @return undefined
+     */
+    handleUpdateStylesDidLoad : function (args) {
+        var response = Colby.responseFromXMLHttpRequest(args.status.xhr);
+        args.status.waiting = undefined;
+        args.status.xhr = undefined;
+
+        if (response.wasSuccessful) {
+            args.spec.themeID = response.themeID;
+        } else {
+            Colby.displayResponse(response);
+        }
+
+        if (args.status.pending) {
+            CBResponsiveImageViewEditor.handleStylesChanged(args);
+        } else {
+            args.specChangedCallback();
+        }
     },
 };
