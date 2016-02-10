14e61d47f2758af6b2e51be92677b2b92bcdb97a
Matt Calkins (MacBook Air) <matt@mattifesto.com>
2016-02-10 14:35:06 -0800

    Iterate CBUIHeader

--- javascript/CBUI.js
+++ javascript/CBUI.js
@@ -38,8 +38,10 @@ var CBUI = {
 
     /**
      * @param Element args.centerElement
-     * @param Element args.leftElement
-     * @param Element args.rightElement
+     * @param Element args.leftElement (deprecated)
+     * @param [Element] args.leftElements
+     * @param Element args.rightElement (deprecated)
+     * @param [Element] args.rightElements
      *
      * @return Element
      */
@@ -47,28 +49,68 @@ var CBUI = {
         var element = document.createElement("div");
         element.className = "CBUIHeader";
 
+        /* left */
         var left = document.createElement("div");
         left.className = "left";
-
+        if (args.leftElements) {
+            args.leftElements.forEach(function (element) {
+                left.appendChild(element);
+            });
+        } else if (args.leftElement) {
+            left.appendChild(args.leftElement);
+        }
         element.appendChild(left);
 
+        /* center */
         var center = document.createElement("div");
         center.className = "center";
-
         if (args.centerElement) {
             center.appendChild(args.centerElement);
         }
-
         element.appendChild(center);
 
+        /* right */
         var right = document.createElement("div");
         right.className = "right";
-
-        if (args.rightElement) {
+        if (args.rightElements) {
+            args.rightElements.forEach(function (element) {
+                right.appendChild(element);
+            });
+        } else if (args.rightElement) {
             right.appendChild(args.rightElement);
         }
-
         element.appendChild(right);
+
+        return element;
+    },
+
+    /**
+     * @param function? args.callback
+     * @param string? args.text
+     *
+     * @return Element
+     */
+    createHeaderButtonItem : function (args) {
+        var element = document.createElement("div");
+        element.className = "CBUIHeaderButtonItem";
+        element.textContent = args.text || null;
+
+        if (args.callback) {
+            element.addEventListener("click", args.callback);
+        }
+
+        return element;
+    },
+
+    /**
+     * @param string text
+     *
+     * @return Element
+     */
+    createHeaderTitle : function (args) {
+        var element = document.createElement("div");
+        element.className = "CBUIHeaderTitle";
+        element.textContent = args.text || null;
 
         return element;
     },
