8606c98f181a1f77a5128899b31aafb94cbc633b
Matt Calkins (MacBook Air) <matt@mattifesto.com>
2016-02-18 15:46:18 -0800

    Add comments

--- classes/CBPageContext/CBPageContext.php
+++ classes/CBPageContext/CBPageContext.php
@@ -1,5 +1,13 @@
 <?php
 
+/**
+ * This class holds context data needed by views and subviews of a class when
+ * the view tree is being walked for rendering or pre-processing.
+ *
+ * The information may be helpful in the following circumstances:
+ *  - A view needs to render the title, description, or publication date.
+ *  - A view needs to load or save a file in the page's data store.
+ */
 final class CBPageContext {
 
     private static $contexts = [];
