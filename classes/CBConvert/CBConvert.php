fb403fa3e7e613585ca237028835cccee6f49fe4
Matt Calkins (MacBook Air) <matt@mattifesto.com>
2016-03-14 22:32:40 -0700

    Update CBConvert
    - Add CBConvert::stringToCSSBackgroundImage()
    - Add CBConvert::stringToCSSValue()

--- classes/CBConvert/CBConvert.php
+++ classes/CBConvert/CBConvert.php
@@ -3,8 +3,21 @@
 final class CBConvert {
 
     /**
-     * This function determines whether a string is a CSS color. If it is a
-     * color then it is sanitized and returned; if not then null is returned.
+     * Determines whether a string is a CSS background image. If so then it is
+     * sanitized and returned; if not then null is returned.
+     *
+     * As this function gets more advanced it may return null more often as it
+     * learns to recognize strings that aren't valid background images.
+     *
+     * @return string|null
+     */
+    public static function stringToCSSBackgroundImage($string) {
+        return CBConvert::stringToCSSValue($string);
+    }
+
+    /**
+     * Determines whether a string is a CSS color. If it is then it is sanitized
+     * and returned; if not then null is returned.
      *
      * As this function gets more advanced it may return null more often as it
      * learns to recognize strings that aren't valid colors.
@@ -12,7 +25,17 @@ final class CBConvert {
      * @return string|null
      */
     public static function stringToCSSColor($string) {
-        $color = str_replace(';', '', trim($string));
-        return empty($color) ? null : $color;
+        return CBConvert::stringToCSSValue($string);
+    }
+
+    /**
+     * Sanitizes a string to be used as a CSS value. If the string is not a CSS
+     * value null is returned.
+     *
+     * @return string|null
+     */
+    public static function stringToCSSValue($string) {
+        $value = str_replace([';', '"', "'"], '', trim($string));
+        return empty($value) ? null : $value;
     }
 }
