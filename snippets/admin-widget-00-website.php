7a68d5f7514ff0ff96b759e9e0c0f9c02112fbd7
Matt Calkins (MacBook Air) <matt@mattifesto.com>
2016-04-06 13:30:20 -0700

    Update website version admin widget
    Remove use of a deprecated constant.

--- snippets/admin-widget-00-website.php
+++ snippets/admin-widget-00-website.php
@@ -1,10 +1,16 @@
+<?php
+
+$version = CBSiteVersionNumber;
+
+?>
+
 <section class="widget">
     <header><h1>Website</h1></header>
 
     <div class="version-numbers">
         <section class="version-number">
             <h1>Version</h1>
-            <div class="number"><?php echo COLBY_SITE_VERSION_NUMBER; ?></div>
+            <div class="number"><?= $version ?></div>
         </section>
     </div>
 </section>
