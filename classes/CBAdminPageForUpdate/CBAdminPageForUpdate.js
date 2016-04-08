457aabe8ac2c5f1c98a173af144c9c6e39e47970
Matt Calkins (MacBook Air) <matt@mattifesto.com>
2016-04-07 19:00:33 -0700

    Update CBAdminPageForUpdate
    - Process now backs up the database before updating.
    - Process now uses Promises for tasks and error handling.
    - Process is now simplified by adding a context object.
    - Process now has a finish task that re-enableds the action link
      whether the process succeeded or failed.
    - Lots of other excited changes related to using Promises effetively.

--- classes/CBAdminPageForUpdate/CBAdminPageForUpdate.js
+++ classes/CBAdminPageForUpdate/CBAdminPageForUpdate.js
@@ -1,5 +1,7 @@
 "use strict";
 /* globals CBUI, CBUIActionLink, Colby */
+/* jshint esversion: 6 */
+/* jshint strict: global */
 
 var ColbySiteUpdater = {
 
@@ -18,13 +20,13 @@ var ColbySiteUpdater = {
         /* action */
         item = CBUI.createSectionItem();
         var action = CBUIActionLink.create({
+            callback : ColbySiteUpdater.update,
             labelText : "Update Site",
         });
-        var updateCallback = ColbySiteUpdater.update.bind(undefined, {
-            disableCallback : action.disableCallback,
-            enableCallback : action.enableCallback,
-        });
-        action.updateCallbackCallback(updateCallback);
+        ColbySiteUpdater.context = {
+            disableActionLinkCallback : action.disableCallback,
+            enableActionLinkCallback : action.enableCallback,
+        };
         item.appendChild(action.element);
         section.appendChild(item);
 
@@ -45,6 +47,17 @@ var ColbySiteUpdater = {
     },
 
     /**
+     * This could become a convenience function of CSULog.
+     *
+     * @param string reason
+     *
+     * @return undefined
+     */
+    displayReason : function (reason) {
+        CSULog.append({message:"Error: " + reason});
+    },
+
+    /**
      * @return undefined
      */
     DOMContentDidLoad : function () {
@@ -53,45 +66,94 @@ var ColbySiteUpdater = {
     },
 
     /**
-     * @param function args.disableCallback
-     * @param function args.enableCallback
-     *
      * @return undefined
      */
-    update : function (args) {
-        args.disableCallback();
-
-        CSULog.append({message:"Requesting Update..."});
-
-        var xhr = new XMLHttpRequest();
-        xhr.onerror = Colby.displayXHRError.bind({xhr:xhr});
-        xhr.onload = ColbySiteUpdater.updateDidLoad.bind(undefined, {
-            enableCallback : args.enableCallback,
-            xhr : xhr,
-        });
-        xhr.open('POST', '/api/?class=CBAdminPageForUpdate&function=updateForAjax', true);
-        xhr.send();
+    finish : function () {
+        setTimeout(ColbySiteUpdater.context.enableActionLinkCallback, 2000);
     },
 
     /**
-     * @param function args.enableCallback
+     * @return Promise
+     */
+    promiseToBackupDatabase : function () {
+        CSULog.append({message:"Requesting database backup..."});
+        return ColbySiteUpdater.promiseToGetAjaxResponse({
+            URL : "/developer/mysql/ajax/backup-database/",
+        });
+    },
+
+    /**
+     * @param string args.URL
+     *
+     * @return Promise
+     */
+    promiseToGetAjaxResponse : function (args) {
+        return new Promise(function (resolve, reject) {
+            var xhr = new XMLHttpRequest();
+            xhr.onerror = ColbySiteUpdater.getAjaxResponseDidError.bind(undefined, {xhr,resolve,reject});
+            xhr.onload = ColbySiteUpdater.getAjaxResponseDidLoad.bind(undefined, {xhr,resolve,reject});
+            xhr.open("POST", args.URL);
+            xhr.send();
+        });
+    },
+
+    /**
+     * @param function args.reject
+     * @param function args.resolve
      * @param XMLHttpRequest args.xhr
      *
      * @return undefined
      */
-    updateDidLoad : function (args) {
+    getAjaxResponseDidError : function (args) {
+        var response = Colby.responseFromXMLHttpRequest(args.xhr);
+        args.reject(response.message);
+    },
+
+    /**
+     * @param function args.reject
+     * @param function args.resolve
+     * @param XMLHttpRequest args.xhr
+     *
+     * @return undefined
+     */
+    getAjaxResponseDidLoad : function (args) {
         var response = Colby.responseFromXMLHttpRequest(args.xhr);
 
         if (response.wasSuccessful) {
-            CSULog.append({message:"Update succeeded"});
+            args.resolve(args);
         } else {
-            CSULog.append({message:"Update failed"});
+            args.reject(response.message);
         }
+    },
 
-        CSULog.append(response);
+    /**
+     * @return Promise
+     */
+    promiseToUpdateSite : function () {
+        CSULog.append({message:"Requesting site update..."});
+        return ColbySiteUpdater.promiseToGetAjaxResponse({
+            URL : "/api/?class=CBAdminPageForUpdate&function=update",
+        });
+    },
 
-        // args.enableCallback();
-        setTimeout(args.enableCallback, 2000);
+    /**
+     * @return undefined
+     */
+    reportSuccess : function () {
+        CSULog.append({message:"Update succeeded"});
+    },
+
+    /**
+     * @return undefined
+     */
+    update : function () {
+        ColbySiteUpdater.context.disableActionLinkCallback();
+
+        ColbySiteUpdater.promiseToBackupDatabase()
+            .then(ColbySiteUpdater.promiseToUpdateSite)
+            .then(ColbySiteUpdater.reportSuccess)
+            .catch(ColbySiteUpdater.displayReason)
+            .then(ColbySiteUpdater.finish);
     },
 };
 
