58f0149fe4784b2e8d738912ec5fe5862e0b6086
Matt Calkins (MacBook Air) <matt@mattifesto.com>
2016-05-27 18:41:46 -0700

    Add CBImage

---
+++ classes/CBImage/CBImage.php
@@ -0,0 +1,39 @@
+<?php
+
+final class CBImage {
+
+    /**
+     * @param hex160 $image->ID
+     * @param string $image->filename (used if $filename parameter is empty)
+     * @param string $image->extension
+     * @param string? $filename
+     * @param string? $flexdir
+     *
+     * @return string
+     */
+    public static function flexpath(stdClass $image, $filename = null, $flexdir = null) {
+        if (empty($filename)) {
+            $filename = empty($image->filename) ? $image->base : $image->filename;
+        }
+
+        $basename = "{$filename}.{$image->extension}";
+
+        return CBDataStore::flexpath($image->ID, $basename, $flexdir);
+    }
+
+    /**
+     * @param stdClass $spec
+     *
+     * @return stdClass
+     */
+    public static function specToModel(stdClass $spec) {
+        return (object)[
+            'className' => __CLASS__,
+            'extension' => $spec->extension,
+            'filename' => empty($spec->filename) ? $spec->base : $spec->filename,
+            'height' => $spec->height,
+            'ID' => $spec->ID,
+            'width' => $spec->width,
+        ];
+    }
+}
