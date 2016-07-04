9fe9373bde0ca41191ea276e6482a56bd1061870
Matt Calkins (MacBook Air) <matt@mattifesto.com>
2016-07-04 10:25:27 -0700

    Update CBTask
    - Improve dequeueing to use a single query to dequeue a row which
      will reduce lock contention.
    - Added `starter` field to make this possible.

--- classes/CBTasks/CBTasks.php
+++ classes/CBTasks/CBTasks.php
@@ -3,37 +3,71 @@
 final class CBTasks {
 
     /**
+     * @param string $className
+     * @param string $function
+     * @param [stdClass] $args
+     *
+     * @return null
+     */
+    public static function add($className, $function, array $argsArray, $priority = 0) {
+        $classNameAsSQL = CBDB::stringToSQL($className);
+        $functionAsSQL = CBDB::stringToSQL($function);
+        $priorityAsSQL = (int)$priority;
+        $values = [];
+
+        foreach ($argsArray as $args) {
+            $argsAsJSONAsSQL = CBDB::stringToSQL(json_encode($args));
+            $values[] = "({$classNameAsSQL},{$functionAsSQL},{$argsAsJSONAsSQL},{$priorityAsSQL})";
+        }
+
+        $values = implode(',', $values);
+        $SQL = <<<EOT
+
+            INSERT INTO `CBTasks`
+            (`className`, `function`, `argsAsJSON`, `priority`)
+            VALUES {$values}
+
+EOT;
+
+        Colby::query($SQL);
+    }
+
+    /**
      * @return null
      */
     public static function doTask() {
-        Colby::query('START TRANSACTION');
-
+        $startedAsSQL = time();
+        $starter = CBHex160::random();
+        $starterAsSQL = CBHex160::toSQL($starter);
         $SQL = <<<EOT
 
-            SELECT `ID`, `className`, `function`, `argsAsJSON`
-            FROM `CBTasks`
+            UPDATE `CBTasks`
+            SET `started` = {$startedAsSQL}, `starter` = {$starterAsSQL}
             WHERE `started` IS NULL
             ORDER BY `started`, `priority`, `ID`
             LIMIT 1
-            FOR UPDATE
+
+EOT;
+
+        Colby::query($SQL);
+
+        if (Colby::mysqli()->affected_rows !== 1) {
+            return;
+        }
+
+        $SQL = <<<EOT
+
+            SELECT `className`, `function`, `argsAsJSON`
+            FROM `CBTasks`
+            WHERE `starter` = {$starterAsSQL}
 
 EOT;
 
         $task = CBDB::SQLToObject($SQL);
 
-        if ($task === false) {
-            Colby::query('ROLLBACK');
-            return;
-        } else {
-            $IDAsSQL = CBHex160::toSQL($task->ID);
-            $time = time();
-            Colby::query("UPDATE `CBTasks` SET `started` = {$time} WHERE `ID` = {$IDAsSQL}");
-            Colby::query('COMMIT');
-        }
-
         // TODO: Add more detailed error handling.
 
-        if (is_callable($function = "{$task->className}::{$task->function}")) {
+        if (is_callable($function = "{$task->className}::{$task->function}ForTask")) {
             if (empty($task->argsAsJSON)) {
                 $args = null;
             } else {
@@ -44,7 +78,7 @@ EOT;
             throw new Exception("The function {$function}() requested by task {$ID} is not callable.");
         }
 
-        Colby::query("DELETE FROM `CBTasks` WHERE ID = {$IDAsSQL}");
+        Colby::query("DELETE FROM `CBTasks` WHERE `starter` = {$starterAsSQL}");
     }
 
     /**
@@ -55,16 +89,16 @@ EOT;
 
         CBTasks::doTask();
 
-        $priority = CBDB::SQLToValue('SELECT MIN(`priority`) FROM `CBTasks` WHERE `started` IS NOT NULL');
+        $priority = CBDB::SQLToValue('SELECT MIN(`priority`) FROM `CBTasks` WHERE `started` IS NULL');
 
         if ($priority === null) {
             // don't set a timeout because there are no tasks
         } else if ($priority < 0) {
             $response->timeout = 0;
         } else if ($priority < 100) {
-            $response->timeout = 5;
+            $response->timeout = 5000;
         } else {
-            $response->timeout = 10;
+            $response->timeout = 10000;
         }
 
         $response->succeeded = true;
@@ -86,14 +120,16 @@ EOT;
 
         CREATE TABLE IF NOT EXISTS `CBTasks` (
             `ID` BIGINT UNSIGNED NOT NULL AUTO_INCREMENT,
+            `argsAsJSON` TEXT,
             `className` VARCHAR(80),
             `function` VARCHAR(80),
-            `argsAsJSON` TEXT,
             `priority` BIGINT NOT NULL DEFAULT 0,
             `started` BIGINT,
+            `starter` BINARY(20) DEFAULT NULL,
             PRIMARY KEY (`ID`),
             KEY `className_started` (`className`, `started`),
-            KEY `started_priority_ID` (`started`, `priority`, `ID`)
+            KEY `started_priority_ID` (`started`, `priority`, `ID`),
+            KEY `starter` (`starter`)
         )
         ENGINE=InnoDB
         DEFAULT CHARSET=utf8
