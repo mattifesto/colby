c8f09a0d4014b9bb29e4995cf24300e83d12c3b3
Matt Calkins (MacBook Air) <matt@mattifesto.com>
2016-07-04 20:22:08 -0700

    Update CBLog
    - Added the `severity` column to the table.
    - Added the entries() function to retrieve a list of messages.

--- classes/CBLog/CBLog.php
+++ classes/CBLog/CBLog.php
@@ -12,10 +12,12 @@ final class CBLog {
                 `ID` BIGINT UNSIGNED NOT NULL AUTO_INCREMENT,
                 `category` VARCHAR(80) NOT NULL,
                 `message` TEXT NOT NULL,
+                `severity` TINYINT NOT NULL,
                 `timestamp` BIGINT NOT NULL,
                 PRIMARY KEY (`ID`),
                 KEY `timestamp` (`timestamp`),
-                KEY `category_timestamp` (`category`, `timestamp`)
+                KEY `category_timestamp` (`category`, `timestamp`),
+                KEY `severity_timestamp` (`severity`, `timestamp`)
             )
             ENGINE=InnoDB
             DEFAULT CHARSET=utf8
@@ -28,11 +30,26 @@ EOT;
 
     /**
      * @param string $category
+     * @param string $severity
+     *
+     *  RFC3164 Severity Codes
+     *  https://tools.ietf.org/html/rfc3164
+     *
+     *  0  Emergency: system is unusable
+     *  1  Alert: action must be taken immediately
+     *  2  Critical: critical conditions
+     *  3  Error: error conditions
+     *  4  Warning: warning conditions
+     *  5  Notice: normal but significant condition
+     *  6  Informational: informational messages
+     *  7  Debug: debug-level messages
+     *
      * @param string $message
      */
-    public static function addMessage($category, $message) {
+    public static function addMessage($category, $severity, $message) {
         $categoryAsSQL = CBDB::stringToSQL($category);
         $messageAsSQL = CBDB::stringToSQL($message);
+        $severityAsSQL = (int)$severity;
         $timestampAsSQL = time();
 
         $SQL = <<<EOT
@@ -40,15 +57,52 @@ EOT;
             INSERT INTO `CBLog` (
                 `category`,
                 `message`,
+                `severity`,
                 `timestamp`
             ) VALUES (
                 {$categoryAsSQL},
                 {$messageAsSQL},
+                {$severityAsSQL},
                 {$timestampAsSQL}
             )
 
 EOT;
 
         Colby::query($SQL);
+    }
+
+    /**
+     * @param int $args->sinceTimestamp
+     * @param int $args->minSeverity
+     *
+     * @return [stdClass]
+     */
+    public static function entries($args) {
+        $whereAsSQL = [];
+
+        if (isset($args->sinceTimestamp)) {
+            $sinceTimestampAsSQL = (int)$args->sinceTimestamp;
+            $whereAsSQL[] = "`timestamp` > {$sinceTimestampAsSQL}";
+        }
+
+        if (isset($args->minSeverity)) {
+            $minSeverityAsSQL = (int)$args->minSeverity;
+            $whereAsSQL[] = "`severity` <= {$minSeverityAsSQL}";
+        } else if (isset($args->category)) {
+
+        }
+
+        $whereAsSQL = implode(' AND ', $whereAsSQL);
+
+        $SQL = <<<EOT
+
+            SELECT `category`, `message`, `timestamp`
+            FROM `CBLog`
+            WHERE {$whereAsSQL}
+            ORDER BY `timestamp`
+
+EOT;
+
+        return CBDB::SQLToObjects($SQL);
     }
 }
