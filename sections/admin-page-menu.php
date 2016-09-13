dcf3faf6287fc1ae330cb567fd75ab3c86f89514
Matt Calkins (MacBook Air) <matt@mattifesto.com>
2016-09-13 13:08:13 -0700

    Update sections/admin-page-menu.php
    - Add a log message when this file is loaded because it is deprecated.

--- sections/admin-page-menu.php
+++ sections/admin-page-menu.php
@@ -5,6 +5,12 @@
  * this file with direct use of the CBAdminPageMenuView.
  */
 
+call_user_func(function () {
+    $URI = $_SERVER['REQUEST_URI'];
+    $file = __FILE__;
+    CBLog::addMessage('Deprecated', 4, "A request for the URI '{$URI}' loaded the deprecated menu file '{$file}'.");
+});
+
 if (!isset($selectedMenuItemID))
 {
     $selectedMenuItemID = null;
