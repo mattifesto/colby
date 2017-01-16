381474bd9efd20ec97b52292137427b1a20d4913
Matt Calkins (MacBook Air) <matt@mattifesto.com>
2017-01-16 12:03:17 -0800

    Release version 239
    Update the backup database function to remove old database backup
    files so they don't use up lots of disk space. Backup files older
    than 30 days are removed and the newest 3 backup files are always
    kept regardless of age.

--- handlers/handle,developer,mysql,ajax,backup-database.php
+++ handlers/handle,developer,mysql,ajax,backup-database.php
@@ -19,6 +19,8 @@ if (!is_dir($absoluteDatabaseBackupsDirectory))
     mkdir($absoluteDatabaseBackupsDirectory, 0777, true);
 }
 
+removeOldBackupFiles($absoluteDatabaseBackupsDirectory);
+
 /**
  * Generate a filename for the database backup file.
  */
@@ -68,3 +70,42 @@ $response->wasSuccessful = true;
 $response->message = "The database was dumped to the file named: \"{$intraSiteFilename}\".";
 
 $response->send();
+
+/**
+ * @param string $directory
+ *
+ * @return null
+ */
+function removeOldBackupFiles($directory) {
+    $files = array_map(function ($filepath) {
+        return (object)[
+            'filepath' => $filepath,
+            'modified' => filemtime($filepath),
+        ];
+    }, glob("{$directory}/*.sql"));
+
+    if (empty($files)) {
+        return;
+    }
+
+    usort($files, function($a, $b) {
+        if ($a->modified < $b->modified) {
+            return -1;
+        } else if ($a->modified === $b->modified) {
+            return 0;
+        } else {
+            return 1;
+        }
+    });
+
+    $countOfFiles = count($files);
+    $minCountOfFiles = 3;
+    $thirtyDaysAgo = time() - (60 * 60 * 24 * 30); /* 30 days */
+
+    foreach ($files as $file) {
+        if ($countOfFiles > $minCountOfFiles && !empty($file->modified) && $file->modified < $thirtyDaysAgo) {
+            unlink($file->filepath);
+            $countOfFiles -= 1;
+        }
+    }
+}
--- version.php
+++ version.php
@@ -1,3 +1,3 @@
 <?php
 
-define('CBSystemVersionNumber', '238.1');
+define('CBSystemVersionNumber', '239');
