0414061cb1e7d2ca275673938afb3c0f50273492
Matt Calkins (MacBook Air) <matt@mattifesto.com>
2017-02-11 15:42:06 -0800

    Garden CBTextView

--- classes/CBTextView/CBTextView.php
+++ classes/CBTextView/CBTextView.php
@@ -5,24 +5,24 @@ final class CBTextView {
     /**
      * @return string
      */
-    public static function modelToSearchText(stdClass $model = null) {
+    static function modelToSearchText(stdClass $model = null) {
         return $model->text;
     }
 
     /**
      * @return void
      */
-    public static function renderModelAsHTML(stdClass $model = null) {
+    static function renderModelAsHTML(stdClass $model = null) {
         echo "<span class=\"CBTextView\">{$model->HTML}</span>";
     }
 
     /**
      * @return stdClass
      */
-    public static function specToModel(stdClass $spec = null) {
-        $model          = CBView::modelWithClassName(__CLASS__);
-        $model->text    = isset($spec->text) ? (string)$spec->text : '';
-        $model->HTML    = ColbyConvert::textToHTML($model->text);
+    static function specToModel(stdClass $spec = null) {
+        $model = CBView::modelWithClassName(__CLASS__);
+        $model->text = isset($spec->text) ? (string)$spec->text : '';
+        $model->HTML = ColbyConvert::textToHTML($model->text);
 
         return $model;
     }
@@ -42,7 +42,7 @@ final class CBTextView {
      *
      * @return string
      */
-    public static function URL($filename) {
+    static function URL($filename) {
         $className = __CLASS__;
         return CBSystemURL . "/classes/{$className}/{$filename}";
     }
