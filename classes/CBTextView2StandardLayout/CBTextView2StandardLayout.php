cc5a6095baad940cb6fee52e14f818d75a770a5a
Matt Calkins (MacBook Air) <matt@mattifesto.com>
2017-02-20 15:06:41 -0800

    Add CBTextView2StandardLayout

---
+++ classes/CBTextView2StandardLayout/CBTextView2StandardLayout.php
@@ -0,0 +1,11 @@
+<?php
+
+final class CBTextView2StandardLayout {
+
+    /**
+     * @return [string]
+     */
+    static function requiredCSSURLs() {
+        return [Colby::flexnameForCSSForClass(CBSystemURL, __CLASS__)];
+    }
+}
