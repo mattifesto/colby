947354cc0539f4d85856180e2464923f03165ea1
Matt Calkins (MacBook Air) <matt@mattifesto.com>
2017-03-17 12:17:52 -0700

    Remove CBSiteName use in sections/standard-page-footer.php

--- sections/standard-page-footer.php
+++ sections/standard-page-footer.php
@@ -6,6 +6,6 @@ CBHTMLOutput::addCSSURL(CBSystemURL . '/sections/standard-page-footer.css');
 
 <footer class="standard-page-footer">
     <div class="copyright">
-        Copyright &copy; 1984 - <?php echo gmdate('Y'), ' ', CBSiteNameHTML; ?>
+        Copyright &copy; 1984 - <?php echo gmdate('Y'), ' ', cbhtml(CBSitePreferences::siteName()); ?>
     </div>
 </footer>
