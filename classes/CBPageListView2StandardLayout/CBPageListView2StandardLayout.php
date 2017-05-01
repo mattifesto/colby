43a978ec46aafb2094086fb7306c11f67ab2fb94
Matt Calkins (MacBook Air) <matt@mattifesto.com>
2017-05-01 12:18:54 -0700

    Add CBPageListView2StandardLayout

---
+++ classes/CBPageListView2StandardLayout/CBPageListView2StandardLayout.php
@@ -0,0 +1,11 @@
+<?php
+
+final class CBPageListView2StandardLayout {
+
+    /**
+     * @return [string]
+     */
+    static function requiredCSSURLs() {
+        return [Colby::flexnameForCSSForClass(CBSystemURL, __CLASS__)];
+    }
+}
