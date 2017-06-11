b0c34a591de42b7026deeb875353c90f11b17ae7
Matt Calkins (MacBook Air) <matt@mattifesto.com>
2017-06-11 16:16:29 -0700

    Add CBAdminPageForTasks

--- classes/CBAdminPageForTasks/CBAdminPageForTasks.js
+++ classes/CBAdminPageForTasks/CBAdminPageForTasks.js
@@ -6,69 +6,253 @@ var CBAdminPageForTasks = {
     /**
      * @return Element
      */
-    create : function () {
+    create: function () {
         var element = document.createElement("div");
+        element.className = "CBAdminPageForTasks";
 
-        CBAdminPageForTasks.fetchTasks({element:element});
+        var allButtonElement = document.createElement("button");
+        allButtonElement.textContent = "Start Verification for all Pages";
+        allButtonElement.addEventListener("click", CBAdminPageForTasks.startVerificationForAllPages);
+        element.appendChild(allButtonElement);
+
+        var newButtonElement = document.createElement("button");
+        newButtonElement.textContent = "Start Verification for new Pages";
+        newButtonElement.addEventListener("click", CBAdminPageForTasks.startVerificationForNewPages);
+        element.appendChild(newButtonElement);
+
+        var scheduleButton = document.createElement("button");
+        scheduleButton.textContent = "Schedule a Task";
+        scheduleButton.addEventListener("click", CBAdminPageForTasks.scheduleATask);
+        element.appendChild(scheduleButton);
+
+        var statusElement = CBAdminPageForTasks.createStatusElement();
+        element.appendChild(statusElement);
+
+        var issuesElement = CBAdminPageForTasks.createIssuesElement();
+        element.appendChild(issuesElement);
 
         return element;
     },
 
     /**
-     * @return undefined
+     * @return Element
      */
-    DOMContentDidLoad : function () {
-        var main = document.getElementsByTagName("main")[0];
-        main.appendChild(CBAdminPageForTasks.create());
-    },
-
-    /**
-     * @param Object task
-     */
-    elementForTask : function (task) {
+    createKeyElement: function (className, ID) {
         var element = document.createElement("div");
-        element.className = "task";
-        element.textContent = task.className + "::" + task.function;
+        element.className = "key";
+
+        var classNameElement = document.createElement("div");
+        classNameElement.className = "className";
+        classNameElement.textContent = className;
+
+        var IDElement = document.createElement("div");
+        IDElement.className = "ID";
+        IDElement.textContent = ID;
+
+        element.appendChild(classNameElement);
+        element.appendChild(IDElement);
 
         return element;
     },
 
     /**
-     * @param Element args.element
+     * @param object output
      *
-     * @return undefined
+     * @return Element
      */
-    fetchTasks : function (args) {
-        var xhr = new XMLHttpRequest();
-        xhr.onerror = Colby.displayXHRError.bind(undefined, {xhr:xhr});
-        xhr.onload = CBAdminPageForTasks.fetchTasksDidLoad.bind(undefined, {element:args.element,xhr:xhr});
-        xhr.open("POST", "/api/?class=CBTasks&function=fetchTasks");
-        xhr.send();
-    },
+    createOutputElement: function (output) {
+        var element = document.createElement("div");
+        element.className = "output";
+        var message = document.createElement("div");
+        message.className = "message";
+        message.textContent = output.message;
 
-    /**
-     * @param Element args.element
-     * @param XMLHttpRequest args.xhr
-     *
-     * @return undefined
-     */
-    fetchTasksDidLoad : function (args) {
-        var response = Colby.responseFromXMLHttpRequest(args.xhr);
+        var control = document.createElement("div");
+        var checkbox = document.createElement("input");
+        checkbox.type = "checkbox";
+        var checkboxText = document.createElement("span");
+        checkboxText.textContent = "show details";
+        control.appendChild(checkbox);
+        control.appendChild(checkboxText);
 
-        if (response.wasSuccessful) {
-            if (response.tasks.length > 0) {
-                args.element.textContent = "";
-                response.tasks.forEach(function (task) {
-                    var taskElement = CBAdminPageForTasks.elementForTask(task);
-                    args.element.appendChild(taskElement);
-                });
+        var details = document.createElement("div");
+        details.className = "details";
+
+        checkbox.addEventListener("click", function () {
+            if (checkbox.checked) {
+                details.classList.add("show");
             } else {
-                args.element.textContent = "No tasks";
+                details.classList.remove("show");
             }
-        } else {
-            Colby.displayResponse(response);
+        });
+
+        if (output.linkURI) {
+            var link = document.createElement("a");
+            link.textContent = output.linkText ? output.linkText : "link";
+            link.href = output.linkURI;
+
+            details.appendChild(link);
         }
+
+        element.appendChild(message);
+        element.appendChild(control);
+        element.appendChild(details);
+
+        return element;
+    },
+
+    /**
+     * @param object response
+     *
+     * @return Element
+     */
+    createStatusContent: function (response) {
+        var element = document.createElement("div");
+        element.textContent = "Available Tasks: " + response.countOfAvailableTasks +
+                              " Scheduled Tasks: " + response.countOfScheduledTasks +
+                              " Completed Last Minute: " + response.countOfTasksCompletedInTheLastMinute +
+                              " Completed Last Hour: " + response.countOfTasksCompletedInTheLastHour +
+                              " Completed Last 24 Hours: " + response.countOfTasksCompletedInTheLast24Hours;
+
+        return element;
+    },
+
+    /**
+     * @return Element
+     */
+    createStatusElement: function () {
+        var element = document.createElement("div");
+        element.className = "status";
+
+        CBAdminPageForTasks.fetchStatus(element);
+
+        return element;
+    },
+
+    /**
+     * @return Element
+     */
+    createIssuesElement: function () {
+        var element = document.createElement("div");
+        element.className = "issues";
+
+        var issuesContainer = document.createElement("div");
+
+        var button = document.createElement("button");
+        button.textContent = "Fetch Issues";
+        button.addEventListener("click", CBAdminPageForTasks.fetchIssues.bind(undefined, issuesContainer, button));
+
+        element.appendChild(button);
+        element.appendChild(issuesContainer);
+
+        CBAdminPageForTasks.fetchIssues(issuesContainer, button);
+
+        return element;
+    },
+
+    /**
+     * @return null
+     */
+    fetchIssues: function (container, button) {
+        container.textContent = "";
+        button.disabled = true;
+
+        CBAdminPageForTasks.fetchStatusPromise =
+             fetchIssues()
+            .then(display)
+            .catch(Colby.displayError);
+
+        function fetchIssues() {
+            return Colby.fetchAjaxResponse("/api/?class=CBAdminPageForTasks&function=fetchIssues");
+        }
+
+        function display(response) {
+            button.disabled = false;
+
+            response.issues.forEach(function (output) {
+                var element = CBAdminPageForTasks.createOutputElement(output);
+                /*
+                var element = document.createElement("div");
+                var description = document.createElement("div");
+                description.textContent = output.message;
+
+                element.appendChild(description);
+
+                if (output.linkURI) {
+                    var link = document.createElement("a");
+                    link.textContent = output.linkText ? output.linkText : "link";
+                    link.href = output.linkURI;
+
+                    element.appendChild(link);
+                }
+                */
+                container.appendChild(element);
+            });
+        }
+    },
+
+    /**
+     * @return undefined
+     */
+    fetchStatus: function (element) {
+        CBAdminPageForTasks.fetchStatusPromise =
+             fetchStatus()
+            .then(display)
+            .then(wake)
+            .then(restart)
+            .catch(Colby.displayError);
+
+        function fetchStatus() {
+            return Colby.fetchAjaxResponse("/api/?class=CBAdminPageForTasks&function=fetchStatus");
+        }
+
+        function display(response) {
+            element.textContent = "";
+            element.appendChild(CBAdminPageForTasks.createStatusContent(response));
+
+            return response;
+        }
+
+        function wake() {
+            return Colby.fetchAjaxResponse("/api/?class=CBTasks2&function=wakeScheduledTasks");
+        }
+
+        function restart() {
+            setTimeout(CBAdminPageForTasks.fetchStatus, 1000, element);
+        }
+    },
+
+    /**
+     * @return undefined
+     */
+    scheduleATask: function () {
+        CBAdminPageForTasks.restartVerificationForAllPagesPromise =
+             Colby.fetchAjaxResponse("/api/?class=CBAdminPageForTasks&function=scheduleATask")
+            .catch(Colby.displayError);
+    },
+
+    /**
+     * @return undefined
+     */
+    startVerificationForAllPages: function () {
+        CBAdminPageForTasks.restartVerificationForAllPagesPromise =
+             Colby.fetchAjaxResponse("/api/?class=CBPageVerificationTask&function=startForAllPages")
+            .catch(Colby.displayError);
+    },
+
+    /**
+     * @return undefined
+     */
+    startVerificationForNewPages: function () {
+        CBAdminPageForTasks.restartVerificationForNewPagesPromise =
+             Colby.fetchAjaxResponse("/api/?class=CBPageVerificationTask&function=startForNewPages")
+            .catch(Colby.displayError);
     },
 };
 
-document.addEventListener("DOMContentLoaded", CBAdminPageForTasks.DOMContentDidLoad);
+document.addEventListener("DOMContentLoaded", function () {
+    Colby.CBTasks2DispatchAlways = true;
+    Colby.CBTasks2DispatchDelay = 1000;
+    var main = document.getElementsByTagName("main")[0];
+    main.appendChild(CBAdminPageForTasks.create());
+});
--- classes/CBAdminPageForTasks/CBAdminPageForTasks.php
+++ classes/CBAdminPageForTasks/CBAdminPageForTasks.php
@@ -25,9 +25,99 @@ final class CBAdminPageForTasks {
     }
 
     /**
+     * @return null
+     *
+     *      Ajax: {
+     *          recent: [object]
+     *          upcoming: [object]
+     *      }
+     */
+    static function fetchIssuesForAjax() {
+        $response = new CBAjaxResponse();
+
+        $SQL = 'SELECT `output` FROM CBTasks2 WHERE `completed` IS NOT NULL AND `severity` < 7 ORDER BY `severity` DESC LIMIT 10';
+        $response->issues = CBDB::SQLToArray($SQL, ['valueIsJSON' => true]);
+
+        $response->wasSuccessful = true;
+        $response->send();
+    }
+
+    /**
+     * @return object
+     */
+    static function fetchIssuesForAjaxPermissions() {
+        return (object)['group' => 'Administrators'];
+    }
+
+    /**
+     * @return null
+     *
+     *      Ajax: {
+     *          recent: [object]
+     *          upcoming: [object]
+     *      }
+     */
+    static function fetchStatusForAjax() {
+        $response = new CBAjaxResponse();
+
+        $response->countOfAvailableTasks = CBTasks2::countOfAvailableTasks();
+        $response->countOfScheduledTasks = CBTasks2::countOfScheduledTasks();
+        $response->countOfTasksCompletedInTheLastMinute = CBTasks2::countOfTasksCompletedSince(time() - 60);
+        $response->countOfTasksCompletedInTheLastHour = CBTasks2::countOfTasksCompletedSince(time() - (60 * 60));
+        $response->countOfTasksCompletedInTheLast24Hours = CBTasks2::countOfTasksCompletedSince(time() - (60 * 60 * 24));
+
+        $response->wasSuccessful = true;
+        $response->send();
+    }
+
+    /**
+     * @return object
+     */
+    static function fetchStatusForAjaxPermissions() {
+        return (object)['group' => 'Administrators'];
+    }
+
+    /**
      * @return [string]
      */
-    public static function requiredJavaScriptURLs() {
+    static function requiredCSSURLs() {
+        return [Colby::flexnameForCSSForClass(CBSystemURL, __CLASS__)];
+    }
+
+    /**
+     * @return [string]
+     */
+    static function requiredJavaScriptURLs() {
         return [Colby::flexnameForJavaScriptForClass(CBSystemURL, __CLASS__)];
+    }
+
+    /**
+     * @return null
+     */
+    static function scheduleATaskForAjax() {
+        $response = new CBAjaxResponse();
+
+        $SQL = <<<EOT
+
+            SELECT      LOWER(HEX(`archiveID`))
+            FROM        `ColbyPages`
+            ORDER BY    RAND()
+            LIMIT       1
+
+EOT;
+
+        $ID = CBDB::SQLToValue($SQL);
+
+        CBTasks2::updateTask('CBPageVerificationTask', $ID, null, null, time() + 5);
+
+        $response->wasSuccessful = true;
+        $response->send();
+    }
+
+    /**
+     * @return object
+     */
+    static function scheduleATaskForAjaxPermissions() {
+        return (object)['group' => 'Administrators'];
     }
 }
