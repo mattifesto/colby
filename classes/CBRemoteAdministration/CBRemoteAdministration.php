223e76104037982f0b35992fbc034653014b8e2a
Matt Calkins (MacBook Air) <matt@mattifesto.com>
2017-06-25 08:05:59 -0700

    Add CBRemoteAdministration::ping() and support

--- classes/CBRemoteAdministration/CBRemoteAdministration.php
+++ classes/CBRemoteAdministration/CBRemoteAdministration.php
@@ -67,4 +67,97 @@ EOT;
     static function logoutForAjaxPermissions() {
         return (object)['group' => 'Public'];
     }
+
+    /**
+     * This function should by called by a cron-like process every five minutes
+     * to keep scheduled tasks operating. Administrative processes should be
+     * associated with scheduled tasks.
+     *
+     * @return object
+     *
+     *      bool    didExecute
+     *                  true if ping was executed; otherwise false
+     *
+     *      int     previouslyPinged
+     *                  0 if this is the first ping for this site
+     *
+     */
+    static function ping() {
+        $now = time();
+        $spec = CBModels::fetchSpecByID(CBRemoteAdministration::pingModelID());
+        $returnValue = (object)[
+            'didExecute' => false,
+        ];
+
+        if ($spec === false) {
+            $spec = (object)[
+                'className' => __CLASS__,
+                'ID' => CBRemoteAdministration::pingModelID(),
+            ];
+        }
+
+        $returnValue->previouslyPinged = CBModel::value($spec, 'pinged', 0);
+
+        if (($now - $returnValue->previouslyPinged) < (60 * 4 /* 4 minutes */)) {
+            return $spec;
+        }
+
+        try {
+            $spec->pinged = $now;
+
+            Colby::query('START TRANSACTION');
+            CBModels::save([$spec]);
+            Colby::query('COMMIT');
+        } catch (Exception $exception) {
+            Colby::query('ROLLBACK');
+
+            if ($exception instanceof CBModelVersionMismatchException) {
+                // Another request beat us to the punch, no big deal.
+                return;
+            }
+
+            throw $exception;
+        }
+
+        CBTasks2::wakeScheduledTasks();
+
+        $returnValue->didExecute = true;
+
+        return $returnValue;
+    }
+
+    /**
+     * @return ajax
+     */
+    static function pingForAjax() {
+        $response = new CBAjaxResponse();
+
+        CBRemoteAdministration::ping();
+
+        $response->wasSuccessful = true;
+        $response->send();
+    }
+
+    static function pingForAjaxPermissions() {
+        return (object)['group' => 'Public'];
+    }
+
+    /**
+     * @return hex160
+     */
+    static function pingModelID() {
+        return '9893b4a401686ac0e85707b6c19a01405481cc38';
+    }
+
+    /**
+     * @param object $spec
+     *
+     * @return object
+     */
+    static function specToModel(stdClass $spec) {
+        return (object)[
+            'className' => __CLASS__,
+            'pinged' => CBModel::value($spec, 'pinged', null, 'intval'),
+        ];
+    }
 }
