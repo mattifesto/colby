52b2a62c359a291647766fc2f13fd73c58e288dc
Matt Calkins (MacBook Air) <matt@mattifesto.com>
2017-06-25 08:03:25 -0700

    Add CBLogMaintenanceTask

---
+++ classes/CBLogMaintenanceTask/CBLogMaintenanceTask.php
@@ -0,0 +1,32 @@
+<?php
+
+final class CBLogMaintenanceTask {
+
+    /**
+     * When we do an install or update schedule this task to run. This is also
+     * the time at which the priority of the task can be changed if needed.
+     *
+     * @return null
+     */
+    static function install() {
+        CBTasks2::updateTask(__CLASS__, CBHex160::zero(), null, null, time());
+    }
+
+    /**
+     * @param hex160 $ID
+     *
+     * @return null
+     */
+    static function CBTasks2_Execute($ID) {
+        if ($ID !== CBHex160::zero()) {
+            throw new RuntimeException("An invalid ID {$ID} was passed to " . __METHOD__ . '()');
+        }
+
+        CBLog::removeExpiredEntries();
+        CBLog::addMessage(__CLASS__, 6, 'Performed regular maintenance on the CBLog table.');
+
+        return (object)[
+            'scheduled' => time() + (60 * 60 * 24), /* 24 hours from now */
+        ];
+    }
+}
