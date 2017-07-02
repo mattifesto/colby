b235d52990c2d1ab76112d0105b6a25e74c0e554
Matt Calkins (MacBook Air) <matt@mattifesto.com>
2017-07-01 20:20:55 -0700

    Add CBUIExpandableRow

---
+++ classes/CBUIExpandableRow/CBUIExpandableRow.php
@@ -0,0 +1,18 @@
+<?php
+
+final class CBUIExpandableRow {
+
+    /**
+     * @return [string]
+     */
+    static function requiredJavaScriptURLs() {
+        return [Colby::flexnameForJavaScriptForClass(CBSystemURL, __CLASS__)];
+    }
+
+    /**
+     * @return [string]
+     */
+    static function requiredCSSURLs() {
+        return [Colby::flexnameForCSSForClass(CBSystemURL, __CLASS__)];
+    }
+}
