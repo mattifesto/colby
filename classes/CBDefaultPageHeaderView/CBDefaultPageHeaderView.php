80e04347dfd391f01c6b58a50115730b8bd6251e
Matt Calkins (MacBook Air) <matt@mattifesto.com>
2017-07-15 18:53:42 -0700

    Update CBDefaultPageHeaderView

--- classes/CBDefaultPageHeaderView/CBDefaultPageHeaderView.php
+++ classes/CBDefaultPageHeaderView/CBDefaultPageHeaderView.php
@@ -3,13 +3,50 @@
 final class CBDefaultPageHeaderView {
 
     /**
+     * The $item parameter is a CBMenuItem model but only the following
+     * properties are used.
+     *
+     * @param string? $item->textAsHTML
+     * @param string? $item->URLAsHTML
+     *
+     * @return null
+     */
+    static function renderMenuItem(stdClass $item) {
+        $textAsHTML = CBModel::value($item, 'textAsHTML', '');
+
+        ?>
+
+        <li>
+            <a href="<?= $item->URLAsHTML ?>"><?= $textAsHTML ?></a>
+        </li>
+
+        <?php
+    }
+
+    /**
      * @return null
      */
     static function renderModelAsHTML(stdClass $model) {
         ?>
 
         <header class="CBDefaultPageHeaderView">
-            <?= cbhtml(CBSitePreferences::siteName()); ?>
+            <nav class="CBDarkTheme">
+                <ul>
+                    <?php
+
+                    CBDefaultPageHeaderView::renderMenuItem((object)[
+                        'textAsHTML' => cbhtml(CBSitePreferences::siteName()),
+                        'URLAsHTML' => '/'
+                    ]);
+
+                    $menu = CBModels::fetchModelByID(CBMainMenu::ID);
+
+                    array_walk($menu->items, 'CBDefaultPageHeaderView::renderMenuItem');
+
+                    ?>
+
+                </ul>
+            </nav>
         </header>
 
         <?php
