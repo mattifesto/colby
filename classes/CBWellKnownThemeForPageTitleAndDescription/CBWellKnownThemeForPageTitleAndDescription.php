9f62bd8b1c93a054e189fbca158a43fc2f0c009a
Matt Calkins (MacBook Air) <matt@mattifesto.com>
2017-07-24 09:59:51 -0700

    Remove CBWellKnownThemeForPageTitleAndDescription

--- classes/CBWellKnownThemeForPageTitleAndDescription/CBWellKnownThemeForPageTitleAndDescription.php
+++ classes/CBWellKnownThemeForPageTitleAndDescription/CBWellKnownThemeForPageTitleAndDescription.php
@@ -1,5 +1,11 @@
 <?php
 
+/**
+ * @deprecated 2017.07.24
+ *
+ * Replaced with CBPageTitleAndDescriptionView.css. Themes in general are also
+ * deprecated. Remove this file once install has been run on all websites.
+ */
 final class CBWellKnownThemeForPageTitleAndDescription {
 
     const ID = '664d22662308f7443e4c3b43683d4934de087b86';
@@ -7,40 +13,9 @@ final class CBWellKnownThemeForPageTitleAndDescription {
     /**
      * @return null
      */
-    public static function install() {
-        $spec = CBModels::fetchSpecByID(CBWellKnownThemeForPageTitleAndDescription::ID);
-
-        if ($spec === false) {
-            $spec = (object)[
-                'ID' => CBWellKnownThemeForPageTitleAndDescription::ID,
-            ];
-        }
-
-        $originalSpec = clone $spec;
-
-        /* set or reset required properties */
-        $spec->className = 'CBTheme';
-        $spec->classNameForKind = 'CBPageTitleAndDescriptionView';
-        $spec->classNameForTheme = 'CBWellKnownThemeForPageTitleAndDescription';
-        $spec->description = 'The default theme for CBPageTitleAndDescriptionView.';
-        $spec->title = 'CBWellKnownThemeForPageTitleAndDescription';
-
-        if ($spec != $originalSpec) {
-            CBModels::save([$spec]);
-        }
-    }
-
-    /**
-     * @return [string]
-     */
-    public static function requiredCSSURLs() {
-        $className = __CLASS__;
-        $URLs = [Colby::flexnameForCSSForClass(CBSystemURL, $className)];
-
-        if (is_file(Colby::flexnameForCSSForClass(CBSiteDirectory, $className))) {
-            $URLs[] = Colby::flexnameForCSSForClass(CBSitePreferences::siteURL(), $className);
-        }
-
-        return $URLs;
+    static function install() {
+        Colby::query('START TRANSACTION');
+        CBModels::deleteByID(CBWellKnownThemeForPageTitleAndDescription::ID);
+        Colby::query('COMMIT');
     }
 }
