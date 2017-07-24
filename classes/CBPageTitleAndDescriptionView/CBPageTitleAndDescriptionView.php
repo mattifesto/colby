aaf7414fbff8ef93007bcef7b9f4f5294b4fd477
Matt Calkins (MacBook Air) <matt@mattifesto.com>
2017-07-24 10:01:22 -0700

    Update CBPageTitleAndDescriptionView
    - Add CSSClassNames
    - Deprecate descriptionColor, hideDescription, publishedColor,
      titleColor, and useLightTextColors
    - Remove theme support
    - Add style sheet
    - Use CBHTMLOutput::addCSS() for local CSS

--- classes/CBPageTitleAndDescriptionView/CBPageTitleAndDescriptionView.php
+++ classes/CBPageTitleAndDescriptionView/CBPageTitleAndDescriptionView.php
@@ -3,50 +3,47 @@
 final class CBPageTitleAndDescriptionView {
 
     /**
-     * @param bool? $model->hideDescription
+     * @param string? $model->descriptionColor @deprecated use CSS
+     * @param bool? $model->hideDescription  @deprecated use CSS
+     * @param string? $model->publishedColor @deprecated use CSS
      * @param bool? $model->showPublicationDate
-     * @param hex160? $model->themeID
-     * @param bool? $model->useLightTextColors
+     * @param string? $model->titleColor @deprecated use CSS
+     * @param bool? $model->useLightTextColors @deprecated use CSS
      *
      * @return null
      */
-    public static function renderModelAsHTML(stdClass $model = null) {
-        if (empty($themeID = CBModel::value($model, 'themeID'))) {
-            $themeID = CBWellKnownThemeForPageTitleAndDescription::ID;
-        };
+    static function renderModelAsHTML(stdClass $model = null) {
+        $CSSClassNames = CBModel::valueAsArray($model, 'CSSClassNames');
 
-        CBTheme::useThemeWithID($themeID);
-
-        $class = implode(' ', CBTheme::IDToCSSClasses($themeID));
-        $class = "CBPageTitleAndDescriptionView {$class}";
+        if (!in_array('custom', $CSSClassNames)) {
+            $CSSClassNames[] = 'CBPageTitleAndDescriptionView_default';
+        }
 
         if (!empty($model->stylesID)) {
-            $stylesClass = CBTheme::IDToCSSClass($model->stylesID);
-            $class = "{$class} {$stylesClass}";
+            $CSSClassNames[] = "T{$model->stylesID}";
         }
 
+        /* @deprecated use CBDarkTheme CSS class name */
         if (!empty($model->useLightTextColors)) {
-            $class = "{$class} light";
+            $CSSClassNames[] = 'light';
         }
 
-        if (empty($model->stylesID)) {
-            $styleElement = null;
-        } else {
-            $styleElement = "<style>{$model->stylesCSS}</style>";
+        if (!empty($model->stylesCSS)) {
+            CBHTMLOutput::addCSS($model->stylesCSS);
         }
 
         $context = CBPageContext::current();
 
-        ?><header class="<?= $class ?>"><?php
-            echo $styleElement;
+        $CSSClassNames = implode(' ', $CSSClassNames);
 
+        ?><header class="CBPageTitleAndDescriptionView <?= $CSSClassNames ?>"><?php
             if (!empty($context->titleAsHTML)) {
                 if (empty($model->titleColor)) {
                     $style = '';
                 } else {
-                    $style = " style='color: {$model->titleColor}'";
+                    $style = "style='color: {$model->titleColor}'";
                 }
-                echo "<h1{$style}>{$context->titleAsHTML}</h1>";
+                echo "<h1 class='title' {$style}>{$context->titleAsHTML}</h1>";
             }
 
             if (!empty($context->descriptionAsHTML) && empty($model->hideDescription)) {
@@ -73,33 +70,42 @@ final class CBPageTitleAndDescriptionView {
     }
 
     /**
-     * @param stdClass $spec
-     *
-     * @return stdClass
+     * @return [string]
      */
-    public static function specToModel(stdClass $spec) {
+    static function requiredCSSURLs() {
+        return [Colby::flexnameForCSSForClass(CBSystemURL, __CLASS__)];
+    }
+
+    /**
+     * @param object $spec
+     *
+     * @return object
+     */
+    static function specToModel(stdClass $spec) {
         $colorForProperty = function ($propertyName) use ($spec) {
             return CBModel::value($spec, $propertyName, null, 'CBConvert::stringToCSSColor');
         };
 
         $model = (object)[
             'className' => __CLASS__,
+            'CSSClassNames' => CBModel::valueAsNames($spec, 'CSSClassNames'),
+            'showPublicationDate' => CBModel::value($spec, 'showPublicationDate', false, 'boolval'),
+
+            /* the following properties are all deprecated */
             'descriptionColor' => $colorForProperty('descriptionColor'),
             'hideDescription' => CBModel::value($spec, 'hideDescription', false, 'boolval'),
             'publishedColor' => $colorForProperty('publishedColor'),
-            'showPublicationDate' => CBModel::value($spec, 'showPublicationDate', false, 'boolval'),
-            'themeID' => CBModel::value($spec, 'themeID'),
             'titleColor' => $colorForProperty('titleColor'),
             'useLightTextColors' => CBModel::value($spec, 'useLightTextColors', false, 'boolval'),
         ];
 
-        /* view styles */
+        // localCSS (uses nonstandard stylesCSS property for this view)
+        $localCSSTemplate = CBModel::value($spec, 'stylesTemplate', '', 'trim');
 
-        $stylesTemplate = empty($spec->stylesTemplate) ? '' : trim($spec->stylesTemplate);
-
-        if (!empty($stylesTemplate)) {
-            $model->stylesID = CBHex160::random();
-            $model->stylesCSS = CBTheme::stylesTemplateToStylesCSS($stylesTemplate, $model->stylesID);
+        if (!empty($localCSSTemplate)) {
+            $localCSSClassName = 'ID_' . CBHex160::random();
+            $model->CSSClassNames[] = $localCSSClassName;
+            $model->stylesCSS = CBView::localCSSTemplateToLocalCSS($localCSSTemplate, 'view', ".{$localCSSClassName}");
         }
 
         return $model;
