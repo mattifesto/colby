16a0d7644d8620e88d53669884882683413df0f8
Matt Calkins (MacBook Air) <matt@mattifesto.com>
2017-07-31 15:27:13 -0700

    Update CBDefaultPageHeaderView
    - Use CBMenuView

--- classes/CBDefaultPageHeaderView/CBDefaultPageHeaderView.php
+++ classes/CBDefaultPageHeaderView/CBDefaultPageHeaderView.php
@@ -3,59 +3,23 @@
 final class CBDefaultPageHeaderView {
 
     /**
-     * The $item parameter is a CBMenuItem model but only the following
-     * properties are used.
-     *
-     * @param string? $item->textAsHTML
-     * @param string? $item->URLAsHTML
-     *
-     * @return null
-     */
-    static function renderMenuItem(stdClass $item) {
-        $textAsHTML = CBModel::value($item, 'textAsHTML', '');
-
-        ?>
-
-        <li>
-            <a href="<?= $item->URLAsHTML ?>"><?= $textAsHTML ?></a>
-        </li>
-
-        <?php
-    }
-
-    /**
      * @return null
      */
     static function renderModelAsHTML(stdClass $model) {
         ?>
 
         <header class="CBDefaultPageHeaderView">
-            <nav class="CBDarkTheme">
-                <ul>
-                    <?php
+            <?php
 
-                    CBDefaultPageHeaderView::renderMenuItem((object)[
-                        'textAsHTML' => cbhtml(CBSitePreferences::siteName()),
-                        'URLAsHTML' => '/'
-                    ]);
+            CBView::renderModelAsHTML((object)[
+                'className' => 'CBMenuView',
+                'menuID' => CBWellKnownMenuForMain::ID(),
+                'CSSClassNames' => ['CBDarkTheme'],
+            ]);
 
-                    $menu = CBModels::fetchModelByID(CBMainMenu::ID);
-
-                    array_walk($menu->items, 'CBDefaultPageHeaderView::renderMenuItem');
-
-                    ?>
-
-                </ul>
-            </nav>
+            ?>
         </header>
 
         <?php
-    }
-
-    /**
-     * @return [string]
-     */
-    static function requiredCSSURLs() {
-        return [Colby::flexnameForCSSForClass(CBSystemURL, __CLASS__)];
     }
 }
