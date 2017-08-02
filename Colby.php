1e5c7cfe777e3b5c96a7a92c1247a2bbae16da05
Matt Calkins (MacBook Air) <matt@mattifesto.com>
2017-08-02 12:46:48 -0700

    Add root init.php and Colby.php (deprecated) files

---
+++ Colby.php
@@ -0,0 +1,18 @@
+<?php
+
+/**
+ * @deprecated 2017.08.02 Use init.php
+ *
+ * The file that used to be this file has been moved to:
+ *
+ *      classes/Colby/Colby.php
+ *
+ * Because the Colby class is now so similar to other classes in that it has
+ * associated JavaScript and CSS files, all of the Colby files have been moved
+ * into the classes/Colby directory. 'Colby' can now be used as a required class
+ * name with CBHTMLOutput.
+ *
+ * Raw system initialization code is being moved to init.php.
+ */
+
+include __DIR__ . '/init.php';
---
+++ init.php
@@ -0,0 +1,7 @@
+<?php
+
+define('CBSystemDirectory', __DIR__);
+
+require_once CBSystemDirectory . '/version.php';
+require_once CBSystemDirectory . '/functions.php';
+require_once CBSystemDirectory . '/classes/Colby/Colby.php';
