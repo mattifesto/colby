9f1a110df83107a17c6b59e2189e60efe114f5e2
Matt Calkins (MacBook Air) <matt@mattifesto.com>
2017-08-04 19:50:02 -0700

    Garden CBModelVersionMismatchException

--- classes/CBModelVersionMismatchException/CBModelVersionMismatchException.php
+++ classes/CBModelVersionMismatchException/CBModelVersionMismatchException.php
@@ -3,7 +3,7 @@
 final class CBModelVersionMismatchException extends Exception {
 
     /**
-     * @return CBModelVersionMismatchExceptionß
+     * @return CBModelVersionMismatchException
      */
     public function __construct($message = 'This model has been saved by another session since you started editing it, saving your most recent changes would overwrite the changes made in that session. Reloading your editing page will fetch the changes made by the other session and allow you to save again. If someone else is currently editing this model coordinate your editing with them.', $code = 0, Throwable $previous = null) {
         return parent::__construct($message, $code, $previous);
