db39524a06c7af180bc60bc317db1b166030324e
Matt Calkins (MacBook Air) <matt@mattifesto.com>
2017-08-08 13:20:30 -0700

    Implement CBModelTests

--- classes/CBModelTests/CBModelTests.php
+++ classes/CBModelTests/CBModelTests.php
@@ -3,9 +3,34 @@
 final class CBModelTests {
 
     /**
-     * This test runs a CBModel_toModel test for all known models.
+     * This test runs a CBModel::toModel test for all known classes.
      */
     static function toModelTest() {
+        $classNames = CBModelTests::getClassNames();
 
+        foreach ($classNames as $className) {
+            $spec = (object)[
+                'className' => $className,
+            ];
+
+            $model = CBModel::toModel($spec);
+        }
+    }
+
+    /**
+     * @return [string]
+     */
+    static function getClassNames() {
+        $classNames = [];
+
+        foreach (Colby::$libraryDirectories as $libraryDirectory) {
+            $libraryClassesDirectory = $libraryDirectory . '/classes';
+            $libraryClassDirectories = glob("{$libraryClassesDirectory}/*" , GLOB_ONLYDIR);
+            $libraryClassNames = array_map('basename', $libraryClassDirectories);
+
+            $classNames = array_merge($classNames, $libraryClassNames);
+        }
+
+        return array_values(array_unique($classNames));
     }
 }
