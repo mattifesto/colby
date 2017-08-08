99db2e8390925f3b5b59055cb684579ac2aad723
Matt Calkins (MacBook Air) <matt@mattifesto.com>
2017-08-08 11:06:57 -0700

    Update CBUser

--- classes/CBUser/CBUser.php
+++ classes/CBUser/CBUser.php
@@ -3,18 +3,25 @@
 final class CBUser {
 
     /**
-     * @param stdClass $spec
+     * @param object $spec
      *
-     * @return stdClass
+     * @return object|null
      */
-    static function specToModel(stdClass $spec) {
+    static function CBModel_toModel(stdClass $spec) {
+        $userID = CBModel::value($spec, 'userID', null, 'intval');
+
+        if (empty($userID)) {
+            CBLog::addMessage(__METHOD__, 4, "The spec `userID` property is empty.");
+            return null;
+        }
+
         return (object)[
             'className' => __CLASS__,
             'description' => CBModel::value($spec, 'description', '', 'trim'),
-            'facebook' => clone $spec->facebook,
-            'lastLoggedIn' => intval($spec->lastLoggedIn),
+            'facebook' => clone CBModel::valueAsObject($spec, 'facebook'),
+            'lastLoggedIn' => CBModel::value($spec, 'lastLoggedIn', 0, 'intval'),
             'title' => CBModel::value($spec, 'title', '', 'trim'),
-            'userID' => intval($spec->userID),
+            'userID' => $userID,
         ];
     }
 }
