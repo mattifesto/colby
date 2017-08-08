c4e27d58a47621081907da361b7458f2d5b42fb3
Matt Calkins (MacBook Air) <matt@mattifesto.com>
2017-08-08 13:18:54 -0700

    Update /text/ handler

--- handlers/handle,test.php
+++ handlers/handle,test.php
@@ -5,10 +5,16 @@ $response = new CBAjaxResponse();
 if (!ColbyUser::current()->isOneOfThe('Developers')) {
     $response->message  = "You do not have permission to run tests.";
 } else {
-    $class                      = $_GET['class'] . 'Tests';
-    $function                   = isset($_GET['function']) ? $_GET['function'] . 'Test' : 'test';
-    $response->message          = call_user_func("{$class}::{$function}");
-    $response->wasSuccessful    = true;
+    $className = $_GET['class'] . 'Tests';
+    $functionName = isset($_GET['function']) ? $_GET['function'] . 'Test' : 'test';
+
+    if (is_callable($function = "{$className}::{$functionName}")) {
+        $response->message = call_user_func($function);
+    } else {
+        throw new Exception("The function {$function}() is not callable.");
+    }
+
+    $response->wasSuccessful = true;
 }
 
 $response->send();
