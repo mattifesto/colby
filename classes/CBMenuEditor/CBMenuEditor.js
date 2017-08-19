2f29b55620656e3d063df457abe6d22f83ac50c2
Matt Calkins (MacBook Air) <matt@mattifesto.com>
2017-08-19 15:14:34 -0700

    Update CBMenuEditor

--- classes/CBMenuEditor/CBMenuEditor.js
+++ classes/CBMenuEditor/CBMenuEditor.js
@@ -1,4 +1,8 @@
-"use strict";
+"use strict"; /* jshint strict: global */
+/* global
+    CBArrayEditor,
+    CBUI,
+    CBUIStringEditor */
 
 var CBMenuEditor = {
 
@@ -21,18 +25,30 @@ var CBMenuEditor = {
 
         element.appendChild(CBUI.createHalfSpace());
 
+        /* title */
+
+        element.appendChild(CBUI.createHalfSpace());
         section = CBUI.createSection();
         item = CBUI.createSectionItem();
-
         item.appendChild(CBUIStringEditor.createEditor({
             labelText : "Title",
             propertyName : "title",
             spec : args.spec,
             specChangedCallback : args.specChangedCallback,
         }).element);
+        section.appendChild(item);
 
+        item = CBUI.createSectionItem();
+        item.appendChild(CBUIStringEditor.createEditor({
+            labelText : "Title URI",
+            propertyName : "titleURI",
+            spec : args.spec,
+            specChangedCallback : args.specChangedCallback,
+        }).element);
         section.appendChild(item);
         element.appendChild(section);
+
+        /* menu items */
 
         element.appendChild(CBUI.createHalfSpace());
 
