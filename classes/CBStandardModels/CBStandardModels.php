f59489601aa137f346b412f0546ff638726fc3e6
Matt Calkins (MacBook Air) <matt@mattifesto.com>
2017-08-20 18:59:41 -0700

    Remove CBWellKnownThemeForPageTitleAndDescription

--- classes/CBStandardModels/CBStandardModels.php
+++ classes/CBStandardModels/CBStandardModels.php
@@ -10,7 +10,4 @@ final class CBStandardModels {
 
     /* @deprecated all themes are deprecated */
     const CBThemeIDForCBMenuViewForMainMenu = '9d5e09b9312025f64d9a6d430737eeb2236a89a7';
-
-    /* @deprecated use CBWellKnownThemeForPageTitleAndDescription::ID */
-    const CBThemeIDForCBPageTitleAndDescriptionView = '664d22662308f7443e4c3b43683d4934de087b86';
 }
