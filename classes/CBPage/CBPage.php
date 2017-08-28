bf65f88f9210adc9be41bf34f95407f5e4eab32a
Matt Calkins (MacBook Air) <matt@mattifesto.com>
2017-08-28 10:50:04 -0700

    Add CBPage

---
+++ classes/CBPage/CBPage.php
@@ -0,0 +1,22 @@
+<?php
+
+final class CBPage {
+
+    /**
+     * @param object $model
+     *
+     * @return null
+     */
+    static function render(stdClass $model) {
+        $className = CBModel::value($model, 'className', '');
+
+        if (is_callable($function = "{$className}::CBPage_render")) {
+            call_user_func($function, $model);
+        } else {
+            $ID = CBModel::value($model, 'ID', '(no ID)');
+            $title = CBModel::value($model, 'title', '(no title)');
+
+            throw new Exception("The page, {$title} ({$ID}), was unable to render.");
+        }
+    }
+}
