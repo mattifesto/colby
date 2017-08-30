bb582d9d10a1e73a506d31026dfbb64287309cb6
Matt Calkins (MacBook Air) <matt@mattifesto.com>
2017-08-30 12:35:53 -0700

    Update handlers/handle-front-page.php

--- handlers/handle-front-page.php
+++ handlers/handle-front-page.php
@@ -1,27 +1,29 @@
 <?php
 
-CBHTMLOutput::begin();
-CBHTMLOutput::setTitleHTML('Front Page');
-CBHTMLOutput::setDescriptionHTML('This is the default front page.');
+$content = <<<EOT
 
-?>
+This is the default front page. To replace it create the file
 
-<main>
-    <style>
+`handlers/handle-front-page.php`
 
-        main > p {
-            line-height:    1.5;
-            margin:         100px auto;
-            text-align:     center;
-            width:          540px;
-        }
+in the main website directory. Alternatively, create a page in the admin area
+and set it as the front page. A front page created in the admin area has higher
+priority than a handler.
 
-    </style>
+EOT;
 
-    <p>This is the default front page. To replace it create the file<br><br><code>handlers/handle-front-page.php</code><br><br>in the main website directory. Alternatively, create a page in the admin area and set it as the front page. A front page created in the admin area has higher priority than a handler.
-
-</main>
-
-<?php
-
-CBHTMLOutput::render();
+CBPage::renderSpec((object)[
+    'className' => 'CBViewPage',
+    'title' => 'Default Front Page',
+    'layout' => (object)[
+        'className' => 'CBPageLayout',
+        'CSSClassNames' => 'center',
+    ],
+    'sections' => [
+        (object)[
+            'className' => 'CBTextView2',
+            'contentAsCommonMark' => $content,
+            'CSSClassNames' => 'center',
+        ],
+    ],
+]);
