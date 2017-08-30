d325e376fffd72103d4561e4392ca67b03c0cf3c
Matt Calkins (MacBook Air) <matt@mattifesto.com>
2017-08-30 12:35:24 -0700

    Add CBPage::renderSpec()

--- classes/CBPage/CBPage.php
+++ classes/CBPage/CBPage.php
@@ -19,4 +19,13 @@ final class CBPage {
             throw new Exception("The page, {$title} ({$ID}), was unable to render.");
         }
     }
+
+    /**
+     * @param object $spec
+     *
+     * @return null
+     */
+    static function renderSpec(stdClass $spec) {
+        CBPage::render(CBModel::toModel($spec));
+    }
 }
