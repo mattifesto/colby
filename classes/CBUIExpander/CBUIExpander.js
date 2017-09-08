0f9761e6899636f7a13ace73d72647744f1a1358
Matt Calkins (MacBook Air) <matt@mattifesto.com>
2017-09-08 16:46:43 -0700

    Added "builder" functionality to CBUIExpander
    - Make it possible to declare an expander and it's content in markup

--- classes/CBUIExpander/CBUIExpander.js
+++ classes/CBUIExpander/CBUIExpander.js
@@ -5,8 +5,55 @@
 var CBUIExpander = {
 
     /**
-     * @param string args.links
+     *
+     */
+    build: function (args) {
+        var links, message, pre;
+        var elements = document.getElementsByClassName("CBUIExpander_builder");
+
+        for (var i = 0; i < elements.length; i++) {
+            var element = elements[i];
+            links = message = pre = undefined;
+
+            if (!element.classList.contains("built")) {
+
+                try {
+                    message = JSON.parse(element.dataset.message);
+                } catch (error) {}
+
+                try {
+                    pre = JSON.parse(element.dataset.pre);
+                } catch (error) {}
+
+                try {
+                    links = JSON.parse(element.dataset.links);
+                } catch (error) {}
+
+                if (typeof message !== "string") {
+                    message = "Cannot parse message";
+                }
+
+                if (typeof pre !== "string") {
+                    pre = undefined;
+                }
+
+                var expander = CBUIExpander.create({
+                    links: links,
+                    message: message,
+                    pre: pre,
+                });
+
+                element.appendChild(expander.element);
+
+                element.classList.add("built");
+            }
+        }
+    },
+
+    /**
+     * @param [{text: string, URI: string}] args.links
      * @param string args.message
+     * @param string args.pre
      *
      * @return object
      */
@@ -30,15 +77,33 @@ var CBUIExpander = {
 
         var contentElement = document.createElement("div");
         contentElement.className = "content";
-        contentElement.textContent = args.message;
 
-        (args.links || []).forEach(function (link) {
-            var a = document.createElement("a");
-            a.textContent = link.text;
-            a.href = link.URI;
+        var messageElement = document.createElement("div");
+        messageElement.className = "message";
+        messageElement.textContent = args.message;
+        contentElement.appendChild(messageElement);
 
-            contentElement.appendChild(a);
-        });
+        if (typeof args.pre === "string") {
+            var preElement = document.createElement("div");
+            preElement.className = "pre";
+            preElement.textContent = args.pre;
+            contentElement.appendChild(preElement);
+        }
+
+        if (Array.isArray(args.links)) {
+            var linksElement = document.createElement("div");
+            linksElement.className = "links";
+
+            args.links.forEach(function (link) {
+                var a = document.createElement("a");
+                a.textContent = link.text;
+                a.href = link.URI;
+
+                linksElement.appendChild(a);
+            });
+
+            contentElement.appendChild(linksElement);
+        }
 
         toggleElement.addEventListener("click", function () {
             element.classList.toggle("expanded");
@@ -57,3 +122,5 @@ var CBUIExpander = {
         };
     },
 };
+
+Colby.afterDOMContentLoaded(CBUIExpander.build);
