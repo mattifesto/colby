474b186bde0d74461c656735be2ef97b76955e39
Matt Calkins (MacBook Air) <matt@mattifesto.com>
2017-09-13 08:47:05 -0700

    Garden CBThemedMenuViewEditor

--- classes/CBThemedMenuViewEditor/CBThemedMenuViewEditor.php
+++ classes/CBThemedMenuViewEditor/CBThemedMenuViewEditor.php
@@ -5,30 +5,21 @@ final class CBThemedMenuViewEditor {
     /**
      * @return [string]
      */
-    static function requiredClassNames() {
-        return ['CBUI', 'CBResponsiveEditor', 'CBUISelector', 'CBUIThemeSelector'];
+    static function CBHTMLOutput_requiredClassNames() {
+        return ['CBUI', 'CBUISelector', 'CBUIThemeSelector'];
     }
 
     /**
      * @return [string]
      */
-    static function requiredCSSURLs() {
-        return [CBThemedMenuViewEditor::URL('CBThemedMenuViewEditor.css')];
+    static function CBHTMLOutput_CSSURLs() {
+        return [Colby::flexpath(__CLASS__, 'css', cbsysurl())];
     }
 
     /**
      * @return [string]
      */
-    static function requiredJavaScriptURLs() {
-        return [CBThemedMenuViewEditor::URL('CBThemedMenuViewEditor.js')];
+    static function CBHTMLOutput_JavaScriptURLs() {
+        return [Colby::flexpath(__CLASS__, 'js', cbsysurl())];
     }
-
-    /**
-     * @return string
-     */
-    static function URL($filename) {
-        $className = __CLASS__;
-        return CBSystemURL . "/classes/{$className}/{$filename}";
-    }
-
 }
