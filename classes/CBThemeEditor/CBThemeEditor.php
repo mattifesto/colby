f315778b4cc247e2e98ee05b9484ed2256c10c27
Matt Calkins (MacBook Air) <matt@mattifesto.com>
2017-09-13 08:52:52 -0700

    Garden CBThemeEditor

--- classes/CBThemeEditor/CBThemeEditor.php
+++ classes/CBThemeEditor/CBThemeEditor.php
@@ -5,31 +5,21 @@ final class CBThemeEditor {
     /**
      * @return [string]
      */
-    static function requiredClassNames() {
+    static function CBHTMLOutput_requiredClassNames() {
         return ['CBUI', 'CBUIStringEditor'];
     }
 
     /**
      * @return [string]
      */
-    static function requiredCSSURLs() {
-        return [CBThemeEditor::URL('CBThemeEditor.css')];
+    static function CBHTMLOutput_CSSURLs() {
+        return [Colby::flexpath(__CLASS__, 'css', cbsysurl())];
     }
 
     /**
      * @return [string]
      */
-    static function requiredJavaScriptURLs() {
-        return [CBThemeEditor::URL('CBThemeEditor.js')];
-    }
-
-    /**
-     * @param string $filename
-     *
-     * @return string
-     */
-    static function URL($filename) {
-        $className = __CLASS__;
-        return CBSystemURL . "/classes/{$className}/{$filename}";
+    static function CBHTMLOutput_JavaScriptURLs() {
+        return [Colby::flexpath(__CLASS__, 'js', cbsysurl())];
     }
 }
