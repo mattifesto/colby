56c73c9f7fabed086d7b8077bcb13797d830d74a
Matt Calkins (MacBook Air) <matt@mattifesto.com>
2017-10-21 09:25:19 -0700

    Update CBDataStores to use utf8mb4

--- classes/CBDataStores/CBDataStores.php
+++ classes/CBDataStores/CBDataStores.php
@@ -21,8 +21,8 @@ final class CBDataStores {
                 PRIMARY KEY     (`ID`)
             )
             ENGINE=InnoDB
-            DEFAULT CHARSET=utf8
-            COLLATE=utf8_unicode_ci
+            DEFAULT CHARSET=utf8mb4
+            COLLATE=utf8mb4_unicode_520_ci
 
 EOT;
 
