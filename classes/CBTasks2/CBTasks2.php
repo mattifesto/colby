6f4ab1bb6aaf50f8cc0f731b30a5c360f0c53132
Matt Calkins (MacBook Air) <matt@mattifesto.com>
2017-10-21 09:29:41 -0700

    Update CBTasks2 to use utf8mb4

--- classes/CBTasks2/CBTasks2.php
+++ classes/CBTasks2/CBTasks2.php
@@ -111,8 +111,8 @@ EOT;
                 KEY `group_completed` (`IDForGroup`, `completed`)
             )
             ENGINE=InnoDB
-            DEFAULT CHARSET=utf8
-            COLLATE=utf8_unicode_ci
+            DEFAULT CHARSET=utf8mb4
+            COLLATE=utf8mb4_unicode_520_ci
 
 EOT;
 
