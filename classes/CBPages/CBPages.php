78b2b5c9edd98fcc5e606e38b4ddab980d7241e8
Matt Calkins (MacBook Air) <matt@mattifesto.com>
2017-10-21 09:28:20 -0700

    Update CBPages to use utf8mb4

--- classes/CBPages/CBPages.php
+++ classes/CBPages/CBPages.php
@@ -54,8 +54,8 @@ class CBPages {
                 {$constraint}
             )
             ENGINE=InnoDB
-            DEFAULT CHARSET=utf8
-            COLLATE=utf8_unicode_ci
+            DEFAULT CHARSET=utf8mb4
+            COLLATE=utf8mb4_unicode_520_ci
 
 EOT;
 
