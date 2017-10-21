dd96992c749b86dd8f7cf47ebe9a16ea11078f74
Matt Calkins (MacBook Air) <matt@mattifesto.com>
2017-10-21 09:25:45 -0700

    Update CBImages to use utf8mb4

--- classes/CBImages/CBImages.php
+++ classes/CBImages/CBImages.php
@@ -149,8 +149,8 @@ class CBImages {
                 PRIMARY KEY (`ID`)
             )
             ENGINE=InnoDB
-            DEFAULT CHARSET=utf8
-            COLLATE=utf8_unicode_ci
+            DEFAULT CHARSET=utf8mb4
+            COLLATE=utf8mb4_unicode_520_ci
 
 EOT;
 
