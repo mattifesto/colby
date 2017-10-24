81e726bfdbd5b08438528616c68b679c4c3e758e
Matt Calkins (MacBook Air) <matt@mattifesto.com>
2017-10-24 10:44:55 -0700

    snippets/admin-widget-01-database.php -> CBStatusWidgetForColby

---
+++ classes/CBStatusWidgetForColby/CBStatusWidgetForColby.php
@@ -0,0 +1,11 @@
+<?php
+
+final class CBStatusWidgetForColby {
+
+    /**
+     * @return [<title>, <key>, <value>]
+     */
+    static function CBStatusAdminPage_data() {
+        return ['Colby', 'Version', CBSystemVersionNumber];
+    }
+}
--- snippets/admin-widget-01-database.php
+++
@@ -1,9 +0,0 @@
-<section class="widget">
-    <header><h1>Colby</h1></header>
-    <div class="version-numbers">
-        <section class="version-number">
-            <h1>Version</h1>
-            <div class="number"><?= CBSystemVersionNumber ?></div>
-        </section>
-    </div>
-</section>
