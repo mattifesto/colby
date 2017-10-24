dab17f2cf4184585d8719e451d5c1551d9e027a4
Matt Calkins (MacBook Air) <matt@mattifesto.com>
2017-10-24 10:41:19 -0700

    Update handlers/handle,admin.php

--- handlers/handle,admin.php
+++ handlers/handle,admin.php
@@ -27,6 +27,35 @@ CBView::render((object)[
     <div class="CBLibraryListView">
         <?php
 
+        $statusWidgetFilepaths = Colby::globFiles('classes/CBStatusWidgetFor*');
+        $statusWidgetClassNames = array_map(function ($filepath) {
+            return basename($filepath, '.php');
+        }, $statusWidgetFilepaths);
+
+        sort($statusWidgetClassNames);
+
+        array_walk($statusWidgetClassNames, function($className) {
+            if (is_callable($function = "{$className}::CBStatusAdminPage_data")) {
+                $data = call_user_func($function);
+
+                ?>
+
+                <section class="widget">
+                    <header><h1><?= cbhtml($data[0]) ?></h1></header>
+
+                    <div class="version-numbers">
+                        <section class="version-number">
+                            <h1><?= cbhtml($data[1]) ?></h1>
+                            <div class="number"><?= cbhtml($data[2]) ?></div>
+                        </section>
+                    </div>
+                </section>
+
+                <?php
+            }
+        });
+
+        /* deprecated: use status widget classes */
         $widgetClassNames = Colby::globFiles('classes/*AdminWidgetFor*');
         $widgetClassNames = array_map(function ($className) {
             return basename($className, '.php');
@@ -40,7 +69,7 @@ CBView::render((object)[
             }
         });
 
-        /* deprecated: use widget classes */
+        /* deprecated: use status widget classes */
         $adminWidgetFilenames = Colby::globFiles('snippets/admin-widget-*.php');
 
         foreach ($adminWidgetFilenames as $adminWidgetFilename) {
@@ -52,11 +81,9 @@ CBView::render((object)[
 
     <?php
 
-    CBAdminPageForGeneral::renderDuplicatePublishedURIWarnings();
-
-    CBAdminPageForGeneral::renderSiteConfigurationIssuesView();
-
-    CBAdminPageForGeneral::renderPingStatus();
+    CBStatusAdminPage::renderDuplicatePublishedURIWarnings();
+    CBStatusAdminPage::renderSiteConfigurationIssuesView();
+    CBStatusAdminPage::renderPingStatus();
 
     ?>
 
