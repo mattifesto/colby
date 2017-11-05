16f3434311eca14bf49586145c8b8e75b4691490
Matt Calkins (MacBook Air) <matt@mattifesto.com>
2017-11-05 13:47:27 -0800

    Update snippets/exception-stack-trace.php

--- snippets/exception-stack-trace.php
+++ snippets/exception-stack-trace.php
@@ -1,22 +1,5 @@
 <?php
 
-if (isset($_SERVER['SERVER_NAME'])) {
-    echo $_SERVER['SERVER_NAME'], "\n";
-}
-
-if (isset($_SERVER['REQUEST_URI'])) {
-    $parts = explode('?', $_SERVER['REQUEST_URI'], 2);
-    echo implode("\n", $parts);
-    echo "\n";
-}
-
-if (isset($_POST['ajax'])) {
-    $ajaxModelAsJSON = cb_post_value('ajax');
-    $ajaxModel = json_decode($ajaxModelAsJSON);
-    $ajaxFunctionClassName = CBModel::value($ajaxModel, 'functionClassName', '(unset)');
-    $ajaxFunctionName = CBModel::value($ajaxModel, 'functionName', '(unset)');
-    echo "\nAjax:\n{$ajaxFunctionClassName}\n{$ajaxFunctionName}\n";
-}
-
+echo CBRequest::requestInformationAsMessage();
 echo "\n\n";
 echo CBConvert::throwableToStackTrace($exception);
