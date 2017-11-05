500e45dbfe4ff1b693b8360c2e0920e7cbc196a7
Matt Calkins (MacBook Air) <matt@mattifesto.com>
2017-11-05 13:10:29 -0800

    Add CBRequest::requestInformationAsMessage()

--- classes/CBRequest/CBRequest.php
+++ classes/CBRequest/CBRequest.php
@@ -15,7 +15,7 @@ final class CBRequest {
      *          ...
      *      ]
      *
-     * @return  {string}
+     * @return  string
      *  The string returned will be properly URL encoded but will not have HTML
      *  entities encoded.
      *
@@ -54,14 +54,14 @@ final class CBRequest {
      * there are repeated slashes the URL is not canonical and will be
      * rewritten.
      *
-     * @return [{string}]
+     * @return [string]
      */
     static function decodedPathToDecodedStubs($decodedPath) {
         return preg_split('/[\/]+/', $decodedPath, null, PREG_SPLIT_NO_EMPTY);
     }
 
     /**
-     * @return {string}
+     * @return string
      */
     static function decodedPathToCanonicalEncodedPath($decodedPath) {
         $stubs = CBRequest::decodedPathToDecodedStubs($decodedPath);
@@ -71,7 +71,41 @@ final class CBRequest {
     }
 
     /**
-     * @return {string}
+     * @return string
+     */
+    static function requestInformationAsMessage() {
+        $message = '';
+
+        if (isset($_SERVER['SERVER_NAME'])) {
+            $message .= "Host: " . $_SERVER['SERVER_NAME'] . "\n";
+        }
+
+        if (isset($_SERVER['REQUEST_URI'])) {
+            $parts = explode('?', $_SERVER['REQUEST_URI'], 2);
+            $message .= "URI: $parts[0]\n";
+            if (isset($parts[1])) {
+                $message .= "Query String: $parts[2]\n";
+            }
+            $message .= "\n";
+        }
+
+        if (isset($_POST['ajax'])) {
+            $ajaxModelAsJSON = cb_post_value('ajax');
+            $ajaxModel = json_decode($ajaxModelAsJSON);
+            $ajaxFunctionClassName = CBModel::value($ajaxModel, 'functionClassName', '(unset)');
+            $ajaxFunctionName = CBModel::value($ajaxModel, 'functionName', '(unset)');
+            $message .= "Ajax:\nclassName: {$ajaxFunctionClassName}\nfunction: {$ajaxFunctionName}\n";
+            $args = CBModel::valueAsObject($ajaxModel, 'args');
+            if (!empty($args)) {
+                $message .= "Arguments: " . json_encode($args, JSON_PRETTY_PRINT | JSON_UNESCAPED_SLASHES). "\n";
+            }
+        }
+
+        return $message;
+    }
+
+    /**
+     * @return string
      */
     static function requestURIToOriginalEncodedPath($requestURI = null) {
         $requestURI = ($requestURI !== null) ? $requestURI : $_SERVER['REQUEST_URI'];
@@ -82,7 +116,7 @@ final class CBRequest {
     }
 
     /**
-     * @return {string}
+     * @return string
      */
     static function requestURIToOriginalEncodedQueryString($requestURI = null) {
         $requestURI = ($requestURI !== null) ? $requestURI : $_SERVER['REQUEST_URI'];
