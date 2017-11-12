d29603911fc5a1a4aaf0359d65d1f7d196918819
Matt Calkins (MacBook Air) <matt@mattifesto.com>
2017-11-11 17:09:36 -0800

    Add CBContentStyleSheet

---
+++ classes/CBContentStyleSheet/CBContentStyleSheet.php
@@ -0,0 +1,11 @@
+<?php
+
+final class CBContentStyleSheet {
+
+    /**
+     * @return [string]
+     */
+    static function CBHTMLOutput_CSSURLs() {
+        return [Colby::flexpath(__CLASS__, 'css', cbsysurl())];
+    }
+}
