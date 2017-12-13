a15bf7b63bd784ee0cc097b088eb030b309efb1f
Matt Calkins (MacBook Air) <matt@mattifesto.com>
2017-12-13 12:46:25 -0800

    Move status page rendering from handle,admin.php to CBStatusAdminPage

--- classes/CBStatusAdminPage/CBStatusAdminPage.php
+++ classes/CBStatusAdminPage/CBStatusAdminPage.php
@@ -6,6 +6,102 @@
 final class CBStatusAdminPage {
 
     /**
+     * @param string $pageStub
+     *
+     * @return [string]
+     */
+    static function CBAdmin_menuNamePath(string $pageStub) {
+        return ['general', 'status'];
+    }
+
+    /**
+     * @param string $pageStub
+     *
+     * @return void
+     */
+    static function CBAdmin_render($pageStub) {
+        CBHTMLOutput::setTitleHTML('Website Status');
+        CBHTMLOutput::setDescriptionHTML('The status of the website');
+
+        ?>
+
+        <main class="CBUIRoot">
+            <div class="CBLibraryListView">
+                <?php
+
+                $statusWidgetFilepaths = Colby::globFiles('classes/CBStatusWidgetFor*');
+                $statusWidgetClassNames = array_map(function ($filepath) {
+                    return basename($filepath, '.php');
+                }, $statusWidgetFilepaths);
+
+                sort($statusWidgetClassNames);
+
+                array_walk($statusWidgetClassNames, function($className) {
+                    if (is_callable($function = "{$className}::CBStatusAdminPage_data")) {
+                        $data = call_user_func($function);
+
+                        ?>
+
+                        <section class="widget">
+                            <header><h1><?= cbhtml($data[0]) ?></h1></header>
+
+                            <div class="version-numbers">
+                                <section class="version-number">
+                                    <h1><?= cbhtml($data[1]) ?></h1>
+                                    <div class="number"><?= cbhtml($data[2]) ?></div>
+                                </section>
+                            </div>
+                        </section>
+
+                        <?php
+                    }
+                });
+
+                /* deprecated: use status widget classes */
+                $widgetClassNames = Colby::globFiles('classes/*AdminWidgetFor*');
+                $widgetClassNames = array_map(function ($className) {
+                    return basename($className, '.php');
+                }, $widgetClassNames);
+
+                sort($widgetClassNames);
+
+                array_walk($widgetClassNames, function($className) {
+                    if (is_callable($function = "{$className}::render")) {
+                        call_user_func($function);
+                    }
+                });
+
+                /* deprecated: use status widget classes */
+                $adminWidgetFilenames = Colby::globFiles('snippets/admin-widget-*.php');
+
+                foreach ($adminWidgetFilenames as $adminWidgetFilename) {
+                    include $adminWidgetFilename;
+                }
+
+                ?>
+            </div>
+
+            <?php
+
+            CBStatusAdminPage::renderDuplicatePublishedURIWarnings();
+            CBStatusAdminPage::renderSiteConfigurationIssuesView();
+            CBStatusAdminPage::renderPingStatus();
+
+            ?>
+
+        </main>
+
+        <?php
+    }
+
+    /**
+     * @return [string]
+     */
+    static function CBHTMLOutput_CSSURLs() {
+        return [Colby::flexpath(__CLASS__, 'v358.css', cbsysurl())];
+    }
+
+    /**
      * @return null
      */
     static function renderDuplicatePublishedURIWarnings() {
