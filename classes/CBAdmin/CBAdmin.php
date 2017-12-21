464091bcc0db1bf65f1f965f7aad8cffa62ca527
Matt Calkins (MacBook Air) <matt@mattifesto.com>
2017-12-20 17:55:00 -0800

    Add comments to CBAdmin

--- classes/CBAdmin/CBAdmin.php
+++ classes/CBAdmin/CBAdmin.php
@@ -1,10 +1,41 @@
 <?php
 
+/**
+ * This class exists to facilitate the rendering of administrative pages. Its
+ * render() function is called by the `handle,admin.php` handler.
+ *
+ * Admin pages differ from public pages in the following ways:
+ *
+ *      - They do not require pretty URIs
+ *      - Their content is not searchable
+ *      - They should not be added to the site map
+ *
+ * Admin pages rendered with this class will have a URI in this format:
+ *
+ *      /admin/c=<className>
+ *
+ * The class should implement the CBAdmin_render() and CBAdmin_menuNamePath()
+ * interfaces. This class will add the class name to the CBHTMLOutput required
+ * classes so that it can implement the CBHTMLOutput interfaces to specify its
+ * own dependencies.
+ *
+ * A single admin page class may render multiple admin pages based on another
+ * query variable. They will usually be rendering similar pages. Vastly
+ * different pages or pages with different sets of functionality should still
+ * be separated into multiple classes.
+ *
+ * A class that has other HTML rendering functions, such as a view, should not
+ * also render admin pages because the CBHTMLOutput dependencies will likely be
+ * different.
+ */
 final class CBAdmin {
 
     /**
      * @param string $className
-     * @param string $pageStub
+     * @param string $pageStub (deprecated)
+     *
+     *      The functions should manually gather query variables if they render
+     *      different content.
      *
      * @return void
      */
