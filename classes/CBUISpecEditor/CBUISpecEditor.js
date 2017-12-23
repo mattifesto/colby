ae74723a3d3b7301df9395832e39d09a248bb629
Matt Calkins (MacBook Air) <matt@mattifesto.com>
2017-12-23 10:02:08 -0800

    Garden CBUISpecEditor

--- classes/CBUISpecEditor/CBUISpecEditor.js
+++ classes/CBUISpecEditor/CBUISpecEditor.js
@@ -7,11 +7,9 @@
 var CBUISpecEditor = {
 
     /**
-     * This function is the core funtion to call to create an editor for a spec.
-     * It has all of the rules built into it and if an editor is being created
-     * anywhere else for a spec it should be changed to call this function.
-     * It's uncertain if this function should live in this file or have its own
-     * class.
+     * This the core function to call to create an editor for a spec. It has all
+     * of the rules built into it and if an editor is being created anywhere
+     * else for a spec it should be changed to call this function.
      *
      * @param object args
      *
