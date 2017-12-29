3275c190d255fcbf6f41dce334bccaaeb8fb0394
Matt Calkins (MacBook Air) <matt@mattifesto.com>
2017-12-29 15:59:52 -0800

    Make CBPageSettingsForAdminPages use CBEqualize

--- classes/CBPageSettingsForAdminPages/CBPageSettingsForAdminPages.php
+++ classes/CBPageSettingsForAdminPages/CBPageSettingsForAdminPages.php
@@ -45,9 +45,6 @@ final class CBPageSettingsForAdminPages {
      */
     static function renderHeadContent() { ?>
         <meta name="viewport" content="width=device-width, initial-scale=1">
-        <script src="<?= CBSystemURL ?>/javascript/html5shiv.js"></script>
-        <script src="<?= CBSystemURL ?>/javascript/ColbyEqualize.js"></script>
-        <link rel="stylesheet" href="<?= CBSystemURL ?>/css/equalize.css">
         <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Open+Sans:400">
         <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Open+Sans:400italic">
         <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Open+Sans:700">
@@ -72,4 +69,11 @@ final class CBPageSettingsForAdminPages {
             }
         </style>
     <?php }
+
+    /**
+     * @return [string]
+     */
+    static function CBHTMLOutput_requiredHeadClassNames() {
+        return ['CBEqualize'];
+    }
 }
