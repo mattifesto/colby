9285f55418ede63b09525eb219161ee7be89fd43
Matt Calkins (MacBook Air) <matt@mattifesto.com>
2017-12-29 16:02:48 -0800

    Garden CBPageSettingsForAdminPages

--- classes/CBPageSettingsForAdminPages/CBPageSettingsForAdminPages.php
+++ classes/CBPageSettingsForAdminPages/CBPageSettingsForAdminPages.php
@@ -43,7 +43,7 @@ final class CBPageSettingsForAdminPages {
     /**
      * @return  null
      */
-    static function renderHeadContent() { ?>
+    static function CBHTMLOutput_renderHeadContent() { ?>
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Open+Sans:400">
         <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Open+Sans:400italic">
