7c105c42e3185f9706c0529b2c8de09e8c6aa769
Matt Calkins (MacBook Air) <matt@mattifesto.com>
2017-12-29 16:00:39 -0800

    Make CBPageSettingsForResponsivePages use CBEqualize

--- classes/CBPageSettingsForResponsivePages/CBPageSettingsForResponsivePages.php
+++ classes/CBPageSettingsForResponsivePages/CBPageSettingsForResponsivePages.php
@@ -13,9 +13,6 @@ final class CBPageSettingsForResponsivePages {
      */
     static function renderHeadContent() { ?>
         <meta name="viewport" content="width=device-width, initial-scale=1">
-        <script src="<?= CBSystemURL ?>/javascript/html5shiv.js"></script>
-        <script src="<?= CBSystemURL ?>/javascript/ColbyEqualize.js"></script>
-        <link rel="stylesheet" href="<?= CBSystemURL ?>/css/equalize.css">
         <link href='https://fonts.googleapis.com/css?family=Open+Sans:400' rel='stylesheet'>
         <link href='https://fonts.googleapis.com/css?family=Open+Sans:400italic' rel='stylesheet'>
         <link href='https://fonts.googleapis.com/css?family=Open+Sans:600' rel='stylesheet'>
@@ -44,5 +41,12 @@ final class CBPageSettingsForResponsivePages {
             })(window,document,'script','dataLayer','<?= $googleTagManagerID ?>');</script>
             <!-- End Google Tag Manager -->
         <?php }
+    }
+
+    /**
+     * @return [string]
+     */
+    static function CBHTMLOutput_requiredHeadClassNames() {
+        return ['CBEqualize'];
     }
 }
