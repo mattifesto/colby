2ce5417fa78959255bbf7f1196151e764b0d5b24
Matt Calkins (MacBook Air) <matt@mattifesto.com>
2018-01-17 13:16:02 -0800

    Remove CBUISelector.css

--- classes/CBUISelector/CBUISelector.php
+++ classes/CBUISelector/CBUISelector.php
@@ -13,13 +13,6 @@ final class CBUISelector {
     /**
      * @return [string]
      */
-    static function CBHTMLOutput_CSSURLs() {
-        return [Colby::flexpath(__CLASS__, 'css', cbsysurl())];
-    }
-
-    /**
-     * @return [string]
-     */
     static function CBHTMLOutput_JavaScriptURLs() {
         return [Colby::flexpath(__CLASS__, 'v368.js', cbsysurl())];
     }
