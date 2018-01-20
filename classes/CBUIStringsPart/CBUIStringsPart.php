f1f47c332a9f852be20f45f1dad7f42f188a65e2
Matt Calkins (MacBook Air) <matt@mattifesto.com>
2018-01-20 10:27:05 -0800

    Remove hyphens from CBUIStringsPart CSS class names

--- classes/CBUIStringsPart/CBUIStringsPart.php
+++ classes/CBUIStringsPart/CBUIStringsPart.php
@@ -6,7 +6,7 @@ final class CBUIStringsPart {
      * @return [string]
      */
     static function CBHTMLOutput_CSSURLs() {
-        return [Colby::flexpath(__CLASS__, 'v368.css', cbsysurl())];
+        return [Colby::flexpath(__CLASS__, 'v370.css', cbsysurl())];
     }
 
     /**
