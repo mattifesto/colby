817284870bb9d8f2e251ef7afb16feab4e2ea969
Matt Calkins (MacBook Air) <matt@mattifesto.com>
2018-01-22 18:53:52 -0800

    Fix CBRequiredClassNamesResolver circular dependency detection

--- classes/CBRequiredClassNamesResolver/CBRequiredClassNamesResolver.php
+++ classes/CBRequiredClassNamesResolver/CBRequiredClassNamesResolver.php
@@ -8,6 +8,7 @@
 final class CBRequiredClassNamesResolver {
 
     private $resolvedClassNames = [];
+    private $resolvingClassNames = [];
 
     /**
      * @return CBClassRequirementsResolver
@@ -21,18 +22,46 @@ final class CBRequiredClassNamesResolver {
      */
     private function resolve(array $classNames) {
         foreach ($classNames as $className) {
+
+            /**
+             * If the class name has already been resolved we don't need to
+             * resolve it again.
+             */
             if (in_array($className, $this->resolvedClassNames)) {
                 continue;
             }
 
+            /**
+             * If the class name is already in the list of currently resolving
+             * class names, there is a circular dependency.
+             */
+            if (in_array($className, $this->resolvingClassNames)) {
+                $dependencies  = implode(' > ', $this->resolvingClassNames);
+                throw new RuntimeException("{$className} has a circular dependency: {$dependencies} > {$className}");
+            }
+
+            /**
+             * While we resolve this class name's dependencies it will be pushed
+             * onto the stack of currently resolving class names.
+             */
+            array_push($this->resolvingClassNames, $className);
+
+            /**
+             * Resolve this class name's dependencies.
+             */
             if (is_callable($function = "{$className}::CBHTMLOutput_requiredClassNames") || is_callable($function = "{$className}::requiredClassNames")) {
                 $this->resolve(call_user_func($function));
             }
 
-            if (in_array($className, $this->resolvedClassNames)) {
-                throw new RuntimeException("{$className} has a circular dependency.");
-            }
+            /**
+             * We are done resolving this class name's dependencies so we can
+             * pop it off the stack of currently resolving class names.
+             */
+            array_pop($this->resolvingClassNames);
 
+            /**
+             * Add this class name to the list of resolved class names.
+             */
             $this->resolvedClassNames[] = $className;
         }
     }
