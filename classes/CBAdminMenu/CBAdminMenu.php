6284375164ff80f903aeed1ef985c7436e796124
Matt Calkins (MacBook Air) <matt@mattifesto.com>
2018-01-25 18:00:47 -0800

    Add CBAdminMenu

---
+++ classes/CBAdminMenu/CBAdminMenu.php
@@ -0,0 +1,35 @@
+<?php
+
+final class CBAdminMenu {
+
+    const ID = '3924c0a0581171f86f0708bfa799a3d8c34bd390';
+
+    /**
+     * This function resets the main admin menu during install. To add a menu
+     * item create a class with an install dependency on this class and in the
+     * CBInstall_install function add a menu item.
+     *
+     * @return void
+     */
+    static function CBInstall_install(): void {
+        $spec = (object)[
+            'className' => 'CBMenu',
+            'ID' => CBAdminMenu::ID,
+            'title' => 'Administration',
+            'titleURI' => '/admin/',
+            'items' => [
+                (object)[
+                    'className' => 'CBMenuItem',
+                    'name' => 'home',
+                    'text' => 'Home',
+                    'URL' => '/',
+                ],
+            ],
+        ];
+
+        CBDB::transaction(function () use ($spec) {
+            CBModels::deleteByID(CBAdminMenu::ID);
+            CBModels::save($spec);
+        });
+    }
+}
