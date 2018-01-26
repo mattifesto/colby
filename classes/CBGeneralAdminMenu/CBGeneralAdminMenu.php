659583aeae48da63e7ac7bde3d2c5b28bc243b0d
Matt Calkins (MacBook Air) <matt@mattifesto.com>
2018-01-25 18:14:11 -0800

    Add CBGeneralAdminMenu

---
+++ classes/CBGeneralAdminMenu/CBGeneralAdminMenu.php
@@ -0,0 +1,66 @@
+<?php
+
+final class CBGeneralAdminMenu {
+
+    const ID = '1668c3011f7be273731903b012a5884628300898';
+
+    /**
+     * @return void
+     */
+    static function CBInstall_install(): void {
+        $adminMenuSpec = CBModels::fetchSpecByID(CBAdminMenu::ID);
+
+        $adminMenuSpec->items[] = (object)[
+            'className' => 'CBMenuItem',
+            'name' => 'general',
+            'text' => 'General',
+            'URL' => '/admin/',
+        ];
+
+        $spec = (object)[
+            'className' => 'CBMenu',
+            'ID' => CBGeneralAdminMenu::ID,
+            'title' => 'General',
+            'titleURI' => '/admin/',
+            'items' => [
+                (object)[
+                    'className' => 'CBMenuItem',
+                    'name' => 'status',
+                    'text' => 'Status',
+                    'URL' => '/admin/',
+                ],
+                (object)[
+                    'className' => 'CBMenuItem',
+                    'name' => 'tasks',
+                    'text' => 'Tasks',
+                    'URL' => '/admin/page/?class=CBTasks2AdminPage',
+                ],
+                (object)[
+                    'className' => 'CBMenuItem',
+                    'name' => 'logs',
+                    'text' => 'Logs',
+                    'URL' => '/admin/page/?class=CBLogAdminPage',
+                ],
+                (object)[
+                    'className' => 'CBMenuItem',
+                    'name' => 'users',
+                    'text' => 'Users',
+                    'URL' => '/admin/page/?class=CBAdminPageForUsers',
+                ],
+            ],
+        ];
+
+        CBDB::transaction(function () use ($adminMenuSpec, $spec) {
+            CBModels::save($adminMenuSpec);
+            CBModels::deleteByID(CBGeneralAdminMenu::ID);
+            CBModels::save($spec);
+        });
+    }
+
+    /**
+     * @return [string]
+     */
+    static function CBInstall_requiredClassNames(): array {
+        return ['CBAdminMenu'];
+    }
+}
