b2a35d579047ec5f9e3fef0ab850cfefe74c2750
Matt Calkins (MacBook Air) <matt@mattifesto.com>
2018-01-28 17:03:14 -0800

    Make CBTextView2StandardLayout use CBTextColor3

--- classes/CBTextView2StandardLayout/CBTextView2StandardLayout.php
+++ classes/CBTextView2StandardLayout/CBTextView2StandardLayout.php
@@ -1,11 +1,15 @@
 <?php
 
+/**
+ * @deprecated 2018.01.28 This appears to be deprecated. Do research to find out
+ * if it's used in certain view instances.
+ */
 final class CBTextView2StandardLayout {
 
     /**
      * @return [string]
      */
     static function CBHTMLOutput_CSSURLs() {
-        return [Colby::flexnameForCSSForClass(CBSystemURL, __CLASS__)];
+        return [Colby::flexpath(__CLASS__, 'v374.css', cbsysurl())];
     }
 }
