4bc4b83b983057e23aa4839440fe637cf134ea09
Matt Calkins (MacBook Air) <matt@mattifesto.com>
2018-01-31 13:51:49 -0800

    Update CBMenuView JavaScript and CSS version numbers

--- classes/CBMenuView/CBMenuView.php
+++ classes/CBMenuView/CBMenuView.php
@@ -6,14 +6,14 @@ final class CBMenuView {
      * @return [string]
      */
     static function CBHTMLOutput_CSSURLs() {
-        return [Colby::flexnameForCSSForClass(CBSystemURL, __CLASS__)];
+        return [Colby::flexpath(__CLASS__, 'v376.css', cbsysurl())];
     }
 
     /**
      * @return [string]
      */
     static function CBHTMLOutput_JavaScriptURLs() {
-        return [Colby::flexnameForJavaScriptForClass(CBSystemURL, __CLASS__)];
+        return [Colby::flexpath(__CLASS__, 'v376.js', cbsysurl())];
     }
 
     /**
