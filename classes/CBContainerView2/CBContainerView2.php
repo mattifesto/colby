445ac0436da026a51cfec642b0bc916c2da6b162
Matt Calkins (MacBook Air) <matt@mattifesto.com>
2018-02-13 11:15:58 -0800

    Fix CBContainerView2::CBModel_toSearchText()

--- classes/CBContainerView2/CBContainerView2.php
+++ classes/CBContainerView2/CBContainerView2.php
@@ -43,15 +43,18 @@ final class CBContainerView2 {
     }
 
     /**
-     * @param object $model
+     * @param model $model
      *
      * @return string
      */
-    static function CBModel_toSearchText(stdClass $model) {
-        $subviews = CBModel::valueAsObjects($model, 'subviews');
-        $strings = array_map('CBModel::toSearchText', $subviews);
-        $strings = array_filter($strings);
-        return implode(' ', $strings);
+    static function CBModel_toSearchText(stdClass $model): string {
+        return implode(
+            ' ',
+            array_map(
+                'CBModel::toSearchText',
+                CBModel::valueToArray($model, 'subviews')
+            )
+        );
     }
 
     /**
