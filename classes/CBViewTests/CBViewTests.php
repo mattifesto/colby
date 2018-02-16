d679e40a3dc48a490856f5b8c6aa5e2d23fc1a02
Matt Calkins (MacBook Air) <matt@mattifesto.com>
2018-02-15 17:56:26 -0800

    Garden CBViewTests

--- classes/CBViewTests/CBViewTests.php
+++ classes/CBViewTests/CBViewTests.php
@@ -8,8 +8,8 @@ final class CBViewTests {
      * @NOTE 2018.02.13
      *
      *      This test exposes the awkwardness in trying to render a view for
-     *      purposes other than rendering it to the current page. The renderg of
-     *      views below may be affecting the CBHTMLOutput state.
+     *      purposes other than rendering it to the current page. The rendering
+     *      of views below may be affecting the CBHTMLOutput state.
      *
      *      We can't use CBHTMLOutput::begin() and reset() because begin() will
      *      set an exception handler. It's not vitally important, but I created
