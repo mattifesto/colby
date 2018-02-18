72eda1ee986dc35c9d95bf2af7759574eb78d211
Matt Calkins (MacBook Air) <matt@mattifesto.com>
2018-02-18 13:11:12 -0800

    Add CBMenuTests::CBTests_classTest()

---
+++ classes/CBMenuTests/CBMenuTests.php
@@ -0,0 +1,76 @@
+<?php
+
+final class CBMenuTests {
+
+    static function CBTests_classTest() {
+        $spec = (object)[
+            'className' => 'CBMenu',
+            'items' => [
+                null,
+                2,
+                (object)[
+                    'className' => 'CBTestView',
+                    'value' => 42,
+                ],
+                (object)[
+                    'className' => 'jdkflskdljsdkfd',
+                ],
+            ],
+        ];
+
+        $expectedModel = (object)[
+            'className' => 'CBMenu',
+            'title' => '',
+            'titleURI' => '',
+            'items' => [
+                (object)[
+                    'className' => 'CBTestView',
+                    'value' => 42,
+                ],
+            ],
+        ];
+
+        $model = CBModel::build($spec);
+
+        if ($model != $expectedModel) {
+            return (object)[
+                'message' =>
+                    "The result built model does not match the expected built model.\n\n" .
+                    CBConvertTests::resultAndExpectedToMessage($model, $expectedModel),
+            ];
+        }
+
+        $searchText = CBModel::toSearchText($model);
+        $expectedSearchText = 'CBMenu';
+
+        if ($searchText !== $expectedSearchText) {
+            return (object)[
+                'message' =>
+                    "The result search text does not match the expected search text.\n\n" .
+                    CBConvertTests::resultAndExpectedToMessage($searchText, $expectedSearchText),
+            ];
+        }
+
+        $upgradedSpec = CBModel::upgrade($spec);
+        $expectedUpgradedSpec = (object)[
+            'className' => 'CBMenu',
+            'items' => [
+                (object)[
+                    'className' => 'CBTestView',
+                    'value' => 42,
+                ],
+                (object)[
+                    'className' => 'jdkflskdljsdkfd',
+                ],
+            ],
+        ];
+
+        if ($upgradedSpec != $expectedUpgradedSpec) {
+            return (object)[
+                'message' =>
+                    "The result upgraded spec does not match the expected upgraded spec.\n\n" .
+                    CBConvertTests::resultAndExpectedToMessage($upgradedSpec, $expectedUpgradedSpec),
+            ];
+        }
+    }
+}
