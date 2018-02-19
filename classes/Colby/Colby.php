1fdbe592d2312d50a963ec06714c3d66e335b881
Matt Calkins (MacBook Air) <matt@mattifesto.com>
2018-02-18 18:32:52 -0800

    Update Colby JavaScript version number

--- classes/Colby/Colby.php
+++ classes/Colby/Colby.php
@@ -73,7 +73,7 @@ final class Colby {
      * @return [string]
      */
     static function CBHTMLOutput_JavaScriptURLs() {
-        return [Colby::flexpath(__CLASS__, 'v379.js', cbsysurl())];
+        return [Colby::flexpath(__CLASS__, 'v391.js', cbsysurl())];
     }
 
     /**
