3088dcd292075248157994ccbcb967f6841b0628
Matt Calkins (MacBook Air) <matt@mattifesto.com>
2018-02-22 15:44:44 -0800

    Fix CBUIExpander message property when receiving non-string parameter

--- classes/CBUIExpander/CBUIExpander.php
+++ classes/CBUIExpander/CBUIExpander.php
@@ -13,7 +13,7 @@ final class CBUIExpander {
      * @return [string]
      */
     static function CBHTMLOutput_JavaScriptURLs() {
-        return [Colby::flexpath(__CLASS__, 'v374.js', cbsysurl())];
+        return [Colby::flexpath(__CLASS__, 'v393.js', cbsysurl())];
     }
 
     /**
