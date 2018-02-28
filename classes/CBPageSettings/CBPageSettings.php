388c192fb2bd8c6d705ed9bea552e9dcb6b0b5b4
Matt Calkins (MacBook Air) <matt@mattifesto.com>
2018-02-28 12:58:13 -0800

    Remove error_log() from CBPageSettings

--- classes/CBPageSettings/CBPageSettings.php
+++ classes/CBPageSettings/CBPageSettings.php
@@ -14,7 +14,7 @@ final class CBPageSettings {
             if (is_callable($function = "{$className}::CBPageSettings_htmlElementClassNames")) {
                 $classNames = call_user_func($function);
                 $htmlElementClassNames = array_merge($htmlElementClassNames, $classNames);
-            } error_log(json_encode($htmlElementClassNames));
+            }
         }
 
         return array_values(array_unique($htmlElementClassNames));
