5c6b2f180c94a2fe42dc717b33d5f4a4c02eacce
Matt Calkins (MacBook Air) <matt@mattifesto.com>
2018-02-28 14:22:27 -0800

    Replace CBHTMLOutput_renderHeadContent() with CBPageSettings_renderHeadElementHTML() in CBPageSettingsForStaticPages

--- classes/CBPageSettingsForStaticPages/CBPageSettingsForStaticPages.php
+++ classes/CBPageSettingsForStaticPages/CBPageSettingsForStaticPages.php
@@ -18,7 +18,7 @@ final class CBPageSettingsForStaticPages {
     /**
      * @return void
      */
-    static function CBHTMLOutput_renderHeadContent(): void {
+    static function CBPageSettings_renderHeadElementHTML(): void {
         ?>
 
         <meta name="viewport" content="width=device-width, initial-scale=1">
