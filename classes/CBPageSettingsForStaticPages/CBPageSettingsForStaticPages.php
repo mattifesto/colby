231fea33b68342f5306ce31b4092d2ed6fe4642d
Matt Calkins (MacBook Air) <matt@mattifesto.com>
2018-03-01 10:10:10 -0800

    Use CBGoogleTagManagerPageSettingsPart in CBPageSettingsForStaticPages

--- classes/CBPageSettingsForStaticPages/CBPageSettingsForStaticPages.php
+++ classes/CBPageSettingsForStaticPages/CBPageSettingsForStaticPages.php
@@ -16,28 +16,13 @@ final class CBPageSettingsForStaticPages {
     }
 
     /**
-     * @return void
-     */
-    static function CBPageSettings_renderPreContentHTML(): void {
-        $googleTagManagerID = CBSitePreferences::googleTagManagerID();
-
-        if ($googleTagManagerID !== '') { ?>
-            <!-- Google Tag Manager -->
-            <noscript><iframe src="//www.googletagmanager.com/ns.html?id=<?= $googleTagManagerID ?>"
-            height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
-            <script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-            new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-            j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-            '//www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-            })(window,document,'script','dataLayer','<?= $googleTagManagerID ?>');</script>
-            <!-- End Google Tag Manager -->
-        <?php }
-    }
-
-    /**
      * @return [string]
      */
     static function CBPageSettings_requiredClassNames(): array {
-        return ['CBEqualizePageSettingsPart', 'CBResponsiveViewportPageSettingsPart'];
+        return [
+            'CBEqualizePageSettingsPart',
+            'CBResponsiveViewportPageSettingsPart',
+            'CBGoogleTagManagerPageSettingsPart',
+        ];
     }
 }
