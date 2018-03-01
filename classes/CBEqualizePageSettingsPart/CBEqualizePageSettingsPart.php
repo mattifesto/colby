0fe84b469ed55753b975b6c11e2edca9ba426f3f
Matt Calkins (MacBook Air) <matt@mattifesto.com>
2018-03-01 09:54:04 -0800

    Rename CBEqualize -> CBEqualizePageSettingsPart

--- classes/CBEqualizePageSettingsPart/CBEqualizePageSettingsPart.php
+++ classes/CBEqualizePageSettingsPart/CBEqualizePageSettingsPart.php
@@ -1,13 +1,13 @@
 <?php
 
-final class CBEqualize {
+final class CBEqualizePageSettingsPart {
 
     /**
      * @return void
      */
     static function CBPageSettings_renderHeadElementHTML(): void { ?>
-        <script src="<?= cbsysurl() . '/classes/CBEqualize/html5shiv.v362.js' ?>"></script>
-        <script src="<?= cbsysurl() . '/classes/CBEqualize/es6-promise.auto.min.v362.js' ?>"></script>
+        <script src="<?= cbsysurl() . '/classes/CBEqualizePageSettingsPart/html5shiv.v362.js' ?>"></script>
+        <script src="<?= cbsysurl() . '/classes/CBEqualizePageSettingsPart/es6-promise.auto.min.v362.js' ?>"></script>
         <script src="<?= Colby::flexpath(__CLASS__, 'v362.js', cbsysurl()) ?>"></script>
         <link rel="stylesheet" href="<?= Colby::flexpath(__CLASS__, 'v375.css', cbsysurl()) ?>">
     <?php }
