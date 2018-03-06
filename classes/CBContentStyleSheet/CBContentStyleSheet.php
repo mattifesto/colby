b68150c9410f15308d2a9d1977265ebe3ed8f10e
Matt Calkins (MacBook Air) <matt@mattifesto.com>
2018-03-06 15:09:31 -0800

    Update top margin styles in CBContentStyleSheet

--- classes/CBContentStyleSheet/CBContentStyleSheet.php
+++ classes/CBContentStyleSheet/CBContentStyleSheet.php
@@ -6,6 +6,6 @@ final class CBContentStyleSheet {
      * @return [string]
      */
     static function CBHTMLOutput_CSSURLs() {
-        return [Colby::flexpath(__CLASS__, 'v380.css', cbsysurl())];
+        return [Colby::flexpath(__CLASS__, 'v397.css', cbsysurl())];
     }
 }
