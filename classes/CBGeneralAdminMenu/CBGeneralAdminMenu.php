adaf6b97bd01e70a1b43eb4bb2b11aeed7486b63
Matt Calkins (MacBook Air) <matt@mattifesto.com>
2018-03-05 18:57:13 -0800

    Update CBGeneralAdminMenu.php

--- classes/CBGeneralAdminMenu/CBGeneralAdminMenu.php
+++ classes/CBGeneralAdminMenu/CBGeneralAdminMenu.php
@@ -8,7 +8,7 @@ final class CBGeneralAdminMenu {
      * @return void
      */
     static function CBInstall_install(): void {
-        $adminMenuSpec = CBModels::fetchSpecByID(CBAdminMenu::ID);
+        $adminMenuSpec = CBModels::fetchSpecByID(CBAdminMenu::ID());
 
         $adminMenuSpec->items[] = (object)[
             'className' => 'CBMenuItem',
