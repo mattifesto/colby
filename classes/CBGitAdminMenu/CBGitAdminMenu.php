ba8c9370691b4d6712eafce96902fad4487b7cb3
Matt Calkins (MacBook Air) <matt@mattifesto.com>
2018-03-05 19:34:19 -0800

    Add CBGitAdminMenu

---
+++ classes/CBGitAdminMenu/CBGitAdminMenu.php
@@ -0,0 +1,46 @@
+<?php
+
+final class CBGitAdminMenu {
+
+    /**
+     * @return void
+     */
+    static function CBInstall_install(): void {
+        $developAdminMenuSpec = CBModels::fetchSpecByID(CBDevelopAdminMenu::ID());
+
+        $developAdminMenuSpec->items[] = (object)[
+            'className' => 'CBMenuItem',
+            'name' => 'git',
+            'submenuID' => CBGitAdminMenu::ID(),
+            'text' => 'Git',
+            'URL' => '/admin/?c=CBGitAdmin',
+        ];
+
+        $spec = (object)[
+            'className' => 'CBMenu',
+            'ID' => CBGitAdminMenu::ID(),
+            'title' => 'Git',
+            'titleURI' => '/admin/?c=CBGitAdmin',
+        ];
+
+        CBDB::transaction(function () use ($developAdminMenuSpec, $spec) {
+            CBModels::save($developAdminMenuSpec);
+            CBModels::deleteByID(CBGitAdminMenu::ID());
+            CBModels::save($spec);
+        });
+    }
+
+    /**
+     * @return [string]
+     */
+    static function CBInstall_requiredClassNames(): array {
+        return ['CBDevelopAdminMenu'];
+    }
+
+    /**
+     * @return hex160
+     */
+    static function ID(): string {
+        return '72ca15bae2ff97a62e841ad72233aa5ffdab4bec';
+    }
+}
