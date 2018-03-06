6bc38d4dc9616dbdf36f4453f18978469a3da4a2
Matt Calkins (MacBook Air) <matt@mattifesto.com>
2018-03-06 13:01:46 -0800

    Change CBUIExpander padding

--- classes/CBUIExpander/CBUIExpander.php
+++ classes/CBUIExpander/CBUIExpander.php
@@ -6,14 +6,14 @@ final class CBUIExpander {
      * @return [string]
      */
     static function CBHTMLOutput_CSSURLs() {
-        return [Colby::flexpath(__CLASS__, 'v374.css', cbsysurl())];
+        return [Colby::flexpath(__CLASS__, 'v397.css', cbsysurl())];
     }
 
     /**
      * @return [string]
      */
     static function CBHTMLOutput_JavaScriptURLs() {
-        return [Colby::flexpath(__CLASS__, 'v393.js', cbsysurl())];
+        return [Colby::flexpath(__CLASS__, 'v397.js', cbsysurl())];
     }
 
     /**
