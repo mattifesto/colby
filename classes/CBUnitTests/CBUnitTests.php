206879c26505c51e5e8b584bfcbffa43a66a3f52
Matt Calkins (MacBook Air) <matt@mattifesto.com>
2018-03-27 11:22:09 -0700

    Remove test entries for CBPageVerificationTask from CBUnitTests.php

--- classes/CBUnitTests/CBUnitTests.php
+++ classes/CBUnitTests/CBUnitTests.php
@@ -95,8 +95,6 @@ class CBUnitTests {
             ['CBView',                  'render'],
             ['CBView',                  'toSubviews'],
             ['CBViewPage',              'save'],
-            ['CBPageVerificationTask',  'importThumbnailURLToImage'],
-            ['CBPageVerificationTask',  'upgradeThumbnailURLToImage'],
             ['Colby',                   'encryption'],
             ['ColbyMarkaroundParser',   'orderedList'],
             ['ColbyMarkaroundParser',   'paragraph'],
