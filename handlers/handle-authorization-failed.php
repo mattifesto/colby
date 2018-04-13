e2d445b9e8f8e08583d90bb478b366dad1d7147f
Matt Calkins (MacBook Air) <matt@mattifesto.com>
2018-04-13 12:15:41 -0700

    Update handlers/handle-authorization-failed.php
    - Use CBModelTemplateCatalog::fetchLivePageTemplate()
    - Use CBPage::renderSpec()
    - Use CBFacebookSignInView

--- handlers/handle-authorization-failed.php
+++ handlers/handle-authorization-failed.php
@@ -1,67 +1,31 @@
 <?php
 
+$spec = CBModelTemplateCatalog::fetchLivePageTemplate();
 $currentUserIsLoggedIn = ColbyUser::current()->isLoggedIn();
-$title = $currentUserIsLoggedIn ? 'Authorization Failed' : 'Please Log In';
+$spec->title = $currentUserIsLoggedIn ? 'Authorization Failed' : 'Please Log In';
+$spec->description = 'You are not authorized to view this page.';
 
-CBHTMLOutput::begin();
-CBHTMLOutput::pageInformation()->classNameForPageSettings = 'CBPageSettingsForAdminPages';
-CBHTMLOutput::pageInformation()->title = $title;
-CBHTMLOutput::pageInformation()->description = 'You are not authorized to view this page.';
+if ($currentUserIsLoggedIn) {
+    $message = <<<EOT
 
-?>
+        --- p center
+        You are not authorized to view this page.
+        ---
 
-<main style="flex: 1 0 auto">
-    <style>
+EOT;
 
-        main {
-            padding-top:  100px;
-            text-align: center;
-        }
+    $spec->sections = [
+        (object)[
+            'className' => 'CBMessageView',
+            'markup' => $message,
+        ],
+    ];
+} else {
+    $spec->sections = [
+        (object)[
+            'className' => 'CBFacebookSignInView',
+        ],
+    ];
+}
 
-        a.CBLogInLink {
-            background-color:   hsl(210, 100%, 70%);
-            box-shadow:
-                0 0 0 1px white,
-                0 0 0 5px hsl(210, 100%, 90%);
-            color:              hsla(0, 0%, 20%, 0.5);
-            display:            inline-block;
-            font-size:          20px;
-            font-weight:        bold;
-            letter-spacing:     1px;
-            margin-top:         50px;
-            padding:            15px 50px;
-            text-decoration:    none;
-            transition:         box-shadow 0.15s;
-        }
-
-        a.CBLogInLink:hover {
-            box-shadow:
-                0 0 0 2px white,
-                0 0 0 6px hsl(210, 100%, 70%);
-        }
-
-    </style>
-
-    <?php
-
-    if ($currentUserIsLoggedIn) {
-        ?>
-
-        <p>You are not authorized to view this page.
-
-        <?php
-    } else {
-        ColbyUser::renderSignInWithFacebookButton();
-    }
-
-    ?>
-
-</main>
-
-<?php
-
-CBView::render((object)[
-    'className' => 'CBAdminPageFooterView',
-]);
-
-CBHTMLOutput::render();
+CBPage::renderSpec($spec);
