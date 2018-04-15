f137ef39bb370df1e824b74a6b90d2d6fa837c04
Matt Calkins (MacBook Air) <matt@mattifesto.com>
2018-04-15 11:13:49 -0700

    Remove CBPageSettings_defaultClassName class install from setup

--- setup/index.php
+++ setup/index.php
@@ -223,7 +223,6 @@ class ColbyInstaller {
             /* class files */
 
             $classfiles = [
-                ['CBPageSettings_defaultClassName', 'php'],
                 ['CBXPageFrame', 'php'],
                 ['CBXPageFrame', 'css'],
                 ['CBXPageSettings', 'php'],
