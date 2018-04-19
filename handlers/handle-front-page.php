a19f74b468f1ecb16aa94137ad6295ae404a1b72
Matt Calkins (MacBook Air) <matt@mattifesto.com>
2018-04-18 18:02:11 -0700

    Update handlers/handle-front-page.php
    - Use fetchLivePageTemplate() on CBModelTemplateCatalog
    - Use CBMessageView

--- handlers/handle-front-page.php
+++ handlers/handle-front-page.php
@@ -1,29 +1,29 @@
 <?php
 
-$content = <<<EOT
+$message = <<<EOT
 
-This is the default front page. To replace it create the file
+        --- center
+        This is the default front page. To replace it create the file:
 
-`handlers/handle-front-page.php`
+        (handlers/handle-front-page.php (code))
 
-in the main website directory. Alternatively, create a page in the admin area
-and set it as the front page. A front page created in the admin area has higher
-priority than a handler.
+        in the main website directory. Alternatively, create a page in the admin
+        area and set it as the front page. A front page created in the admin
+        area has higher priority than a handler.
+        ---
 
 EOT;
 
-CBPage::renderSpec((object)[
-    'className' => 'CBViewPage',
+$spec = CBModelTemplateCatalog::fetchLivePageTemplate();
+
+CBModel::merge($spec, (object)[
     'title' => 'Default Front Page',
-    'layout' => (object)[
-        'className' => 'CBPageLayout',
-        'CSSClassNames' => 'center',
-    ],
     'sections' => [
         (object)[
-            'className' => 'CBTextView2',
-            'contentAsCommonMark' => $content,
-            'CSSClassNames' => 'center',
+            'className' => 'CBMessageView',
+            'markup' => $message,
         ],
     ],
 ]);
+
+CBPage::renderSpec($spec);
