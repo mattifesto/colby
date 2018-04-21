b5302988220e6885c37d820a28284c6a45db423e
Matt Calkins (MacBook Air) <matt@mattifesto.com>
2018-04-21 14:59:47 -0700

    Add setup/templates/BlogPostPageTemplate.php

---
+++ setup/templates/BlogPostPageTemplate.php
@@ -0,0 +1,49 @@
+<?php
+
+final class PREFIXBlogPostPageTemplate {
+
+    /**
+     * @return void
+     */
+    static function CBInstall_install(): void {
+        CBModelTemplateCatalog::install(__CLASS__);
+    }
+
+    /**
+     * @return [string]
+     */
+    static function CBInstall_requiredClassNames(): array {
+        return ['CBModelTemplateCatalog'];
+    }
+
+    /**
+     * @return model
+     */
+    static function CBModelTemplate_spec(): stdClass {
+        return (object)[
+            'className' => 'CBViewPage',
+            'classNameForKind' => 'PREFIXBlogPostPageKind',
+            'classNameForSettings' => 'PREFIXPageSettings',
+            'frameClassName' => 'PREFIXPageFrame',
+            'sections' => [
+                (object)[
+                    'className' => 'CBPageTitleAndDescriptionView',
+                    'showPublicationDate' => true,
+                ],
+                (object)[
+                    'className' => 'CBArtworkView',
+                ],
+                (object)[
+                    'className' => 'CBMessageView',
+                ],
+            ],
+        ];
+    }
+
+    /**
+     * @return string
+     */
+    static function CBModelTemplate_title(): string {
+        return 'Blog Post';
+    }
+}
