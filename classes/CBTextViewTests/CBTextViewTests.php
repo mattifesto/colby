cf48aa9e8458fa6081c6f8c0548216835f863948
Matt Calkins (MacBook Air) <matt@mattifesto.com>
2018-06-02 12:06:47 -0700

    Use CBLog buffer in CBTextViewTests.php

--- classes/CBTextViewTests/CBTextViewTests.php
+++ classes/CBTextViewTests/CBTextViewTests.php
@@ -34,7 +34,46 @@ EOT;
             'markup' => CBMessageMarkup::stringToMarkup($text),
         ];
 
+        CBLog::bufferStart();
+
         $result = CBModel::upgrade($original);
+
+        $buffer = CBLog::bufferContents();
+
+        CBLog::bufferEndClean();
+
+        $bufferIsValid = function ($buffer): bool {
+            if (count($buffer) !== 1) {
+                return false;
+            }
+
+            $sourceID = CBModel::valueAsID($buffer[0], 'sourceID');
+
+            if ($sourceID !== '00754cfbd72404dc44861189397751acabdc4ce7') {
+                return false;
+            }
+
+            return true;
+        };
+
+        if (!$bufferIsValid($buffer)) {
+            $bufferAsMessage = CBMessageMarkup::stringToMessage(
+                CBConvert::valueToPrettyJSON($buffer)
+            );
+
+            $message = <<<EOT
+
+                The log entry buffer is not what was expected:
+
+                --- pre\n{$bufferAsMessage}
+                ---
+
+EOT;
+
+            return (object)[
+                'message' => $message,
+            ];
+        }
 
         if ($result != $expected) {
             $message = CBConvertTests::resultAndExpectedToMessage($result, $expected);
