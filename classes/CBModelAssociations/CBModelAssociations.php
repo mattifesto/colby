af233368bcb0f8d701cfab875e7f98e7f1e815e5
Matt Calkins (MacBook Air) <matt@mattifesto.com>
2018-06-12 14:40:20 -0700

    Garden CBModelAssociations.php

--- classes/CBModelAssociations/CBModelAssociations.php
+++ classes/CBModelAssociations/CBModelAssociations.php
@@ -3,6 +3,28 @@
 final class CBModelAssociations {
 
     /**
+     * @return void
+     */
+    static function CBInstall_install(): void {
+        $SQL = <<<EOT
+
+            CREATE TABLE IF NOT EXISTS `CBModelAssociations` (
+                `ID` BINARY(20) NOT NULL,
+                `className` VARCHAR(80) NOT NULL,
+                `associatedID` BINARY(20) NOT NULL,
+
+                PRIMARY KEY (`ID`, `className`, `associatedID`)
+            )
+            ENGINE=InnoDB
+            DEFAULT CHARSET=utf8mb4
+            COLLATE=utf8mb4_unicode_520_ci
+
+EOT;
+
+        Colby::query($SQL);
+    }
+
+    /**
      * This function should be called inside a transaction.
      *
      * @param hex160 $ID
@@ -231,28 +253,6 @@ EOT;
      */
     static function makeSpecForAjaxPermissions() {
         return (object)['group' => 'Public'];
-    }
-
-    /**
-     * @return void
-     */
-    static function CBInstall_install(): void {
-        $SQL = <<<EOT
-
-            CREATE TABLE IF NOT EXISTS `CBModelAssociations` (
-                `ID` BINARY(20) NOT NULL,
-                `className` VARCHAR(80) NOT NULL,
-                `associatedID` BINARY(20) NOT NULL,
-
-                PRIMARY KEY (`ID`, `className`, `associatedID`)
-            )
-            ENGINE=InnoDB
-            DEFAULT CHARSET=utf8mb4
-            COLLATE=utf8mb4_unicode_520_ci
-
-EOT;
-
-        Colby::query($SQL);
     }
 
     /**
