23734615dc48d9ce0ccf21e2ab45614c176e640b
Matt Calkins (MacBook Air) <matt@mattifesto.com>
2018-06-12 15:00:32 -0700

    Garden CBModelAssociations.php

--- classes/CBModelAssociations/CBModelAssociations.php
+++ classes/CBModelAssociations/CBModelAssociations.php
@@ -3,28 +3,6 @@
 final class CBModelAssociations {
 
     /**
-     * @return void
-     */
-    static function CBInstall_install(): void {
-        $SQL = <<<EOT
-
-            CREATE TABLE IF NOT EXISTS `CBModelAssociations` (
-                `ID` BINARY(20) NOT NULL,
-                `className` VARCHAR(80) NOT NULL,
-                `associatedID` BINARY(20) NOT NULL,
-
-                PRIMARY KEY (`ID`, `className`, `associatedID`)
-            )
-            ENGINE=InnoDB
-            DEFAULT CHARSET=utf8mb4
-            COLLATE=utf8mb4_unicode_520_ci
-
-EOT;
-
-        Colby::query($SQL);
-    }
-
-    /**
      * @param ID $ID
      * @param string $className
      * @param ID $associatedID
@@ -47,6 +25,28 @@ EOT;
             )
             ON DUPLICATE KEY UPDATE
                 associatedID = associatedID
+
+EOT;
+
+        Colby::query($SQL);
+    }
+
+    /**
+     * @return void
+     */
+    static function CBInstall_install(): void {
+        $SQL = <<<EOT
+
+            CREATE TABLE IF NOT EXISTS `CBModelAssociations` (
+                `ID` BINARY(20) NOT NULL,
+                `className` VARCHAR(80) NOT NULL,
+                `associatedID` BINARY(20) NOT NULL,
+
+                PRIMARY KEY (`ID`, `className`, `associatedID`)
+            )
+            ENGINE=InnoDB
+            DEFAULT CHARSET=utf8mb4
+            COLLATE=utf8mb4_unicode_520_ci
 
 EOT;
 
