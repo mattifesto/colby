781b6f08775ede5bf4537470bb5727cfc698e7fc
Matt Calkins (MacBook Air) <matt@mattifesto.com>
2018-06-17 16:07:00 -0700

    Add CBAdministratorUserSettingsManager

---
+++ classes/CBAdministratorUserSettingsManager/CBAdministratorUserSettingsManager.php
@@ -0,0 +1,44 @@
+<?php
+
+final class CBAdministratorUserSettingsManager {
+
+    /**
+     * @return void
+     */
+    static function CBInstall_install(): void {
+        CBUserSettingsManagerCatalog::installUserSettingsManager(__CLASS__, 10);
+    }
+
+    /**
+     * @return [string]
+     */
+    static function CBInstall_requiredClassNames(): array {
+        return ['CBUserSettingsManagerCatalog'];
+    }
+
+    /**
+     * @param ID $targetUserID
+     *
+     * @return void
+     */
+    static function CBUserSettingsManager_render(string $targetUserID): void {
+        $targetUserModel = CBModels::fetchModelByID($targetUserID);
+        $targetUserNumber = CBModel::valueAsInt($targetUserModel, 'userID');
+
+        if (empty($targetUserNumber)) {
+            return;
+        }
+
+        $targetUserData = (object)[
+            'id' => $targetUserNumber,
+        ];
+
+        if (ColbyUser::currentUserIsMemberOfGroup('Administrators')) {
+            CBGroupUserSettings::renderUserSettings($targetUserData, 'Administrators');
+        }
+
+        if (ColbyUser::currentUserIsMemberOfGroup('Developers')) {
+            CBGroupUserSettings::renderUserSettings($targetUserData, 'Developers');
+        }
+    }
+}
