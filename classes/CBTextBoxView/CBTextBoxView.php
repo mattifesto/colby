4f23dc87bc35bedb22cff2bce77374b37f3f5056
Matt Calkins (MacBook Air) <matt@mattifesto.com>
2018-06-26 10:13:44 -0700

    Install CBTextBoxView

--- classes/CBTextBoxView/CBTextBoxView.php
+++ classes/CBTextBoxView/CBTextBoxView.php
@@ -9,6 +9,20 @@
 final class CBTextBoxView {
 
     /**
+     * @return void
+     */
+    static function CBInstall_install(): void {
+        CBViewCatalog::installView(__CLASS__, (object)['isUnsupported' => true]);
+    }
+
+    /**
+     * @return [string]
+     */
+    static function CBInstall_requiredClassNames(): array {
+        return ['CBViewCatalog'];
+    }
+
+    /**
      * @param model $spec
      *
      * @return model
@@ -54,29 +68,5 @@ EOT;
         unset($spec->titleAsMarkaround);
 
         return $spec;
-    }
-
-    /**
-     * @deprecated 2018.04.01
-     *
-     *      This function just deletes a page that used to be a test page for
-     *      this class. Once it has run once on each site install for this class
-     *      can be removed.
-     *
-     * @return void
-     */
-    static function CBInstall_install(): void {
-        $pageID = 'cb2679b7268a68cfcb3fa42e4feeabb456e719ed';
-
-        CBDB::transaction(function () use ($pageID) {
-            CBModels::deleteByID($pageID);
-        });
-    }
-
-    /**
-     * @return [string]
-     */
-    static function CBInstall_requiredClassNames(): array {
-        return ['CBModels', 'CBTasks2', 'CBLog', 'CBPages', 'CBViewPage'];
     }
 }
