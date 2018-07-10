9977501bf46af0c40c9c6d71565726928c590a38
Matt Calkins (MacBook Air) <matt@mattifesto.com>
2018-07-09 19:51:47 -0700

    Add the "contain" and "large" optional classes to CBUIThumbnailPart

--- classes/CBUIThumbnailPart/CBUIThumbnailPart.php
+++ classes/CBUIThumbnailPart/CBUIThumbnailPart.php
@@ -6,7 +6,7 @@ final class CBUIThumbnailPart {
      * @return [string]
      */
     static function CBHTMLOutput_CSSURLs() {
-        return [Colby::flexpath(__CLASS__, 'v361.css', cbsysurl())];
+        return [Colby::flexpath(__CLASS__, 'v436.css', cbsysurl())];
     }
 
     /**
