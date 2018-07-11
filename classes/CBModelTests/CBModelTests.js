0772236037dbbe47e7c51275a18a1e22c83552b6
Matt Calkins (MacBook Air) <matt@mattifesto.com>
2018-07-11 09:15:39 -0700

    Add nonequal subtests to CBTest_equals() in CBModelTests.js

--- classes/CBModelTests/CBModelTests.js
+++ classes/CBModelTests/CBModelTests.js
@@ -57,7 +57,72 @@ var CBModelTests = {
                 );
                 let message = `
 
-                    CBModel.equal() was expected to consider the following values were expected to be equal.
+                    CBModel.equal() was expected to consider the following
+                    values to be equal.
+
+                    --- pre\n${valuesAsMessage}
+                    ---
+
+                `;
+
+                return {
+                    message: message,
+                };
+            }
+        }
+
+        let nonequalValues = [
+            [false, true],
+            [false, []],
+            [0, 1],
+            [0, null],
+            ["", "hi"],
+            ["", false],
+            [undefined, null],
+            [undefined, {a:1}],
+            [null, 1],
+            [[1,2,3], [1,2,4]],
+            [{}, []],
+            [{a:1,b:2}, {a:1,b:3}],
+            [[], {}],
+            [42.42, 42.43],
+            [
+                {
+                    foo: "bar",
+                    num: 1,
+                    so: true,
+                    non: null,
+                    obj: {
+                        name: "bob",
+                        quan: 5, /* different */
+                    },
+                    arr: [5, 4, "3"],
+                },
+                {
+                    num: 1,
+                    non: null,
+                    arr: [5, 4, "3"],
+                    foo: "bar",
+                    so: true,
+                    obj: {
+                        quan: 6, /* different */
+                        name: "bob",
+                    },
+                }
+            ],
+        ];
+
+        for (let i = 0; i < nonequalValues.length; i += 1) {
+            let values = nonequalValues[i];
+
+            if (CBModel.equals(values[0], values[1])) {
+                let valuesAsMessage = CBMessageMarkup.stringToMessage(
+                    JSON.stringify(values)
+                );
+                let message = `
+
+                    CBModel.equal() was expected to consider the following
+                    values to be not equal.
 
                     --- pre\n${valuesAsMessage}
                     ---
