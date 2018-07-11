fcccc1e01a7a58d1d4a2acdc4c74af29f527f76a
Matt Calkins (MacBook Air) <matt@mattifesto.com>
2018-07-11 07:59:31 -0700

    Add equals() in CBModel.js

--- classes/CBModel/CBModel.js
+++ classes/CBModel/CBModel.js
@@ -9,6 +9,66 @@
 var CBModel = {
 
     /**
+     * This function determines if two models are effectively the same. This
+     * function enforces the fact that the objects are either models or types
+     * that are allowed for model properties. For instance, if the type of a
+     * value is "function", false will be returned even if the functions are
+     * equal because functions are not allowed property types of models.
+     *
+     * This function currently does allow loose equality.
+     *
+     * @param mixed value1
+     * @param mixed value2
+     *
+     * @return bool
+     */
+    equals: function (value1, value2) {
+        let type = typeof value1;
+
+        if (type !== typeof value2) {
+            return false;
+        }
+
+        if (Array.isArray(value1) && Array.isArray(value2)) {
+            if (value1.length !== value2.length) {
+                return false;
+            }
+
+            return value1.every(function (value, index) {
+                return CBModel.equals(value, value2[index]);
+            });
+        }
+
+        if (type === "object") {
+            if (value1 === null && value2 === null) {
+                return true;
+            }
+
+            if (value1 === null || value2 === null) {
+                return false;
+            }
+
+            let keys1 = Object.keys(value1);
+            let keys2 = Object.keys(value2);
+
+            if (keys1.length !== keys2.length) {
+                return false;
+            }
+        }
+
+        switch (type) {
+            case "boolean":
+            case "number":
+            case "string":
+            case "undefined":
+                return value1 === value2;
+
+            default:
+                return false;
+        }
+    },
+
+    /**
      * @param mixed model
      * @param string keyPath
      *
