57936d46da52ccf1739d2f0c1ba4256a15627983
Matt Calkins (MacBook Air) <matt@mattifesto.com>
2018-07-18 16:14:56 -0700

    Remove outdated polyfills, update and move classList polyfill in CBEqualizePageSettingsPart

--- classes/CBEqualizePageSettingsPart/CBEqualizePageSettingsPart.php
+++ classes/CBEqualizePageSettingsPart/CBEqualizePageSettingsPart.php
@@ -3,11 +3,16 @@
 final class CBEqualizePageSettingsPart {
 
     /**
+     * html5shiv.js -> IE 11
+     * es6-promise.auto.min.js -> IE 11
+     * classList.min.js -> IE 11
+     *
      * @return void
      */
     static function CBPageSettings_renderHeadElementHTML(): void { ?>
         <script src="<?= cbsysurl() . '/classes/CBEqualizePageSettingsPart/html5shiv.v362.js' ?>"></script>
         <script src="<?= cbsysurl() . '/classes/CBEqualizePageSettingsPart/es6-promise.auto.min.v362.js' ?>"></script>
+        <script src="<?= cbsysurl() . '/classes/CBEqualizePageSettingsPart/classList.min.v438.js' ?>"></script>
         <script src="<?= Colby::flexpath(__CLASS__, 'v362.js', cbsysurl()) ?>"></script>
         <link rel="stylesheet" href="<?= Colby::flexpath(__CLASS__, 'v414.css', cbsysurl()) ?>">
     <?php }
