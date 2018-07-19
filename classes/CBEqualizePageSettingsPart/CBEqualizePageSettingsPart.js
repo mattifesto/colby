8e2f6ed1fd80adaabcb23971b57938932336f0fc
Matt Calkins (MacBook Air) <matt@mattifesto.com>
2018-07-19 11:43:12 -0700

    Add Number.isInteger() polyfill in CBEqualizePageSettingsPart.js

--- classes/CBEqualizePageSettingsPart/CBEqualizePageSettingsPart.js
+++ classes/CBEqualizePageSettingsPart/CBEqualizePageSettingsPart.js
@@ -3,7 +3,18 @@
 
 /**
  * For IE 11
+ * From https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Number/isFinite
  */
 Number.isFinite = Number.isFinite || function(value) {
     return typeof value === 'number' && isFinite(value);
+};
+
+/**
+ * For IE 11
+ * From https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Number/isInteger
+ */
+Number.isInteger = Number.isInteger || function(value) {
+  return typeof value === 'number' &&
+    isFinite(value) &&
+    Math.floor(value) === value;
 };
--- classes/CBEqualizePageSettingsPart/CBEqualizePageSettingsPart.php
+++ classes/CBEqualizePageSettingsPart/CBEqualizePageSettingsPart.php
@@ -13,7 +13,7 @@ final class CBEqualizePageSettingsPart {
         <script src="<?= cbsysurl() . '/classes/CBEqualizePageSettingsPart/html5shiv.v362.js' ?>"></script>
         <script src="<?= cbsysurl() . '/classes/CBEqualizePageSettingsPart/es6-promise.auto.min.v362.js' ?>"></script>
         <script src="<?= cbsysurl() . '/classes/CBEqualizePageSettingsPart/classList.min.v438.js' ?>"></script>
-        <script src="<?= Colby::flexpath(__CLASS__, 'v362.js', cbsysurl()) ?>"></script>
+        <script src="<?= Colby::flexpath(__CLASS__, 'v438.js', cbsysurl()) ?>"></script>
         <link rel="stylesheet" href="<?= Colby::flexpath(__CLASS__, 'v414.css', cbsysurl()) ?>">
     <?php }
 }
