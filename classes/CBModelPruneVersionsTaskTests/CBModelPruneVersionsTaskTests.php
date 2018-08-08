eb93988ea2653d5632f69e673fbbfcf8526dc968
Matt Calkins (MacBook Air) <matt@mattifesto.com>
2018-08-08 11:23:14 -0700

    Add CBTest_assignActions(), remove calculateVersionsToPruneTest() in CBModelPruneVersionsTaskTests.php

--- classes/CBModelPruneVersionsTaskTests/CBModelPruneVersionsTaskTests.php
+++ classes/CBModelPruneVersionsTaskTests/CBModelPruneVersionsTaskTests.php
@@ -3,248 +3,224 @@
 final class CBModelPruneVersionsTaskTests {
 
     /**
-     * @return ?object
+     * @return object
      */
-    static function calculateVersionsToPruneTest(): ?stdClass {
-        $time       = time();
-        $now        = time();
-        $oneMinute  = 60;
+    static function CBTest_assignActions(): stdClass {
+        $now = time();
+        $one_second = 1;
+        $one_minute = 60 * $one_second;
+        $one_hour = 60 * $one_minute;
+        $one_day = 24 * $one_hour;
+        $one_week = 7 * $one_day;
 
-        /* test 1 */
+        $tests = [
 
-        $versions = [
-            (object)[
-                'timestamp' => $time,
-                'version' => 100,
+            /* 0 to 1 hour */
+
+            [
+                'deathspan: 5 seconds, lifespan: less than 10 seconds', /* name */
+                $one_second * 5, /* deathspan */
+                $one_second * 5, /* lifespan */
+                'prune', /* action */
+            ],
+            [
+                'deathspan: 5 seconds, lifespan: greater than 10 seconds', /* name */
+                $one_second * 5, /* deathspan */
+                $one_second * 15, /* lifespan */
+                'keep', /* action */
+            ],
+
+            [
+                'deathspan: 55 minutes, lifespan: less than 10 seconds', /* name */
+                $one_minute * 55, /* deathspan */
+                $one_second * 5, /* lifespan */
+                'prune', /* action */
+            ],
+            [
+                'deathspan: 55 minutes, lifespan: greater than 10 seconds', /* name */
+                $one_minute * 55, /* deathspan */
+                $one_second * 15, /* lifespan */
+                'keep', /* action */
+            ],
+
+            /* 1 hour to 1 day */
+
+            [
+                'deathspan: 65 minutes, lifespan: less than 1 minute', /* name */
+                $one_minute * 65, /* deathspan */
+                $one_second * 55, /* lifespan */
+                'prune', /* action */
+            ],
+            [
+                'deathspan: 65 minutes, lifespan: greater than 1 minute', /* name */
+                $one_minute * 65, /* deathspan */
+                $one_second * 65, /* lifespan */
+                'keep', /* action */
+            ],
+
+            [
+                'deathspan: 23 hours, lifespan: less than 1 minute', /* name */
+                $one_hour * 23, /* deathspan */
+                $one_second * 55, /* lifespan */
+                'prune', /* action */
+            ],
+            [
+                'deathspan: 23 hours, lifespan: greater than 1 minute', /* name */
+                $one_hour * 23, /* deathspan */
+                $one_second * 65, /* lifespan */
+                'keep', /* action */
+            ],
+
+            /* 1 day to 1 week */
+
+            [
+                'deathspan: 25 hours, lifespan: less than 10 minutes', /* name */
+                $one_hour * 25, /* deathspan */
+                $one_minute * 9, /* lifespan */
+                'prune', /* action */
+            ],
+            [
+                'deathspan: 25 hours, lifespan: greater than 10 minutes', /* name */
+                $one_hour * 25, /* deathspan */
+                $one_minute * 11, /* lifespan */
+                'keep', /* action */
+            ],
+
+            [
+                'deathspan: 6 days, lifespan: less than 10 minutes', /* name */
+                $one_day * 6, /* deathspan */
+                $one_minute * 9, /* lifespan */
+                'prune', /* action */
+            ],
+            [
+                'deathspan: 6 days, lifespan: greater than 10 minutes', /* name */
+                $one_day * 6, /* deathspan */
+                $one_minute * 11, /* lifespan */
+                'keep', /* action */
+            ],
+
+            /* 1 week to 30 days */
+
+            [
+                'deathspan: 8 days, lifespan: less than 1 hour', /* name */
+                $one_day * 8, /* deathspan */
+                $one_minute * 55, /* lifespan */
+                'prune', /* action */
+            ],
+            [
+                'deathspan: 8 days, lifespan: greater than 1 hour', /* name */
+                $one_day * 8, /* deathspan */
+                $one_minute * 65, /* lifespan */
+                'keep', /* action */
+            ],
+
+            [
+                'deathspan: 29 days, lifespan: less than 1 hour', /* name */
+                $one_day * 29, /* deathspan */
+                $one_minute * 55, /* lifespan */
+                'prune', /* action */
+            ],
+            [
+                'deathspan: 20 days, lifespan: greater than 1 hour', /* name */
+                $one_day * 29, /* deathspan */
+                $one_minute * 65, /* lifespan */
+                'keep', /* action */
+            ],
+
+            /* 30 days to 90 days */
+
+            [
+                'deathspan: 31 days, lifespan: less than 10 hours', /* name */
+                $one_day * 31, /* deathspan */
+                $one_hour * 9, /* lifespan */
+                'prune', /* action */
+            ],
+            [
+                'deathspan: 31 days, lifespan: greater than 10 hours', /* name */
+                $one_day * 31, /* deathspan */
+                $one_hour * 11, /* lifespan */
+                'keep', /* action */
+            ],
+
+            [
+                'deathspan: 89 days, lifespan: less than 10 hours', /* name */
+                $one_day * 89, /* deathspan */
+                $one_hour * 9, /* lifespan */
+                'prune', /* action */
+            ],
+            [
+                'deathspan: 89 days, lifespan: greater than 10 hours', /* name */
+                $one_day * 89, /* deathspan */
+                $one_hour * 11, /* lifespan */
+                'keep', /* action */
+            ],
+
+            /* greater than 90 days */
+
+            [
+                'deathspan: 91 days, lifespan: greater than 10 hours', /* name */
+                $one_day * 91, /* deathspan */
+                $one_hour * 11, /* lifespan */
+                'prune', /* action */
+            ],
+            [
+                'deathspan: 365 days, lifespan: 365 days', /* name */
+                $one_day * 365, /* deathspan */
+                $one_day * 365, /* lifespan */
+                'prune', /* action */
+            ],
+            [
+                'deathspan: 100,000 days, lifespan: 100,000 days', /* name */
+                $one_day * 100000, /* deathspan */
+                $one_day * 100000, /* lifespan */
+                'prune', /* action */
             ],
         ];
 
-        $result = CBModelPruneVersionsTask::calculateVersionsToPrune($versions);
-        $expected = [];
+        foreach ($tests as $test) {
+            $name = $test[0];
+            $deathspan = $test[1];
+            $lifespan = $test[2];
+            $action = $test[3];
 
-        if ($result != $expected) {
-            return (object)[
-                'failed' => true,
-                'message' =>
-                    "Test 1 failed:\n\n" .
-                    CBConvertTests::resultAndExpectedToMessage($result, $expected),
+            $replaced = $now - $deathspan;
+            $timestamp = $replaced - $lifespan;
+
+            $versions = [
+                (object)[
+                    'version' => 100,
+                    'timestamp' => $now,
+                ],
+                (object)[
+                    'version' => 50,
+                    'timestamp' => $timestamp,
+                    'replaced' => $replaced,
+                ],
             ];
+            $expected = [
+                (object)[
+                    'version' => 100,
+                    'timestamp' => $now,
+                    'action' => 'keep',
+                ],
+                (object)[
+                    'version' => 50,
+                    'timestamp' => $timestamp,
+                    'replaced' => $replaced,
+                    'action' => $action,
+                ],
+            ];
+
+            CBModelPruneVersionsTask::assignActions($versions);
+
+            if ($versions != $expected) {
+                return CBTest::resultMismatchFailure($name, $versions, $expected);
+            }
         }
 
-        /* test 2 */
-
-        $versions = [
-            (object)[
-                'timestamp' => $time,
-                'version' => 100,
-            ],
-            (object)[
-                'timestamp' => $time - 10,
-                'version' => 90,
-            ],
-            (object)[
-                'timestamp' => $time - (60 * 60 * 24 * 100),
-                'version' => 80,
-            ],
+        return (object)[
+            'succeeded' => true,
         ];
-
-        $result = CBModelPruneVersionsTask::calculateVersionsToPrune($versions);
-        $expected = [];
-
-        if ($result != $expected) {
-            return (object)[
-                'failed' => true,
-                'message' =>
-                    "Test 2 failed:\n\n" .
-                    CBConvertTests::resultAndExpectedToMessage($result, $expected),
-            ];
-        }
-
-        /* test 3 */
-
-        $versions = [
-            (object)[
-                'timestamp' => $time,
-                'version' => 100,
-            ],
-            (object)[
-                'timestamp' => $time - 10,
-                'version' => 90,
-            ],
-            (object)[
-                'timestamp' => $time - 20,
-                'version' => 85,
-            ],
-            (object)[
-                'timestamp' => $time - (60 * 60 * 24 * 100),
-                'version' => 80,
-            ],
-        ];
-
-        $result = CBModelPruneVersionsTask::calculateVersionsToPrune($versions);
-        $expected = [90];
-
-        if ($result != $expected) {
-            return (object)[
-                'failed' => true,
-                'message' =>
-                    "Test 3 failed:\n\n" .
-                    CBConvertTests::resultAndExpectedToMessage($result, $expected),
-            ];
-        }
-
-        /* test 4 */
-
-        $versions = [
-            (object)[
-                'timestamp' => $time,
-                'version' => 100,
-            ],
-            (object)[
-                'timestamp' => $time - (60 * 60 * 24 * 100),
-                'version' => 80,
-            ],
-            (object)[
-                'timestamp' => $time - (60 * 60 * 24 * 110),
-                'version' => 70,
-            ],
-            (object)[
-                'timestamp' => $time - (60 * 60 * 24 * 120),
-                'version' => 60,
-            ],
-            (object)[
-                'timestamp' => $time - (60 * 60 * 24 * 130),
-                'version' => 50,
-            ],
-        ];
-
-        $result = CBModelPruneVersionsTask::calculateVersionsToPrune($versions);
-        $expected = [80, 70];
-
-        if ($result != $expected) {
-            return (object)[
-                'failed' => true,
-                'message' =>
-                    "Test 4 failed:\n\n" .
-                    CBConvertTests::resultAndExpectedToMessage($result, $expected),
-            ];
-        }
-
-        /* test 5 */
-
-        $versions = [
-            (object)[
-                'timestamp' => $time,
-                'version' => 100,
-            ],
-            (object)[
-                'timestamp' => $time - 10,
-                'version' => 90,
-            ],
-            (object)[
-                'timestamp' => $time - 20,
-                'version' => 85,
-            ],
-            (object)[
-                'timestamp' => $time - 30,
-                'version' => 84,
-            ],
-            (object)[
-                'timestamp' => $time - 40,
-                'version' => 83,
-            ],
-            (object)[
-                'timestamp' => $time - (60 * 60 * 24 * 29),
-                'version' => 82,
-            ],
-            (object)[
-                'timestamp' => $time - (60 * 60 * 24 * 30),
-                'version' => 81,
-            ],
-            (object)[
-                'timestamp' => $time - (60 * 60 * 24 * 100),
-                'version' => 80,
-            ],
-            (object)[
-                'timestamp' => $time - (60 * 60 * 24 * 110),
-                'version' => 70,
-            ],
-            (object)[
-                'timestamp' => $time - (60 * 60 * 24 * 120),
-                'version' => 60,
-            ],
-        ];
-
-        $result = CBModelPruneVersionsTask::calculateVersionsToPrune($versions);
-        $expected = [90, 85, 84, 83, 80, 70, 60];
-
-        if ($result != $expected) {
-            return (object)[
-                'failed' => true,
-                'message' =>
-                    "Test 5 failed:\n\n" .
-                    CBConvertTests::resultAndExpectedToMessage($result, $expected),
-            ];
-        }
-
-        /* test 6 */
-
-        $versions = [
-            (object)[
-                'timestamp' => $now,
-                'version' => 200,
-            ],
-            (object)[
-                'timestamp' => $now - (3 * $oneMinute),
-                'version' => 190,
-            ],
-            (object)[
-                'timestamp' => $now - (6 * $oneMinute),
-                'version' => 180,
-            ],
-            (object)[
-                'timestamp' => $now - (9 * $oneMinute),
-                'version' => 170,
-            ],
-            (object)[
-                'timestamp' => $now - (12 * $oneMinute),
-                'version' => 160,
-            ],
-            (object)[
-                'timestamp' => $now - (15 * $oneMinute),
-                'version' => 150,
-            ],
-            (object)[
-                'timestamp' => $now - (18 * $oneMinute),
-                'version' => 140,
-            ],
-            (object)[
-                'timestamp' => $now - (21 * $oneMinute),
-                'version' => 130,
-            ],
-            (object)[
-                'timestamp' => $now - (24 * $oneMinute),
-                'version' => 120,
-            ],
-            (object)[
-                'timestamp' => $now - (27 * $oneMinute),
-                'version' => 110,
-            ],
-        ];
-
-        $result = CBModelPruneVersionsTask::calculateVersionsToPrune($versions);
-        $expected = [190, 180, 170, 150, 140, 130, 110];
-
-        if ($result != $expected) {
-            return (object)[
-                'failed' => true,
-                'message' =>
-                    "Test 6 failed:\n\n" .
-                    CBConvertTests::resultAndExpectedToMessage($result, $expected),
-            ];
-        }
-
-        return null;
     }
 
     /**
@@ -252,7 +228,7 @@ final class CBModelPruneVersionsTaskTests {
      */
     static function CBUnitTests_tests(): array {
         return [
-            ['CBModelPruneVersionsTask', 'calculateVersionsToPrune'],
+            ['CBModelPruneVersionsTask', 'assignActions'],
         ];
     }
 }
