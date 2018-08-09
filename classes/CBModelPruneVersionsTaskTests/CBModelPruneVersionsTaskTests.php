14db715aae1e5edafb17d0854a326ea051e958fb
Matt Calkins (MacBook Air) <matt@mattifesto.com>
2018-08-09 11:31:40 -0700

    Update time spans in CBTest_assignActions() in CBModelPruneVersionsTaskTests.php

--- classes/CBModelPruneVersionsTaskTests/CBModelPruneVersionsTaskTests.php
+++ classes/CBModelPruneVersionsTaskTests/CBModelPruneVersionsTaskTests.php
@@ -7,172 +7,139 @@ final class CBModelPruneVersionsTaskTests {
      */
     static function CBTest_assignActions(): stdClass {
         $now = time();
-        $one_second = 1;
-        $one_minute = 60 * $one_second;
-        $one_hour = 60 * $one_minute;
-        $one_day = 24 * $one_hour;
-        $one_week = 7 * $one_day;
 
         $tests = [
 
-            /* 0 to 1 hour */
+            /* 0 to 1,000 */
 
             [
-                'deathspan: 5 seconds, lifespan: less than 10 seconds', /* name */
-                $one_second * 5, /* deathspan */
-                $one_second * 5, /* lifespan */
+                'deathspan: greater than 0, lifespan: less than 10', /* name */
+                5, /* deathspan */
+                5, /* lifespan */
                 'prune', /* action */
             ],
             [
-                'deathspan: 5 seconds, lifespan: greater than 10 seconds', /* name */
-                $one_second * 5, /* deathspan */
-                $one_second * 15, /* lifespan */
+                'deathspan: greater than 0, lifespan: greater than 10', /* name */
+                 5, /* deathspan */
+                15, /* lifespan */
                 'keep', /* action */
             ],
 
             [
-                'deathspan: 55 minutes, lifespan: less than 10 seconds', /* name */
-                $one_minute * 55, /* deathspan */
-                $one_second * 5, /* lifespan */
+                'deathspan: less than 1,000, lifespan: less than 10', /* name */
+                900, /* deathspan */
+                  5, /* lifespan */
                 'prune', /* action */
             ],
             [
-                'deathspan: 55 minutes, lifespan: greater than 10 seconds', /* name */
-                $one_minute * 55, /* deathspan */
-                $one_second * 15, /* lifespan */
+                'deathspan: less than 1,000, lifespan: greater than 10', /* name */
+                900, /* deathspan */
+                 15, /* lifespan */
                 'keep', /* action */
             ],
 
-            /* 1 hour to 1 day */
+            /* 1,000 to 100,000 */
 
             [
-                'deathspan: 65 minutes, lifespan: less than 1 minute', /* name */
-                $one_minute * 65, /* deathspan */
-                $one_second * 55, /* lifespan */
+                'deathspan: greater than 1,000, lifespan: less than 100', /* name */
+                1100, /* deathspan */
+                  90, /* lifespan */
                 'prune', /* action */
             ],
             [
-                'deathspan: 65 minutes, lifespan: greater than 1 minute', /* name */
-                $one_minute * 65, /* deathspan */
-                $one_second * 65, /* lifespan */
-                'keep', /* action */
-            ],
-
-            [
-                'deathspan: 23 hours, lifespan: less than 1 minute', /* name */
-                $one_hour * 23, /* deathspan */
-                $one_second * 55, /* lifespan */
-                'prune', /* action */
-            ],
-            [
-                'deathspan: 23 hours, lifespan: greater than 1 minute', /* name */
-                $one_hour * 23, /* deathspan */
-                $one_second * 65, /* lifespan */
-                'keep', /* action */
-            ],
-
-            /* 1 day to 1 week */
-
-            [
-                'deathspan: 25 hours, lifespan: less than 10 minutes', /* name */
-                $one_hour * 25, /* deathspan */
-                $one_minute * 9, /* lifespan */
-                'prune', /* action */
-            ],
-            [
-                'deathspan: 25 hours, lifespan: greater than 10 minutes', /* name */
-                $one_hour * 25, /* deathspan */
-                $one_minute * 11, /* lifespan */
+                'deathspan: greater than 1,000, lifespan: greater than 100', /* name */
+                1100, /* deathspan */
+                 110, /* lifespan */
                 'keep', /* action */
             ],
 
             [
-                'deathspan: 6 days, lifespan: less than 10 minutes', /* name */
-                $one_day * 6, /* deathspan */
-                $one_minute * 9, /* lifespan */
+                'deathspan: less than 100,000, lifespan: less than 100', /* name */
+                99000, /* deathspan */
+                90, /* lifespan */
                 'prune', /* action */
             ],
             [
-                'deathspan: 6 days, lifespan: greater than 10 minutes', /* name */
-                $one_day * 6, /* deathspan */
-                $one_minute * 11, /* lifespan */
+                'deathspan: less than 100,000, lifespan: greater than 100', /* name */
+                99000, /* deathspan */
+                  110, /* lifespan */
                 'keep', /* action */
             ],
 
-            /* 1 week to 30 days */
+            /* 100,000 to 1,000,000 */
 
             [
-                'deathspan: 8 days, lifespan: less than 1 hour', /* name */
-                $one_day * 8, /* deathspan */
-                $one_minute * 55, /* lifespan */
+                'deathspan: greater than 100,000, lifespan: less than 1,000', /* name */
+                110000, /* deathspan */
+                   900, /* lifespan */
                 'prune', /* action */
             ],
             [
-                'deathspan: 8 days, lifespan: greater than 1 hour', /* name */
-                $one_day * 8, /* deathspan */
-                $one_minute * 65, /* lifespan */
-                'keep', /* action */
-            ],
-
-            [
-                'deathspan: 29 days, lifespan: less than 1 hour', /* name */
-                $one_day * 29, /* deathspan */
-                $one_minute * 55, /* lifespan */
-                'prune', /* action */
-            ],
-            [
-                'deathspan: 20 days, lifespan: greater than 1 hour', /* name */
-                $one_day * 29, /* deathspan */
-                $one_minute * 65, /* lifespan */
-                'keep', /* action */
-            ],
-
-            /* 30 days to 90 days */
-
-            [
-                'deathspan: 31 days, lifespan: less than 10 hours', /* name */
-                $one_day * 31, /* deathspan */
-                $one_hour * 9, /* lifespan */
-                'prune', /* action */
-            ],
-            [
-                'deathspan: 31 days, lifespan: greater than 10 hours', /* name */
-                $one_day * 31, /* deathspan */
-                $one_hour * 11, /* lifespan */
+                'deathspan: greater than 100,000, lifespan: greater than 1,000', /* name */
+                110000, /* deathspan */
+                  1100, /* lifespan */
                 'keep', /* action */
             ],
 
             [
-                'deathspan: 89 days, lifespan: less than 10 hours', /* name */
-                $one_day * 89, /* deathspan */
-                $one_hour * 9, /* lifespan */
+                'deathspan: less than 1,000,000, lifespan: less than 1,000', /* name */
+                990000, /* deathspan */
+                   900, /* lifespan */
                 'prune', /* action */
             ],
             [
-                'deathspan: 89 days, lifespan: greater than 10 hours', /* name */
-                $one_day * 89, /* deathspan */
-                $one_hour * 11, /* lifespan */
+                'deathspan: less than 1,000,000, lifespan: greater than 1,000', /* name */
+                990000, /* deathspan */
+                  1100, /* lifespan */
                 'keep', /* action */
             ],
 
-            /* greater than 90 days */
+            /* 1,000,000 to 10,000,000 */
 
             [
-                'deathspan: 91 days, lifespan: greater than 10 hours', /* name */
-                $one_day * 91, /* deathspan */
-                $one_hour * 11, /* lifespan */
+                'deathspan: greater than 1,000,000, lifespan: less than 10,000', /* name */
+                1100000, /* deathspan */
+                   9000, /* lifespan */
                 'prune', /* action */
             ],
             [
-                'deathspan: 365 days, lifespan: 365 days', /* name */
-                $one_day * 365, /* deathspan */
-                $one_day * 365, /* lifespan */
+                'deathspan: greater than 1,000,000, lifespan: greater than 10,000', /* name */
+                1100000, /* deathspan */
+                  11000, /* lifespan */
+                'keep', /* action */
+            ],
+
+            [
+                'deathspan: less than 10,000,000, lifespan: less than 10,000', /* name */
+                9900000, /* deathspan */
+                   9900, /* lifespan */
                 'prune', /* action */
             ],
             [
-                'deathspan: 100,000 days, lifespan: 100,000 days', /* name */
-                $one_day * 100000, /* deathspan */
-                $one_day * 100000, /* lifespan */
+                'deathspan: less than 10,000,000, lifespan: greater than 10,000', /* name */
+                9900000, /* deathspan */
+                  11000, /* lifespan */
+                'keep', /* action */
+            ],
+
+            /* greater than 10,000,000 */
+
+            [
+                'deathspan: 11,000,000, lifespan: 1', /* name */
+                11000000, /* deathspan */
+                1, /* lifespan */
+                'prune', /* action */
+            ],
+            [
+                'deathspan: 11,000,000, lifespan: 10,000,000', /* name */
+                11000000, /* deathspan */
+                10000000, /* lifespan */
+                'prune', /* action */
+            ],
+            [
+                'deathspan: 100,000,000,000, lifespan: 10,000,000', /* name */
+                100000000000, /* deathspan */
+                10000000, /* lifespan */
                 'prune', /* action */
             ],
         ];
