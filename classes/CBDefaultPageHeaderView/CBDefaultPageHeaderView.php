06e9570badb561d19deb3064b0fd1ad7c6fb47d2
Matt Calkins (MacBook Air) <matt@mattifesto.com>
2018-08-19 10:29:27 -0700

    Remove CBWellKnownMenuForMain

--- classes/CBDefaultPageHeaderView/CBDefaultPageHeaderView.php
+++ classes/CBDefaultPageHeaderView/CBDefaultPageHeaderView.php
@@ -1,5 +1,11 @@
 <?php
 
+/**
+ * @deprecated 2018.08.19
+ *
+ *      This class should be replaced with a site specific page header view.
+ *      This can be deleted once that transition is confirmed.
+ */
 final class CBDefaultPageHeaderView {
 
     /**
@@ -13,11 +19,17 @@ final class CBDefaultPageHeaderView {
         <header class="CBDefaultPageHeaderView CBDarkTheme">
             <?php
 
+            /**
+             * CBWellKnownMenuForMain has been removed.
+             */
+
+            /*
             CBView::render((object)[
                 'className' => 'CBMenuView',
                 'menuID' => CBWellKnownMenuForMain::ID(),
                 'selectedItemName' => $selectedMainMenuItemName,
             ]);
+            */
 
             ?>
         </header>
