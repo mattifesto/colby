b05b809b33cd9c43ca8c11888bc457fb3c297fc0
Matt Calkins (MacBook Air) <matt@mattifesto.com>
2018-08-30 15:14:34 -0700

    Remove setup/install-database.php

--- setup/install-database.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-/**
- * 2017.11.26 @deprecated use CBInstall::install()
- */
-
-CBInstall::install();
--- setup/update.php
+++ setup/update.php
@@ -1,24 +1,16 @@
 <?php
 
 /**
- * This file will only be used if the site does not have its own
- * `setup/update.php` file.
+ * @deprecated 2018.08.30
  *
- * 2014.05.14
- * TODO: This file should be copied to the site's setup directory when
- * the initial file copy installation occurs. Then this file will never be
- * run, but will serve as the correct template and can then be modified as
- * the site needs updates and new libraries.
+ *      This file can still be overridden by creating a "setup/update.php" file
+ *      in the website directory.
  *
- * When this file is copied, this comment block should be removed.
+ *      This should not be done. Instead, all installation should be done by
+ *      implementing the CBInstall interfaces in classes.
+ *
+ *      This file will be removed once all websites have been confirmed to use
+ *      only CBInstall interface installation code.
  */
 
-/**
- * Library database installation scripts.
- *
- * Each library should a 'setup/install-database.php` file. This file should
- * have a "create if not exists" attitude as it will be run both for
- * installation and update. Updates should be added to the end of this file.
- */
-
-include CBSystemDirectory . '/setup/install-database.php';
+CBInstall::install();
