a2d166eebbdfa4bf0621d983dd7b7705deea9ffd
Matt Calkins (MacBook Air) <matt@mattifesto.com>
2018-09-09 19:56:38 -0700

    Remove multi-page use in CBDocumentation.php

--- classes/CBDocumentation/CBDocumentation.php
+++ classes/CBDocumentation/CBDocumentation.php
@@ -3,131 +3,66 @@
 final class CBDocumentation {
 
     /**
-     * @return [object]
+     * @return [string]
      */
-    static function CBAdmin_menuItems(): array {
+    static function CBAdmin_menuNamePath(): array {
         return [
-            (object)[
-                'mainMenuItemName' => 'help',
-                'menuItem' => (object)[
-                    'className' => 'CBMenuItem',
-                    'name' => 'CBPageHelpers',
-                    'text' => 'CBPageHelpers',
-                    'URL' => '/admin/?c=CBDocumentation&p=CBPageHelpers',
-                ],
-            ],
-            (object)[
-                'mainMenuItemName' => 'help',
-                'menuItem' => (object)[
-                    'className' => 'CBMenuItem',
-                    'name' => 'Markaround',
-                    'text' => 'Markaround',
-                    'URL' => '/admin/?c=CBDocumentation&p=Markaround',
-                ],
-            ],
+            'help'
         ];
     }
 
     /**
-     * @param string $pageStub
-     *
-     * @return [string]
-     */
-    static function CBAdmin_menuNamePath(string $pageStub) {
-        switch ($pageStub) {
-            case 'CBPageHelpers':
-                return ['help', 'CBPageHelpers'];
-
-            case 'Markaround':
-                return ['help', 'Markaround'];
-
-            default:
-                return ['help'];
-        }
-    }
-
-    /**
-     * @param string $pageStub
-     *
      * @return void
      */
-    static function CBAdmin_render(string $pageStub): void {
-        switch ($pageStub) {
-            case 'CBPageHelpers':
-                CBView::renderspec((object)[
-                    'className' => 'CBMessageView',
-                    'CSSClassNames' => 'CBAPIStyleSheet',
-                    'markup' => file_get_contents(__DIR__ . '/CBPageHelpers_documentation.mmk'),
-                ]);
-                break;
+    static function CBAdmin_render(): void {
+        $CSS = <<<EOT
 
-            case 'Markaround':
-                CBView::renderSpec((object)[
-                    'className' => 'CBThemedTextView',
-                    'contentAsMarkaround' => file_get_contents(__DIR__ . '/Markaround_documentation.markaround'),
-                    'stylesTemplate' => <<<EOT
+            main.CBDocumentation {
+                background-color: var(--CBBackgroundColor);
+            }
 
-                        view {
-                            background-color: var(--CBBackgroundColor);
-                        }
+            .CBDocumentation_container {
+                text-align: center;
+            }
 
-EOT
-                ]);
-                break;
-
-            default:
-
-                $CSS = <<<EOT
-
-                    main.CBDocumentation {
-                        background-color: var(--CBBackgroundColor);
-                    }
-
-                    .CBDocumentation_container {
-                        text-align: center;
-                    }
-
-                    .CBDocumentation_container a {
-                        display: flex;
-                        align-items: center;
-                        justify-content: center;
-                        height: 44px;
-                    }
+            .CBDocumentation_container a {
+                display: flex;
+                align-items: center;
+                justify-content: center;
+                height: 44px;
+            }
 EOT;
 
-                CBHTMLOutput::addCSS($CSS);
+        CBHTMLOutput::addCSS($CSS);
 
-                CBHTMLOutput::pageInformation()->title = 'Documentation';
+        CBHTMLOutput::pageInformation()->title = 'Documentation';
 
-                CBView::renderSpec((object)[
-                    'className' => 'CBPageTitleAndDescriptionView',
-                ]);
+        CBView::renderSpec((object)[
+            'className' => 'CBPageTitleAndDescriptionView',
+        ]);
 
-                $menuModel = CBModelCache::fetchModelByID(CBHelpAdminMenu::ID());
+        $menuModel = CBModelCache::fetchModelByID(CBHelpAdminMenu::ID());
 
-                ?><div class="CBDocumentation_container"><?php
+        ?><div class="CBDocumentation_container"><?php
 
-                $items = CBModel::valueToArray($menuModel, 'items');
+        $items = CBModel::valueToArray($menuModel, 'items');
 
-                array_walk(
-                    $items,
-                    function ($item) {
-                        $textAsHTML = cbhtml(CBModel::valueToString($item, 'text'));
-                        $URLAsHTML = cbhtml(CBModel::valueToString($item, 'URL'));
+        array_walk(
+            $items,
+            function ($item) {
+                $textAsHTML = cbhtml(CBModel::valueToString($item, 'text'));
+                $URLAsHTML = cbhtml(CBModel::valueToString($item, 'URL'));
 
-                        ?>
+                ?>
 
-                        <div>
-                            <a href="<?= $URLAsHTML ?>"><?= $textAsHTML ?></a>
-                        </div>
+                <div>
+                    <a href="<?= $URLAsHTML ?>"><?= $textAsHTML ?></a>
+                </div>
 
-                        <?php
-                    }
-                );
+                <?php
+            }
+        );
 
-                ?></div><?php
-
-                break;
-        }
+        ?></div><?php
     }
 }
