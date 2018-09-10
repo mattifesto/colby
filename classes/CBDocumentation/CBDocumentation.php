a6f344613e8c85d5474b90b669d21260869f8d16
Matt Calkins (MacBook Air) <matt@mattifesto.com>
2018-09-09 18:28:56 -0700

    Add a default page in CBDocumentation.php

--- classes/CBDocumentation/CBDocumentation.php
+++ classes/CBDocumentation/CBDocumentation.php
@@ -76,6 +76,57 @@ EOT
                 break;
 
             default:
+
+                $CSS = <<<EOT
+
+                    main.CBDocumentation {
+                        background-color: var(--CBBackgroundColor);
+                    }
+
+                    .CBDocumentation_container {
+                        text-align: center;
+                    }
+
+                    .CBDocumentation_container a {
+                        display: flex;
+                        align-items: center;
+                        justify-content: center;
+                        height: 44px;
+                    }
+EOT;
+
+                CBHTMLOutput::addCSS($CSS);
+
+                CBHTMLOutput::pageInformation()->title = 'Documentation';
+
+                CBView::renderSpec((object)[
+                    'className' => 'CBPageTitleAndDescriptionView',
+                ]);
+
+                $menuModel = CBModelCache::fetchModelByID(CBHelpAdminMenu::ID());
+
+                ?><div class="CBDocumentation_container"><?php
+
+                $items = CBModel::valueToArray($menuModel, 'items');
+
+                array_walk(
+                    $items,
+                    function ($item) {
+                        $textAsHTML = cbhtml(CBModel::valueToString($item, 'text'));
+                        $URLAsHTML = cbhtml(CBModel::valueToString($item, 'URL'));
+
+                        ?>
+
+                        <div>
+                            <a href="<?= $URLAsHTML ?>"><?= $textAsHTML ?></a>
+                        </div>
+
+                        <?php
+                    }
+                );
+
+                ?></div><?php
+
                 break;
         }
     }
