8c0dc834a38d0ac7790d8decda7e5eb9581a4110
Matt Calkins (MacBook Air) <matt@mattifesto.com>
2018-09-10 17:28:02 -0700

    Garden CBViewPage.php

--- classes/CBViewPage/CBViewPage.php
+++ classes/CBViewPage/CBViewPage.php
@@ -6,7 +6,9 @@ final class CBViewPage {
      * @return [string]
      */
     static function CBHTMLOutput_CSSURLs(): array {
-        return [Colby::flexpath(__CLASS__, 'v418.css', cbsysurl())];
+        return [
+            Colby::flexpath(__CLASS__, 'v418.css', cbsysurl()),
+        ];
     }
 
     /**
