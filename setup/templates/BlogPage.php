27d0ef9968060ffa31a2478b55fd8450030ff0ab
Matt Calkins (MacBook Air) <matt@mattifesto.com>
2018-09-10 17:29:22 -0700

    Use CBModelUpdater, add main menu item in setup/templates/BlogPage.php

--- setup/templates/BlogPage.php
+++ setup/templates/BlogPage.php
@@ -1,50 +1,19 @@
 <?php
 
+/**
+ * @TODO 2018_09_10
+ *
+ *      File should be renamed to Page_blog.php
+ *      Class should be renamed to PREFIXPage_blog
+ */
 final class PREFIXBlogPage {
 
     /**
      * @return void
      */
     static function CBInstall_configure(): void {
-        $originalSpec = CBModels::fetchSpecByID(PREFIXBlogPage::ID());
-
-        if (empty($originalSpec)) {
-            $spec = CBModelTemplateCatalog::fetchLivePageTemplate();
-
-            CBModel::merge($spec, (object)[
-                'ID' => PREFIXBlogPage::ID(),
-                'isPublished' => true,
-                'publicationTimeStamp' => time(),
-                'selectedMainMenuItemName' => 'blog',
-                'URI' => 'blog',
-                'sections' => [
-                    (object)[
-                        'className' => 'CBPageTitleAndDescriptionView',
-                    ],
-                    (object)[
-                        'className' => 'CBPageListView2',
-                        'classNameForKind' => 'PREFIXBlogPostPageKind',
-                    ],
-                ],
-            ]);
-        } else {
-            $spec = CBModel::clone($originalSpec);
-        }
-
-        /* properties reset every update */
-
-        CBModel::merge($spec, (object)[
-            'className' => 'CBViewPage',
-            'title' => 'Blog',
-        ]);
-
-        /* save if modified */
-
-        if ($spec != $originalSpec) {
-            CBDB::transaction(function () use ($spec) {
-                CBModels::save($spec);
-            });
-        }
+        PREFIXBlogPage::installPage();
+        PREFIXBlogPage::installMenuItem();
     }
 
     /**
@@ -52,5 +21,89 @@ final class PREFIXBlogPage {
      */
     static function ID(): string {
         return RANDOMID;
+    }
+
+    /**
+     * @return void
+     */
+    private static function installMenuItem(): void {
+        $updater = CBModelUpdater::fetch(
+            (object)[
+                'ID' => PREFIXMainMenu::ID(),
+            ]
+        );
+
+        $menuSpec = $updater->working;
+
+        CBMenu::addOrReplaceItem(
+            $menuSpec,
+            (object)[
+                'className' => 'CBMenuItem',
+                'name' => 'blog',
+                'text' => 'Blog',
+                'URL' => '/blog/',
+            ]
+        );
+
+        CBModelUpdater::save($updater);
+    }
+
+    /**
+     * @return void
+     */
+    private static function installPage(): void {
+        $updater = CBModelUpdater::fetch(
+            CBModelTemplateCatalog::fetchLivePageTemplate(
+                (object)[
+                    'ID' => PREFIXBlogPage::ID(),
+                    'classNameForKind' => 'PREFIXGeneratedPageKind',
+                    'isPublished' => true,
+                    'selectedMenuItemNames' => 'blog',
+                    'title' => 'Blog',
+                    'URI' => 'blog',
+                ]
+            )
+        );
+
+        $pageSpec = $updater->working;
+
+        /* publicationTimeStamp */
+
+        if (CBModel::valueAsInt($pageSpec, 'publicationTimeStamp') === null) {
+            $pageSpec->publicationTimeStamp = time();
+        }
+
+        /* page title and description view */
+
+        $sourceID = RANDOMID;
+
+        CBSubviewUpdater::unshift(
+            $pageSpec,
+            'sourceID',
+            $sourceID,
+            (object)[
+                'className' => 'CBPageTitleAndDescriptionView',
+                'sourceID' => $sourceID,
+            ]
+        );
+
+        /* page list view */
+
+        $sourceID = RANDOMID;
+
+        CBSubviewUpdater::push(
+            $pageSpec,
+            'sourceID',
+            $sourceID,
+            (object)[
+                'className' => 'CBPageListView2',
+                'classNameForKind' => 'CBBlogPostPageKind',
+                'sourceID' => $sourceID,
+            ]
+        );
+
+        /* save */
+
+        CBModelUpdater::save($updater);
     }
 }
