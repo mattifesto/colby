733113d61131d2e23935afe2502589128bc25656
Matt Calkins (MacBook Air) <matt@mattifesto.com>
2018-10-15 10:16:37 -0700

    Remove documentation comments from CBTest.php

--- classes/CBTest/CBTest.php
+++ classes/CBTest/CBTest.php
@@ -3,41 +3,6 @@
 final class CBTest {
 
     /**
-     * How to create a test for functionality in the class MyClass.
-     *
-     * 1. Create a class named MyClassTests
-     *
-     *      final class MyClassTests {
-     *          ...
-     *      }
-     *
-     * 2. Create a function named CBTest_myTest() which will perform the tests
-     *
-     *      static function CBTest_myTest(): stdClass {
-     *          ...
-     *      }
-     *
-     * 3. Make CBTest_myTest() return an object after performing the tests
-     *
-     *      return (object)[
-     *          'succeeded' => false,
-     *          'message' => 'This is what went wrong.',
-     *      ];
-     *
-     *      This object returned by the test function will be passed without
-     *      modification to the Ajax caller, so be mindful of any other property
-     *      values you set.
-     *
-     * 4. Implement CBUnitTests_tests() to register all of the test functions in
-     *    MyClassTests
-     *
-     *      static function CBUnitTests_tests(): array {
-     *          return [
-     *              ['MyClass', 'myTest'],
-     *              ['MyClass', 'myOtherTest'],
-     *          ];
-     *      }
-     *
      * @param object $args
      *
      *      {
