d5097ff420e2478f069f19662641b1629c637ed1
Matt Calkins (MacBook Air) <matt@mattifesto.com>
2018-11-05 09:35:04 -0800

    Add render(), CBMenuItem_name(), CBMenuItem_render() in CBMenuItem.php

--- classes/CBMenuItem/CBMenuItem.php
+++ classes/CBMenuItem/CBMenuItem.php
@@ -3,6 +3,31 @@
 final class CBMenuItem {
 
     /**
+     * @param object $menuItemModel
+     *
+     * @return string
+     */
+    static function CBMenuItem_name(stdClass $menuItemModel): string {
+        return CBModel::valueToString($menuItemModel, 'name');
+    }
+
+    /**
+     * @param object $menuItemModel
+     *
+     * @return void
+     */
+    static function CBMenuItem_render(stdClass $menuItemModel): void {
+        $textAsHTML = cbhtml(CBModel::valueToString($menuItemModel, 'text'));
+        $URLAsHTML = cbhtml(CBModel::valueToString($menuItemModel, 'URL'));
+
+        ?>
+
+        <a class="CBMenuItem" href="<?= $URLAsHTML ?>"><span><?= $textAsHTML ?></span></a>
+
+        <?php
+    }
+
+    /**
      * @param model $spec
      *
      *      {
@@ -30,5 +55,52 @@ final class CBMenuItem {
         $model->URLAsHTML = cbhtml($model->URL);
 
         return $model;
+    }
+
+    /**
+     * @param model $menuItemModel
+     * @param string $selectedMenuItemName
+     *
+     * @return void
+     */
+    static function render(
+        stdClass $menuItemModel,
+        string $selectedMenuItemName = ''
+    ): void {
+        $className = CBModel::valueToString($menuItemModel, 'className');
+
+        if (empty($className)) {
+            $className = "CBMenuItem";
+        }
+
+        CBHTMLOutput::requireClassName($className);
+
+        if (is_callable($function = "{$className}::CBMenuItem_isHidden")) {
+            $isHidden = call_user_func($function, $menuItemModel);
+
+            if ($isHidden) {
+                return;
+            }
+        }
+
+        $name = '';
+
+        if (is_callable($function = "{$className}::CBMenuItem_name")) {
+            $name = call_user_func($function, $menuItemModel);
+        }
+
+        $classes = 'CBMenuView_menuItem';
+
+        if ($name !== '' && $name === $selectedMenuItemName) {
+            $classes .= ' selected';
+        }
+
+        echo "<li class=\"{$classes}\">";
+
+        if (is_callable($function = "{$className}::CBMenuItem_render")) {
+            call_user_func($function, $menuItemModel);
+        }
+
+        echo '</li>';
     }
 }
