a591886a06d6fd419a7b138f106bd388f6f2c155
Matt Calkins (MacBook Air) <matt@mattifesto.com>
2018-11-14 17:48:28 -0800

    Fix IE11 scrollbar issue in CBUIStringEditor

--- classes/CBUIStringEditor/CBUIStringEditor.php
+++ classes/CBUIStringEditor/CBUIStringEditor.php
@@ -6,20 +6,26 @@ final class CBUIStringEditor {
      * @return [string]
      */
     static function CBHTMLOutput_CSSURLs() {
-        return [Colby::flexpath(__CLASS__, 'css', cbsysurl())];
+        return [
+            Colby::flexpath(__CLASS__, 'v467.css', cbsysurl()),
+        ];
     }
 
     /**
      * @return [string]
      */
     static function CBHTMLOutput_JavaScriptURLs() {
-        return [Colby::flexpath(__CLASS__, 'v420.js', cbsysurl())];
+        return [
+            Colby::flexpath(__CLASS__, 'v420.js', cbsysurl()),
+        ];
     }
 
     /**
      * @return [string]
      */
     static function CBHTMLOutput_requiredClassNames() {
-        return ['Colby'];
+        return [
+            'Colby',
+        ];
     }
 }
