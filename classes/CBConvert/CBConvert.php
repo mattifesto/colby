7531cfb3c012e1448612bf538b5ce445876e50b8
Matt Calkins (MacBook Air) <matt@mattifesto.com>
2018-11-29 10:12:15 -0800

    Update the version number for CBConvert.js

--- classes/CBConvert/CBConvert.php
+++ classes/CBConvert/CBConvert.php
@@ -7,7 +7,7 @@ final class CBConvert {
      */
     static function CBHTMLOutput_JavaScriptURLs(): array {
         return [
-            Colby::flexpath(__CLASS__, 'v466.js', cbsysurl()),
+            Colby::flexpath(__CLASS__, 'v468.js', cbsysurl()),
         ];
     }
 
