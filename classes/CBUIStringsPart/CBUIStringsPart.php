376d6747cb8636b047b3ca6d7c3916e045ff3597
Matt Calkins (MacBook Air) <matt@mattifesto.com>
2018-12-03 14:33:16 -0800

    Add CBUIStringsPart_titledescription_full, CBUIStringsPart_price CSS classes in CBUIStringsPart.css

--- classes/CBUIStringsPart/CBUIStringsPart.php
+++ classes/CBUIStringsPart/CBUIStringsPart.php
@@ -5,15 +5,19 @@ final class CBUIStringsPart {
     /**
      * @return [string]
      */
-    static function CBHTMLOutput_CSSURLs() {
-        return [Colby::flexpath(__CLASS__, 'v466.css', cbsysurl())];
+    static function CBHTMLOutput_CSSURLs(): array {
+        return [
+            Colby::flexpath(__CLASS__, 'v468.css', cbsysurl()),
+        ];
     }
 
     /**
      * @return [string]
      */
-    static function CBHTMLOutput_JavaScriptURLs() {
-        return [Colby::flexpath(__CLASS__, 'v371.js', cbsysurl())];
+    static function CBHTMLOutput_JavaScriptURLs(): array {
+        return [
+            Colby::flexpath(__CLASS__, 'v371.js', cbsysurl()),
+        ];
     }
 
     /**
