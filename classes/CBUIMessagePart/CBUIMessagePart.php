39ac16b822088c50ee6f1c7589d1fccde144972e
Matt Calkins (MacBook Air) <matt@mattifesto.com>
2018-12-25 16:39:03 -0800

    Fix max-width in CBUIMessagePart.css

--- classes/CBUIMessagePart/CBUIMessagePart.php
+++ classes/CBUIMessagePart/CBUIMessagePart.php
@@ -6,14 +6,18 @@ final class CBUIMessagePart {
      * @return [string]
      */
     static function CBHTMLOutput_CSSURLs() {
-        return [Colby::flexpath(__CLASS__, 'v440.css', cbsysurl())];
+        return [
+            Colby::flexpath(__CLASS__, 'v468.css', cbsysurl()),
+        ];
     }
 
     /**
      * @return [string]
      */
     static function CBHTMLOutput_JavaScriptURLs() {
-        return [Colby::flexpath(__CLASS__, 'v368.js', cbsysurl())];
+        return [
+            Colby::flexpath(__CLASS__, 'v368.js', cbsysurl()),
+        ];
     }
 
     /**
