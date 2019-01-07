eead601f65bc792c3f0419400f31420052649046
Matt Calkins (MacBook Air) <matt@mattifesto.com>
2019-01-06 19:06:37 -0800

    Garden CBMutator.php

--- classes/CBMutator/CBMutator.php
+++ classes/CBMutator/CBMutator.php
@@ -4,8 +4,6 @@
  * A mutator is an object that represents a value. The value can be retreived
  * and modified. Most importantly listeners can receive notifications when the
  * value changes.
- *
- * CBUIRadioButton instances change the value of a CBMutator.
  */
 final class CBMutator {
 
