85eabf606fa1ffc871ef863197c5af1d77e848a2
Matt Calkins (MacBook Air) <matt@mattifesto.com>
2019-02-05 17:02:03 -0800

    Remove fetchPrimaryImageModel(), replacePrimaryImage(), insertSecondaryImage() in CBModelImages.php

--- classes/CBModelImages/CBModelImages.php
+++ classes/CBModelImages/CBModelImages.php
@@ -9,80 +9,25 @@ final class CBModelImages {
      *
      *      {
      *          modelID: ID
-     *          imageModelID: ID
+     *          imageID: ID
      *      }
      *
      * @return void
      */
-    static function CBAjax_replacePrimaryImage(stdClass $args): void {
-        CBModelImages::replacePrimaryImage(
-            $args->modelID,
-            $args->imageModelID
-        );
+    static function CBAjax_replaceImagesModel(stdClass $args): void {
+        $associationKey = __CLASS__;
 
+        CBModelAssociations::replaceAssociatedID(
+            $args->modelID,
+            $associationKey,
+            $args->imageID
+        );
     }
 
     /**
      * @return string
      */
-    static function CBAjax_replacePrimaryImage_group(): string {
+    static function CBAjax_replaceImagesModel_group(): string {
         return 'Administrators';
     }
-
-    /* -- functions -- -- -- -- -- */
-
-    /**
-     * @param ID $modelID
-     *
-     * @return object|null
-     */
-    static function fetchPrimaryImageModel($modelID): ?stdClass {
-        $associationKey = 'CBModelImages_CBModel_CBImage_primary';
-
-        return CBModelAssociations::fetchAssociatedModel(
-            $modelID,
-            $associationKey
-        );
-    }
-
-    /**
-     * @param ID $modelID
-     * @param ID $imageModelID
-     *
-     * @return void
-     */
-    static function replacePrimaryImage(
-        string $modelID,
-        string $imageModelID
-    ): void {
-        $associationKey = 'CBModelImages_CBModel_CBImage_primary';
-
-        CBModelAssociations::replaceAssociatedID(
-            $modelID,
-            $associationKey,
-            $imageModelID
-        );
-    }
-
-    /**
-     * @TODO 2019_01_23
-     *
-     *      Uncomment this function when it is first needed.
-     *      insertSecondaryImage() or addSecondaryImage() ?
-     *      Insert might be good because it suggests SQL activity.
-     *
-     * @param ID $modelID
-     * @param ID $imageModelID
-     *
-     * @return void
-     */ /*
-     static function insertSecondaryImage($modelID, $imageModelID): void {
-        $associationKey = 'CBModelImages_CBModel_CBImage_secondary';
-
-        CBModelAssociations::add(
-            $modelID,
-            $associationKey,
-            $imageModelID
-        );
-    } */
 }
