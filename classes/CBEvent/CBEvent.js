5bff46e6c5b3cded27a734c9bd8694eb90668ce3
Matt Calkins (MacBook Air) <matt@mattifesto.com>
2019-04-16 10:57:08 -0700

    Add removeListener() in CBEvent.js

--- classes/CBEvent/CBEvent.js
+++ classes/CBEvent/CBEvent.js
@@ -20,10 +20,13 @@ var CBEvent = {
         return {
             addListener: addListener,
             dispatch: dispatch,
+            removeListener: removeListener,
         };
 
+        /* -- closures -- -- -- -- -- */
+
         /**
-         * closure in CBEvent.create()
+         * closure in create()
          *
          * @param function listener
          *
@@ -31,7 +34,9 @@ var CBEvent = {
          */
         function addListener(listener) {
             if (typeof listener !== "function") {
-                throw new TypeError("The parameter to addListener must be a function.");
+                throw new TypeError(
+                    "The parameter to CBEvent.addListener() must be a function."
+                );
             }
 
             if (!listeners.includes(listener)) {
@@ -40,7 +45,7 @@ var CBEvent = {
         }
 
         /**
-         * closure in CBEvent.create()
+         * closure in create()
          *
          * A call to dispatch() on an event while dispatch is happening for that
          * event will be ignored.
@@ -66,6 +71,28 @@ var CBEvent = {
             );
 
             dispatchIsHappening = false;
+        }
+
+        /**
+         * closure in create()
+         *
+         * @param function listener
+         *
+         * @return undefined
+         */
+        function removeListener(listener) {
+            if (typeof listener !== "function") {
+                throw new TypeError(
+                    "The parameter to CBEvent.removeListener() must be a " +
+                    "function."
+                );
+            }
+
+            let index = listeners.indexOf(listener);
+
+            if (index > -1) {
+                listeners.splice(index, 1);
+            }
         }
     },
 };
