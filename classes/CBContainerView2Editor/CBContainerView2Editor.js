39bb625bbfdbd45734672b75407617ff89ae06b2
Matt Calkins (MacBook Air) <matt@mattifesto.com>
2019-05-28 13:42:42 -0700

    Replace use of Colby.imageToURL() in CBContainerView2Editor.js

--- classes/CBContainerView2Editor/CBContainerView2Editor.js
+++ classes/CBContainerView2Editor/CBContainerView2Editor.js
@@ -3,13 +3,17 @@
 /* jshint esversion: 6 */
 /* exported CBContainerView2Editor */
 /* globals
-    CBContainerView2Editor_addableClassNames,
+    CBImage,
+    CBModel,
     CBUI,
     CBUIImageChooser,
     CBUISpec,
     CBUISpecArrayEditor,
     CBUIStringEditor,
-    Colby */
+    Colby,
+
+    CBContainerView2Editor_addableClassNames,
+*/
 
 var CBContainerView2Editor = {
 
@@ -19,12 +23,16 @@ var CBContainerView2Editor = {
      * @return string|undefined
      */
     CBUISpec_toDescription: function (spec) {
-        if (typeof spec.title === "string" && spec.title.trim() !== "") {
-            return spec.title.trim();
+        let title = CBModel.valueToString(spec, "title").trim();
+
+        if (title !== "") {
+            return title;
         } else {
             if (Array.isArray(spec.subviews)) {
                 for (let i = 0; i < spec.subviews.length; i++) {
-                    let description = CBUISpec.specToDescription(spec.subviews[i]);
+                    let description = CBUISpec.specToDescription(
+                        spec.subviews[i]
+                    );
 
                     if (description) {
                         return description;
@@ -33,6 +41,8 @@ var CBContainerView2Editor = {
             }
         }
     },
+    /* CBUISpec_toDescription() */
+
 
     /**
      * @param object spec
@@ -41,11 +51,16 @@ var CBContainerView2Editor = {
      */
     CBUISpec_toThumbnailURI: function (spec) {
         if (spec.image) {
-            return Colby.imageToURL(spec.image, 'rw320');
+            return CBImage.toURL(
+                spec.image,
+                'rw320'
+            );
         } else {
             if (Array.isArray(spec.subviews)) {
                 for (let i = 0; i < spec.subviews.length; i++) {
-                    let thumbnailURI = CBUISpec.specToThumbnailURI(spec.subviews[i]);
+                    let thumbnailURI = CBUISpec.specToThumbnailURI(
+                        spec.subviews[i]
+                    );
 
                     if (thumbnailURI) {
                         return thumbnailURI;
@@ -54,6 +69,8 @@ var CBContainerView2Editor = {
             }
         }
     },
+    /* CBUISpec_toThumbnailURI() */
+
 
     /**
      * @param object args.spec
@@ -70,12 +87,16 @@ var CBContainerView2Editor = {
 
         section = CBUI.createSection();
 
-        var chooser = CBUIImageChooser.createFullSizedChooser({
-            imageChosenCallback : handleImageChosen,
-            imageRemovedCallback : handleImageRemoved,
-        });
+        var chooser = CBUIImageChooser.createFullSizedChooser(
+            {
+                imageChosenCallback : handleImageChosen,
+                imageRemovedCallback : handleImageRemoved,
+            }
+        );
 
-        chooser.setImageURLCallback(Colby.imageToURL(args.spec.image, "rw960"));
+        chooser.setImageURLCallback(
+            CBImage.toURL(args.spec.image, "rw960")
+        );
 
         item = CBUI.createSectionItem();
         item.appendChild(chooser.element);
@@ -85,14 +106,19 @@ var CBContainerView2Editor = {
         element.appendChild(CBUI.createHalfSpace());
 
         section = CBUI.createSection();
-
         item = CBUI.createSectionItem();
-        item.appendChild(CBUIStringEditor.createEditor({
-            labelText : "Title",
-            propertyName : "title",
-            spec : args.spec,
-            specChangedCallback : args.specChangedCallback,
-        }).element);
+
+        item.appendChild(
+            CBUIStringEditor.createEditor(
+                {
+                    labelText : "Title",
+                    propertyName : "title",
+                    spec : args.spec,
+                    specChangedCallback : args.specChangedCallback,
+                }
+            ).element
+        );
+
         section.appendChild(item);
 
         element.appendChild(section);
@@ -104,12 +130,14 @@ var CBContainerView2Editor = {
                 args.spec.subviews = [];
             }
 
-            let editor = CBUISpecArrayEditor.create({
-                addableClassNames: CBContainerView2Editor_addableClassNames,
-                navigateToItemCallback: args.navigateToItemCallback,
-                specs: args.spec.subviews,
-                specsChangedCallback: args.specChangedCallback,
-            });
+            let editor = CBUISpecArrayEditor.create(
+                {
+                    addableClassNames: CBContainerView2Editor_addableClassNames,
+                    navigateToItemCallback: args.navigateToItemCallback,
+                    specs: args.spec.subviews,
+                    specsChangedCallback: args.specChangedCallback,
+                }
+            );
 
             editor.title = "Views";
 
@@ -119,32 +147,42 @@ var CBContainerView2Editor = {
 
         /* CSSClassNames */
 
-        element.appendChild(CBUI.createSectionHeader({
-            paragraphs: [
-                `
-                Supported Class Names:
-                `,`
-                flow: Flow subviews from left to right and wrap into new lines.
-                Center each line of children. Example scenario: displaying a
-                collection of images.
-                `,`
-                hero1: Present the view as a full window view. The minimum
-                height is the window height. The background image will always
-                cover the entire view which will hide some of the edges of the
-                image depending on the shape of the window.
-                `
-           ],
-        }));
+        element.appendChild(
+            CBUI.createSectionHeader(
+                {
+                    paragraphs: [
+                        `
+                        Supported Class Names:
+                        `,`
+                        flow: Flow subviews from left to right and wrap into new
+                        lines. Center each line of children. Example scenario:
+                        displaying a collection of images.
+                        `,`
+                        hero1: Present the view as a full window view. The
+                        minimum height is the window height. The background
+                        image will always cover the entire view which will hide
+                        some of the edges of the image depending on the shape of
+                        the window.
+                        `
+                   ],
+                }
+            )
+        );
 
         section = CBUI.createSection();
-
         item = CBUI.createSectionItem();
-        item.appendChild(CBUIStringEditor.createEditor({
-            labelText : "CSS Class Names",
-            propertyName : "CSSClassNames",
-            spec : args.spec,
-            specChangedCallback : args.specChangedCallback,
-        }).element);
+
+        item.appendChild(
+            CBUIStringEditor.createEditor(
+                {
+                    labelText : "CSS Class Names",
+                    propertyName : "CSSClassNames",
+                    spec : args.spec,
+                    specChangedCallback : args.specChangedCallback,
+                }
+            ).element
+        );
+
         section.appendChild(item);
 
         element.appendChild(section);
@@ -154,45 +192,70 @@ var CBContainerView2Editor = {
         element.appendChild(CBUI.createHalfSpace());
 
         section = CBUI.createSection();
-
         item = CBUI.createSectionItem();
-        item.appendChild(CBUIStringEditor.createEditor({
-            labelText : "Local CSS Template",
-            propertyName : "localCSSTemplate",
-            spec : args.spec,
-            specChangedCallback : args.specChangedCallback,
-        }).element);
+
+        item.appendChild(
+            CBUIStringEditor.createEditor(
+                {
+                    labelText : "Local CSS Template",
+                    propertyName : "localCSSTemplate",
+                    spec : args.spec,
+                    specChangedCallback : args.specChangedCallback,
+                }
+            ).element
+        );
+
         section.appendChild(item);
         element.appendChild(section);
 
-        element.appendChild(CBUI.createHalfSpace());
+        element.appendChild(
+            CBUI.createHalfSpace()
+        );
 
         return element;
 
+
+        /* -- closures -- -- -- -- -- */
+
         /**
+         * @param object chooserArgs
          *
+         * @return undefined
          */
         function handleImageChosen(chooserArgs) {
             var ajaxURI = "/api/?class=CBImages&function=upload";
             var formData = new FormData();
             formData.append("image", chooserArgs.file);
 
-            CBContainerView2Editor.promise = Colby.fetchAjaxResponse(ajaxURI, formData)
-                .then(handleImageUploaded);
+            CBContainerView2Editor.promise = Colby.fetchAjaxResponse(
+                ajaxURI,
+                formData
+            ).then(
+                handleImageUploaded
+            );
 
             function handleImageUploaded(response) {
                 args.spec.image = response.image;
                 args.specChangedCallback();
-                chooserArgs.setImageURLCallback(Colby.imageToURL(args.spec.image, "rw960"));
+                chooserArgs.setImageURLCallback(
+                    CBImage.toURL(args.spec.image, "rw960")
+                );
             }
         }
+        /* handleImageChosen() */
+
 
         /**
+         * @param object chooserArgs
          *
+         * @return undefined
          */
         function handleImageRemoved(chooserArgs) {
             args.spec.image = undefined;
             args.specChangedCallback();
         }
+        /* handleImageRemoved() */
     },
+    /* createEditor() */
 };
+/* CBContainerView2Editor */
--- classes/CBContainerView2Editor/CBContainerView2Editor.php
+++ classes/CBContainerView2Editor/CBContainerView2Editor.php
@@ -2,26 +2,48 @@
 
 final class CBContainerView2Editor {
 
-    /**
-     * @return [string]
-     */
-    static function CBHTMLOutput_requiredClassNames() {
-        return ['CBUIImageChooser', 'CBUISpec', 'CBUISpecArrayEditor', 'CBUIStringEditor'];
-    }
+    /* -- CBHTMLOutput interfaces -- -- -- -- -- */
 
     /**
      * @return [string]
      */
     static function CBHTMLOutput_JavaScriptURLs() {
-        return [Colby::flexpath(__CLASS__, 'v366.js', cbsysurl())];
+        return [
+            Colby::flexpath(__CLASS__, 'v474.js', cbsysurl()),
+        ];
     }
+    /* CBHTMLOutput_JavaScriptURLs() */
+
 
     /**
      * @return [[string, mixed]]
      */
     static function CBHTMLOutput_JavaScriptVariables() {
         return [
-            ['CBContainerView2Editor_addableClassNames', CBPagesPreferences::classNamesForAddableViews()]
+            [
+                'CBContainerView2Editor_addableClassNames',
+                CBPagesPreferences::classNamesForAddableViews(),
+            ]
         ];
     }
+    /* CBHTMLOutput_JavaScriptVariables() */
+
+
+    /**
+     * @return [string]
+     */
+    static function CBHTMLOutput_requiredClassNames() {
+        return [
+            'CBImage',
+            'CBModel',
+            'CBUI',
+            'CBUIImageChooser',
+            'CBUISpec',
+            'CBUISpecArrayEditor',
+            'CBUIStringEditor',
+            'Colby',
+        ];
+    }
+    /* CBHTMLOutput_requiredClassNames() */
 }
+/* CBContainerView2Editor */
