24876cc591cd9590a366cec27df69d82f2021d41
Matt Calkins (MacBook Air) <matt@mattifesto.com>
2019-06-22 08:55:38 -0700

    Garden handlers/handle-default.php

--- handlers/handle-default.php
+++ handlers/handle-default.php
@@ -2,25 +2,33 @@
 
 header($_SERVER['SERVER_PROTOCOL'] . ' 404 Not Found');
 
-$URL = cbsiteurl() .
-       htmlspecialchars(preg_replace('/([\/\?&])/', ' $1 ', $_SERVER['REQUEST_URI']));
+$URL = cbsiteurl() . htmlspecialchars(
+    preg_replace(
+        '/([\/\?&])/',
+        ' $1 ',
+        $_SERVER['REQUEST_URI']
+    )
+);
+
 $cm = <<<EOT
 The page you requested was not found.
 
 `{$URL}`
 EOT;
 
-CBPage::renderSpec((object)[
-    'className' => 'CBViewPage',
-    'title' => 'Page Not Found',
-    'layout' => (object)[
-        'className' => 'CBPageLayout',
-    ],
-    'sections' => [
-        (object)[
-            'className' => 'CBTextView2',
-            'contentAsCommonMark' => $cm,
-            'CSSClassNames' => 'center',
+CBPage::renderSpec(
+    (object)[
+        'className' => 'CBViewPage',
+        'title' => 'Page Not Found',
+        'layout' => (object)[
+            'className' => 'CBPageLayout',
         ],
-    ],
-]);
+        'sections' => [
+            (object)[
+                'className' => 'CBTextView2',
+                'contentAsCommonMark' => $cm,
+                'CSSClassNames' => 'center',
+            ],
+        ],
+    ]
+);
