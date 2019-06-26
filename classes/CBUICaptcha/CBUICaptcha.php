7d65d2f92377e6ab64a122224f78890abc699ff6
Matt Calkins (MacBook Air) <matt@mattifesto.com>
2019-06-26 15:36:55 -0700

    Replace use of CBSiteDirectory, Colby::flexnameForJavaScriptForClass(), Colby::flexnameForCSSForClass() in CBUICaptcha.php

--- classes/CBUICaptcha/CBUICaptcha.php
+++ classes/CBUICaptcha/CBUICaptcha.php
@@ -2,12 +2,18 @@
 
 final class CBUICaptcha {
 
+    /* -- CBHTMLOutput interfaces -- -- -- -- -- */
+
     /**
      * @return [string]
      */
     static function CBHTMLOutput_CSSURLs() {
-        return [Colby::flexnameForCSSForClass(CBSystemURL, __CLASS__)];
+        return [
+            Colby::flexpath(__CLASS__, 'v484.css', cbsysurl()),
+        ];
     }
+    /* CBHTMLOutput_CSSURLs() */
+
 
     /**
      * @return [string]
@@ -15,18 +21,27 @@ final class CBUICaptcha {
     static function CBHTMLOutput_JavaScriptURLs() {
         return [
             'https://www.google.com/recaptcha/api.js',
-            Colby::flexnameForJavaScriptForClass(CBSystemURL, __CLASS__),
+            Colby::flexpath(__CLASS__, 'v484.js', cbsysurl()),
         ];
     }
+    /* CBHTMLOutput_JavaScriptURLs() */
+
 
     /**
      * @return [[string, mixed]]
      */
-    static function requiredJavaScriptVariables() {
+    static function CBHTMLOutput_JavaScriptVariables() {
         return [
-            ['CBUICaptchaReCAPTCHASiteKey', CBSitePreferences::reCAPTCHASiteKey()],
+            [
+                'CBUICaptchaReCAPTCHASiteKey',
+                CBSitePreferences::reCAPTCHASiteKey(),
+            ],
         ];
     }
+    /* CBHTMLOutput_JavaScriptVariables() */
+
+
+    /* -- functions -- -- -- -- -- */
 
     /**
      * @return bool
@@ -35,7 +50,12 @@ final class CBUICaptcha {
         $secretKey = CBSitePreferences::reCAPTCHASecretKey();
         $ch = curl_init();
 
-        curl_setopt($ch, CURLOPT_URL, 'https://www.google.com/recaptcha/api/siteverify');
+        curl_setopt(
+            $ch,
+            CURLOPT_URL,
+            'https://www.google.com/recaptcha/api/siteverify'
+        );
+
         curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
         curl_setopt($ch, CURLOPT_HEADER, false);
         curl_setopt($ch, CURLOPT_POST, true);
@@ -45,7 +65,7 @@ final class CBUICaptcha {
         ]);
 
         if (false) { /* enable to help debug cURL issues */
-            $fp = fopen(CBSiteDirectory . '/php_curl_errors.txt', 'w');
+            $fp = fopen(cbsitedir() . '/php_curl_errors.txt', 'w');
             curl_setopt($ch, CURLOPT_VERBOSE, 1);
             curl_setopt($ch, CURLOPT_STDERR, $fp);
         }
@@ -56,7 +76,9 @@ final class CBUICaptcha {
             $errno = curl_errno($ch);
             $error = curl_error($ch);
 
-            throw new RuntimeException("cURL error number: {$errno}. cURL error: {$error}");
+            throw new RuntimeException(
+                "cURL error number: {$errno}. cURL error: {$error}"
+            );
         }
 
         curl_close($ch);
@@ -64,4 +86,5 @@ final class CBUICaptcha {
         $result = json_decode($result);
         return $result->success;
     }
+    /* responseKeyIsValid() */
 }
