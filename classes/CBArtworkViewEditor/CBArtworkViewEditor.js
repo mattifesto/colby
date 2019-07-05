d772bf8f9d3cbcfea303211d250bf2418dbef39e
Matt Calkins (MacBook Air) <matt@mattifesto.com>
2019-07-05 09:44:09 -0700

    Garden CBArtworkViewEditor.js

--- classes/CBArtworkViewEditor/CBArtworkViewEditor.js
+++ classes/CBArtworkViewEditor/CBArtworkViewEditor.js
@@ -28,10 +28,11 @@ var CBArtworkViewEditor = {
      */
     createEditor: function (args) {
         var section, item;
-        var element = document.createElement("div");
-        element.className = "CBArtworkViewEditor";
+        var element = CBUI.createElement("CBArtworkViewEditor");
 
-        element.appendChild(CBUI.createHalfSpace());
+        element.appendChild(
+            CBUI.createHalfSpace()
+        );
 
         section = CBUI.createSection();
 
@@ -43,94 +44,145 @@ var CBArtworkViewEditor = {
         );
 
         item = CBUI.createSectionItem();
+
         item.appendChild(chooser.element);
         section.appendChild(item);
 
         item = CBUI.createSectionItem();
-        item.appendChild(CBUIStringEditor.createEditor({
-            labelText : "Alternative Text",
-            propertyName : "alternativeText",
-            spec : args.spec,
-            specChangedCallback : args.specChangedCallback,
-        }).element);
+
+        item.appendChild(
+            CBUIStringEditor.createEditor(
+                {
+                    labelText: "Alternative Text",
+                    propertyName: "alternativeText",
+                    spec: args.spec,
+                    specChangedCallback: args.specChangedCallback,
+                }
+            ).element
+        );
+
         section.appendChild(item);
 
         item = CBUI.createSectionItem();
-        item.appendChild(CBUIStringEditor.createEditor({
-            labelText : "Caption",
-            propertyName : "captionAsMarkdown",
-            spec : args.spec,
-            specChangedCallback : args.specChangedCallback,
-        }).element);
+
+        item.appendChild(
+            CBUIStringEditor.createEditor(
+                {
+                    labelText: "Caption",
+                    propertyName: "captionAsMarkdown",
+                    spec: args.spec,
+                    specChangedCallback: args.specChangedCallback,
+                }
+            ).element
+        );
+
         section.appendChild(item);
 
         item = CBUI.createSectionItem();
-        item.appendChild(CBUISelector.create({
-            labelText : "Maximum Display Width",
-            navigateToItemCallback: args.navigateToItemCallback,
-            options: [
-                {title: "160 CSS pixels", value: "rw320"},
-                {title: "320 CSS pixels", value: "rw640"},
-                {title: "480 CSS pixels", value: "rw960"},
-                {title: "640 CSS pixels", value: "rw1280"},
-                {title: "800 CSS pixels (default)"},
-                {title: "960 CSS pixels", value: "rw1920"},
-                {title: "1280 CSS pixels", value: "rw2560"},
+        item.appendChild(
+            CBUISelector.create(
                 {
-                    title: "Image Width",
-                    description:
-                    "The maximum width in CSS pixels is half the count of" +
-                    " horizontal pixels of the uploaded image.",
-                    value: "original",
-                },
-                {
-                    title: "Page Width",
-                    description:
-                    "The uploaded image will always use the full width of" +
-                    " the page regardless of its size.",
-                    value: "page",
-                },
-            ],
-            propertyName : "size",
-            spec : args.spec,
-            specChangedCallback : args.specChangedCallback,
-        }).element);
+                    labelText: "Maximum Display Width",
+                    navigateToItemCallback: args.navigateToItemCallback,
+                    options: [
+                        {
+                            title: "160 CSS pixels",
+                            value: "rw320",
+                        },
+                        {
+                            title: "320 CSS pixels",
+                            value: "rw640",
+                        },
+                        {
+                            title: "480 CSS pixels",
+                            value: "rw960",
+                        },
+                        {
+                            title: "640 CSS pixels",
+                            value: "rw1280",
+                        },
+                        {
+                            title: "800 CSS pixels (default)",
+                        },
+                        {
+                            title: "960 CSS pixels",
+                            value: "rw1920",
+                        },
+                        {
+                            title: "1280 CSS pixels",
+                            value: "rw2560",
+                        },
+                        {
+                            title: "Image Width",
+                            description:
+                            "The maximum width in CSS pixels is half the count of" +
+                            " horizontal pixels of the uploaded image.",
+                            value: "original",
+                        },
+                        {
+                            title: "Page Width",
+                            description:
+                            "The uploaded image will always use the full width of" +
+                            " the page regardless of its size.",
+                            value: "page",
+                        },
+                    ],
+                    propertyName: "size",
+                    spec: args.spec,
+                    specChangedCallback: args.specChangedCallback,
+                }
+            ).element
+        );
+
         section.appendChild(item);
 
         element.appendChild(section);
 
         /* CSSClassNames */
 
-        element.appendChild(CBUI.createHalfSpace());
+        element.appendChild(
+            CBUI.createHalfSpace()
+        );
 
-        element.appendChild(CBUI.createSectionHeader({
-            paragraphs: [
-                `
-                Supported Class Names:
-                `,`
-                hideSocial: This will hide links to share the image via social
-                networks.
-                `,`
-                left: Align the caption text to the left.
-                `,`
-                right: Align the caption text to the right.
-                `
-           ],
-        }));
+        element.appendChild(
+            CBUI.createSectionHeader(
+                {
+                    paragraphs: [
+                        `
+                        Supported Class Names:
+                        `,`
+                        hideSocial: This will hide links to share the image via
+                        social networks.
+                        `,`
+                        left: Align the caption text to the left.
+                        `,`
+                        right: Align the caption text to the right.
+                        `
+                   ],
+                }
+            )
+        );
 
         section = CBUI.createSection();
 
         item = CBUI.createSectionItem();
-        item.appendChild(CBUIStringEditor.createEditor({
-            labelText : "CSS Class Names",
-            propertyName : "CSSClassNames",
-            spec : args.spec,
-            specChangedCallback : args.specChangedCallback,
-        }).element);
+        item.appendChild(
+            CBUIStringEditor.createEditor(
+                {
+                    labelText: "CSS Class Names",
+                    propertyName: "CSSClassNames",
+                    spec: args.spec,
+                    specChangedCallback: args.specChangedCallback,
+                }
+            ).element
+        );
+
         section.appendChild(item);
         element.appendChild(section);
 
-        element.appendChild(CBUI.createHalfSpace());
+        element.appendChild(
+            CBUI.createHalfSpace()
+        );
 
         /* set thumbnail */
 
