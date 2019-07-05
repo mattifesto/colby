e556db6b7c9891cf75dc6de462c88aff2f407517
Matt Calkins (MacBook Air) <matt@mattifesto.com>
2019-07-04 20:45:08 -0700

    Remove CBAjax_errorTest() in CBUnitTests.php

--- classes/CBUnitTests/CBUnitTests.php
+++ classes/CBUnitTests/CBUnitTests.php
@@ -5,27 +5,13 @@ class CBUnitTests {
     /**
      * @return null
      */
-    static function CBAjax_errorTest() {
-        //throw new RuntimeException(str_repeat("This is a test of a long message. ", 1000));
-        throw new RuntimeException('Test exception thrown inside the PHP implementation of CBUnitTests::CBAjax_errorTest()');
-    }
-
-    /**
-     * @return string
-     */
-    static function CBAjax_errorTest_group() {
-        return 'Developers';
-    }
-
-    /**
-     * @return null
-     */
     static function getListOfTestsForAjax() {
         $response = new CBAjaxResponse();
         $response->tests = CBTest::phpTests();
         $response->wasSuccessful = true;
         $response->send();
     }
+
 
     /**
      * @return object
