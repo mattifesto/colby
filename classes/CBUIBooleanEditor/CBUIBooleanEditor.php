6e1e98e00dedb7bd39625a0a70f40b2aa9c94a8e
Matt Calkins (MacBook Air) <matt@mattifesto.com>
2019-07-08 11:51:15 -0700

    Fix flex in CBUIBooleanEditor.css

--- classes/CBUIBooleanEditor/CBUIBooleanEditor.php
+++ classes/CBUIBooleanEditor/CBUIBooleanEditor.php
@@ -1,38 +1,38 @@
 <?php
 
 /**
- * @deprecated 2018.03.09 use CBUIStringsPart and CBUIBooleanSwitchPart
+ * @deprecated 2018_03_09 use CBUIStringsPart and CBUIBooleanSwitchPart
  */
 final class CBUIBooleanEditor {
 
     /**
      * @return [string]
      */
-    static function requiredClassNames() {
-        return ['CBUI'];
-    }
-
-    /**
-     * @return [string]
-     */
     static function CBHTMLOutput_CSSURLs() {
-        return [CBUIBooleanEditor::URL('CBUIBooleanEditor.css')];
+        return [
+            Colby::flexpath(__CLASS__, 'v485.css', cbsysurl()),
+        ];
     }
+    /* CBHTMLOutput_CSSURLs() */
+
 
     /**
      * @return [string]
      */
     static function CBHTMLOutput_JavaScriptURLs() {
-        return [CBUIBooleanEditor::URL('CBUIBooleanEditor.js')];
+        return [
+            Colby::flexpath(__CLASS__, 'v485.js', cbsysurl()),
+        ];
     }
+    /* CBHTMLOutput_JavaScriptURLs() */
 
     /**
-     * @param string $filename
-     *
-     * @return string
+     * @return [string]
      */
-    static function URL($filename) {
-        $className = __CLASS__;
-        return CBSystemURL . "/classes/{$className}/{$filename}";
+    static function CBHTMLOutput_requiredClassNames() {
+        return [
+            'CBUI',
+        ];
     }
 }
+/* CBUIBooleanEditor */
