5515abbd98ed1b84a5443471f1a75f538142d9ca
Matt Calkins (MacBook Air) <matt@mattifesto.com>
2019-07-11 08:40:37 -0700

    Garden CBModelUpgradeTask.php

--- classes/CBModelUpgradeTask/CBModelUpgradeTask.php
+++ classes/CBModelUpgradeTask/CBModelUpgradeTask.php
@@ -2,6 +2,8 @@
 
 final class CBModelUpgradeTask {
 
+    /* -- CBInstall interfaces -- -- -- -- -- */
+
     /**
      * Restart this task for every existing model on every time the site is
      * upgraded. These tasks run a slightly higher than normal priority.
@@ -9,33 +11,51 @@ final class CBModelUpgradeTask {
      * @return void
      */
     static function CBInstall_install(): void {
-        $IDs = CBDB::SQLToArray('SELECT LOWER(HEX(ID)) FROM CBModels');
+        $IDs = CBDB::SQLToArray(
+            'SELECT LOWER(HEX(ID)) FROM CBModels'
+        );
 
-        CBTasks2::restart('CBModelUpgradeTask', $IDs, /* priority: */ 90);
+        CBTasks2::restart(
+            'CBModelUpgradeTask',
+            $IDs,
+            90 /* priority: */
+        );
     }
+    /* CBInstall_install */
+
 
     /**
      * @return [string]
      */
     static function CBInstall_requiredClassNames(): array {
-        return ['CBModels', 'CBTasks2'];
+        return [
+            'CBModels',
+            'CBTasks2'
+        ];
     }
+    /* CBInstall_requiredClassNames() */
+
+
+    /* -- CBTasks2 intrfaces -- -- -- -- -- */
 
     /**
-     * @param ID $ID
+     * @param string $ID
      *
      * @return ?object
      */
-    static function CBTasks2_run($ID): ?stdClass {
+    static function CBTasks2_run(string $ID): ?stdClass {
         $spec = CBModels::fetchSpecByID($ID);
         $upgradedSpec = CBModel::upgrade($spec);
 
         if ($upgradedSpec != $spec) {
-            CBDB::transaction(function () use ($upgradedSpec) {
-                CBModels::save($upgradedSpec);
-            });
+            CBDB::transaction(
+                function () use ($upgradedSpec) {
+                    CBModels::save($upgradedSpec);
+                }
+            );
         }
 
         return null;
     }
+    /* CBTasks2_run() */
 }
