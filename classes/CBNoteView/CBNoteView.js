5c2a2f9d1c211125bcb7c63c22f67ff0471d6f2b
Matt Calkins (MacBook Air) <matt@mattifesto.com>
2019-07-18 14:52:24 -0700

    Add CBNoteView

---
+++ classes/CBNoteView/CBNoteView.js
@@ -0,0 +1,36 @@
+"use strict";
+/* jshint strict: global */
+/* jshint esversion: 6 */
+/* exported CBNoteView */
+/* global
+    CBModel,
+    CBUI,
+*/
+
+var CBNoteView = {
+
+    /**
+     * @param object model
+     *
+     * @return object
+     *
+     *      {
+     *          element: Element (readonly)
+     *      }
+     */
+    CBView_create: function (model) {
+        let element = CBUI.createElement("CBNoteView");
+        let textElement = CBUI.createElement("CBNoteView_text");
+
+        element.appendChild(textElement);
+
+        textElement.textContent = CBModel.valueToString(model, "note.text");
+
+        return {
+            get element() {
+                return element;
+            }
+        };
+    },
+    /* CBView_create() */
+};
---
+++ classes/CBNoteView/CBNoteView.php
@@ -0,0 +1,37 @@
+<?php
+
+final class CBNoteView {
+
+    /**
+     * @return [string]
+     */
+    static function CBHTMLOutput_CSSURLs(): array {
+        return [
+            Colby::flexpath(__CLASS__, 'v487.css', cbsysurl()),
+        ];
+    }
+    /* CBHTMLOutput_CSSURLs() */
+
+
+    /**
+     * @return [string]
+     */
+    static function CBHTMLOutput_JavaScriptURLs(): array {
+        return [
+            Colby::flexpath(__CLASS__, 'v487.js', cbsysurl()),
+        ];
+    }
+    /* CBHTMLOutput_JavaScriptURLs() */
+
+
+    /**
+     * @return [string]
+     */
+    static function CBHTMLOutput_requiredClassNames(): array {
+        return [
+            'CBModel',
+            'CBUI',
+        ];
+    }
+    /* CBHTMLOutput_requiredClassNames() */
+}
