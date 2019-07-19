ff8fa12096025f3d6d37a8fdd2dde76b9c5917b2
Matt Calkins (MacBook Air) <matt@mattifesto.com>
2019-07-18 20:15:37 -0700

    Add CBUI_container_horizontalEqual in CBUI.css

--- classes/CBUI/CBUI.php
+++ classes/CBUI/CBUI.php
@@ -9,7 +9,7 @@ final class CBUI {
      */
     static function CBHTMLOutput_CSSURLs() {
         return [
-            Colby::flexpath(__CLASS__, 'v485.css', cbsysurl()),
+            Colby::flexpath(__CLASS__, 'v487.css', cbsysurl()),
         ];
     }
     /* CBHTMLOutput_CSSURLs() */
