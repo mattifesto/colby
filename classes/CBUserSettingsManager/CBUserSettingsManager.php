92e095159dc3b3c95f7d9fd3dae0bf4eaca510e9
Matt Calkins (MacBook Air) <matt@mattifesto.com>
2019-07-25 09:16:53 -0700

    Garden CBUserSettingsManager.php

--- classes/CBUserSettingsManager/CBUserSettingsManager.php
+++ classes/CBUserSettingsManager/CBUserSettingsManager.php
@@ -1,5 +1,13 @@
 <?php
 
+/**
+ * To show settings on individual user admin pages, creating a class that
+ * implements the CBUserSettingsManager_render() interface. The function may
+ * choose to only display settings interface elements if the administrative user
+ * is allowed to set them, for instance if only developers are allowed to change
+ * the settings for users. The function may choose to only display settings for
+ * users that can have them set, such as only for wholesale customer users.
+ */
 final class CBUserSettingsManager {
 
     /**
@@ -8,11 +16,16 @@ final class CBUserSettingsManager {
      *
      * @return void
      */
-    static function render(string $managerClassName, string $targetUserID): void {
+    static function render(
+        string $managerClassName,
+        string $targetUserID
+    ): void {
         CBHTMLOutput::requireClassName($managerClassName);
 
-        if (is_callable($function = "{$managerClassName}::CBUserSettingsManager_render")) {
-            call_user_func($function, $targetUserID);
+        $functionName = "{$managerClassName}::CBUserSettingsManager_render";
+
+        if (is_callable($functionName)) {
+            call_user_func($functionName, $targetUserID);
         }
     }
 }
