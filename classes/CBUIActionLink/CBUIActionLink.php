0bc462f846e18c6913e62b5e185448cfe26c9ffc
Matt Calkins (MacBook Air) <matt@mattifesto.com>
2019-08-05 10:06:06 -0700

    Remove requiredClassNames() in CBUIActionLink.php

--- classes/CBUIActionLink/CBUIActionLink.php
+++ classes/CBUIActionLink/CBUIActionLink.php
@@ -5,31 +5,18 @@ final class CBUIActionLink {
     /**
      * @return [string]
      */
-    static function requiredClassNames() {
-        return [];
+    static function CBHTMLOutput_CSSURLs(): array {
+        return [
+            Colby::flexpath(__CLASS__, 'css', cbsysurl()),
+        ];
     }
 
     /**
      * @return [string]
      */
-    static function CBHTMLOutput_CSSURLs() {
-        return [CBUIActionLink::URL('CBUIActionLink.css')];
-    }
-
-    /**
-     * @return [string]
-     */
-    static function CBHTMLOutput_JavaScriptURLs() {
-        return [CBUIActionLink::URL('CBUIActionLink.js')];
-    }
-
-    /**
-     * @param string $filename
-     *
-     * @return string
-     */
-    static function URL($filename) {
-        $className = __CLASS__;
-        return CBSystemURL . "/classes/{$className}/{$filename}";
+    static function CBHTMLOutput_JavaScriptURLs(): array {
+        return [
+            Colby::flexpath(__CLASS__, 'js', cbsysurl()),
+        ];
     }
 }
