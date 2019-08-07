9accdda092b28054fd4efbe22edeb9b88640adcd
Matt Calkins (MacBook Air) <matt@mattifesto.com>
2019-08-07 08:48:50 -0700

    Remove support for requiredClassNames() interface in CBHTMLOutput.php

--- classes/CBHTMLOutput/CBHTMLOutput.php
+++ classes/CBHTMLOutput/CBHTMLOutput.php
@@ -353,7 +353,6 @@ EOT;
             $requiredClassNames,
             [
                 'CBHTMLOutput_requiredClassNames',
-                'requiredClassNames', /* deprecated */
             ]
         );
 
