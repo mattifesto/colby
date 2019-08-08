bdc87ab66f0682a1459d536a2543958ed3e417ca
Matt Calkins (MacBook Air) <matt@mattifesto.com>
2019-08-08 08:56:15 -0700

    Replace use of CBGeneralAdminMenu::ID in CBTasks2Admin.php

--- classes/CBTasks2Admin/CBTasks2Admin.php
+++ classes/CBTasks2Admin/CBTasks2Admin.php
@@ -46,7 +46,9 @@ final class CBTasks2Admin {
      * @return void
      */
     static function CBInstall_install(): void {
-        $spec = CBModels::fetchSpecByID(CBGeneralAdminMenu::ID);
+        $spec = CBModels::fetchSpecByID(
+            CBGeneralAdminMenu::getModelID()
+        );
 
         $spec->items[] = (object)[
             'className' => 'CBMenuItem',
