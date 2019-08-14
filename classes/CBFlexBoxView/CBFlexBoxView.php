1399a67bcf62e5ef5f34fc39296f54a6fdeb3f70
Matt Calkins (MacBook Air) <matt@mattifesto.com>
2019-08-14 16:43:24 -0700

    Implement CBModel_build() in CBFlexBoxView.php

--- classes/CBFlexBoxView/CBFlexBoxView.php
+++ classes/CBFlexBoxView/CBFlexBoxView.php
@@ -1,7 +1,7 @@
 <?php
 
 /**
- * @deprecated 2018.03.21
+ * @deprecated 2018_03_21
  *
  *      This is a temporary class to upgrade the few remaining CBFlexBoxView
  *      models to CBContainerView2 models.
@@ -12,20 +12,43 @@ final class CBFlexBoxView {
      * @return void
      */
     static function CBInstall_install(): void {
-        CBViewCatalog::installView(__CLASS__, (object)['isUnsupported' => true]);
+        CBViewCatalog::installView(
+            __CLASS__,
+            (object)[
+                'isUnsupported' => true,
+            ]
+        );
     }
+
 
     /**
      * @return [string]
      */
     static function CBInstall_requiredClassNames(): array {
-        return ['CBViewCatalog'];
+        return [
+            'CBViewCatalog',
+        ];
     }
 
+
     /**
-     * @param model $spec
+     * @param object $spec
      *
-     * @return model
+     * @return object
+     *
+     *      This function returns an empty model because all models should
+     *      build, however this model gets upgraded so there should
+     *      theoretically be no models of this class remaining.
+     */
+    static function CBModel_build(stdClass $spec): stdClass {
+        return (object)[];
+    }
+
+
+    /**
+     * @param object $spec
+     *
+     * @return object
      */
     static function CBModel_upgrade(stdClass $spec): stdClass {
         $specAsJSONAsMessage = CBMessageMarkup::stringToMessage(
@@ -41,11 +64,13 @@ final class CBFlexBoxView {
 
 EOT;
 
-        CBLog::log((object)[
-            'message' => $message,
-            'sourceClassName' => __CLASS__,
-            'sourceID' => '7a17c8d3bdc1a02f3930f873ddd7df8f78ae3bfd',
-        ]);
+        CBLog::log(
+            (object)[
+                'message' => $message,
+                'sourceClassName' => __CLASS__,
+                'sourceID' => '7a17c8d3bdc1a02f3930f873ddd7df8f78ae3bfd',
+            ]
+        );
 
         $spec->className = 'CBContainerView2';
 
@@ -57,4 +82,5 @@ EOT;
 
         return $spec;
     }
+    /* CBModel_upgrade() */
 }
