2e6cfff7b61fa3aa630aa6ce36f0cc4af0a45940
Matt Calkins (MacBook Air) <matt@mattifesto.com>
2019-10-23 14:40:00 -0700

    Garden CBModelCache.php

--- classes/CBModelCache/CBModelCache.php
+++ classes/CBModelCache/CBModelCache.php
@@ -5,6 +5,8 @@ final class CBModelCache {
     private static $cache = [];
     private static $neededModelIDs = [];
 
+
+
     /**
      * This is the only function in this class that will query the database
      * directly.
@@ -14,17 +16,31 @@ final class CBModelCache {
      * @return void
      */
     static function cacheModelsByID(array $IDs): void {
-        $IDsToFetch = array_unique(array_merge($IDs, CBModelCache::$neededModelIDs));
-        $IDsToFetch = array_filter($IDsToFetch, function($ID) {
-            return empty(CBModelCache::modelByID($ID));
-        });
+        $IDsToFetch = array_unique(
+            array_merge(
+                $IDs,
+                CBModelCache::$neededModelIDs
+            )
+        );
+
+        $IDsToFetch = array_filter(
+            $IDsToFetch,
+            function($ID) {
+                return empty(CBModelCache::modelByID($ID));
+            }
+        );
 
         if (!empty($IDsToFetch)) {
-            CBModelCache::$cache = array_merge(CBModelCache::$cache, CBModels::fetchModelsByID($IDsToFetch));
+            CBModelCache::$cache = array_merge(
+                CBModelCache::$cache,
+                CBModels::fetchModelsByID($IDsToFetch)
+            );
         }
 
         CBModelCache::$neededModelIDs = [];
     }
+
+
 
     /**
      * This is the function to call if you need a model now. If the model isn't
@@ -43,12 +59,16 @@ final class CBModelCache {
         return CBModelCache::modelByID($ID);
     }
 
+
+
     /**
      * @return void
      */
     static function fetchModelLazilyByID($ID): void {
         CBModelCache::$neededModelIDs[] = $ID;
     }
+
+
 
     /**
      * @param [ID] $IDs
@@ -67,19 +87,29 @@ final class CBModelCache {
             $model = CBModelCache::modelByID($ID);
 
             if (!empty($model)) {
-                array_push($models, $model);
+                array_push(
+                    $models,
+                    $model
+                );
             }
         }
 
         return $models;
     }
 
+
+
     /**
      * @return void
      */
     static function fetchModelsLazilyByID(array $IDs): void {
-        CBModelCache::$neededModelIDs = array_merge(CBModelCache::$neededModelIDs, $IDs);
+        CBModelCache::$neededModelIDs = array_merge(
+            CBModelCache::$neededModelIDs,
+            $IDs
+        );
     }
+
+
 
     /**
      * @param ID $ID
@@ -89,8 +119,14 @@ final class CBModelCache {
      *      Returns the model if it's cached; otherwise null.
      */
     static function modelByID($ID): ?stdClass {
-        return isset(CBModelCache::$cache[$ID]) ? CBModelCache::$cache[$ID] : null;
+        return (
+            isset(CBModelCache::$cache[$ID]) ?
+            CBModelCache::$cache[$ID] :
+            null
+        );
     }
+
+
 
     /**
      * @param [ID] $IDs
@@ -98,8 +134,13 @@ final class CBModelCache {
      * @return void
      */
     static function uncacheByID(array $IDs): void {
-        CBModelCache::$cache = array_filter(CBModelCache::$cache, function ($key) use ($IDs) {
-            return isset($IDs[$key]);
-        }, ARRAY_FILTER_USE_KEY);
+        CBModelCache::$cache = array_filter(
+            CBModelCache::$cache,
+            function ($key) use ($IDs) {
+                return isset($IDs[$key]);
+            },
+            ARRAY_FILTER_USE_KEY
+        );
     }
+
 }
