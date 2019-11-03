6462cd76d3ce03433b6c4414fd1a43f5af3300d8
Matt Calkins (MacBook Air) <matt@mattifesto.com>
2019-11-02 17:58:25 -0700

    Remove CBEqualizePageSettingsPart.js

--- classes/CBEqualizePageSettingsPart/CBEqualizePageSettingsPart.php
+++ classes/CBEqualizePageSettingsPart/CBEqualizePageSettingsPart.php
@@ -10,11 +10,9 @@ final class CBEqualizePageSettingsPart {
      * @return void
      */
     static function CBPageSettings_renderHeadElementHTML(): void {
-        $JavaScriptURL = Colby::flexpath(__CLASS__, 'v468.js', cbsysurl());
         $CSSURL = Colby::flexpath(__CLASS__, 'v468.css', cbsysurl());
 
         ?>
-        <script src="<?= $JavaScriptURL ?>"></script>
         <link rel="stylesheet" href="<?= $CSSURL ?>">
         <?php
     }
