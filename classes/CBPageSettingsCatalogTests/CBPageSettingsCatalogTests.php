4c046e5fb50a984f897c0816979485444b2fb1d8
Matt Calkins (MacBook Air) <matt@mattifesto.com>
2019-11-04 09:30:29 -0800

    Replace CBUnitTests_tests() in CBPageSettingsCatalogTests.php

--- classes/CBPageSettingsCatalogTests/CBPageSettingsCatalogTests.php
+++ classes/CBPageSettingsCatalogTests/CBPageSettingsCatalogTests.php
@@ -2,31 +2,71 @@
 
 final class CBPageSettingsCatalogTests {
 
+    /* -- CBTest interfaces -- -- -- -- -- */
+
+
+
+    /**
+     * @return [object]
+     */
+    static function CBTest_getTests(): array {
+        return [
+            (object)[
+                'name' => 'test',
+                'title' => 'CBPageSettingsCatalog',
+                'type' => 'server',
+            ],
+        ];
+    }
+
+
+
+    /* -- tests -- -- -- -- -- */
+
+
+
     /**
      * @return object
      */
     static function CBTest_test(): stdClass {
-        CBPageSettingsCatalog::$testID = '0fc48f5a3d1ee3322de14822184bb162e15efdbc';
+        CBPageSettingsCatalog::$testID =
+        '0fc48f5a3d1ee3322de14822184bb162e15efdbc';
 
         CBPageSettingsCatalog::CBInstall_install();
-        CBPageSettingsCatalog::install('CBPageSettingsCatalogTests_pageSettings1');
-        CBPageSettingsCatalog::install('CBPageSettingsCatalogTests_pageSettings2');
-        CBPageSettingsCatalog::install('CBPageSettingsCatalogTests_fake');
-        CBPageSettingsCatalog::install('CBPageSettingsCatalogTests_pageSettings1');
-        CBPageSettingsCatalog::install('CBPageSettingsCatalogTests_pageSettings2');
+
+        CBPageSettingsCatalog::install(
+            'CBPageSettingsCatalogTests_pageSettings1'
+        );
+
+        CBPageSettingsCatalog::install(
+            'CBPageSettingsCatalogTests_pageSettings2'
+        );
+
+        CBPageSettingsCatalog::install(
+            'CBPageSettingsCatalogTests_fake'
+        );
+
+        CBPageSettingsCatalog::install(
+            'CBPageSettingsCatalogTests_pageSettings1'
+        );
+
+        CBPageSettingsCatalog::install(
+            'CBPageSettingsCatalogTests_pageSettings2'
+        );
 
         $pageSettingsClassNames = CBPageSettingsCatalog::fetchClassNames();
+
         $expectedClassNames = [
             'CBPageSettingsCatalogTests_pageSettings1',
             'CBPageSettingsCatalogTests_pageSettings2',
         ];
 
         if ($pageSettingsClassNames != $expectedClassNames) {
-            return (object)[
-                'message' =>
-                    "The page settings class names do not match the expected class names\n\n" .
-                    CBConvertTests::resultAndExpectedToMessage($pageSettingsClassNames, $expectedClassNames),
-            ];
+            return CBTest::resultMismatchFailure(
+                'test 1',
+                $pageSettingsClassNames,
+                $expectedClassNames
+            );
         }
 
         CBModels::deleteByID(CBPageSettingsCatalog::$testID);
@@ -37,20 +77,16 @@ final class CBPageSettingsCatalogTests {
             'succeeded' => true,
         ];
     }
+    /* CBTest_test() */
 
-
-    /**
-     * @return [[<class>, <name>]]
-     */
-    static function CBUnitTests_tests(): array {
-        return [
-            ['CBPageSettingsCatalog', 'test']
-        ];
-    }
 }
+
+
 
 final class CBPageSettingsCatalogTests_pageSettings1 {
 }
+
+
 
 final class CBPageSettingsCatalogTests_pageSettings2 {
 }
