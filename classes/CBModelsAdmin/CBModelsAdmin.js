a08444a430ecd47a13691d6c4b1f94a221716e63
Matt Calkins (MacBook Air) <matt@mattifesto.com>
2019-11-06 09:22:27 -0800

    Add createModelListItemElement() in CBModelsAdmin.js

--- classes/CBModelsAdmin/CBModelsAdmin.js
+++ classes/CBModelsAdmin/CBModelsAdmin.js
@@ -52,6 +52,66 @@
 
 
     /**
+     * @return Element
+     */
+    function createModelListItemElement(modelListItem) {
+        var sectionItem = CBUISectionItem4.create();
+
+        sectionItem.callback = function () {
+            window.location = (
+                "/admin/?c=CBModelEditor&ID=" +
+                encodeURIComponent(modelListItem.ID)
+            );
+        };
+
+
+        /* thumbnail part */
+
+        let thumbnailPart = CBUIThumbnailPart.create();
+
+        sectionItem.appendPart(thumbnailPart);
+
+        if (modelListItem.image) {
+            thumbnailPart.src = CBImage.toURL(
+                modelListItem.image,
+                'rs200clc200'
+            );
+        }
+
+
+        /* title and description part */
+
+        var titleAndDescriptionPart =
+        CBUITitleAndDescriptionPart.create();
+
+        var title =
+        modelListItem.title ?
+        modelListItem.title.trim() :
+        '';
+
+        if (title === "") {
+            title = CBModelsAdmin_modelClassName + " (no title)";
+        }
+
+        titleAndDescriptionPart.title = title;
+        titleAndDescriptionPart.description = modelListItem.ID;
+
+        sectionItem.appendPart(titleAndDescriptionPart);
+
+
+        /* navigation arrow */
+
+        sectionItem.appendPart(
+            CBUINavigationArrowPart.create()
+        );
+
+        return sectionItem.element;
+    }
+    /* createModelSectionItemElement() */
+
+
+
+    /**
      * @return undefined
      */
     function renderClassNameList() {
@@ -126,87 +186,28 @@
             )
         );
 
-        mainElement.appendChild(
-            CBUI.createHalfSpace()
-        );
-
         if (CBModelsAdmin_modelList.length > 0) {
-            var section = CBUI.createSection();
-
-            CBModelsAdmin_modelList.forEach(function (model) {
-                var sectionItem = CBUISectionItem4.create();
-
-                sectionItem.callback = function () {
-                    window.location = (
-                        "/admin/?c=CBModelEditor&ID=" +
-                        encodeURIComponent(model.ID)
-                    );
-                };
-
-                /* thumbnail part */
-
-                let thumbnailPart = CBUIThumbnailPart.create();
-
-                sectionItem.appendPart(thumbnailPart);
-
-                if (model.image) {
-                    thumbnailPart.src = CBImage.toURL(
-                        model.image,
-                        'rs200clc200'
-                    );
-                }
-
-                /* title and description part */
-
-                var titleAndDescriptionPart =
-                CBUITitleAndDescriptionPart.create();
-
-                var title = model.title ? model.title.trim() : '';
-
-                if (title === "") {
-                    title = CBModelsAdmin_modelClassName + " (no title)";
-                }
-
-                titleAndDescriptionPart.title = title;
-                titleAndDescriptionPart.description = model.ID;
-
-                sectionItem.appendPart(titleAndDescriptionPart);
-
-                sectionItem.appendPart(CBUINavigationArrowPart.create());
-
-                /* export
-                var exportCommandElement = document.createElement("div");
-                exportCommandElement.className = "command";
-                exportCommandElement.textContent = "Export";
-
-                exportCommandElement.addEventListener("click", function () {
-                    window.location = "/admin/models/export/?ID=" +
-                                      encodeURIComponent(model.ID);
-                });
-
-                item.commandsElement.appendChild(exportCommandElement);
-
-                /* inspect
-                var inspectCommandElement = document.createElement("div");
-                inspectCommandElement.className = "command";
-                inspectCommandElement.textContent = "Inspect";
-
-                inspectCommandElement.addEventListener("click", function () {
-                    window.location = "/admin/?c=CBModelInspector&ID=" +
-                                      encodeURIComponent(model.ID);
-                });
-
-                item.commandsElement.appendChild(inspectCommandElement);
-                */
-
-                section.appendChild(sectionItem.element);
-            });
-
-            mainElement.appendChild(section);
-
-            mainElement.appendChild(
-                CBUI.createHalfSpace()
+            let sectionContainerElement = CBUI.createElement(
+                "CBUI_sectionContainer"
             );
+
+            let sectionElement = CBUI.createElement(
+                "CBUI_section"
+            );
+
+            sectionContainerElement.appendChild(
+                sectionElement
+            );
+
+            CBModelsAdmin_modelList.forEach(
+                function (modelListItem) {
+                    sectionElement.appendChild(
+                        createModelListItemElement(modelListItem)
+                    );
+                }
+            );
+
+            mainElement.appendChild(sectionContainerElement);
         }
     }
     /* renderModelList() */
