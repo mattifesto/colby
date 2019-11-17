264ff6650bfcfea3678c9da2384c3772f090fa54
Matt Calkins (MacBook Air) <matt@mattifesto.com>
2019-11-17 13:20:08 -0800

    Move to associated user groups in CBAdministratorsUserGroup.php

--- classes/CBAdministratorsUserGroup/CBAdministratorsUserGroup.php
+++ classes/CBAdministratorsUserGroup/CBAdministratorsUserGroup.php
@@ -2,17 +2,77 @@
 
 final class CBAdministratorsUserGroup {
 
+    /* -- CBInstall interfaces -- -- -- -- -- */
+
+
+
     /**
      * @return void
      */
     static function CBInstall_install(): void {
-        CBUsers::installUserGroup('Administrators');
+        $groupCBID = CBUserGroup::userGroupClassNameToCBID(
+            __CLASS__
+        );
+
+        CBModelUpdater::update(
+            (object)[
+                'ID' => $groupCBID,
+                'className' => 'CBUserGroup',
+                'deprecatedGroupName' => 'Administrators',
+                'userGroupClassName' => __CLASS__,
+            ]
+        );
+
+        $tableName = 'ColbyUsersWhoAreAdministrators';
+
+        $SQL = <<<EOT
+
+            SELECT  COUNT(*)
+
+            FROM    information_schema.TABLES
+
+            WHERE   TABLE_SCHEMA = DATABASE() AND
+                    TABLE_NAME = '{$tableName}'
+
+        EOT;
+
+        $count = CBConvert::valueAsInt(
+            CBDB::SQLToValue($SQL)
+        );
+
+        if ($count === 0) {
+            return;
+        }
+
+        $SQL = <<<EOT
+
+            SELECT  userID
+
+            FROM    {$tableName}
+
+        EOT;
+
+        $userNumericIDs = CBDB::SQLToArrayOfNullableStrings($SQL);
+
+        $userCBIDs = CBUsers::userNumericIDsToUserCBIDs($userNumericIDs);
+
+        CBUserGroup::addUsers(__CLASS__, $userCBIDs);
+
+        Colby::query(
+            "DROP TABLE {$tableName}"
+        );
     }
+    /* CBInstall_install() */
+
+
 
     /**
      * @return [string]
      */
     static function CBInstall_requiredClassNames(): array {
-        return ['CBUsers'];
+        return [
+            'CBUsers'
+        ];
     }
+
 }
