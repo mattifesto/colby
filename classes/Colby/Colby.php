819cd68b71f52fa0851681c7eaea804ea3dd7742
Matt Calkins (MacBook Air) <matt@mattifesto.com>
2019-11-29 13:47:50 -0800

    Update expected shopping cart library version number to 130

--- classes/Colby/Colby.php
+++ classes/Colby/Colby.php
@@ -47,7 +47,7 @@ final class Colby {
 
         if (defined('SCLibraryVersionNumber')) {
             $actualLibraryVersionNumber = SCLibraryVersionNumber;
-            $expectedLibraryVersionNumber = '129';
+            $expectedLibraryVersionNumber = '130';
 
             if (
                 $actualLibraryVersionNumber !==
