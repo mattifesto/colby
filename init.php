ea66e0a54aef5e6bbf64d444e872e589436a2875
Matt Calkins (MacBook Air) <matt@mattifesto.com>
2019-12-09 14:30:58 -0800

    Use set_exception_handler() in init.php

--- init.php
+++ init.php
@@ -1,82 +1,85 @@
 <?php
 
-try {
+set_exception_handler(
+    function (Throwable $throwable) {
+        $message = $throwable->getMessage();
+        $filename = $throwable->getFile();
+        $line = $throwable->getLine();
 
-    /**
-     * Colby sites always run with all error reporting turned on.
-     */
-
-    error_reporting(E_ALL | E_STRICT);
-
-    /**
-     * These constants are set but shouldn't be used. Use the cbsitedir() and
-     * cbsysdir() functions instead.
-     */
-
-    define(
-        'CBSiteDirectory',
-        realpath($_SERVER['DOCUMENT_ROOT'])
-    );
-
-    define(
-        'CBSystemDirectory',
-        __DIR__
-    );
-
-    /**
-     * @return string
-     */
-    function cbsitedir() {
-        return CBSiteDirectory;
+        error_log(
+            "\"{$message}\" in {$filename} line {$line}" .
+            ' | error log entry made in ' .
+            __FILE__
+        );
     }
+);
 
-    /**
-     * @return string
-     */
-    function cbsysdir() {
-        return CBSystemDirectory;
-    }
 
-    /* deprecated */
-    define(
-        'COLBY_SYSTEM_DIRECTORY',
-        CBSystemDirectory
-    );
 
-    /* deprecated */
-    define(
-        'COLBY_SITE_DIRECTORY',
-        CBSiteDirectory
-    );
+/**
+ * Colby sites always run with all error reporting turned on.
+ */
 
-    /**
-     * Includes performed before setting up error handling should use
-     * `require` or `require_once` to halt execution if they aren't
-     * successful.
-     *
-     * Includes performed after setting up error handling should use
-     * `include` or `include_once` which will invoke the error handling
-     * mechanism if they aren't successful.
-     *
-     * Only the following lines should use `require_once`.
-     */
+error_reporting(E_ALL | E_STRICT);
 
-    require_once cbsitedir() . '/version.php';
-    require_once cbsysdir() . '/version.php';
-    require_once cbsysdir() . '/functions.php';
-    require_once cbsysdir() . '/classes/Colby/Colby.php';
 
-    CBSitePreferences::initialize();
-} catch (Throwable $throwable) {
-    $message = $throwable->getMessage();
-    $filename = $throwable->getFile();
-    $line = $throwable->getLine();
+/**
+ * These constants are set but shouldn't be used. Use the cbsitedir() and
+ * cbsysdir() functions instead.
+ */
 
-    error_log(
-        "An error was caught in " .
-        __FILE__ .
-        ": \"{$message}\" in {$filename} line {$line}"
-    );
+define(
+    'CBSiteDirectory',
+    realpath($_SERVER['DOCUMENT_ROOT'])
+);
 
-    exit(1);
+define(
+    'CBSystemDirectory',
+    __DIR__
+);
+
+/**
+ * @return string
+ */
+function cbsitedir() {
+    return CBSiteDirectory;
 }
+
+/**
+ * @return string
+ */
+function cbsysdir() {
+    return CBSystemDirectory;
+}
+
+/* deprecated */
+define(
+    'COLBY_SYSTEM_DIRECTORY',
+    CBSystemDirectory
+);
+
+/* deprecated */
+define(
+    'COLBY_SITE_DIRECTORY',
+    CBSiteDirectory
+);
+
+
+/**
+ * Includes performed before setting up error handling should use
+ * `require` or `require_once` to halt execution if they aren't
+ * successful.
+ *
+ * Includes performed after setting up error handling should use
+ * `include` or `include_once` which will invoke the error handling
+ * mechanism if they aren't successful.
+ *
+ * Only the following lines should use `require_once`.
+ */
+
+require_once cbsitedir() . '/version.php';
+require_once cbsysdir() . '/version.php';
+require_once cbsysdir() . '/functions.php';
+require_once cbsysdir() . '/classes/Colby/Colby.php';
+
+CBSitePreferences::initialize();
