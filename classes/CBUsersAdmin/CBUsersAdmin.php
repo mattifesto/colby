a22bb22c7d71db1b5e8fcc0d7139ba9087adad2e
Matt Calkins (MacBook Air) <matt@mattifesto.com>
2019-12-13 11:22:14 -0800

    Add CBAdmin_getUserGroupClassName() in CBUsersAdmin.php

--- classes/CBUsersAdmin/CBUsersAdmin.php
+++ classes/CBUsersAdmin/CBUsersAdmin.php
@@ -7,6 +7,15 @@ final class CBUsersAdmin {
 
 
     /**
+     * @return string
+     */
+    static function CBAdmin_getUserGroupClassName(): string {
+        return 'CBAdministratorsUserGroup';
+    }
+
+
+
+    /**
      * @return [string]
      */
     static function CBAdmin_menuNamePath(): array {
