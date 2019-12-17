be893338a2ef085851a6839b2a43bfe3c9c94147
Matt Calkins (MacBook Air) <matt@mattifesto.com>
2019-12-16 20:07:56 -0800

    Add CBTest_upgrade() in CBHideByUserGroupViewTests.php

--- classes/CBHideByUserGroupViewTests/CBHideByUserGroupViewTests.php
+++ classes/CBHideByUserGroupViewTests/CBHideByUserGroupViewTests.php
@@ -4,15 +4,21 @@ final class CBHideByUserGroupViewTests {
 
     /* -- CBTest interfaces -- -- -- -- -- */
 
+
+
     static function CBTest_getTests(): array {
         return [
             (object)[
                 'name' => 'general',
-                'title' => 'CBHideByUserGroupView',
+                'type' => 'server',
+            ],
+            (object)[
+                'name' => 'upgrade',
                 'type' => 'server',
             ],
         ];
     }
+    /* CBTest_getTests() */
 
 
 
@@ -36,8 +42,9 @@ final class CBHideByUserGroupViewTests {
             'className' => 'CBHideByUserGroupView',
             'hideFromMembers' => false,
             'hideFromNonmembers' => false,
-            'groupName' => "",
+            'groupName' => null,
             'subviews' => CBViewTests::testSubviewModels(),
+            'userGroupClassName' => null,
         ];
 
         $actualModel = CBModel::build($spec);
@@ -91,5 +98,146 @@ final class CBHideByUserGroupViewTests {
         ];
     }
     /* CBTests_general() */
+
+
+
+    /**
+     * @return object
+     */
+    static function CBTest_upgrade(): stdClass {
+        $tests = [
+            (object)[
+                'name' => 'empty',
+
+                'originalSpec' => (object)[
+                    'className' => 'CBHideByUserGroupView',
+                ],
+
+                'expectedUpgradedSpec' => (object)[
+                    'className' => 'CBHideByUserGroupView',
+                    'subviews' => [],
+                ],
+            ],
+            (object)[
+                'name' => 'invalid group name',
+
+                'originalSpec' => (object)[
+                    'className' => 'CBHideByUserGroupView',
+                    'groupName' => '  $  ',
+                ],
+
+                'expectedUpgradedSpec' => (object)[
+                    'className' => 'CBHideByUserGroupView',
+                    'subviews' => [],
+                ],
+            ],
+            (object)[
+                'name' => 'white space around group name',
+
+                'originalSpec' => (object)[
+                    'className' => 'CBHideByUserGroupView',
+                    'groupName' => '  Administrators  ',
+                ],
+
+                'expectedUpgradedSpec' => (object)[
+                    'className' => 'CBHideByUserGroupView',
+                    'subviews' => [],
+                    'userGroupClassName' => 'CBAdministratorsUserGroup',
+                ],
+            ],
+            (object)[
+                'name' => 'public',
+
+                'originalSpec' => (object)[
+                    'className' => 'CBHideByUserGroupView',
+                    'groupName' => 'Public',
+                ],
+
+                'expectedUpgradedSpec' => (object)[
+                    'className' => 'CBHideByUserGroupView',
+                    'subviews' => [],
+                    'userGroupClassName' => 'CBPublicUserGroup',
+                ],
+            ],
+            (object)[
+                'name' => 'simple upgrade administrators',
+
+                'originalSpec' => (object)[
+                    'className' => 'CBHideByUserGroupView',
+                    'groupName' => 'Administrators',
+                ],
+
+                'expectedUpgradedSpec' => (object)[
+                    'className' => 'CBHideByUserGroupView',
+                    'subviews' => [],
+                    'userGroupClassName' => 'CBAdministratorsUserGroup'
+                ],
+            ],
+            (object)[
+                'name' => 'simple upgrade developers',
+
+                'originalSpec' => (object)[
+                    'className' => 'CBHideByUserGroupView',
+                    'groupName' => 'Developers',
+                ],
+
+                'expectedUpgradedSpec' => (object)[
+                    'className' => 'CBHideByUserGroupView',
+                    'subviews' => [],
+                    'userGroupClassName' => 'CBDevelopersUserGroup'
+                ],
+            ],
+            (object)[
+                'name' => 'both properties set',
+
+                'originalSpec' => (object)[
+                    'className' => 'CBHideByUserGroupView',
+                    'groupName' => 'Administrators',
+                    'userGroupClassName' => 'CBDevelopersUserGroup',
+                ],
+
+                'expectedUpgradedSpec' => (object)[
+                    'className' => 'CBHideByUserGroupView',
+                    'subviews' => [],
+                    'userGroupClassName' => 'CBDevelopersUserGroup',
+                ],
+            ],
+            (object)[
+                'name' => 'old property set to user group class name',
+
+                'originalSpec' => (object)[
+                    'className' => 'CBHideByUserGroupView',
+                    'groupName' => 'CBDevelopersUserGroup',
+                ],
+
+                'expectedUpgradedSpec' => (object)[
+                    'className' => 'CBHideByUserGroupView',
+                    'subviews' => [],
+                    'userGroupClassName' => 'CBDevelopersUserGroup'
+                ],
+            ],
+        ];
+
+        for ($index = 0; $index < count($tests); $index +=1) {
+            $test = $tests[$index];
+
+            $actualUpgradeSpec = CBModel::upgrade(
+                $test->originalSpec
+            );
+
+            if ($actualUpgradeSpec != $test->expectedUpgradedSpec) {
+                return CBTest::resultMismatchFailure(
+                    $test->name,
+                    $actualUpgradeSpec,
+                    $test->expectedUpgradedSpec
+                );
+            }
+        }
+
+        return (object)[
+            'succeeded' => true,
+        ];
+    }
+    /* CBTest_upgrade() */
 
 }
