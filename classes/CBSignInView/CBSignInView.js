3ea78a464f5ae8ca992977d38bcbd723c8ca65d5
Matt Calkins (MacBook Air) <matt@mattifesto.com>
2020-01-03 16:28:49 -0800

    Add CBSignInView

---
+++ classes/CBSignInView/CBSignInView.js
@@ -0,0 +1,343 @@
+"use strict";
+/* jshint strict: global */
+/* jshint esversion: 6 */
+/* global
+    CBErrorHandler,
+    CBUI,
+    CBUIPanel,
+    CBUIPasswordEditor,
+    CBUIStringEditor,
+    Colby,
+*/
+
+
+
+Colby.afterDOMContentLoaded(
+    function afterDOMContentLoaded() {
+        let viewElements = document.getElementsByClassName("CBSignInView");
+
+        for (let index = 0; index < viewElements.length; index += 1) {
+            let viewElement = viewElements.item(index);
+
+            viewElement.appendChild(
+                createSignInElement()
+            );
+
+            viewElement.appendChild(
+                createCreateAccountElement()
+            );
+        }
+        /* for */
+
+
+
+        /**
+         * @return Element
+         */
+        function createCreateAccountElement() {
+            let accountInformationIsValid = false;
+
+            let element = CBUI.createElement(
+                "CBSignInView_createAccount"
+            );
+
+            let createAccountTitleElement = CBUI.createElement(
+                "CBUI_title1"
+            );
+
+            element.appendChild(
+                createAccountTitleElement
+            );
+
+            createAccountTitleElement.textContent = "Create Account";
+
+            let sectionContainerElement = CBUI.createElement(
+                "CBUI_sectionContainer"
+            );
+
+            element.appendChild(
+                sectionContainerElement
+            );
+
+            let sectionElement = CBUI.createElement(
+                "CBUI_section"
+            );
+
+            sectionContainerElement.appendChild(
+                sectionElement
+            );
+
+            let fullNameEditor = CBUIStringEditor.create();
+            fullNameEditor.title = "full name";
+            fullNameEditor.changed = function () { verify(); };
+
+            sectionElement.appendChild(
+                fullNameEditor.element
+            );
+
+            let emailEditor = CBUIStringEditor.create();
+            emailEditor.title = "email";
+            emailEditor.changed = function () { verify(); };
+
+            sectionElement.appendChild(
+                emailEditor.element
+            );
+
+            let password1Editor = CBUIPasswordEditor.create();
+            password1Editor.title = "password";
+            password1Editor.changed = function () { verify(); };
+
+            sectionElement.appendChild(
+                password1Editor.element
+            );
+
+            let password2Editor = CBUIPasswordEditor.create();
+            password2Editor.title = "confirm password";
+            password2Editor.changed = function () { verify(); };
+
+            sectionElement.appendChild(
+                password2Editor.element
+            );
+
+            /* status area */
+
+            let statusElement = CBUI.createElement(
+                "CBUI_container1"
+            );
+
+            element.appendChild(
+                statusElement
+            );
+
+
+            /* create account button */
+
+            let createAccountButtonContainerElement = CBUI.createElement(
+                "CBUI_container1"
+            );
+
+            element.appendChild(
+                createAccountButtonContainerElement
+            );
+
+            let createAccountButtonElement = CBUI.createElement(
+                "CBUI_button1"
+            );
+
+            createAccountButtonContainerElement.appendChild(
+                createAccountButtonElement
+            );
+
+            createAccountButtonElement.textContent = "Create Account";
+
+            createAccountButtonElement.addEventListener(
+                "click",
+                function () {
+                    if (accountInformationIsValid !== true) {
+                        CBUIPanel.displayText(
+                            "The account information is not valid."
+                        );
+
+                        return;
+                    }
+
+                    Colby.callAjaxFunction(
+                        "CBUser",
+                        "createAccount",
+                        {
+                            "email": emailEditor.value.trim(),
+                            "fullName": fullNameEditor.value.trim(),
+                            "password": password1Editor.value,
+                        }
+                    ).then(
+                        function (response) {
+                            if (response.succeeded === true) {
+                                //window.location.reload();
+                            } else {
+                                CBUIPanel.displayCBMessage(
+                                    response.cbmessage
+                                );
+                            }
+                        }
+                    ).catch(
+                        function (error) {
+                            CBErrorHandler.displayAndReport(error);
+                        }
+                    );
+                }
+            );
+
+            return element;
+
+
+
+            /* -- closures -- -- -- -- -- */
+
+
+
+            /**
+             * @return void
+             */
+            function verify() {
+                accountInformationIsValid = false;
+
+                let isActive = false;
+                let statusTexts = [];
+
+                let fullName = fullNameEditor.value.trim();
+
+                if (fullName !== "") {
+                    isActive = true;
+                } else {
+                    statusTexts.push("enter a name");
+                }
+
+                let email = emailEditor.value.trim();
+
+                if (email !== "") {
+                    isActive = true;
+                }
+
+                if (/^\w+@\w+\.\w+/.test(email) === false) {
+                    statusTexts.push("fix email address");
+                }
+
+                let password1 = password1Editor.value;
+
+                if (password1 !== "") {
+                    isActive = true;
+                }
+
+                let password2 = password2Editor.value;
+
+                if (password1 !== "") {
+                    isActive = true;
+                }
+
+                if (password1 !== password2) {
+                    statusTexts.push("no match");
+                }
+
+                if (password1 === "" && password2 === "") {
+                    statusTexts.push("enter a password");
+                }
+
+                if (isActive === true) {
+                    if (statusTexts.length === 0) {
+                        accountInformationIsValid = true;
+                    }
+
+                    statusElement.textContent = statusTexts.join(", ");
+                } else {
+                    statusElement.textContent = "";
+                }
+            }
+            /* verify() */
+
+        }
+        /* createCreateAccountElement() */
+
+
+
+        /**
+         * @return Element
+         */
+        function createSignInElement() {
+            let element = CBUI.createElement(
+                "CBSignInView_signIn"
+            );
+
+            let signInTitleElement = CBUI.createElement(
+                "CBUI_title1"
+            );
+
+            element.appendChild(
+                signInTitleElement
+            );
+
+            signInTitleElement.textContent = "Sign In";
+
+            let sectionContainerElement = CBUI.createElement(
+                "CBUI_sectionContainer"
+            );
+
+            element.appendChild(
+                sectionContainerElement
+            );
+
+            let sectionElement = CBUI.createElement(
+                "CBUI_section"
+            );
+
+            sectionContainerElement.appendChild(
+                sectionElement
+            );
+
+            let emailEditor = CBUIStringEditor.create();
+            emailEditor.title = "email";
+
+            sectionElement.appendChild(
+                emailEditor.element
+            );
+
+            let passwordEditor = CBUIPasswordEditor.create();
+            passwordEditor.title = "password";
+
+            sectionElement.appendChild(
+                passwordEditor.element
+            );
+
+            /* sign in button */
+
+            let signInButtonContainerElement = CBUI.createElement(
+                "CBUI_container1"
+            );
+
+            element.appendChild(
+                signInButtonContainerElement
+            );
+
+            let signInButtonElement = CBUI.createElement(
+                "CBUI_button1"
+            );
+
+            signInButtonContainerElement.appendChild(
+                signInButtonElement
+            );
+
+            signInButtonElement.textContent = "Sign In";
+
+            signInButtonElement.addEventListener(
+                "click",
+                function () {
+                    Colby.callAjaxFunction(
+                        "CBUser",
+                        "signIn",
+                        {
+                            email: emailEditor.value.trim(),
+                            password: passwordEditor.value,
+                        }
+                    ).then(
+                        function (response) {
+                            if (response.succeeded === true) {
+                                window.location.reload();
+                            } else {
+                                CBUIPanel.displayCBMessage(
+                                    response.cbmessage
+                                );
+                            }
+                        }
+                    ).catch(
+                        function (error) {
+                            CBErrorHandler.displayAndReport(error);
+                        }
+                    );
+                }
+            );
+
+            return element;
+        }
+        /* createSignInElement() */
+    }
+    /* afterDOMContentLoaded() */
+);
---
+++ classes/CBSignInView/CBSignInView.php
@@ -0,0 +1,81 @@
+<?php
+
+final class CBSignInView {
+
+    /* -- CBHTMLOutput interfaces -- -- -- -- -- */
+
+
+
+    /**
+     * @return [string]
+     */
+    static function CBHTMLOutput_JavaScriptURLs(): array {
+        return [
+            Colby::flexpath(__CLASS__, 'v563.js', cbsysurl()),
+        ];
+    }
+
+
+
+    /**
+     * @return [string]
+     */
+    static function CBHTMLOutput_requiredClassNames(): array {
+        return [
+            'CBErrorHandler',
+            'CBUI',
+            'CBUIPanel',
+            'CBUIPasswordEditor',
+            'CBUIStringEditor',
+            'Colby',
+        ];
+    }
+    /* CBHTMLOutput_requiredClassNames() */
+
+
+
+    /* -- CBModel interfaces -- -- -- -- -- */
+
+
+
+    /**
+     * @param object $viewSpec
+     *
+     * @return void
+     */
+    static function CBModel_build(
+        stdClass $viewSpec
+    ): stdClass {
+        return (object)[];
+    }
+
+
+
+    /* -- CBView interfaces -- -- -- -- -- */
+
+
+
+    /**
+     * @param object $viewModel
+     *
+     * @return void
+     */
+    static function CBView_render(
+        stdClass $viewModel
+    ): void {
+        $currentUserCBID = ColbyUser::getCurrentUserCBID();
+
+        if ($currentUserCBID !== null) {
+            return;
+        }
+
+        ?>
+
+        <div class="CBSignInView">
+        </div>
+
+        <?php
+    }
+    /* CBView_render() */
+
+}
