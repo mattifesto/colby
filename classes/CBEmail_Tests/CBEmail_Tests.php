2d94ba4ced7e6e4e5460a90031108d1ec26c9b84
Matt Calkins (MacBook Air) <matt@mattifesto.com>
2020-01-05 19:09:54 -0800

    Add CBTest_sendCBMessage() in CBEmail_Tests.php

---
+++ classes/CBEmail_Tests/CBEmail_Tests.php
@@ -0,0 +1,94 @@
+<?php
+
+final class CBEmail_Tests {
+
+    /* -- CBTest interfaces -- -- -- -- -- */
+
+
+
+    /**
+     * @return [object]
+     */
+    static function CBTest_getTests(): array {
+        return [
+            (object)[
+                'name' => 'sendCBMessage',
+                'type' => 'interactive_server',
+            ]
+        ];
+    }
+    /* getTests() */
+
+
+
+    /* -- tests -- -- -- -- -- */
+
+
+
+    /**
+     * @return object
+     */
+    static function CBTest_sendCBMessage(): stdClass {
+        $currentUserCBID = ColbyUser::getCurrentUserCBID();
+
+        if ($currentUserCBID === null) {
+            return (object)[
+                'message' => 'No user is signed in.'
+            ];
+        }
+
+        $currentUserModel = CBModels::fetchModelByIDNullable(
+            $currentUserCBID
+        );
+
+        $currentUserEmail = CBModel::valueToString(
+            $currentUserModel,
+            'email'
+        );
+
+        if ($currentUserEmail === '') {
+            return (object)[
+                'message' => 'The current user does not have an email address.',
+            ];
+        }
+
+        $currentUserFullName = CBModel::valueToString(
+            $currentUserModel,
+            'title'
+        );
+
+        $urlAsMessage = CBMessageMarkup::stringToMessage(
+            cbsiteurl()
+        );
+
+        $cbmessage = <<<EOT
+
+            This is an email from ($urlAsMessage (a $urlAsMessage)).
+
+            This is a second paragraph.
+
+            --- pre
+            Computer Data
+            -------------
+            Compile
+            Link
+            F00D
+            ---
+
+        EOT;
+
+
+        CBEmail::sendCBMessage(
+            $currentUserEmail,
+            $currentUserFullName,
+            'CBTest_sendCBMessage',
+            $cbmessage
+        );
+
+        return (object)[
+            'succeeded' => true,
+        ];
+    }
+    /* CBTest_sendCBMessage() */
+
+}
