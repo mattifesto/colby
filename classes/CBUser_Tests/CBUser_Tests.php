0a1f640d1754617531f0325eebab2c8a9186fec3
Matt Calkins (MacBook Air) <matt@mattifesto.com>
2020-01-07 10:21:55 -0800

    Add CBTest_facebookUserIDToUserCBID(), CBTest_resetTestFacebookUser() in CBUser_Tests.php

--- classes/CBUser_Tests/CBUser_Tests.php
+++ classes/CBUser_Tests/CBUser_Tests.php
@@ -12,8 +12,23 @@ final class CBUser_Tests {
     static function CBTest_getTests(): array {
         return [
             (object)[
+                'name' => 'facebookUserIDToUserCBID',
+                'type' => 'server',
+            ],
+            (object)[
                 'name' => 'emailToUserCBID',
                 'type' => 'server',
+            ],
+
+            (object)[
+                'description' => <<<EOT
+
+                    Resets the test user that has a Facebook account but no
+                    email address or password to be used for testing.
+
+                EOT,
+                'name' => 'resetTestFacebookUser',
+                'type' => 'interactive_server',
             ],
         ];
     }
@@ -35,7 +50,11 @@ final class CBUser_Tests {
             'email' => 'bob_toodles_mcgarnagan@example.com',
         ];
 
-        CBModels::deleteByID($testUserSpec->ID);
+        CBDB::transaction(
+            function () use ($testUserSpec) {
+                CBModels::deleteByID($testUserSpec->ID);
+            }
+        );
 
 
         /* test 1 */
@@ -57,7 +76,11 @@ final class CBUser_Tests {
 
         /* test 2 */
 
-        CBModels::save($testUserSpec);
+        CBDB::transaction(
+            function () use ($testUserSpec) {
+                CBModels::save($testUserSpec);
+            }
+        );
 
         $actualResult = CBUser::emailToUserCBID(
             $testUserSpec->email
@@ -76,7 +99,11 @@ final class CBUser_Tests {
 
         /* test 3 */
 
-        CBModels::deleteByID($testUserSpec->ID);
+        CBDB::transaction(
+            function () use ($testUserSpec) {
+                CBModels::deleteByID($testUserSpec->ID);
+            }
+        );
 
         $actualResult = CBUser::emailToUserCBID(
             $testUserSpec->email
@@ -97,5 +124,124 @@ final class CBUser_Tests {
         ];
     }
     /* CBTest_emailToUserCBID() */
+
+
+
+    /**
+     * @return object
+     */
+    static function CBTest_facebookUserIDToUserCBID(): stdClass {
+        $testUserSpec = (object)[
+            'className' => 'CBUser',
+            'ID' => '5a49b666af184ea80d39e0bd61cf0e1bad2f3a0a',
+            'facebookUserID' => 1001,
+            'title' => __METHOD__ . '()',
+        ];
+
+        CBDB::transaction(
+            function () use ($testUserSpec) {
+                CBModels::deleteByID($testUserSpec->ID);
+            }
+        );
+
+
+        /* test 1 */
+
+        $actualResult = CBUser::facebookUserIDToUserCBID(
+            $testUserSpec->facebookUserID
+        );
+
+        $expectedResult = null;
+
+        if ($actualResult !== $expectedResult) {
+            return CBTest::resultMismatchFailure(
+                'test 1',
+                $actualResult,
+                $expectedResult
+            );
+        }
+
+
+        /* test 2 */
+
+        CBDB::transaction(
+            function () use ($testUserSpec) {
+                CBModels::save($testUserSpec);
+            }
+        );
+
+        $actualResult = CBUser::facebookUserIDToUserCBID(
+            $testUserSpec->facebookUserID
+        );
+
+        $expectedResult = $testUserSpec->ID;
+
+        if ($actualResult !== $expectedResult) {
+            return CBTest::resultMismatchFailure(
+                'test 2',
+                $actualResult,
+                $expectedResult
+            );
+        }
+
+
+        /* test 3 */
+
+        CBDB::transaction(
+            function () use ($testUserSpec) {
+                CBModels::deleteByID($testUserSpec->ID);
+            }
+        );
+
+        $actualResult = CBUser::facebookUserIDToUserCBID(
+            $testUserSpec->facebookUserID
+        );
+
+        $expectedResult = null;
+
+        if ($actualResult !== $expectedResult) {
+            return CBTest::resultMismatchFailure(
+                'test 3',
+                $actualResult,
+                $expectedResult
+            );
+        }
+
+        return (object)[
+            'succeeded' => true,
+        ];
+    }
+    /* CBTest_facebookUserIDToUserCBID() */
+
+
+
+    /**
+     * @return object
+     */
+    static function CBTest_resetTestFacebookUser(): stdClass {
+        $userSpec = (object)[
+            'className' => 'CBUser',
+            'ID' => 'cd886ea6c33fd3bf5601c36b398a5ead535448eb',
+            'facebookUserID' => 1,
+            'title' => 'Test User via CBTest_resetTestFacebookUser()',
+        ];
+
+        CBDB::transaction(
+            function () use ($userSpec) {
+                CBModels::deleteByID($userSpec->ID);
+            }
+        );
+
+        CBDB::transaction(
+            function () use ($userSpec) {
+                CBModels::save($userSpec);
+            }
+        );
+
+        return (object)[
+            'succeeded' => true,
+        ];
+    }
+    /* CBTest_resetTestFacebookUser() */
 
 }
