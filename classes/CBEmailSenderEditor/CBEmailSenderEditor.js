c1621bb1ce57b2f0938f97855273d1cc40b41e32
Matt Calkins (MacBook Air) <matt@mattifesto.com>
2020-01-06 20:01:15 -0800

    Add CBEmailSenderEditor

---
+++ classes/CBEmailSenderEditor/CBEmailSenderEditor.js
@@ -0,0 +1,133 @@
+"use strict";
+/* jshint strict: global */
+/* jshint esversion: 6 */
+/* exported CBEmailSenderEditor */
+/* global
+    CBUI,
+    CBUIStringEditor,
+*/
+
+
+var CBEmailSenderEditor = {
+
+    /* -- CBUISpecEditor interfaces -- -- -- -- -- */
+
+
+
+    /**
+     * @param object args
+     *
+     *      {
+     *          spec: object
+     *          specChangedCallback: function
+     *      }
+     *
+     * @return Element
+     */
+    CBUISpecEditor_createEditorElement(
+        args
+    ) {
+        let spec = args.spec;
+        let specChangedCallback = args.specChangedCallback;
+
+        let elements = CBUI.createElementTree(
+            [
+                "CBEmailSenderEditor",
+                "CBUI_sectionContainer",
+                "CBUI_section",
+            ]
+        );
+
+        let editorElement = elements[0];
+        let sectionElement = elements[2];
+
+        sectionElement.appendChild(
+            CBUIStringEditor.createSpecPropertyEditorElement(
+                "Title",
+                spec,
+                "title",
+                specChangedCallback
+            )
+        );
+
+        elements = CBUI.createElementTree(
+            [
+                "CBUI_sectionContainer",
+                "CBUI_section",
+            ]
+        );
+
+        sectionElement = elements[1];
+
+        editorElement.appendChild(
+            elements[0]
+        );
+
+        sectionElement.appendChild(
+            CBUIStringEditor.createSpecPropertyEditorElement(
+                "SMTP Server Hostname",
+                spec,
+                "SMTPServerHostname",
+                specChangedCallback
+            )
+        );
+
+        sectionElement.appendChild(
+            CBUIStringEditor.createSpecPropertyEditorElement(
+                "SMTP Server Port",
+                spec,
+                "SMTPServerPort",
+                specChangedCallback
+            )
+        );
+
+        sectionElement.appendChild(
+            CBUIStringEditor.createSpecPropertyEditorElement(
+                "SMTP Server Security",
+                spec,
+                "SMTPServerSecurity",
+                specChangedCallback
+            )
+        );
+
+        sectionElement.appendChild(
+            CBUIStringEditor.createSpecPropertyEditorElement(
+                "SMTP Server Username",
+                spec,
+                "SMTPServerUsername",
+                specChangedCallback
+            )
+        );
+
+        sectionElement.appendChild(
+            CBUIStringEditor.createSpecPropertyEditorElement(
+                "SMTP Server Password",
+                spec,
+                "SMTPServerPassword",
+                specChangedCallback
+            )
+        );
+
+        sectionElement.appendChild(
+            CBUIStringEditor.createSpecPropertyEditorElement(
+                "Sending Email Address",
+                spec,
+                "sendingEmailAddress",
+                specChangedCallback
+            )
+        );
+
+        sectionElement.appendChild(
+            CBUIStringEditor.createSpecPropertyEditorElement(
+                "Sending Email Full Name",
+                spec,
+                "sendingEmailFullName",
+                specChangedCallback
+            )
+        );
+
+        return editorElement;
+    },
+    /* CBUISpecEditor_createEditorElement() */
+
+};
---
+++ classes/CBEmailSenderEditor/CBEmailSenderEditor.php
@@ -0,0 +1,31 @@
+<?php
+
+final class CBEmailSenderEditor {
+
+    /* -- CBHTMLOutput interfaces -- -- -- -- -- */
+
+
+
+    /**
+     * @return [string]
+     */
+    static function CBHTMLOutput_JavaScriptURLs(): array {
+        return [
+            Colby::flexpath(__CLASS__, 'v565.js', cbsysurl()),
+        ];
+    }
+
+
+
+    /**
+     * @return [string]
+     */
+    static function CBHTMLOutput_requiredClassNames(): array {
+        return [
+            'CBUI',
+            'CBUIStringEditor',
+        ];
+    }
+    /* CBHTMLOutput_requiredClassNames() */
+
+}
