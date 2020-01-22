600cfa0c689794a8b90dc4fd28dbd64d391127f5
Matt Calkins (MacBook Air) <matt@mattifesto.com>
2020-01-22 10:50:46 -0800

    Add CBSetupView

---
+++ classes/CBSetupView/CBSetupView.js
@@ -0,0 +1,172 @@
+"use strict";
+/* jshint strict: global */
+/* jshint esversion: 8 */
+/* global
+    CBErrorHandler,
+    CBUI,
+    CBUIPanel,
+    CBUIStringEditor,
+    Colby,
+*/
+
+
+
+(function () {
+
+    Colby.afterDOMContentLoaded(afterDOMContentLoaded);
+
+
+
+    /**
+     * @return undefined
+     */
+    async function afterDOMContentLoaded() {
+        let viewElements = document.getElementsByClassName("CBSetupView");
+        let viewElement = viewElements.item(0);
+
+        await setupDatabaseUser(
+            viewElement
+        );
+
+        window.alert("done");
+    }
+    /* afterDOMContentLoaded() */
+
+
+
+    /**
+     * @return Promise -> ???
+     */
+    function setupDatabaseUser(
+        viewElement
+    ) {
+        return new Promise(
+            function (resolve) {
+                setupDatabaseUser_createUserInterface(
+                    viewElement,
+                    resolve
+                );
+            }
+        );
+    }
+    /* setupDatabaseUser() */
+
+
+
+    /**
+     *
+     */
+    function setupDatabaseUser_createUserInterface(
+        viewElement,
+        resolve
+    ) {
+        let elements = CBUI.createElementTree(
+            "CBUI_sectionContainer",
+            "CBUI_section"
+        );
+
+        viewElement.appendChild(elements[0]);
+
+        let sectionElement = elements[1];
+
+        let hostnameEditor = CBUIStringEditor.create();
+        hostnameEditor.title = "MySQL Host";
+
+        sectionElement.appendChild(
+            hostnameEditor.element
+        );
+
+        let usernameEditor = CBUIStringEditor.create();
+        usernameEditor.title = "MySQL Username";
+
+        sectionElement.appendChild(
+            usernameEditor.element
+        );
+
+        let passwordEditor = CBUIStringEditor.create(
+            {
+                inputType: "password",
+            }
+        );
+
+        passwordEditor.title = "MySQL Password";
+
+        sectionElement.appendChild(
+            passwordEditor.element
+        );
+
+        let databaseEditor = CBUIStringEditor.create();
+        databaseEditor.title = "MySQL Database";
+
+        sectionElement.appendChild(
+            databaseEditor.element
+        );
+
+        elements = CBUI.createElementTree(
+            "CBUI_container1",
+            "CBUI_button1"
+        );
+
+        viewElement.appendChild(
+            elements[0]
+        );
+
+        let buttonElement = elements[1];
+        buttonElement.textContent = "Verify Database User";
+
+        buttonElement.addEventListener(
+            "click",
+            closure_setupDatabaseUser_verifyDatabaseUserViaAjax
+        );
+
+
+
+        /**
+         * return undefined
+         */
+        function closure_setupDatabaseUser_verifyDatabaseUserViaAjax() {
+            setupDatabaseUser_verifyDatabaseUserViaAjax(
+                hostnameEditor.value,
+                usernameEditor.value,
+                passwordEditor.value,
+                resolve
+            );
+        }
+        /* closure_setupDatabaseUser_verifyDatabaseUserViaAjax() */
+
+    }
+    /* setupDatabaseUser_createUserInterface() */
+
+
+
+    async function setupDatabaseUser_verifyDatabaseUserViaAjax(
+        hostname,
+        username,
+        password,
+        resolve
+    ) {
+        try {
+            let response = await Colby.callAjaxFunction(
+                "CBSetup",
+                "verifyDatabaseUser",
+                {
+                    hostname,
+                    username,
+                    password,
+                }
+            );
+
+            if (response.succeeded === true) {
+                resolve();
+            } else {
+                CBUIPanel.displayCBMessage(
+                    response.cbmessage
+                );
+            }
+        } catch (error) {
+            CBErrorHandler.displayAndReport(error);
+        }
+    }
+    /* setupDatabaseUser_verifyDatabaseUserViaAjax() */
+
+})();
---
+++ classes/CBSetupView/CBSetupView.php
@@ -0,0 +1,75 @@
+<?php
+
+final class CBSetupView {
+
+    /* -- CBHTMLOutput interfaces -- -- -- -- -- */
+
+
+
+    /**
+     * @return [string]
+     */
+    static function CBHTMLOutput_JavaScriptURLs(): array {
+        return [
+            Colby::flexpath(__CLASS__, 'v566.js', cbsysurl()),
+        ];
+    }
+
+
+
+    /**
+     * @return [string]
+     */
+    static function CBHTMLOutput_requiredClassNames(): array {
+        return [
+            'CBErrorHandler',
+            'CBUI',
+            'CBUIPanel',
+            'CBUIStringEditor',
+            'Colby',
+        ];
+    }
+    /* CBHTMLOutput_requiredClassNames() */
+
+
+
+    /* -- CBModel interfaces -- -- -- -- -- */
+
+
+
+    /**
+     * @param object $viewSpec
+     *
+     * @return object
+     */
+    static function CBModel_build(
+        stdClass $viewSpec
+    ): stdClass {
+        return (object)[];
+    }
+    /* CBModel_build() */
+
+
+
+    /* -- CBView interfaces -- -- -- -- -- */
+
+
+
+    /**
+     * @param object $viewModel
+     *
+     * @return void
+     */
+    static function CBView_render(
+        stdClass $viewModel
+    ): void {
+        ?>
+
+        <div class="CBSetupView">
+        </div>
+
+        <?php
+    }
+    /* CBView_render() */
+
+}
