8a553249d8a0a59c44e14457fa7c61bb5bb2faae
Matt Calkins (MacBook Air) <matt@mattifesto.com>
2020-01-23 09:47:31 -0800

    Implement CBInstall_install() in CBPHPAdmin.php

--- classes/CBPHPAdmin/CBPHPAdmin.php
+++ classes/CBPHPAdmin/CBPHPAdmin.php
@@ -36,26 +36,96 @@ final class CBPHPAdmin {
 
 
 
+    /* -- CBHTMLOutput interfaces -- -- -- -- -- */
+
+
+
     /**
      * @return [string]
      */
     static function CBHTMLOutput_JavaScriptURLs() {
-        return [Colby::flexpath(__CLASS__, 'v374.js', cbsysurl())];
+        return [
+            Colby::flexpath(__CLASS__, 'v374.js', cbsysurl()),
+        ];
     }
+
+
 
     /**
      * @return [[<name>, <value>]]
      */
     static function CBHTMLOutput_JavaScriptVariables() {
         return [
-            ['CBPHPAdmin_iniValues', ini_get_all(null, false)],
+            [
+                'CBPHPAdmin_iniValues',
+                ini_get_all(null, false),
+            ],
         ];
     }
+    /* CBHTMLOutput_JavaScriptVariables() */
+
+
 
     /**
      * @return [string]
      */
     static function CBHTMLOutput_requiredClassNames() {
-        return ['CBUI', 'CBUISectionItem4', 'CBUIStringsPart'];
+        return [
+            'CBUI',
+            'CBUISectionItem4',
+            'CBUIStringsPart',
+        ];
     }
+    /* CBHTMLOutput_requiredClassNames() */
+
+
+
+    /* -- CBInstall interfaces -- -- -- -- -- */
+
+
+
+    /**
+     * @return void
+     */
+    static function CBInstall_install(): void {
+        $updater = CBModelUpdater::fetch(
+            (object)[
+                'ID' => CBDevelopAdminMenu::getModelCBID(),
+            ]
+        );
+
+        $items = CBModel::valueToArray(
+            $updater->working,
+            'items'
+        );
+
+        array_push(
+            $items,
+            (object)[
+                'className' => 'CBMenuItem',
+                'name' => 'php',
+                'text' => 'PHP',
+                'URL' => CBAdmin::getAdminPageURL(
+                    'CBPHPAdmin'
+                ),
+            ]
+        );
+
+        $updater->working->items = $items;
+
+        CBModelUpdater::save($updater);
+    }
+    /* CBInstall_install() */
+
+
+
+    /**
+     * @return [string]
+     */
+    static function CBInstall_requiredClassNames(): array {
+        return [
+            'CBDevelopAdminMenu',
+        ];
+    }
+
 }
