64f13387193aa54e73b72b90d64319bcc44b0f55
Matt Calkins (MacBook Air) <matt@mattifesto.com>
2020-01-26 16:17:55 -0800

    Change $expectedSCLibraryVersionNumber to 137 in Colby.php

--- classes/Colby/Colby.php
+++ classes/Colby/Colby.php
@@ -47,7 +47,7 @@ final class Colby {
 
         if (defined('SCLibraryVersionNumber')) {
             $actualSCLibraryVersionNumber = SCLibraryVersionNumber;
-            $expectedSCLibraryVersionNumber = '136';
+            $expectedSCLibraryVersionNumber = '137';
 
             if (
                 $actualSCLibraryVersionNumber !==
