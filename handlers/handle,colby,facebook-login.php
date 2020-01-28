cc8239593f23477a18998b379cace0863f2acb5a
Matt Calkins (MacBook Air) <matt@mattifesto.com>
2020-01-28 13:38:10 -0800

    Garden handlers/handle,colby,facebook-login.php

--- handlers/handle,colby,facebook-login.php
+++ handlers/handle,colby,facebook-login.php
@@ -1,10 +1,28 @@
 <?php
 
-$state = $_GET['state'];
+/**
+ * @NOTE 2020_01_28
+ *
+ *      Visiting /colby/facebook-login/ when you are alread signed in will
+ *      behave as if you are not already signed in. This is probably not the
+ *      correct behavior, but we are currently hardening the sign in/sign out
+ *      processes since signing in using email has recently been added.
+ */
+
+
+
+/**
+ * @NOTE 2020_01_28
+ *
+ *      The "state" query variable should hold JSON for an object with a
+ *      "colby_redirect_uri" property indicating the URL to go to if login
+ *      succeeds.
+ */
+$stateAsJSON = cb_query_string_value('state');
 
 setcookie(
     CBFacebook::loginStateCookieName,
-    $state,
+    $stateAsJSON,
     time() + (60 * 60 * 24 * 30),
     '/'
 );
