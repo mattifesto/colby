bff18574b226ace829ed94ab708bd1b9acdb339f
Matt Calkins (MacBook Air) <matt@mattifesto.com>
2020-02-01 19:06:34 -0800

    Update CBDevelopersUserGroupUserSettingsManager

--- classes/CBDevelopersUserGroupUserSettingsManager/CBDevelopersUserGroupUserSettingsManager.php
+++ classes/CBDevelopersUserGroupUserSettingsManager/CBDevelopersUserGroupUserSettingsManager.php
@@ -2,6 +2,112 @@
 
 final class CBDevelopersUserGroupUserSettingsManager {
 
+    /* -- CBAjax interfaces -- -- -- -- -- */
+
+
+
+    /**
+     * @param object $args
+     *
+     *      {
+     *          targetUserCBID: CBID
+     *      }
+     *
+     * @return object
+     *
+     *      {
+     *          accessWasDenied: bool
+     *          currentUserCanChange: bool
+     *          targetUserIsMemberOfUserGroup: bool
+     *      }
+     */
+    static function CBAjax_fetchTargetUserData(
+        stdClass $args
+    ): stdClass {
+        $targetUserCBID = CBModel::valueAsCBID(
+            $args,
+            'targetUserCBID'
+        );
+
+        if ($targetUserCBID === null) {
+            if ($targetUserCBID === null) {
+                throw new CBExceptionWithValue(
+                    'The "targetUserCBID" argument is not valid.',
+                    $args,
+                    '12c5ca3809e0f6b4a86e899cc7bd6bc06d1618a2'
+                );
+            }
+        }
+
+        $canView = CBUserSettingsManager::currentUserCanViewForTargetUser(
+            __CLASS__,
+            $targetUserCBID
+        );
+
+        if ($canView !== true) {
+            return (object)[
+                'accessWasDenied' => true,
+            ];
+        }
+
+        $targetUserIsMemberOfUserGroup = CBUserGroup::userIsMemberOfUserGroup(
+            $targetUserCBID,
+            'CBDevelopersUserGroup'
+        );
+
+        return (object)[
+            'currentUserCanChange' => true,
+
+            'targetUserIsMemberOfUserGroup' => (
+                $targetUserIsMemberOfUserGroup
+            ),
+        ];
+    }
+    /* CBAjax_fetchTargetUserData() */
+
+
+
+    /**
+     * @return string
+     */
+    static function CBAjax_fetchTargetUserData_getUserGroupClassName(): string {
+        return 'CBPublicUserGroup';
+    }
+
+
+
+    /* -- CBHTMLOutput interfaces -- -- -- -- -- */
+
+
+
+    /**
+     * @return [string]
+     */
+    static function CBHTMLOutput_JavaScriptURLs(): array {
+        return [
+            Colby::flexpath(__CLASS__, 'v570.js', cbsysurl()),
+        ];
+    }
+
+
+
+    /**
+     * @return [string]
+     */
+    static function CBHTMLOutput_requiredClassNames(): array {
+        return [
+            'CBErrorHandler',
+            'CBException',
+            'CBModel',
+            'CBUI',
+            'CBUIBooleanSwitchPart',
+            'Colby',
+        ];
+    }
+    /* CBHTMLOutput_requiredClassNames() */
+
+
+
     /* -- CBInstall interfaces -- -- -- -- -- */
 
 
@@ -36,30 +142,18 @@ final class CBDevelopersUserGroupUserSettingsManager {
     /**
      * @param CBID $targetUserCBID
      *
-     * @return void
+     * @return bool
      */
-    static function CBUserSettingsManager_render(
+    static function CBUserSettingsManager_currentUserCanViewForTargetUser(
         string $targetUserCBID
-    ): void {
-        echo '<div class="', __CLASS__, '">';
-
-        $userHasAuthority = CBUserGroup::userIsMemberOfUserGroup(
+    ): bool {
+        $currentUserIsADeveloper = CBUserGroup::userIsMemberOfUserGroup(
             ColbyUser::getCurrentUserCBID(),
             'CBDevelopersUserGroup'
         );
 
-        if ($userHasAuthority) {
-            CBView::render(
-                (object)[
-                    'className' => 'CBUserGroupMembershipToggleView',
-                    'userCBID' => $targetUserCBID,
-                    'userGroupClassName' => 'CBDevelopersUserGroup',
-                ]
-            );
-        }
-
-        echo '</div>';
+        return $currentUserIsADeveloper;
     }
-    /* CBUserSettingsManager_render() */
+    /* CBUserSettingsManager_currentUserCanViewForTargetUser() */
 
 }
