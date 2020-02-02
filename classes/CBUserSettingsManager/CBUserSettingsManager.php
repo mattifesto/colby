56cbc3d14fec49e2b14b992101c007b3a25a81f3
Matt Calkins (MacBook Air) <matt@mattifesto.com>
2020-02-01 19:17:09 -0800

    Remove render() in CBUserSettingsManager.php

--- classes/CBUserSettingsManager/CBUserSettingsManager.php
+++ classes/CBUserSettingsManager/CBUserSettingsManager.php
@@ -1,13 +1,5 @@
 <?php
 
-/**
- * To show settings on individual user admin pages, creating a class that
- * implements the CBUserSettingsManager_render() interface. The function may
- * choose to only display settings interface elements if the administrative user
- * is allowed to set them, for instance if only developers are allowed to change
- * the settings for users. The function may choose to only display settings for
- * users that can have them set, such as only for wholesale customer users.
- */
 final class CBUserSettingsManager {
 
     /* -- CBHTMLOutput interfaces -- -- -- -- -- */
@@ -78,39 +70,5 @@ final class CBUserSettingsManager {
         }
     }
     /* currentUserCanViewForTargetUser() */
-
-
-
-    /**
-     * @deprecated 2020_01_29
-     *
-     *      All user settings manager user interface should be created in
-     *      JavaScript. Classes should replace the implementation of
-     *      CBUserSettingsManager_render() with JavaScript.
-     *
-     * @param string $managerClassName
-     * @param string $targetUserCBID
-     *
-     * @return void
-     */
-    static function render(
-        string $managerClassName,
-        string $targetUserCBID
-    ): void {
-        $functionName = (
-            $managerClassName .
-            '::CBUserSettingsManager_render'
-        );
-
-        if (is_callable($functionName)) {
-            CBHTMLOutput::requireClassName($managerClassName);
-
-            call_user_func(
-                $functionName,
-                $targetUserCBID
-            );
-        }
-    }
-    /* render() */
 
 }
