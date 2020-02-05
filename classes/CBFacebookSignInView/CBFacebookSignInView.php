cba913e10bdb5564ea5f11414b5e9ed1b5d126f3
Matt Calkins (MacBook Air) <matt@mattifesto.com>
2020-02-05 11:15:48 -0800

    Add "destinationURL" to the CBFacebookSignInView model

--- classes/CBFacebookSignInView/CBFacebookSignInView.php
+++ classes/CBFacebookSignInView/CBFacebookSignInView.php
@@ -2,36 +2,85 @@
 
 final class CBFacebookSignInView {
 
+    /* -- CBHTMLOutput interfaces -- -- -- -- -- */
+
+
+
     /**
      * @return [string]
      */
     static function CBHTMLOutput_CSSURLs(): array {
-        return [Colby::flexpath(__CLASS__, 'css', cbsysurl())];
+        return [
+            Colby::flexpath(__CLASS__, 'v571.css', cbsysurl()),
+        ];
     }
 
+
+
     /**
-     * @param model $spec
-     *
-     * @return ?model
+     * @return [string]
      */
-    static function CBModel_build(stdClass $spec): ?stdClass {
-        return (object)[];
+    static function CBHTMLOutput_requiredClassNames(): array {
+        return [
+            'CBUI',
+        ];
     }
 
+
+
+    /* -- CBModel interfaces -- -- -- -- -- */
+
+
+
     /**
-     * @param model $model
+     * @param object $spec
+     *
+     * @return object
+     */
+    static function CBModel_build(
+        stdClass $spec
+    ): stdClass {
+        return (object)[
+            'destinationURL' => CBModel::valueToString(
+                $spec,
+                'destinationURL'
+            ),
+        ];
+    }
+    /* CBModel_build() */
+
+
+
+    /* -- CBView interfaces -- -- -- -- -- */
+
+
+
+    /**
+     * @param object $viewModel
      *
      * @return void
      */
-    static function CBView_render(stdClass $model): void {
+    static function CBView_render(
+        stdClass $viewModel
+    ): void {
+        $destinationURL = CBModel::valueToString(
+            $viewModel,
+            'destinationURL'
+        );
+
         ?>
 
-        <div class="CBFacebookSignInView">
-            <a class="button" href="<?= CBFacebook::loginURL() ?>">
-                Sign in with Facebook
+        <div class="CBFacebookSignInView CBUI_view">
+            <a
+                class="CBFacebookSignInView_button"
+                href="<?= CBFacebook::loginURL($destinationURL) ?>"
+            >
+                Sign In with Facebook
             </a>
         </div>
 
         <?php
     }
+    /* CBView_render() */
+
 }
