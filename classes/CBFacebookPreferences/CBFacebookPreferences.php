5b7906aad5d4664dd7e466696503e005f7ff1175
Matt Calkins (MacBook Air) <matt@mattifesto.com>
2020-02-14 19:58:05 -0800

    Add CBFacebookPreferences

---
+++ classes/CBFacebookPreferences/CBFacebookPreferences.php
@@ -0,0 +1,137 @@
+<?php
+
+final class CBFacebookPreferences {
+
+    /* -- CBInstall interfaces -- -- -- -- -- */
+
+
+
+    /**
+     * @return void
+     */
+    static function CBInstall_install(): void {
+        CBModelUpdater::update(
+            (object)[
+                'className' => 'CBFacebookPreferences',
+                'ID' => CBFacebookPreferences::getModelCBID(),
+            ]
+        );
+    }
+    /* CBInstall_install() */
+
+
+
+    /**
+     * @return [string]
+     */
+    static function CBInstall_requiredClassNames(): array {
+        return [
+            'CBModelUpdater',
+        ];
+    }
+    /* CBInstall_requiredClassNames() */
+
+
+
+    /* -- CBModel interfaces -- -- -- -- -- */
+
+
+
+    /**
+     * @param object $spec
+     *
+     * @return object
+     */
+    static function CBModel_build(
+        stdClass $spec
+    ): stdClass {
+        return (object)[
+            'appID' => trim(
+                CBModel::valueToString(
+                    $spec,
+                    'appID'
+                )
+            ),
+
+            'appSecret' => trim(
+                CBModel::valueToString(
+                    $spec,
+                    'appSecret'
+                )
+            ),
+        ];
+    }
+    /* CBModel_build() */
+
+
+
+    /* -- functions -- -- -- -- -- */
+
+
+
+    /**
+     * @return string
+     *
+     *      Returns an empty string if there is not app ID.
+     */
+    static function getAppID(): string {
+        $model = CBModelCache::fetchModelByID(
+            CBFacebookPreferences::getModelCBID()
+        );
+
+        $appID = CBModel::valueToString(
+            $model,
+            'appID'
+        );
+
+        if ($appID !== '') {
+            return $appID;
+        }
+
+        if (defined('CBFacebookAppID')) {
+            return CBFacebookAppID;
+        } else {
+            return '';
+        }
+    }
+    /* getAppID() */
+
+
+
+    /**
+     * @return string
+     *
+     *      Returns an empty string if there is no app secret.
+     */
+    static function getAppSecret(): string {
+        $model = CBModelCache::fetchModelByID(
+            CBFacebookPreferences::getModelCBID()
+        );
+
+        $appSecret = CBModel::valueToString(
+            $model,
+            'appSecret'
+        );
+
+        if ($appSecret !== '') {
+            return $appSecret;
+        }
+
+        if (defined('CBFacebookAppSecret')) {
+            return CBFacebookAppSecret;
+        } else {
+            return '';
+        }
+    }
+    /* getAppSecret() */
+
+
+
+    /**
+     * @return CBID
+     */
+    static function getModelCBID(): string {
+        return 'e7e73278178d6f1f8a6726697766ceff0ee8798c';
+    }
+
+}
