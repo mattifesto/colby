9db95daab069dceab1f58170fce52fc70edf53b0
Matt Calkins (MacBook Air) <matt@mattifesto.com>
2020-02-23 08:49:56 -0800

    Set $expectedSCLibraryVersionNumber to 142 in Colby.php

--- classes/Colby/Colby.php
+++ classes/Colby/Colby.php
@@ -47,7 +47,7 @@ final class Colby {
 
         if (defined('SCLibraryVersionNumber')) {
             $actualSCLibraryVersionNumber = SCLibraryVersionNumber;
-            $expectedSCLibraryVersionNumber = '141';
+            $expectedSCLibraryVersionNumber = '142';
 
             if (
                 $actualSCLibraryVersionNumber !==
