1f4d361c745d3f97df1b75087d8e7b0752d7a381
Matt Calkins (MacBook Air) <matt@mattifesto.com>
2020-02-26 12:22:13 -0800

    Remove issues over 6 months old in CBStatusAdminPage.php

--- classes/CBStatusAdminPage/CBStatusAdminPage.php
+++ classes/CBStatusAdminPage/CBStatusAdminPage.php
@@ -197,80 +197,6 @@ final class CBStatusAdminPage {
 
         $issueCBMessages = [];
 
-        $deprecatedConstants = [
-            ['CBSiteIsBeingDebugged', 'Use site preferences.'],
-            ['CBSiteIsBeingDubugged', 'Use site preferences.'],
-            ['COLBY_SITE_IS_BEING_DEBUGGED', 'Use site preferences.'],
-            ['CBShouldDisallowRobots', 'Use site preferences.'],
-            ['CBGoogleAnalyticsID', 'Use site preferences'],
-
-            [
-                'GOOGLE_UNIVERSAL_ANALYTICS_TRACKING_ID',
-                'Use site preferences'
-            ],
-
-            [
-                'CBSiteConfiguration::defaultClassNameForPageSettings',
-                'Use site preferences.'
-            ],
-
-            [
-                'COLBY_SITE_ERRORS_SEND_EMAILS',
-                'Use CBSiteDoesSendEmailErrorReports instead.'
-            ],
-
-            // 2017_03_17
-            ['COLBY_SITE_NAME', 'Remove it and use site preferences.'],
-
-            // 2017_03_17
-            ['CBSiteName', 'Remove it and use site preferences.'],
-
-            // 2017_03_17
-            ['CBSiteNameHTML', 'Remove it and use site preferences.'],
-
-            // 2017_03_19
-            ['COLBY_SITE_URL', 'Remove it and use site preferences.'],
-
-            // 2017_03_22
-            [
-                'CBSiteAdministratorEmail',
-                'Remove it and use site preferences.'
-            ],
-
-            // 2017_03_22
-            [
-                'COLBY_SITE_ADMINISTRATOR',
-                'Remove it and use site preferences.'
-            ],
-
-            // 2017_07_08
-            ['COLBY_MYSQL_HOST', 'Use CBMySQLHost instead.'],
-
-            // 2017_07_08
-            ['COLBY_MYSQL_USER', 'Use CBMySQLUser instead.'],
-
-            // 2017_07_08
-            ['COLBY_MYSQL_PASSWORD', 'Use CBMySQLPassword instead.'],
-
-            // 2017_07_08
-            ['COLBY_MYSQL_DATABASE', 'Use CBMySQLDatabase instead.'],
-
-            // 2018_11_06
-            [
-                'STRIPE_PAYMENT_ENABLED',
-                'Enable payments in stripe preferences.',
-            ],
-        ];
-
-        foreach ($deprecatedConstants as $constant) {
-            if (defined($constant[0])) {
-                $issueCBMessages[] = [
-                    $constant[0],
-                    'This constant has been deprecated. ' . $constant[1]
-                ];
-            }
-        }
-
         if (
             sha1_file(cbsitedir() . '/.gitignore') !==
             sha1_file(cbsysdir() . '/setup/gitignore.template.data')
@@ -299,71 +225,6 @@ final class CBStatusAdminPage {
             array_push($issueCBMessages, $message);
         }
 
-        /* 2018_05_05 Page kinds should install themselves. */
-        if (is_callable('CBPageHelpers::classNamesForPageKinds')) {
-            $message = <<<EOT
-
-                Remove the function
-                (classNamesForPageKinds\(\)(code)) from the class
-                (CBPageHelpers(code)).
-
-                Page kind classes should install themselves.
-
-            EOT;
-
-            $issueCBMessages[] = $message;
-        }
-
-        /**
-         * @NOTE 2017_08_17
-         *
-         *      Search for use of deprecated "colby/Colby.php" file.
-         */
-
-        if (is_file($filepath = cbsitedir() . '/index.php')) {
-            $text = file_get_contents($filepath);
-
-            if (preg_match('/Colby\.php/', $text)) {
-                $issueCBMessages[] = [
-                    'colby/Colby.php',
-                    'The "index.php" file for this site references the ' .
-                    'deprecated "colby/Colby.php" file. Replace this ' .
-                    'with a reference to the "colby/init.php" file.'
-                ];
-            }
-        }
-
-        /* 2018_01_12 Unused function */
-
-        if (is_callable('CBPageHelpers::classNameForPageSettings')) {
-            $issueCBMessages[] = [
-                'CBPageHelpers',
-                'The function CBPageHelpers::classNameForPageSettings() ' .
-                'has been removed with no replacement.'
-            ];
-        }
-
-        /* 2018_04_11 */
-
-        if (is_callable('CBPageHelpers::classNameForUnsetPageSettings')) {
-            $message = <<<EOT
-
-                Remove the function
-                (classNameForUnsetPageSettings\(\)(code)) from the class
-                (CBPageHelpers(code)).
-
-                Pages that use page settings are now required to have the
-                (classNamesForSettings(code)) property set on their model.
-
-                Implement (get\(\)(code)) on
-                (CBPageSettings_defaultClassName(code)) as a temporary
-                workaround.
-
-            EOT;
-
-            $issueCBMessages[] = $message;
-        }
-
         $issueCBMessages = array_merge(
             $issueCBMessages,
             CBAdmin::getIssueMessages()
@@ -371,7 +232,7 @@ final class CBStatusAdminPage {
 
         return $issueCBMessages;
     }
-    /* CBHTMLOutput_JavaScriptVariables() */
+    /* getIssueCBMessages() */
 
 
 
