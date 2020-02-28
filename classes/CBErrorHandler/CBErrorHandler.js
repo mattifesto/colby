0279efe3c3508acdc913f69d108acd9669e4a071
Matt Calkins (MacBook Air) <matt@mattifesto.com>
2020-02-28 12:13:04 -0800

    Add report() in CBErrorHandler.js

--- classes/CBErrorHandler/CBErrorHandler.js
+++ classes/CBErrorHandler/CBErrorHandler.js
@@ -8,13 +8,11 @@
 */
 
 
+
 /**
- * @NOTE 2019_09_26
+ * @NOTE 2020_02_28
  *
- * It's uncertain if this class should exist. CBUIPanel has a displayError()
- * function and maybe that code should be moved into this class.
- *
- * Should Colby.reportError() also be moved into this class?
+ * Error handling functions should move here from other classes.
  */
 var CBErrorHandler = {
 
@@ -45,4 +43,38 @@ var CBErrorHandler = {
         Colby.reportError(error);
     },
     /* displayAndReport() */
+
+
+
+     /**
+      * Use this function to report an error to the server.
+      *
+      *      callAjaxFunction(
+      *          "class",
+      *          "function"
+      *      ).catch(
+      *          function (error) {
+      *              CBErrorHandler.report(error);
+      *          }
+      *      );
+      *
+      * This function will filter out errors created in response to a failed Ajax
+      * request because the server generated and previously logged those errors
+      * during the request.
+      *
+      * @param Error error
+      *
+      * @return Promise -> undefined
+      *
+      *      This function returns a promise that will resolve when the request
+      *      to the server to report the error has completed. This is generally
+      *      not an important promise but may be important in cases where you
+      *      want to report the error and wait to navigate to another page so you
+      *      don't cancal the report request to the server.
+      */
+    report(error) {
+        return Colby.reportError(error);
+    },
+    /* report() */
+
 };
--- classes/CBErrorHandler/CBErrorHandler.php
+++ classes/CBErrorHandler/CBErrorHandler.php
@@ -11,7 +11,7 @@ final class CBErrorHandler {
      */
     static function CBHTMLOutput_JavaScriptURLs(): array {
         return [
-            Colby::flexpath(__CLASS__, 'v544.js', cbsysurl()),
+            Colby::flexpath(__CLASS__, 'v584.js', cbsysurl()),
         ];
     }
 
