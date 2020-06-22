f3f07b3847503415a3c0e11067b3903efaa941ab
Matt Calkins (MacBook Air) <matt@mattifesto.com>
2020-06-21 18:22:57 -0700

    Add 'userEmailAddress' query string variable in handlers/handle,colby,user,reset-password.php

--- handlers/handle,colby,user,reset-password.php
+++ handlers/handle,colby,user,reset-password.php
@@ -1,34 +1,48 @@
 <?php
 
+$userEmailAddress = cb_query_string_value('userEmailAddress');
+
 $currentUserCBID = ColbyUser::getCurrentUserCBID();
 
 $sections = [];
 
+$cbmessage = <<<EOT
+
+    After you submit the form below you will be asked to enter a one time
+    password that will be sent to the specified email address. If you do not
+    enter the one time password, no modifications will be made to the password.
+
+EOT;
+
 if ($currentUserCBID !== null) {
-    array_push(
-        $sections,
-        (object)[
-            'className' => 'CBMessageView',
-            'markup' => <<<EOT
+    $cbmessage .= <<<EOT
 
-                You are currently signed in. It you reset the password for an
-                account you will be signed out and signed in as that account.
+        Note: You are currently signed in. If you change/reset the
+        password for an email address for a different account you will
+        be signed out and signed in as that account.
 
-            EOT,
-        ]
-    );
+    EOT;
 }
 
 array_push(
     $sections,
     (object)[
+        'className' => 'CBMessageView',
+        'markup' => $cbmessage,
+    ]
+);
+
+array_push(
+    $sections,
+    (object)[
         'className' => 'CBUser_ResetPasswordView',
+        'userEmailAddress' => $userEmailAddress,
     ]
 );
 
 $pageSpec = CBModelTemplateCatalog::fetchLivePageTemplate(
     (object)[
-        'title' => 'Reset Password',
+        'title' => 'Change/Reset Password',
         'sections' => $sections,
     ]
 );
