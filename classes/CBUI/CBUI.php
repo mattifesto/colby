e136960a6fff72e0e385f706457d23a2ea0f803b
Matt Calkins (MacBook Air) <matt@mattifesto.com>
2020-07-29 18:12:04 -0700

    Fixed potential layout bug in CBUI.css

--- classes/CBUI/CBUI.php
+++ classes/CBUI/CBUI.php
@@ -11,7 +11,7 @@ final class CBUI {
      */
     static function CBHTMLOutput_CSSURLs() {
         return [
-            Colby::flexpath(__CLASS__, 'v594.css', cbsysurl()),
+            Colby::flexpath(__CLASS__, 'v632.css', cbsysurl()),
         ];
     }
 
