1809bc33b6c597d8be4a0aabd3b474b6f956fbb9
Matt Calkins (MacBook Air) <matt@mattifesto.com>
2020-07-29 18:10:48 -0700

    Fixed layout bug in CBUISelectableItemContainer.css

--- classes/CBUISelectableItemContainer/CBUISelectableItemContainer.php
+++ classes/CBUISelectableItemContainer/CBUISelectableItemContainer.php
@@ -2,24 +2,40 @@
 
 final class CBUISelectableItemContainer {
 
+    /* -- CBHTMLOutput interfaces -- -- -- -- -- */
+
+
+
     /**
      * @return [string]
      */
     static function CBHTMLOutput_CSSURLs() {
-        return [Colby::flexpath(__CLASS__, 'v361.css', cbsysurl())];
+        return [
+            Colby::flexpath(__CLASS__, 'v632.css', cbsysurl())
+        ];
     }
+
+
 
     /**
      * @return [string]
      */
     static function CBHTMLOutput_JavaScriptURLs() {
-        return [Colby::flexpath(__CLASS__, 'v371.js', cbsysurl())];
+        return [
+            Colby::flexpath(__CLASS__, 'v371.js', cbsysurl())
+        ];
     }
+
+
 
     /**
      * @return [string]
      */
     static function CBHTMLOutput_requiredClassNames() {
-        return ['CBUICommandPart', 'CBUIStringsPart'];
+        return [
+            'CBUICommandPart',
+            'CBUIStringsPart'
+        ];
     }
+
 }
