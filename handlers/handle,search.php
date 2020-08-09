ee5461e8cbe165fed75f97d044a7805619fbb20c
Matt Calkins (MacBook Air) <matt@mattifesto.com>
2020-08-09 13:33:39 -0700

    Garden handlers/handle,search.php

--- handlers/handle,search.php
+++ handlers/handle,search.php
@@ -22,89 +22,134 @@ $formClass = empty($searchQuery) ? 'no-query' : 'has-query';
 ?>
 
 <main class="CBSearch">
-<form action="<?= cbsiteurl() ?>/search/" class="search <?= $formClass ?>">
-    <div>
-        <input type="text" name="search-for" value="<?php echo $searchQueryHTML; ?>">
-        <input type="submit" value="Search Now">
-    </div>
-</form>
-
-<?php
-
-if (empty($searchQuery)) {
-    goto done;
-}
-
-?>
-
-<section class="CBPageSearchResultsView">
+    <form action="<?= cbsiteurl() ?>/search/" class="search <?= $formClass ?>">
+        <div>
+            <input
+                type="text"
+                name="search-for"
+                value="<?php echo $searchQueryHTML; ?>"
+            >
+            <input
+                type="submit"
+                value="Search Now"
+            >
+        </div>
+    </form>
 
     <?php
 
-    $searchClause = CBPages::searchClauseFromString($searchQuery);
-
-    if (empty($searchClause)) {
-        $summaries = [];
-    } else {
-        $SQL = <<<END
-
-            SELECT      keyValueData
-            FROM        ColbyPages
-            WHERE       published IS NOT NULL AND
-                        {$searchClause}
-            ORDER BY    published
-
-END;
-
-        $summaries = CBDB::SQLToArray($SQL, ['valueIsJSON' => true]);
-    }
-
-    if (count($summaries) > 0) {
-        foreach ($summaries as $model) {
-            $URI = CBConvert::valueToString(CBModel::value($model, 'URI'));
-            $URL = cbsiteurl() . "/{$URI}/";
-            $title = CBConvert::valueToString(CBModel::value($model, 'title'));
-            $description = CBConvert::valueToString(CBModel::value($model, 'description'));
-            $imageURL = CBImage::valueToFlexpath($model, 'image', 'rw320', cbsiteurl());
-
-            if (empty($imageURL)) {
-                $imageURL = CBModel::value($model, 'thumbnailURL');
-            }
-
-            ?>
-
-            <article class="CBSearchResultView result">
-                <div class="CBSearchResultView_thumbnail">
-                    <?php
-
-                    CBArtworkElement::render([
-                        'aspectRatioWidth' => 1,
-                        'aspectRatioHeight' => 1,
-                        'maxWidth' => '150',
-                        'URL' => $imageURL,
-                    ]);
-
-                    ?>
-                </div>
-                <div class="text">
-                    <h1><a href="<?= $URL ?>"><?= cbhtml($title) ?></a></h1>
-                    <div><p><?= cbhtml($description) ?></div>
-                </div>
-            </article>
-
-            <?php
-        }
+    if (empty($searchQuery)) {
+        goto done;
     }
 
     ?>
 
-</section>
+    <section class="CBPageSearchResultsView">
+
+        <?php
+
+        $searchClause = CBPages::searchClauseFromString(
+            $searchQuery
+        );
+
+        if (empty($searchClause)) {
+            $summaries = [];
+        } else {
+            $SQL = <<<END
+
+                SELECT      keyValueData
+                FROM        ColbyPages
+                WHERE       published IS NOT NULL AND
+                            {$searchClause}
+                ORDER BY    published
+
+            END;
+
+            $summaries = CBDB::SQLToArray(
+                $SQL,
+                [
+                    'valueIsJSON' => true,
+                ]
+            );
+        }
+
+        if (count($summaries) > 0) {
+            foreach ($summaries as $model) {
+                $URI = CBModel::valueToString(
+                    $model,
+                    'URI'
+                );
+
+                $URL = cbsiteurl() . "/{$URI}/";
+
+                $title = CBModel::valueToString(
+                    $model,
+                    'title'
+                );
+
+                $description = CBModel::valueToString(
+                    $model,
+                    'description'
+                );
+
+                $imageURL = CBImage::valueToFlexpath(
+                    $model,
+                    'image',
+                    'rw320',
+                    cbsiteurl()
+                );
+
+                if (empty($imageURL)) {
+                    $imageURL = CBModel::valueToString(
+                        $model,
+                        'thumbnailURL'
+                    );
+                }
+
+                ?>
+
+                <article class="CBSearchResultView result">
+                    <div class="CBSearchResultView_thumbnail">
+                        <?php
+
+                        CBArtworkElement::render(
+                            [
+                                'aspectRatioWidth' => 1,
+                                'aspectRatioHeight' => 1,
+                                'maxWidth' => '150',
+                                'URL' => $imageURL,
+                            ]
+                        );
+
+                        ?>
+                    </div>
+                    <div class="text">
+                        <h1>
+                            <a href="<?= $URL ?>"><?= cbhtml($title) ?></a>
+                        </h1>
+                        <div>
+                            <p><?= cbhtml($description) ?>
+                        </div>
+                    </div>
+                </article>
+
+                <?php
+            }
+        }
+
+        ?>
+
+    </section>
+
+    <?php
+
+    done:
+
+    ?>
+
+</main>
 
 <?php
-
-done:
-
-echo '</main>';
 
 CBPageLayout::renderPageFooter();
 
