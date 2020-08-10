e729e5a3a4fc7d7b59d5879de7c78f9a769e1cfe
Matt Calkins (MacBook Air) <matt@mattifesto.com>
2020-08-10 09:33:17 -0700

    Add associatedModelCBID model property, implement CBModels_willDelete(), CBModels_willSave() in CBArtworkCollection.php

--- classes/CBArtworkCollection/CBArtworkCollection.php
+++ classes/CBArtworkCollection/CBArtworkCollection.php
@@ -1,5 +1,14 @@
 <?php
 
+/**
+ * This model contains an array of CBArtwork models. A CBArtworkCollection model
+ * may be saved on its own but associated with another model in cases where that
+ * model is imported and can't be edited.
+ *
+ * In that scenario, the associated model should generate the
+ * CBArtworkCollection's CBID based on its own CBID or use the associations
+ * table.
+ */
 final class CBArtworkCollection {
 
     /* -- CBModel interfaces -- */
@@ -45,13 +54,92 @@ final class CBArtworkCollection {
         }
 
 
+        /* associated model CBID */
+
+        $associatedModelCBID = CBModel::valueAsCBID(
+            $artworkCollectionSpec,
+            'associatedModelCBID'
+        );
+
+
         /* done */
 
         return (object)[
             'artworks' => $artworkModels,
+            'associatedModelCBID' => $associatedModelCBID,
         ];
     }
     /* CBModel_build() */
+
+
+
+    /* -- CBModels interfaces -- */
+
+
+
+    /**
+     * @param [CBID] $artworkCollectionModelCBIDs
+     *
+     * @return void
+     */
+    static function CBModels_willDelete(
+        array $artworkCollectionModelCBIDs
+    ): void {
+        $artworkCollectionModels = CBModels::fetchModelsByID2(
+            $artworkCollectionModelCBIDs
+        );
+
+        $associatedModelCBIDs = array_map(
+            function ($artworkCollectionModel) {
+                return CBModel::valueAsCBID(
+                    $artworkCollectionModel,
+                    'associatedModelCBID'
+                );
+            },
+            $artworkCollectionModels
+        );
+
+        $associatedModelCBIDs = array_filter(
+            $associatedModelCBIDs
+        );
+
+        CBTasks2::restart(
+            'CBArtworkCollection_UpdatedTask',
+            $associatedModelCBIDs
+        );
+    }
+    /* CBModels_willDelete() */
+
+
+
+    /**
+     * @param [object] $artworkCollectionModels
+     *
+     * @return void
+     */
+    static function CBModels_willSave(
+        array $artworkCollectionModels
+    ): void {
+        $associatedModelCBIDs = array_map(
+            function ($artworkCollectionModel) {
+                return CBModel::valueAsCBID(
+                    $artworkCollectionModel,
+                    'associatedModelCBID'
+                );
+            },
+            $artworkCollectionModels
+        );
+
+        $associatedModelCBIDs = array_filter(
+            $associatedModelCBIDs
+        );
+
+        CBTasks2::restart(
+            'CBArtworkCollection_UpdatedTask',
+            $associatedModelCBIDs
+        );
+    }
+    /* CBModels_willSave() */
 
 
 
