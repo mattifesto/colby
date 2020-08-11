741e296a0e2867b4ba727e48aa6cbac91d58bcd6
Matt Calkins (MacBook Air) <matt@mattifesto.com>
2020-08-10 19:26:21 -0700

    Add and render CBDocumentation_ClassListView to show class documentation in CBDocumentation.php

--- classes/CBDocumentation/CBDocumentation.php
+++ classes/CBDocumentation/CBDocumentation.php
@@ -90,7 +90,96 @@ final class CBDocumentation {
         );
 
         ?></div><?php
+
+        CBView::render(
+            (object)[
+                'className' => 'CBDocumentation_ClassListView',
+            ]
+        );
     }
     /* CBAdmin_render() */
 
 }
+/* CBDocumentation */
+
+
+
+/**
+ *
+ */
+final class CBDocumentation_ClassListView {
+
+    /* -- CBHTMLOutput interfaces -- */
+
+
+
+    /**
+     * @return [string]
+     */
+    static function CBHTMLOutput_requiredClassNames(): array {
+        return [
+            'CBContentStyleSheet',
+        ];
+    }
+
+
+
+    /* -- CBView interfaces -- */
+
+
+
+    /**
+     * @param object $viewModel
+     *
+     * @return void
+     */
+    static function CBView_render(
+        stdClass $viewModel
+    ): void {
+        $classNames = CBAdmin::fetchClassNames();
+
+        ?>
+
+        <div class="CBDocumentation_ClassListView CBUI_view_outer">
+            <div class="CBUI_view_inner_text CBContentStyleSheet">
+                <div class="CBUI_title1">
+                    Classes
+                </div>
+
+                <dl>
+                    <?php
+
+                    sort($classNames);
+
+                    foreach ($classNames as $className) {
+                        $descriptionFilepath = Colby::findFile(
+                            "classes/{$className}/{$className}_" .
+                            'CBDocumentation_description.cbmessage'
+                        );
+
+                        ?>
+
+                        <dt><?= cbhtml($className) ?>
+
+                        <?php
+
+                        if ($descriptionFilepath !== null) {
+                            echo '<dd>';
+
+                            echo CBMessageMarkup::messageToHTML(
+                                file_get_contents($descriptionFilepath)
+                            );
+                        }
+                    }
+
+                    ?>
+                </dl>
+            </div>
+        </div>
+
+        <?php
+    }
+    /* CBView_render() */
+
+}
+/* CBDocumentation_ClassListView */
