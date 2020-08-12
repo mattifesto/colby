3a9a1a52213642a7485e709aa8162e4040233c73
Matt Calkins (MacBook Air) <matt@mattifesto.com>
2020-08-11 17:16:41 -0700

    Allow developers to create documentation files for undocumented classes.

--- classes/CBDocumentation/CBDocumentation.php
+++ classes/CBDocumentation/CBDocumentation.php
@@ -30,27 +30,6 @@ final class CBDocumentation {
      * @return void
      */
     static function CBAdmin_render(): void {
-        $CSS = <<<EOT
-
-            main.CBDocumentation {
-                background-color: var(--CBBackgroundColor);
-            }
-
-            .CBDocumentation_container {
-                text-align: center;
-            }
-
-            .CBDocumentation_container a {
-                display: flex;
-                align-items: center;
-                justify-content: center;
-                height: 44px;
-            }
-
-        EOT;
-
-        CBHTMLOutput::addCSS($CSS);
-
         CBHTMLOutput::pageInformation()->title = 'Documentation';
 
         CBView::renderSpec((object)[
@@ -98,6 +77,107 @@ final class CBDocumentation {
         );
     }
     /* CBAdmin_render() */
+
+
+
+    /* -- CBAjax interfaces -- */
+
+
+
+    /**
+     * @param object $args
+     *
+     *      {
+     *          targetClassName: string
+     *      }
+     *
+     * @return void
+     */
+    static function CBAjax_createDocumentationFile(
+        stdClass $args
+    ): void {
+        $targetClassName = CBModel::valueToString(
+            $args,
+            'targetClassName'
+        );
+
+        $targetClassFilepath = Colby::findFile(
+            "classes/{$targetClassName}/{$targetClassName}.php"
+        );
+
+        if ($targetClassFilepath === null) {
+            throw new CBExceptionWithValue(
+                'This class was not found.',
+                $targetClassFilepath,
+                '6d98c1a8a9196c22124daa5eb8857279f28a426d'
+            );
+        }
+
+        $targetClassDocumentationFilepath = (
+            dirname(
+                $targetClassFilepath
+            ) .
+            '/' .
+            $targetClassName .
+            '_CBDocumentation_description.cbmessage'
+        );
+
+        if (!file_exists($targetClassDocumentationFilepath)) {
+            touch($targetClassDocumentationFilepath);
+        }
+    }
+    /* CBAjax_createDocumentationFile() */
+
+
+
+    /**
+     * @return string
+     */
+    static function CBAjax_createDocumentationFile_getUserGroupClassName(
+    ): string {
+        return 'CBDevelopersUserGroup';
+    }
+
+
+
+    /* -- CBHTMLOutput interfaces -- */
+
+
+
+    /**
+     * @return [string]
+     */
+    static function CBHTMLOutput_CSSURLs() {
+        return [
+            Colby::flexpath(__CLASS__, 'v633.css', cbsysurl()),
+        ];
+    }
+
+
+
+    /**
+     * @return [string]
+     */
+    static function CBHTMLOutput_JavaScriptURLs() {
+        return [
+            Colby::flexpath(__CLASS__, 'v633.js', cbsysurl()),
+        ];
+    }
+
+
+
+    /**
+     * @return [string]
+     */
+    static function CBHTMLOutput_requiredClassNames() {
+        return [
+            'CBAjax',
+            'CBUI',
+            'CBUIPanel',
+            'Colby',
+        ];
+    }
+    /* CBHTMLOutput_requiredClassNames() */
 
 }
 /* CBDocumentation */
@@ -163,7 +243,16 @@ final class CBDocumentation_ClassListView {
 
                         <?php
 
-                        if ($descriptionFilepath !== null) {
+                        CBView::render(
+                            (object)[
+                                'className' => 'CBDocumentation_DeveloperView',
+                                'targetClassName' => $className,
+                            ]
+                        );
+
+
+                        if ($descriptionFilepath === null) {
+                        } else {
                             echo '<dd>';
 
                             echo CBMessageMarkup::messageToHTML(
@@ -183,3 +272,73 @@ final class CBDocumentation_ClassListView {
 
 }
 /* CBDocumentation_ClassListView */
+
+
+
+/**
+ *
+ */
+final class CBDocumentation_DeveloperView {
+
+    /* -- CBView interfaces -- */
+
+
+
+    /**
+     * @param object $viewModel
+     *
+     * @return void
+     */
+    static function CBView_render(
+        stdClass $viewModel
+    ): void {
+        $userIsDeveloper = (
+            CBUserGroup::currentUserIsMemberOfUserGroup(
+                'CBDevelopersUserGroup'
+            )
+        );
+
+        if ($userIsDeveloper !== true) {
+            return;
+        }
+
+        $targetClassName = CBModel::valueToString(
+            $viewModel,
+            'targetClassName'
+        );
+
+        $targetClassFilepath = Colby::findFile(
+            "classes/{$targetClassName}/{$targetClassName}.php"
+        );
+
+        if ($targetClassFilepath === null) {
+            return;
+        }
+
+        $documentationFilepath = (
+            dirname(
+                $targetClassFilepath
+            ) .
+            '/' .
+            $targetClassName .
+            '_CBDocumentation_description.cbmessage'
+        );
+
+        if (file_exists($documentationFilepath)) {
+            return;
+        }
+
+        ?>
+
+        <dd
+            class="CBDocumentation_DeveloperView"
+            data-target-class-name="<?= cbhtml($targetClassName) ?>"
+        >
+        </dd>
+
+        <?php
+    }
+    /* CBView_render() */
+
+}
+/* CBDocumentation_DeveloperView */
