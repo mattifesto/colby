3e0c36b22c0bebd538c4bef3419e3a38a8bdef92
Matt Calkins (wolf.mattcalkins.corp.mattifesto.com) <matt@mattifesto.com>
2020-09-24 14:36:49 -0700

    Change "Help" admin menu text to "Documentation"

--- classes/CBHelpAdminMenu/CBHelpAdminMenu.php
+++ classes/CBHelpAdminMenu/CBHelpAdminMenu.php
@@ -2,48 +2,82 @@
 
 final class CBHelpAdminMenu {
 
+    /* -- CBInstall interfaces -- */
+
+
+
     /**
      * @return void
      */
-    static function CBInstall_install(): void {
-        $adminMenuSpec = CBModels::fetchSpecByID(CBAdminMenu::ID());
+    static function CBInstall_install(
+    ): void {
+        $adminMenuSpec = CBModels::fetchSpecByID(
+            CBAdminMenu::ID()
+        );
 
         $adminMenuSpec->items[] = (object)[
             'className' => 'CBMenuItem',
             'name' => 'help',
-            'text' => 'Help',
+            'text' => 'Documentation',
             'URL' => '/admin/?c=CBDocumentation',
         ];
 
         $helpAdminMenuSpec = (object)[
             'className' => 'CBMenu',
             'ID' => CBHelpAdminMenu::ID(),
-            'title' => 'Help',
+            'title' => 'Documentation',
             'titleURI' => '/admin/?c=CBDocumentation',
             'items' => [],
         ];
 
-        CBDB::transaction(function () use ($adminMenuSpec, $helpAdminMenuSpec) {
-            CBModels::save($adminMenuSpec);
-            CBModels::deleteByID(CBHelpAdminMenu::ID());
-            CBModels::save($helpAdminMenuSpec);
-        });
+        CBDB::transaction(
+            function () use (
+                $adminMenuSpec,
+                $helpAdminMenuSpec
+            ) {
+                CBModels::save(
+                    $adminMenuSpec
+                );
+
+                CBModels::deleteByID(
+                    CBHelpAdminMenu::ID()
+                );
+
+                CBModels::save(
+                    $helpAdminMenuSpec
+                );
+            }
+        );
     }
+    /* CBInstall_install() */
+
+
 
     /**
      * @return [string]
      */
-    static function CBInstall_requiredClassNames(): array {
+    static function CBInstall_requiredClassNames(
+    ): array {
         return [
             'CBAdminMenu',
             'CBPagesAdminMenu',
         ];
     }
+    /* CBInstall_requiredClassNames() */
+
+
+
+    /* -- functions -- */
+
+
 
     /**
      * @return ID
      */
-    static function ID(): string {
+    static function ID(
+    ): string {
         return '62eeeabc11366b92bf22017903bffb1fead31764';
     }
+    /* ID() */
+
 }
