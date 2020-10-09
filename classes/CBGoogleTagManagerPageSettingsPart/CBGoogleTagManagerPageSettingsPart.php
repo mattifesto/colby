db206b3f4e4c4704e02de773c8f5b80bc97a281c
Matt Calkins (wolf.mattcalkins.corp.mattifesto.com) <matt@mattifesto.com>
2020-10-09 16:30:59 -0700

    Add support for universal analytics IDs in CBGoogleTagManagerPageSettingsPart.php

--- classes/CBGoogleTagManagerPageSettingsPart/CBGoogleTagManagerPageSettingsPart.php
+++ classes/CBGoogleTagManagerPageSettingsPart/CBGoogleTagManagerPageSettingsPart.php
@@ -2,13 +2,41 @@
 
 final class CBGoogleTagManagerPageSettingsPart {
 
+    /* -- CBPageSettings interfaces -- */
+
+
+
     /**
      * @return void
      */
-    static function CBPageSettings_renderHeadElementHTML(): void {
+    static function CBPageSettings_renderHeadElementHTML(
+    ): void {
         $googleTagManagerID = CBSitePreferences::googleTagManagerID();
 
-        if ($googleTagManagerID !== '') { ?>
+        $isUniversalAnalyticsID = preg_match(
+            '/^UA-/',
+            $googleTagManagerID
+        );
+
+        if ($isUniversalAnalyticsID) {
+            ?>
+
+            <!-- Global site tag (gtag.js) - Google Analytics -->
+            <script async src="https://www.googletagmanager.com/gtag/js?id=<?= $googleTagManagerID ?>"></script>
+            <script>
+              window.dataLayer = window.dataLayer || [];
+              function gtag(){dataLayer.push(arguments);}
+              gtag('js', new Date());
+
+              gtag('config', '<?= $googleTagManagerID ?>');
+            </script>
+
+            <?php
+        }
+
+        else if ($googleTagManagerID !== '') {
+            ?>
+
             <!-- Google Tag Manager -->
             <script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
             new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
@@ -16,20 +44,41 @@ final class CBGoogleTagManagerPageSettingsPart {
             '//www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
             })(window,document,'script','dataLayer','<?= $googleTagManagerID ?>');</script>
             <!-- End Google Tag Manager -->
-        <?php }
+
+            <?php
+        }
     }
+    /* CBPageSettings_renderHeadElementHTML() */
+
+
 
     /**
      * @return void
      */
-    static function CBPageSettings_renderPreContentHTML(): void {
+    static function CBPageSettings_renderPreContentHTML(
+    ): void {
         $googleTagManagerID = CBSitePreferences::googleTagManagerID();
 
-        if ($googleTagManagerID !== '') { ?>
+        $isUniversalAnalyticsID = preg_match(
+            '/^UA-/',
+            $googleTagManagerID
+        );
+
+        if ($isUniversalAnalyticsID) {
+            return;
+        }
+
+        else if ($googleTagManagerID !== '') {
+            ?>
+
             <!-- Google Tag Manager -->
             <noscript><iframe src="//www.googletagmanager.com/ns.html?id=<?= $googleTagManagerID ?>"
             height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
             <!-- End Google Tag Manager -->
-        <?php }
+
+            <?php
+        }
     }
+    /* CBPageSettings_renderPreContentHTML() */
+
 }
