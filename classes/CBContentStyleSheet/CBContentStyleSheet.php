63b0db352223a90aab4de2248873f8f21b56f278
Matt Calkins (wolf.mattcalkins.corp.mattifesto.com) <matt@mattifesto.com>
2020-10-16 20:19:14 -0700

    Improve list styles

--- classes/CBContentStyleSheet/CBContentStyleSheet.php
+++ classes/CBContentStyleSheet/CBContentStyleSheet.php
@@ -2,14 +2,23 @@
 
 final class CBContentStyleSheet {
 
-    /* -- CBHTMLOutput interfaces -- -- -- -- -- */
+    /* -- CBHTMLOutput interfaces -- */
+
+
 
     /**
      * @return [string]
      */
-    static function CBHTMLOutput_CSSURLs() {
+    static function CBHTMLOutput_CSSURLs(
+    ) {
         return [
-            Colby::flexpath(__CLASS__, 'v634.css', cbsysurl()),
+            Colby::flexpath(
+                __CLASS__,
+                'v648.css',
+                cbsysurl()
+            ),
         ];
     }
+    /* CBHTMLOutput_CSSURLs() */
+
 }
