b9c6f9e05f5a73bced16625dbd1aa20b46c4da95
Matt Calkins (wolf.mattcalkins.corp.mattifesto.com) <matt@mattifesto.com>
2020-12-15 10:38:42 -0800

    Add CBUIStringEditor2

---
+++ classes/CBUIStringEditor2/CBUIStringEditor2.js
@@ -0,0 +1,147 @@
+"use strict";
+/* jshint strict: global */
+/* jshint esversion: 6 */
+/* globals
+    CBModel,
+    CBUIStringEditor,
+*/
+
+
+(function () {
+
+    window.CBUIStringEditor2 = {
+        create,
+    };
+
+
+
+    /**
+     * @return object
+     */
+    function
+    create(
+    ) {
+        let stringEditor = CBUIStringEditor.create();
+
+        return {
+            CBUIStringEditor2_getElement,
+            CBUIStringEditor2_getValue,
+            CBUIStringEditor2_initializeObjectPropertyEditor,
+            CBUIStringEditor2_setChangedEventListener,
+            CBUIStringEditor2_setTitle,
+            CBUIStringEditor2_setValue,
+        };
+
+
+
+        /**
+         * @return Element
+         */
+        function
+        CBUIStringEditor2_getElement(
+        ) {
+            return stringEditor.element;
+        }
+        /* CBUIStringEditor2_getElement() */
+
+
+
+        /**
+         * @return string
+         */
+        function
+        CBUIStringEditor2_getValue(
+        ) {
+            return stringEditor.value;
+        }
+        /* CBUIStringEditor2_getValue() */
+
+
+
+        /**
+         * @param object targetObject
+         * @param string targetPropertyName
+         * @param string title
+         * @param function changedEventListener
+         *
+         * @return void
+         */
+        function
+        CBUIStringEditor2_initializeObjectPropertyEditor(
+            targetObject,
+            targetPropertyName,
+            title,
+            changedEventListener
+        ) {
+            CBUIStringEditor2_setTitle(
+                title
+            );
+
+            CBUIStringEditor2_setValue(
+                CBModel.valueToString(
+                    targetObject,
+                    targetPropertyName
+                )
+            );
+
+            CBUIStringEditor2_setChangedEventListener(
+                function () {
+                    targetObject[targetPropertyName] = (
+                        CBUIStringEditor2_getValue()
+                    );
+
+                    changedEventListener();
+                }
+            );
+        }
+        /* CBUIStringEditor2_initializeObjectPropertyEditor() */
+
+
+
+        /**
+         * @param function listener
+         *
+         * @return undefined
+         */
+        function
+        CBUIStringEditor2_setChangedEventListener(
+            listener
+        ) {
+            stringEditor.changed = listener;
+        }
+        /* CBUIStringEditor2_setChangedEventListener() */
+
+
+
+        /**
+         * @param string title
+         *
+         * @return undefined
+         */
+        function
+        CBUIStringEditor2_setTitle(
+            title
+        ) {
+            stringEditor.title = title;
+        }
+        /* CBUIStringEditor2_setTitle() */
+
+
+
+        /**
+         * @param string value
+         *
+         * @return undefined
+         */
+        function
+        CBUIStringEditor2_setValue(
+            value
+        ) {
+            stringEditor.value = value;
+        }
+        /* CBUIStringEditor2_setValue() */
+
+    }
+    /* create() */
+
+})();
---
+++ classes/CBUIStringEditor2/CBUIStringEditor2.php
@@ -0,0 +1,56 @@
+<?php
+
+
+/**
+ * This class was created to replace the CBUIStringEditor class with a class
+ * whose functions are fully distinct and searchable.
+ *
+ * To accomplish this CBUIStringEditor2.create() returns an object the only has
+ * function properties with distinct names. The object has no value properties
+ * of any kind because setting or getting an unsupported value property will not
+ * report any error or be noticeable outside of testing. With value properties,
+ * typos are not easily found.
+ *
+ * This class is an early attempt to create classes that only have distinct
+ * searchable class + function names so that deprecation of all functionality is
+ * fully possible with a simple regular expression search and no need to analyze
+ * any code.
+ */
+final class CBUIStringEditor2 {
+
+    /* -- CBHTMLOutput interfaces -- -- -- -- -- */
+
+
+
+    /**
+     * @return [string]
+     */
+    static function
+    CBHTMLOutput_JavaScriptURLs(
+    ): array {
+        return [
+            Colby::flexpath(
+                __CLASS__,
+                'v675.4.js',
+                cbsysurl()
+            ),
+        ];
+    }
+    /* CBHTMLOutput_JavaScriptURLs() */
+
+
+
+    /**
+     * @return [string]
+     */
+    static function
+    CBHTMLOutput_requiredClassNames(
+    ): array {
+        return [
+            'CBModel',
+            'CBUIStringEditor',
+        ];
+    }
+    /* CBHTMLOutput_requiredClassNames() */
+
+}
