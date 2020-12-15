1cac35f3b6bdb38418e5326c193d8ce47623dc99
Matt Calkins (wolf.mattcalkins.corp.mattifesto.com) <matt@mattifesto.com>
2020-12-14 21:11:48 -0800

    Improve triangle use in CBUIExpander

--- classes/CBUIExpander/CBUIExpander.php
+++ classes/CBUIExpander/CBUIExpander.php
@@ -2,32 +2,58 @@
 
 final class CBUIExpander {
 
-    /**
-     * @return [string]
-     */
-    static function CBHTMLOutput_CSSURLs(): array {
-        return [
-            Colby::flexpath(__CLASS__, 'v463.css', cbsysurl()),
-        ];
-    }
+    /* -- CBHTMLOutput interfaces -- */
+
+
 
     /**
      * @return [string]
      */
-    static function CBHTMLOutput_JavaScriptURLs(): array {
+    static function
+    CBHTMLOutput_CSSURLs(
+    ): array {
         return [
-            Colby::flexpath(__CLASS__, 'v503.js', cbsysurl()),
+            Colby::flexpath(
+                __CLASS__,
+                'v675.4.css',
+                cbsysurl()
+            ),
         ];
     }
+    /* CBHTMLOutput_CSSURLs() */
+
+
 
     /**
      * @return [string]
      */
-    static function CBHTMLOutput_requiredClassNames(): array {
+    static function
+    CBHTMLOutput_JavaScriptURLs(
+    ): array {
+        return [
+            Colby::flexpath(
+                __CLASS__,
+                'v503.js',
+                cbsysurl()
+            ),
+        ];
+    }
+    /* CBHTMLOutput_JavaScriptURLs() */
+
+
+
+    /**
+     * @return [string]
+     */
+    static function
+    CBHTMLOutput_requiredClassNames(
+    ): array {
         return [
             'CBContentStyleSheet',
             'CBConvert',
             'CBMessageMarkup'
         ];
     }
+    /* CBHTMLOutput_requiredClassNames() */
+
 }
