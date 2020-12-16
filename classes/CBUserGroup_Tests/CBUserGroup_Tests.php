d8b2eaafe298fb836089548b23ccc3adf96b315f
Matt Calkins (wolf.mattcalkins.corp.mattifesto.com) <matt@mattifesto.com>
2020-12-16 11:35:38 -0800

    Remove CBTest_deprecatedGroupNameToUserGroupClassName() in CBUserGroup_Tests.php

--- classes/CBUserGroup_Tests/CBUserGroup_Tests.php
+++ classes/CBUserGroup_Tests/CBUserGroup_Tests.php
@@ -12,10 +12,6 @@ final class CBUserGroup_Tests {
     static function CBTest_getTests(): array {
         return [
             (object)[
-                'name' => 'deprecatedGroupNameToUserGroupClassName',
-                'type' => 'server',
-            ],
-            (object)[
                 'name' => 'general',
                 'type' => 'server',
             ],
@@ -30,62 +26,6 @@ final class CBUserGroup_Tests {
 
 
     /* -- tests -- -- -- -- -- */
-
-
-
-    /**
-     * @return object
-     */
-    static function CBTest_deprecatedGroupNameToUserGroupClassName(): stdClass {
-        $tests = [
-            [
-                'Administrators',
-                'CBAdministratorsUserGroup',
-            ],
-            [
-                'Developers',
-                'CBDevelopersUserGroup',
-            ],
-            [
-                'Public',
-                'CBPublicUserGroup',
-            ],
-            [
-                '',
-                null,
-            ],
-            [
-                'random',
-                null,
-            ],
-        ];
-
-        for ($index = 0; $index < count($tests); $index += 1) {
-            $test = $tests[$index];
-            $testName = $test[0];
-            $deprecatedGroupName = $test[0];
-            $expectedUserGroupClassName = $test[1];
-
-            $actualUserGroupClassName = (
-                CBUserGroup::deprecatedGroupNameToUserGroupClassName(
-                    $deprecatedGroupName
-                )
-            );
-
-            if ($actualUserGroupClassName !== $expectedUserGroupClassName) {
-                return CBTest::resultMismatchFailure(
-                    $testName,
-                    $actualUserGroupClassName,
-                    $expectedUserGroupClassName
-                );
-            }
-        }
-
-        return (object)[
-            'succeeded' => true,
-        ];
-    }
-    /* CBTest_deprecatedGroupNameToUserGroupClassName() */
 
 
 
