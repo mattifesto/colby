6b4407e88e6260e645a78def58b054315fb80e0b
Matt Calkins (wolf.mattcalkins.corp.mattifesto.com) <matt@mattifesto.com>
2020-12-23 14:42:29 -0800

    Add CBTest_fetchByCBID() in CBModelUpdater_Tests.php

--- classes/CBModelUpdater_Tests/CBModelUpdater_Tests.php
+++ classes/CBModelUpdater_Tests/CBModelUpdater_Tests.php
@@ -4,27 +4,187 @@ final class CBModelUpdater_Tests {
 
     /* -- CBTest interfaces -- -- -- -- -- */
 
+
+
     /**
      * @return [object]
      */
-    static function CBTest_getTests(): array {
+    static function
+    CBTest_getTests(
+    ): array {
         return [
+
             (object)[
-                'title' => 'CBModelUpdater::updateIfExists()',
+                'name' => 'fetchByCBID',
                 'type' => 'server',
-                'name' => 'updateIfExists'
             ],
+
+            (object)[
+                'name' => 'updateIfExists',
+                'type' => 'server',
+            ],
+
         ];
     }
     /* CBTest_getTests() */
 
 
+
     /* -- tests -- -- -- -- -- */
+
+
 
     /**
      * @return object
      */
-    static function CBTest_updateIfExists(): stdClass {
+    static function
+    CBTest_fetchByCBID(
+    ): stdClass {
+
+        /* prepare */
+
+        $CBID = CBTest::getTemporaryModelCBID();
+
+        CBModels::deleteByID(
+            $CBID
+        );
+
+        $updater = CBModelUpdater::fetchByCBID(
+            $CBID
+        );
+
+        $spec = ($updater->CBModelUpdater_getSpec)();
+
+        /* test */
+
+        $testName = "model doesn't exist test";
+
+        if ($spec !== null) {
+            return CBTest::valueIssueFailure(
+                "{$testName}: The spec should be null.",
+                $spec
+            );
+        }
+
+        /* prepare */
+
+        $spec = (object)[];
+
+        CBModel::setCBID(
+            $spec,
+            $CBID
+        );
+
+        CBModel::setClassName(
+            $spec,
+            'CBViewPage'
+        );
+
+        CBViewPage::setPageSettingsClassName(
+            $spec,
+            'CBPageSettingsForResponsivePages'
+        );
+
+        CBModels::save(
+            $spec
+        );
+
+        $updater = CBModelUpdater::fetchByCBID(
+            $CBID
+        );
+
+        $spec = ($updater->CBModelUpdater_getSpec)();
+
+        $version = CBModel::getVersion(
+            $spec
+        );
+
+        /* test */
+
+        $testName = "model exists test";
+
+        if ($version !== 1) {
+            return CBTest::valueIssueFailure(
+                "{$testName}: The version should be 1.",
+                $spec
+            );
+        }
+
+        /* prepare */
+
+        ($updater->CBModelUpdater_save)();
+
+        $updater = CBModelUpdater::fetchByCBID(
+            $CBID
+        );
+
+        $spec = ($updater->CBModelUpdater_getSpec)();
+
+        $version = CBModel::getVersion(
+            $spec
+        );
+
+        /* test */
+
+        $testName = "after updater saved with no changes";
+
+        if ($version !== 1) {
+            return CBTest::valueIssueFailure(
+                "{$testName}: The version should still be 1.",
+                $spec
+            );
+        }
+
+        /* prepare */
+
+        CBViewPage::setTitle(
+            $spec,
+            'New Title'
+        );
+
+        ($updater->CBModelUpdater_save)();
+
+        $updater = CBModelUpdater::fetchByCBID(
+            $CBID
+        );
+
+        $spec = ($updater->CBModelUpdater_getSpec)();
+
+        $version = CBModel::getVersion(
+            $spec
+        );
+
+        /* test */
+
+        $testName = "after updater saved with changes";
+
+        if ($version !== 2) {
+            return CBTest::valueIssueFailure(
+                "{$testName}: The version should now be 2.",
+                $spec
+            );
+        }
+
+        /* done */
+
+        CBModels::deleteByID(
+            $CBID
+        );
+
+        return (object)[
+            'succeeded' => true,
+        ];
+    }
+    /* CBTest_fetchByCBID() */
+
+
+
+    /**
+     * @return object
+     */
+    static function
+    CBTest_updateIfExists(
+    ): stdClass {
         $modelID = 'dc325defccbfaeb390304b35e4073d2a8c19063a';
         $spec = (object)[
             'className' => 'CBMessageView',
@@ -77,4 +237,5 @@ final class CBModelUpdater_Tests {
         ];
     }
     /* CBTest_updateIfExists() */
+
 }
