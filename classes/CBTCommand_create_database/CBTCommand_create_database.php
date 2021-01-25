5acac5590a6164822c62f3b8141e8cfbf68901f5
Matt Calkins (wolf.mattcalkins.corp.mattifesto.com) <matt@mattifesto.com>
2021-01-24 16:33:08 -0800

    Add CBTCommand_create_database

---
+++ classes/CBTCommand_create_database/CBTCommand_create_database.php
@@ -0,0 +1,42 @@
+<?php
+
+final class CBTCommand_create_database {
+
+    /* -- cbt interfaces -- */
+
+
+
+    /**
+     * @return void
+     */
+    static function
+    cbt_execute(
+    ): void {
+        global $argv;
+
+        if (count($argv) < 3) {
+            echo "not enough parameters\n";
+
+            exit(1);
+        }
+
+        $strings = CBDB::SQLToArrayOfNullableStrings(
+            "show databases;"
+        );
+
+        $databaseName = $argv[2];
+
+        echo implode(
+            "\n",
+            $strings
+        ) . "\n";
+
+        if (false) {
+            echo "ERROR\n";
+
+            exit(1);
+        }
+    }
+    /* cbt_execute() */
+
+}
