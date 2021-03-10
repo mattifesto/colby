3305a159f7f9ecadca963b0c7363df45b567648e
Matt Calkins (wolf.mattcalkins.corp.mattifesto.com) <matt@mattifesto.com>
2021-03-10 10:12:43 -0800

    Add SCPromotionExecutor_CBOrderDiscount

---
+++ classes/SCPromotionExecutor_CBOrderDiscount/SCPromotionExecutor_CBOrderDiscount.php
@@ -0,0 +1,270 @@
+<?php
+
+final class
+SCPromotionExecutor_CBOrderDiscount {
+
+    /* -- CBInstall interfaces -- -- -- -- -- */
+
+
+
+    /**
+     * @return void
+     */
+    static function
+    CBInstall_configure(
+    ): void {
+        $updater = new CBModelUpdater(
+            SCPromotionExecutor_CBOrderDiscount::getRegistrationModelCBID()
+        );
+
+        CBModel::setClassName(
+            $updater->getSpec(),
+            'SCPromotionExecutorRegistration'
+        );
+
+        SCPromotionExecutorRegistration::setDescriptionCBMessage(
+            $updater->getSpec(),
+            <<<EOT
+
+                A promotion using this executor will apply a percentage discount
+                to the items subtotal of the order.
+
+            EOT
+        );
+
+        SCPromotionExecutorRegistration::setExecutorClassName(
+            $updater->getSpec(),
+            __CLASS__
+        );
+
+        SCPromotionExecutorRegistration::setTitle(
+            $updater->getSpec(),
+            'Percentage Discount'
+        );
+
+        $updater->save2();
+    }
+    /* CBInstall_configure() */
+
+
+
+    /* -- CBModel interfaces -- */
+
+
+
+    /**
+     * @param object $promotionExecutorSpec
+     *
+     * @return object
+     */
+    static function
+    CBModel_build(
+        stdClass $promotionExecutorSpec
+    ): stdClass {
+        $promotionExecutorModel = (object)[];
+
+        SCPromotionExecutor_CBOrderDiscount::setIsWholesale(
+            $promotionExecutorModel,
+            SCPromotionExecutor_CBOrderDiscount::getIsWholesale(
+                $promotionExecutorSpec
+            )
+        );
+
+        SCPromotionExecutor_CBOrderDiscount::setMinimumSubtotalInCents(
+            $promotionExecutorModel,
+            SCPromotionExecutor_CBOrderDiscount::getMinimumSubtotalInCents(
+                $promotionExecutorSpec
+            )
+        );
+
+
+        SCPromotionExecutor_CBOrderDiscount::setPercentDiscount(
+            $promotionExecutorModel,
+            SCPromotionExecutor_CBOrderDiscount::getPercentDiscount(
+                $promotionExecutorSpec
+            )
+        );
+
+        return $promotionExecutorSpec;
+    }
+    /* CBModel_build() */
+
+
+
+    /* -- SCPromotionExecutor interfaces -- */
+
+
+
+    /**
+     * @param object $promotionExecutorModel
+     * @param object $orderSpec
+     *
+     * @return object
+     */
+    static function
+    SCPromotionExecutor_generateOrderDiscountOffer(
+        stdClass $promotionExecutorModel,
+        stdClass $orderSpec
+    ): stdClass {
+        $offerSpec = CBModel::createSpec(
+            'SCPromotionOffer_CBOrderDiscount'
+        );
+
+        $orderSubtotalInCents = SCOrder::getSubtotalInCents(
+            $orderSpec
+        );
+
+        $percentDiscount = (
+            SCPromotionExecutor_CBOrderDiscount::getPercentDiscount(
+                $promotionExecutorModel
+            )
+        );
+
+        $discountInCents = round(
+            $orderSubtotalInCents * ($percentDiscount / 100)
+        );
+
+        SCPromotionOffer_CBOrderDiscount::setDiscountInCents(
+            $offerSpec,
+            $discountInCents
+        );
+
+        return $offerSpec;
+    }
+    /* SCPromotionExecutor_generateOrderDiscountOffer() */
+
+
+
+    /* -- accessors -- */
+
+
+
+    /**
+     * @param object $promotionExecutorModel
+     *
+     * @return bool
+     */
+    static function
+    getIsWholesale(
+        stdClass $promotionExecutorModel
+    ): float {
+        return CBModel::valueToBool(
+            $promotionExecutorModel,
+            'SCPromotionExecutor_CBOrderDiscount_isWholesale'
+        );
+    }
+    /* getIsWholesale() */
+
+
+
+    /**
+     * @param object $promotionExecutorSpec
+     * @param bool $isWholesale
+     *
+     * @return void
+     */
+    static function
+    setIsWholesale(
+        stdClass $promotionExecutorSpec,
+        bool $isWholesale
+    ): void {
+        $promotionExecutorSpec
+        ->SCPromotionExecutor_CBOrderDiscount_isWholesale = (
+            $isWholesale
+        );
+    }
+    /* setIsWholesale() */
+
+
+
+    /**
+     * @param object $promotionExecutorModel
+     *
+     * @return int
+     */
+    static function
+    getMinimumSubtotalInCents(
+        stdClass $promotionExecutorModel
+    ): int {
+        return CBModel::valueAsInt(
+            $promotionExecutorModel,
+            'SCPromotionExecutor_CBOrderDiscount_percentDiscount'
+        ) ?? 0;
+    }
+    /* getMinimumSubtotalInCents() */
+
+
+
+    /**
+     * @param object $promotionExecutorSpec
+     * @param int $minimumSubtotalInCents
+     *
+     * @return void
+     */
+    static function
+    setMinimumSubtotalInCents(
+        stdClass $promotionExecutorSpec,
+        int $minimumSubtotalInCents
+    ): void {
+        $promotionExecutorSpec
+        ->SCPromotionExecutor_CBOrderDiscount_minimumSubtotalInCents = (
+            $minimumSubtotalInCents
+        );
+    }
+    /* setMinimumSubtotalInCents() */
+
+
+
+    /**
+     * @param object $promotionExecutorModel
+     *
+     * @return float
+     */
+    static function
+    getPercentDiscount(
+        stdClass $promotionExecutorModel
+    ): float {
+        return CBModel::valueAsNumber(
+            $promotionExecutorModel,
+            'SCPromotionExecutor_CBOrderDiscount_percentDiscount'
+        ) ?? 0.0;
+    }
+    /* getPercentDiscount() */
+
+
+
+    /**
+     * @param object $promotionExecutorSpec
+     * @param float $percentDiscount
+     *
+     * @return void
+     */
+    static function
+    setPercentDiscount(
+        stdClass $promotionExecutorSpec,
+        float $percentDiscount
+    ): void {
+        $promotionExecutorSpec
+        ->SCPromotionExecutor_CBOrderDiscount_percentDiscount = (
+            $percentDiscount
+        );
+    }
+    /* setPercentDiscount() */
+
+
+
+    /* -- functions -- */
+
+
+
+    /**
+     * @return CBID
+     */
+    static function
+    getRegistrationModelCBID(
+    ): string {
+        return '1f1e234e294d7985125af5c5a40e1c337382fa68';
+    }
+    /* getRegistrationModelCBID() */
+
+}
