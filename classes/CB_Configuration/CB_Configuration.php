14fe63eccafc9ad42118fca2e0cc0b2a61e4e5fb
Matt Calkins (wolf.mattcalkins.corp.mattifesto.com) <matt@mattifesto.com>
2021-05-31 08:51:01 -0700

    Add CB_Configuration::[get|set]PrimaryWebsiteDomain()

--- classes/CB_Configuration/CB_Configuration.php
+++ classes/CB_Configuration/CB_Configuration.php
@@ -13,6 +13,43 @@ CB_Configuration {
          * @return string
          */
         static function
+        getPrimaryWebsiteDomain(
+            stdClass $model
+        ): string {
+            return CBModel::valueToString(
+                $model,
+                'CB_Configuration_primaryWebsiteDomain'
+            );
+        }
+        /* getPrimaryWebsiteDomain() */
+
+
+
+        /**
+         * @param object $model
+         * @param string $primaryWebsiteDomain
+         *
+         * @return void
+         */
+        static function
+        setPrimaryWebsiteDomain(
+            stdClass $model,
+            string $primaryWebsiteDomain
+        ): void {
+            $model->CB_Configuration_primaryWebsiteDomain = (
+                $primaryWebsiteDomain
+            );
+        }
+        /* setPrimaryWebsiteDomain() */
+
+
+
+        /**
+         * @param object $model
+         *
+         * @return string
+         */
+        static function
         getServerSpecificWebsiteDomain(
             stdClass $model
         ): string {
