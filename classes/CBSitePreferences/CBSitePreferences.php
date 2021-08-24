9d09e5e426872ace355c54dc016fb3540e9426c3
Matt Calkins (wolf.mattcalkins.corp.mattifesto.com) <matt@mattifesto.com>
2021-08-24 09:48:55 -0700

    Add CBSitePreferences::[get|set]YouTubeAPIKey()

--- classes/CBSitePreferences/CBSitePreferences.php
+++ classes/CBSitePreferences/CBSitePreferences.php
@@ -160,12 +160,16 @@ final class CBSitePreferences {
     /* CBInstall_requiredClassNames() */
 
 
+
     /**
      * @param object $spec
      *
      * @return object
      */
-    static function CBModel_build(stdClass $spec): stdClass {
+    static function
+    CBModel_build(
+        stdClass $spec
+    ): stdClass {
         $model = (object)[
             'custom' => CBKeyValuePair::valueToObject($spec, 'custom'),
             'debug' => CBModel::valueToBool($spec, 'debug'),
@@ -235,6 +239,13 @@ final class CBSitePreferences {
 
         $model->administratorEmails = $administatorEmails;
 
+        CBSitePreferences::setYouTubeAPIKey(
+            $model,
+            CBSitePreferences::getYouTubeAPIKey(
+                $model
+            )
+        );
+
         /* done */
 
         return $model;
@@ -279,6 +290,53 @@ final class CBSitePreferences {
         return $spec;
     }
     /* CBModel_upgrade() */
+
+
+
+    /* -- accessors -- */
+
+
+
+    /**
+     * @param object $sitePreferencesModel
+     *
+     * @return string
+     *
+     *      Returns an empty string if the property value has not been set.
+     */
+    static function
+    getYouTubeAPIKey(
+        stdClass $sitePreferencesModel
+    ): string {
+        return trim(
+            CBModel::valueToString(
+                $sitePreferencesModel,
+                'CBSitePreferences_youtubeAPIKey'
+            )
+        );
+    }
+    /* getYouTubeAPIKey() */
+
+
+
+    /**
+     * @param object $sitePreferencesModel
+     * @param string $youtubeAPIKey
+     *
+     * @return void
+     */
+    static function
+    setYouTubeAPIKey(
+        stdClass $sitePreferencesModel,
+        string $youtubeAPIKey
+    ): void {
+        $sitePreferencesModel->CBSitePreferences_youtubeAPIKey = $youtubeAPIKey;
+    }
+    /* setYouTubeAPIKey() */
+
+
+
+    /* -- functions -- */
 
 
 
@@ -590,6 +648,13 @@ final class CBSitePreferences {
      *      Either way, add documentation here including scenarios for why this
      *      behavior is necessary. It actually may not be a good idea.
      *
+     * @TODO 2021_08_24
+     *
+     *      Non-standard code slows us down every time. This code is bad. If the
+     *      database is down that's a red hot bug that needs to be fixed and
+     *      there isn't likely much to be done until it is. This function should
+     *      be replaced with CBModelCached::fetchModelByID().
+     *
      * @return model
      *
      *      The current CBSitePreferences model.
@@ -629,9 +694,19 @@ final class CBSitePreferences {
      * database is down we can still get the information needed to send an
      * alert email.
      *
+     * @NOTE 2021_08_24
+     *
+     *      This code will eventually be removed. I'm not sure what "race
+     *      conditions" the above comment is referring to and if sending emails
+     *      without a database becomes important, which it has not been, it
+     *      should be implemented specifically with tests.
+     *
      * @return void
      */
-    static function CBModels_willSave(array $models): void {
+    static function
+    CBModels_willSave(
+        array $models
+    ): void {
         array_map(
             function ($model) {
                 $filepath = CBDataStore::flexpath(
@@ -652,6 +727,7 @@ final class CBSitePreferences {
          */
         CBSitePreferences::$model = null;
     }
+    /* CBModels_willSave() */
 
 
 
