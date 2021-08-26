3907d891303e64671d48bc1f008e43a857d65c8d
Matt Calkins (wolf.mattcalkins.corp.mattifesto.com) <matt@mattifesto.com>
2021-08-26 11:32:04 -0700

    Add CB_YouTube_Tests

---
+++ classes/CB_YouTube_Tests/CB_YouTube_Tests.php
@@ -0,0 +1,61 @@
+<?php
+
+final class
+CB_YouTube_Tests {
+
+    /* -- CBTest interfaces -- -- -- -- -- */
+
+
+
+    /**
+     * @return [object]
+     */
+    static function
+    CBTest_getTests(
+    ): array {
+        return [
+            (object)[
+                'name' => 'fetchRecentUploads',
+                'type' => 'server',
+            ],
+        ];
+    }
+    /* CBTest_getTests() */
+
+
+
+    /* -- tests -- -- -- -- -- */
+
+
+
+    /**
+     * @return object
+     */
+    static function
+    CBTest_fetchRecentUploads(
+    ): stdClass {
+        $value = CB_YouTube::fetchRecentUploads();
+
+        $valueAsMessage = CBMessageMarkup::stringToMessage(
+            CBConvert::valueToPrettyJSON(
+                $value
+            )
+        );
+
+        $cbmessage = <<<EOT
+
+        Succeded
+
+        --- pre\n{$valueAsMessage}
+        ---
+
+        EOT;
+
+        return (object)[
+            'succeeded' => true,
+            'message' => $cbmessage,
+        ];
+    }
+    /* CBTest_fetchRecentUploads() */
+
+}
