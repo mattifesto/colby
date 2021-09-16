17376077b648cbc54f56a4b3d2bb6c1c673c344a
Matt Calkins (wolf.mattcalkins.corp.mattifesto.com) <matt@mattifesto.com>
2021-09-16 10:43:13 -0700

    Garden CBPageFrame.php

--- classes/CBPageFrame/CBPageFrame.php
+++ classes/CBPageFrame/CBPageFrame.php
@@ -1,6 +1,11 @@
 <?php
 
-final class CBPageFrame {
+final class
+CBPageFrame {
+
+    /* -- functions -- */
+
+
 
     /**
      * @param ?string $frameClassName
@@ -8,12 +13,30 @@ final class CBPageFrame {
      *
      * @return void
      */
-    static function render(?string $frameClassName, callable $renderContent): void {
-        if (is_callable($function = "{$frameClassName}::CBPageFrame_render")) {
-            CBHTMLOutput::requireClassName($frameClassName);
-            call_user_func($function, $renderContent);
+    static function
+    render(
+        ?string $frameClassName,
+        callable $renderContent
+    ): void {
+        $functionName = "{$frameClassName}::CBPageFrame_render";
+
+        if (
+            is_callable($functionName)
+        ) {
+            CBHTMLOutput::requireClassName(
+                $frameClassName
+            );
+
+            call_user_func(
+                $functionName,
+                $renderContent
+            );
         } else {
-            call_user_func($renderContent);
+            call_user_func(
+                $renderContent
+            );
         }
     }
+    /* render() */
+
 }
