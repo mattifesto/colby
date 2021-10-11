d43c4faea36080c4fb5ae6de58825e5432d7ba25
Matt Calkins (wolf.mattcalkins.corp.mattifesto.com) <matt@mattifesto.com>
2021-10-11 13:04:59 -0700

    Add CB_Brick_Button

---
+++ classes/CB_Brick_Button/CB_Brick_Button.js
@@ -0,0 +1,159 @@
+(function () {
+    "use strict";
+
+    window.CB_Brick_Button = {
+        create: CB_Brick_Button_create,
+    };
+
+
+
+    /**
+     * @return object
+     *
+     *      {
+     *          CB_Brick_Button_setClickedCallback: function
+     *          CB_Brick_Button_setIsDisabled: function
+     *          CB_Brick_Button_getElement: function
+     *          CB_Brick_Button_setTextContent: function
+     *      }
+     */
+    function
+    CB_Brick_Button_create(
+    ) {
+        let clickedCallback;
+        let isDisabled = false;
+
+        let outerElement = document.createElement(
+            "div"
+        );
+
+        outerElement.className = "CB_Brick_Button_outer";
+
+        let innerElement = document.createElement(
+            "div"
+        );
+
+        innerElement.className = "CB_Brick_Button_inner";
+
+        innerElement.addEventListener(
+            "click",
+            function () {
+                if (isDisabled === false) {
+                    CB_Brick_Button_handleClick();
+                }
+            }
+        );
+
+        outerElement.append(
+            innerElement
+        );
+
+
+
+        /* -- accessors -- */
+
+
+
+        function
+        CB_Brick_Button_setIsDisabled(
+            newIsDisabled
+        ) {
+            isDisabled = !!newIsDisabled;
+
+            if (
+                isDisabled
+            ) {
+                outerElement.classList.add(
+                    "CB_Brick_Button_isDisabled"
+                );
+            } else {
+                outerElement.classList.remove(
+                    "CB_Brick_Button_isDisabled"
+                );
+            }
+        }
+        /* CB_Brick_Button_setIsDisabled() */
+
+
+
+        /**
+         * @return Element
+         */
+        function
+        CB_Brick_Button_getElement(
+        ) {
+            return outerElement;
+        }
+        /* CB_Brick_Button_getElement() */
+
+
+
+        /**
+         * @param string newTextContent
+         *
+         * @return undefined
+         */
+        function
+        CB_Brick_Button_setTextContent(
+            newTextContent
+        ) {
+            innerElement.textContent = newTextContent;
+        }
+        /* CB_Brick_Button_setTextContent() */
+
+
+
+        /**
+         * @param function newClickedCallback
+         *
+         * @return undefined
+         */
+        function
+        CB_Brick_Button_setClickedCallback(
+            newClickedCallback
+        ) {
+            if (
+                typeof newClickedCallback === "function"
+            ) {
+                clickedCallback = newClickedCallback;
+            } else {
+                throw new Error(
+                    "The newClickedCallback argument is not a valid callback."
+                );
+            }
+        }
+        /* CB_Brick_Button_setClickedCallback() */
+
+
+
+        /* -- functions -- */
+
+
+
+        /**
+         * @return undefined
+         */
+        function
+        CB_Brick_Button_handleClick(
+        ) {
+            if (
+                isDisabled !== true &&
+                clickedCallback !== undefined
+            ) {
+                clickedCallback();
+            }
+        }
+        /* CB_Brick_Button_handleClick() */
+
+
+
+        return {
+            CB_Brick_Button_setClickedCallback,
+            CB_Brick_Button_setIsDisabled,
+            CB_Brick_Button_getElement,
+            CB_Brick_Button_setTextContent,
+        };
+    }
+    /* CB_Brick_Button_create() */
+
+})();
---
+++ classes/CB_Brick_Button/CB_Brick_Button.php
@@ -0,0 +1,63 @@
+<?php
+
+/**
+ * @NOTE 2021_10_05
+ *
+ *      This class creates a button.
+ */
+final class
+CB_Brick_Button {
+
+    /* -- CBHTMLOutput interfaces -- */
+
+
+
+    /**
+     * @return [string]
+     */
+    static function
+    CBHTMLOutput_CSSURLs(
+    ): array {
+        return [
+            Colby::flexpath(
+                __CLASS__,
+                'v675.38.css',
+                cbsysurl()
+            ),
+        ];
+    }
+    /* CBHTMLOutput_CSSURLs() */
+
+
+
+    /**
+     * @return [string]
+     */
+    static function
+    CBHTMLOutput_JavaScriptURLs(
+    ): array {
+        return [
+            Colby::flexpath(
+                __CLASS__,
+                'v675.38.js',
+                cbsysurl()
+            ),
+        ];
+    }
+    /* CBHTMLOutput_JavaScriptURLs() */
+
+
+
+    /**
+     * @return [string]
+     */
+    static function
+    CBHTMLOutput_requiredClassNames(
+    ): array {
+        return [
+            'CBEqualizePageSettingsPart',
+        ];
+    }
+    /* CBHTMLOutput_requiredClassNames() */
+
+}
