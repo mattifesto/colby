da83f3c14d92f0c60a2009de6c687039f8235bca
Matt Calkins (wolf.mattcalkins.corp.mattifesto.com) <matt@mattifesto.com>
2021-10-20 17:26:05 -0700

    Rewrite much of CB_SettingsManager_Username.js to improve understandability

--- classes/CB_SettingsManager_Username/CB_SettingsManager_Username.js
+++ classes/CB_SettingsManager_Username/CB_SettingsManager_Username.js
@@ -41,12 +41,6 @@
     CBUserSettingsManager_createElement(
         args
     ) {
-        let _currentUsername;
-        let _isCurrentlySaving = false;
-        let _mainKeyValue = CB_Brick_KeyValue.create();
-        let _saveButton = CB_Brick_Button.create();
-        let _usernameEditor = CBUIStringEditor2.create();
-
         let targetUserCBID = CBModel.valueAsCBID(
             args,
             "targetUserCBID"
@@ -62,23 +56,16 @@
             );
         }
 
-        let element = CBUI.createElement(
+        let settingsManagerElement = CBUI.createElement(
             'CB_SettingsManager_Username'
         );
 
-        element.addEventListener(
-            "click",
-            function () {
-                CBUserSettingsManager_Username_showEditor(
-                    targetUserCBID
-                );
-            }
-        );
+        let usernameKeyValueBrick = CB_Brick_KeyValue.create();
 
         {
             let padding10 = CB_Brick_Padding10.create();
 
-            element.append(
+            settingsManagerElement.append(
                 padding10.CB_Brick_Padding10_getOuterElement()
             );
 
@@ -89,52 +76,186 @@
             );
 
             textContainer.CB_Brick_TextContainer_getInnerElement().append(
-                _mainKeyValue.CB_Brick_KeyValue_getElement()
+                usernameKeyValueBrick.CB_Brick_KeyValue_getElement()
             );
         }
 
-        _mainKeyValue.CB_Brick_KeyValue_setHasNavigationArrow(
+        usernameKeyValueBrick.CB_Brick_KeyValue_setHasNavigationArrow(
             true
         );
 
-        _mainKeyValue.CB_Brick_KeyValue_setKey(
+        usernameKeyValueBrick.CB_Brick_KeyValue_setKey(
             "Username"
         );
 
-        _mainKeyValue.CB_Brick_KeyValue_setValue(
-            "test"
+        usernameKeyValueBrick.CB_Brick_KeyValue_setValue(
+            "fetching..."
         );
 
-        CB_SettingsManager_Username_fetchUsername();
 
 
+        (async function () {
+            let usernameSettingsManagerAPI = {
 
+                /**
+                 *
+                 */
+                settingsManager_getTargeUserCBID(
+                ) {
+                    return targetUserCBID;
+                },
 
-        /**
-         * @return Promise -> undefined
-         */
-        async function
-        CB_SettingsManager_Username_fetchUsername(
-        ) {
-            try {
-                let username = await CBAjax.call(
-                    "CB_Username",
-                    "CB_Username_ajax_fetchUsernameByUserCBID",
-                    {
-                        targetUserCBID,
-                    }
+                /**
+                 *
+                 */
+                settingsManager_getUsername(
+                ) {
+                    return usernameKeyValueBrick.CB_Brick_KeyValue_getValue();
+                },
+
+                /**
+                 *
+                 */
+                settingsManager_setUsername(
+                    newUsername
+                ) {
+                    usernameKeyValueBrick.CB_Brick_KeyValue_setValue(
+                        newUsername
+                    );
+                },
+
+            };
+            /* usernameSettingsManagerAPI */
+
+            {
+                let initialUsername = await fetchUsername(
+                    targetUserCBID
                 );
 
-                CB_SettingsManager_Username_setUsername(
-                    username
-                );
-            } catch (error) {
-                CBErrorHandler.report(
-                    error
+                usernameSettingsManagerAPI.settingsManager_setUsername(
+                    initialUsername
                 );
             }
+
+            settingsManagerElement.addEventListener(
+                "click",
+                function () {
+                    CBUINavigationView.navigate(
+                        {
+                            element: createUsernameEditorElement(
+                                usernameSettingsManagerAPI
+                            ),
+                            left: "User",
+                            title: "Change Username",
+                        }
+                    );
+                }
+            );
+        })();
+
+
+
+        return settingsManagerElement;
+    }
+    /* CBUserSettingsManager_createElement() */
+
+
+
+    /**
+     * @param object settingsManagerAPI
+     *
+     * @return Element
+     */
+    function
+    createUsernameEditorElement(
+        settingsManagerAPI
+    ) {
+        let usernameEditorElement = CBUI.createElement();
+
+        let padding10 = CB_Brick_Padding10.create();
+
+        usernameEditorElement.append(
+            padding10.CB_Brick_Padding10_getOuterElement()
+        );
+
+        let textContainer = CB_Brick_TextContainer.create();
+
+        padding10.CB_Brick_Padding10_getInnerElement().append(
+            textContainer.CB_Brick_TextContainer_getOuterElement()
+        );
+
+        let text = CB_Brick_Text.create();
+
+        textContainer.CB_Brick_TextContainer_getInnerElement().append(
+            text.CB_Brick_Text_getOuterElement()
+        );
+
+        let username = settingsManagerAPI.settingsManager_getUsername();
+
+        text.CB_Brick_Text_setText(
+            `Your current username:\n${username}\n\n` +
+            CBConvert.stringToCleanLine(`
+
+                To change your username enter a new username below and press
+                the "save" button. Your previous username will immediately
+                become available for someone else to claim.
+
+            `) +
+            "\n\n" +
+            "Usernames can use the following characters:" +
+            "\n" +
+            "a-z A-Z 0-9 _"
+        );
+
+        {
+            let horizontalBar = CB_Brick_HorizontalBar10.create();
+
+            textContainer.CB_Brick_TextContainer_getInnerElement().append(
+                horizontalBar.CB_Brick_HorizontalBar10_getElement()
+            );
         }
-        /* CB_SettingsManager_Username_fetchUsername() */
+
+        let usernameStringEditor = CBUIStringEditor2.create();
+
+        usernameStringEditor.CBUIStringEditor2_setTitle(
+            "New Username"
+        );
+
+        usernameStringEditor.CBUIStringEditor2_setValue(
+            settingsManagerAPI.settingsManager_getUsername()
+        );
+
+        usernameStringEditor.CBUIStringEditor2_setHasOutline(
+            true
+        );
+
+        textContainer.CB_Brick_TextContainer_getInnerElement().append(
+            usernameStringEditor.CBUIStringEditor2_getElement()
+        );
+
+        {
+            let horizontalBar = CB_Brick_HorizontalBar10.create();
+
+            textContainer.CB_Brick_TextContainer_getInnerElement().append(
+                horizontalBar.CB_Brick_HorizontalBar10_getElement()
+            );
+        }
+
+        let saveButtonBrick = CB_Brick_Button.create();
+
+        saveButtonBrick.CB_Brick_Button_setTextContent(
+            "Save"
+        );
+
+        saveButtonBrick.CB_Brick_Button_setClickedCallback(
+            createUsernameEditorElement_saveUsername
+        );
+
+        textContainer.CB_Brick_TextContainer_getInnerElement().append(
+            saveButtonBrick.CB_Brick_Button_getElement()
+        );
+
+        let isCurrentlySaving = false;
 
 
 
@@ -142,24 +263,21 @@
          * @return Promise -> undefined
          */
         async function
-        CB_SettingsManager_Username_handleSaveClicked(
+        createUsernameEditorElement_saveUsername(
         ) {
+            if (isCurrentlySaving) {
+                return;
+            }
+
             try {
-                if (
-                    _isCurrentlySaving === true
-                ) {
-                    return;
-                }
+                isCurrentlySaving = true;
 
-                _isCurrentlySaving = true;
-
-                _saveButton.CB_Brick_Button_setIsDisabled(
+                saveButtonBrick.CB_Brick_Button_setIsDisabled(
                     true
                 );
 
-
                 let requestedUsername = (
-                    _usernameEditor.CBUIStringEditor2_getValue().trim()
+                    usernameStringEditor.CBUIStringEditor2_getValue().trim()
                 );
 
                 let result = await CBAjax.call(
@@ -167,7 +285,7 @@
                     "CB_Username_ajax_setUsername",
                     {
                         CB_Username_ajax_setUsername_targetUserCBID: (
-                            targetUserCBID
+                            settingsManagerAPI.settingsManager_getTargeUserCBID()
                         ),
                         CB_Username_ajax_setUsername_requestedUsername: (
                             requestedUsername
@@ -178,7 +296,7 @@
                 if (
                     result.CB_Username_ajax_setUsername_succeeded === true
                 ) {
-                    CB_SettingsManager_Username_setUsername(
+                    settingsManagerAPI.settingsManager_setUsername(
                         requestedUsername
                     );
 
@@ -189,133 +307,52 @@
                     );
                 }
             } catch (error) {
-                window.alert("error");
+                window.alert(
+                    error.message
+                );
             } finally {
-                _isCurrentlySaving = false;
+                isCurrentlySaving = false;
 
-                _saveButton.CB_Brick_Button_setIsDisabled(
+                saveButtonBrick.CB_Brick_Button_setIsDisabled(
                     false
                 );
             }
         }
-        /* CB_SettingsManager_Username_handleSaveClicked() */
+        /* createUsernameEditorElement_saveUsername() */
 
 
 
-        function
-        CB_SettingsManager_Username_setUsername(
-            value
-        ) {
-            _currentUsername = value;
-
-            _mainKeyValue.CB_Brick_KeyValue_setValue(
-                value
-            );
-        }
-        /* CB_SettingsManager_Username_setUsername() */
-
-
-
-        /**
-         * @return undefined
-         */
-        function
-        CBUserSettingsManager_Username_showEditor(
-        ) {
-            let element = CBUI.createElement();
-
-            let padding10 = CB_Brick_Padding10.create();
-
-            element.append(
-                padding10.CB_Brick_Padding10_getOuterElement()
-            );
-
-            let textContainer = CB_Brick_TextContainer.create();
-
-            padding10.CB_Brick_Padding10_getInnerElement().append(
-                textContainer.CB_Brick_TextContainer_getOuterElement()
-            );
-
-            let text = CB_Brick_Text.create();
-
-            textContainer.CB_Brick_TextContainer_getInnerElement().append(
-                text.CB_Brick_Text_getOuterElement()
-            );
-
-            text.CB_Brick_Text_setText(
-                `Your current username:\n${_currentUsername}\n\n` +
-                CBConvert.stringToCleanLine(`
-
-                    To change your username enter a new username below and
-                    press the "save" button. Your previous username will immediately
-                    become available for someone else to claim.
-
-                `) +
-                "\n\n" +
-                "Usernames can use the following characters:" +
-                "\n" +
-                "a-z A-Z 0-9 _"
-            );
-
-            {
-                let horizontalBar = CB_Brick_HorizontalBar10.create();
-
-                textContainer.CB_Brick_TextContainer_getInnerElement().append(
-                    horizontalBar.CB_Brick_HorizontalBar10_getElement()
-                );
-            }
-
-            _usernameEditor.CBUIStringEditor2_setTitle(
-                "New Username"
-            );
-
-            _usernameEditor.CBUIStringEditor2_setValue(
-                _currentUsername
-            );
-
-            _usernameEditor.CBUIStringEditor2_setHasOutline(
-                true
-            );
-
-            textContainer.CB_Brick_TextContainer_getInnerElement().append(
-                _usernameEditor.CBUIStringEditor2_getElement()
-            );
-
-            {
-                let horizontalBar = CB_Brick_HorizontalBar10.create();
-
-                textContainer.CB_Brick_TextContainer_getInnerElement().append(
-                    horizontalBar.CB_Brick_HorizontalBar10_getElement()
-                );
-            }
-
-            _saveButton.CB_Brick_Button_setTextContent(
-                "Save"
-            );
-
-            _saveButton.CB_Brick_Button_setClickedCallback(
-                function () {
-                    CB_SettingsManager_Username_handleSaveClicked();
-                }
-            );
-
-            textContainer.CB_Brick_TextContainer_getInnerElement().append(
-                _saveButton.CB_Brick_Button_getElement()
-            );
-
-            CBUINavigationView.navigate(
-                {
-                    element,
-                    left: "User",
-                    title: "Change Username",
-                }
-            );
-        }
-        /* CBUserSettingsManager_Username_showEditor() */
-
-
-
-        return element;
+        return usernameEditorElement;
     }
-    /* CBUserSettingsManager_createElement() */
+    /* createUsernameEditorElement() */
+
+
+
+    /**
+     * @param CBID targetUserCBID
+     *
+     * @return Promise -> string
+     */
+    async function
+    fetchUsername(
+        targetUserCBID
+    ) {
+        try {
+            let username = await CBAjax.call(
+                "CB_Username",
+                "CB_Username_ajax_fetchUsernameByUserCBID",
+                {
+                    targetUserCBID,
+                }
+            );
+
+            return username;
+        } catch (error) {
+            CBErrorHandler.report(
+                error
+            );
+        }
+    }
+    /* fetchUsername() */
+
 })();
