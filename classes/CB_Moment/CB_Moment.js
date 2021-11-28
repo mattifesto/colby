d0407c060aee08ad1caccbfd74a75835462ab944
Matt Calkins (wolf.mattcalkins.corp.mattifesto.com) <matt@mattifesto.com>
2021-11-28 14:39:21 -0800

    Add CB_Moment.getCreatedTimestamp()

--- classes/CB_Moment/CB_Moment.js
+++ classes/CB_Moment/CB_Moment.js
@@ -6,8 +6,31 @@
 (function () {
 
     window.CB_Moment = {
+        getCreatedTimestamp,
         getText,
     };
+
+
+
+    /* -- accessors -- */
+
+
+
+    /**
+     * @param object momentModel
+     *
+     * @return int|undefined
+     */
+    function
+    getCreatedTimestamp(
+        momentModel
+    ) {
+        return CBModel.valueAsInt(
+            momentModel,
+            'CB_Moment_createdTimestamp'
+        );
+    }
+    /* getCreatedTimestamp() */
 
 
 
