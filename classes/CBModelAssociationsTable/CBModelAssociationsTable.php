34c0015cbe72cac1388147490957e395c89b1037
Matt Calkins (wolf.mattcalkins.corp.mattifesto.com) <matt@mattifesto.com>
2021-11-27 19:09:17 -0800

    Add CBModelAssociations_sortingValue_column

--- classes/CBModelAssociationsTable/CBModelAssociationsTable.php
+++ classes/CBModelAssociationsTable/CBModelAssociationsTable.php
@@ -4,11 +4,24 @@ final class
 CBModelAssociationsTable {
 
     /**
-     * @NOTE 2018_12_07
+     * @TODO 2021_11_25
      *
-     *      The "className" column will eventually be renamed to
-     *      "associationKey" because it does not represent an actual class name
-     *      but instead the context of the association for the row.
+     *      Future name changes:
+     *
+     *          ID --> CBModelAssociations_firstCBID_column
+     *
+     *          className --> CBModelAssociations_associationKey_column
+     *
+     *              The "className" column does not represent an actual class
+     *              name but instead the context of the association for the row.
+     *
+     *          associatedID --> CBModelAssociations_secondCBID_column
+     *
+     * @NOTE 2021_11_25
+     *
+     *      The CBModelAssociations_sortingValue_column was added to enable this
+     *      table to hold sorted lists of models, the first of which was a list
+     *      of moments sorted by creation timestamp.
      *
      * @return void
      */
@@ -19,13 +32,42 @@ CBModelAssociationsTable {
 
             CREATE TABLE IF NOT EXISTS
             CBModelAssociations (
-                ID              BINARY(20) NOT NULL,
-                className       VARCHAR(80) NOT NULL,
-                associatedID    BINARY(20) NOT NULL,
 
-                PRIMARY KEY                 (ID, className, associatedID),
-                KEY className_associatedID  (className, associatedID),
-                KEY associatedID            (associatedID)
+                ID
+                    BINARY(20) NOT NULL,
+
+                className
+                    VARCHAR(80) NOT NULL,
+
+                CBModelAssociations_sortingValue_column
+                    BIGINT DEFAULT 0,
+
+                associatedID
+                    BINARY(20) NOT NULL,
+
+
+
+
+                PRIMARY KEY (
+                    ID,
+                    className,
+                    associatedID
+                ),
+
+                KEY CBModelAssociations_sortedListOfModels_key (
+                    ID,
+                    className,
+                    CBModelAssociations_sortingValue_column
+                ),
+
+                KEY className_associatedID (
+                    className,
+                    associatedID
+                ),
+
+                KEY associatedID (
+                    associatedID
+                )
             )
             ENGINE=InnoDB
             DEFAULT CHARSET=utf8mb4
@@ -36,6 +78,41 @@ CBModelAssociationsTable {
         Colby::query(
             $SQL
         );
+
+        /**
+         * Upgrades added in version 675.45
+         */
+
+        if (
+            !CBDBA::tableHasColumnNamed(
+                'CBModelAssociations',
+                'CBModelAssociations_sortingValue_column'
+            )
+        ) {
+            $SQL = <<<EOT
+
+                ALTER TABLE
+                CBModelAssociations
+
+                ADD COLUMN
+                CBModelAssociations_sortingValue_column
+                BIGINT DEFAULT 0
+                AFTER
+                className,
+
+                ADD KEY
+                CBModelAssociations_sortedListOfModels_key (
+                    ID,
+                    className,
+                    CBModelAssociations_sortingValue_column
+                )
+
+            EOT;
+
+            Colby::query(
+                $SQL
+            );
+        }
     }
     /* CBInstall_install() */
 
