01cbc87f3eafa49b5b3455c8367f608c54258887
Matt Calkins (wolf.mattcalkins.corp.mattifesto.com) <matt@mattifesto.com>
2021-12-05 10:04:41 -0800

    Garden handlers/handle-sitemap.php

--- handlers/handle-sitemap.php
+++ handlers/handle-sitemap.php
@@ -16,8 +16,17 @@ $SQL = <<<EOT
 
 EOT;
 
-$pageSummaryModels = CBDB::SQLToArray($SQL, ['valueIsJSON' => true]);
-$pageSummaryModels = array_merge($frontPageSummaryModel, $pageSummaryModels);
+$pageSummaryModels = CBDB::SQLToArray(
+    $SQL,
+    [
+        'valueIsJSON' => true
+    ]
+);
+
+$pageSummaryModels = array_merge(
+    $frontPageSummaryModel,
+    $pageSummaryModels
+);
 
 echo '<?xml version="1.0" encoding="utf-8" ?>';
 
@@ -25,26 +34,46 @@ echo '<?xml version="1.0" encoding="utf-8" ?>';
 <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
    <?php
 
-   array_walk($pageSummaryModels, function ($model) {
-       if (empty($model->URI)) {
-           return;
-       } else if ($model->URI === '/') {
-           $URL = cbsiteurl() . '/';
-       } else {
-           $URL = cbsiteurl() . "/{$model->URI}/";
+   array_walk(
+       $pageSummaryModels,
+       function (
+           $model
+       ) {
+           if (
+               empty($model->URI)
+           ) {
+               return;
+           } else if (
+               $model->URI === '/'
+           ) {
+               $URL = cbsiteurl() . '/';
+           } else {
+               $URL = cbsiteurl() . "/{$model->URI}/";
+           }
+
+           echo '<url>';
+           echo "<loc>{$URL}</loc>";
+
+           $lastmodTimestamp = (
+               empty($model->updated) ?
+               null :
+               (int)$model->updated
+           );
+
+           if (
+               !empty($lastmodTimestamp)
+           ) {
+               $lastmodW3C = gmdate(
+                   DateTime::W3C,
+                   $lastmodTimestamp
+               );
+
+               echo "<lastmod>{$lastmodW3C}</lastmod>";
+           }
+
+           echo '</url>';
        }
-
-       echo '<url>';
-       echo "<loc>{$URL}</loc>";
-
-       $lastmodTimestamp = empty($model->updated) ? null : (int)$model->updated;
-       if (!empty($lastmodTimestamp)) {
-           $lastmodW3C = gmdate(DateTime::W3C, $lastmodTimestamp);
-           echo "<lastmod>{$lastmodW3C}</lastmod>";
-       }
-
-       echo '</url>';
-   });
+   );
 
    ?>
 </urlset>
