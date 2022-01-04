dde8ed458ed736167fb0dcc9433eebba187e6760
Matt Calkins (wolf.mattcalkins.corp.mattifesto.com) <matt@mattifesto.com>
2022-01-04 07:33:31 -0800

    Fix database transaction issues in CBThemedTextViewTestPage

--- classes/CBThemedTextViewTestPage/CBThemedTextViewTestPage.php
+++ classes/CBThemedTextViewTestPage/CBThemedTextViewTestPage.php
@@ -1,32 +1,67 @@
 <?php
 
-final class CBThemedTextViewTestPage {
+final class
+CBThemedTextViewTestPage {
+
+    /* -- CBInstall interfaces -- */
+
+
 
     /**
      * @return void
      */
-    static function CBInstall_configure(): void {
-        CBModels::deleteByID(CBThemedTextViewTestPage::ID());
+    static function
+    CBInstall_configure(
+    ): void {
+        CBDB::transaction(
+            function () {
+                CBModels::deleteByID(
+                    CBThemedTextViewTestPage::ID()
+                );
+            }
+        );
 
         $spec = CBModelTemplateCatalog::fetchLivePageTemplate();
 
-        CBModel::merge($spec, (object)[
-            'ID' => CBThemedTextViewTestPage::ID(),
-            'description' => 'A page for testing and experimenting with CBThemedTextView.',
-            'title' => 'CBThemedTextView Test Page',
-        ]);
+        CBModel::merge(
+            $spec,
+            (object)[
+                'ID' => CBThemedTextViewTestPage::ID(),
+                'description' => (
+                    'A page for testing CBThemedTextView.'
+                ),
+                'title' => 'CBThemedTextView Test Page',
+            ]
+        );
 
         include __DIR__ . '/sections.php';
 
-        CBDB::transaction(function () use ($spec) {
-            CBModels::save($spec);
-        });
+        CBDB::transaction(
+            function () use (
+                $spec
+            ) {
+                CBModels::save(
+                    $spec
+                );
+            }
+        );
     }
+    /* CBInstall_configure() */
+
+
+
+    /* -- functions -- */
+
+
 
     /**
-     * @return hex160
+     * @return CBID
      */
-    static function ID(): string {
+    static function
+    ID(
+    ): string {
         return 'f42f10168d15e025bb99231f034b5bf319da7388';
     }
+    /* ID() */
+
 }
