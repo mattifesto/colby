6d4a616a9505e596e7e1667258627b1d7396a6f3
Matt Calkins (wolf.mattcalkins.corp.mattifesto.com) <matt@mattifesto.com>
2022-01-04 07:32:54 -0800

    Fix database transaction issues in CBTextView2TestPage

--- classes/CBTextView2TestPage/CBTextView2TestPage.php
+++ classes/CBTextView2TestPage/CBTextView2TestPage.php
@@ -1,6 +1,11 @@
 <?php
 
-final class CBTextView2TestPage {
+final class
+CBTextView2TestPage {
+
+    /* -- CBInstall interfaces -- */
+
+
 
     /**
      * This page can be used as a sandbox. When the site is updated, this page
@@ -9,28 +14,58 @@ final class CBTextView2TestPage {
      *
      * @return void
      */
-    static function CBInstall_configure(): void {
-        CBModels::deleteByID(CBTextView2TestPage::ID());
+    static function
+    CBInstall_configure(
+    ): void {
+        CBDB::transaction(
+            function () {
+                CBModels::deleteByID(
+                    CBTextView2TestPage::ID()
+                );
+            }
+        );
 
         $spec = CBModelTemplateCatalog::fetchLivePageTemplate();
 
-        CBModel::merge($spec, (object)[
-            'ID' => CBTextView2TestPage::ID(),
-            'description' => 'A page for testing and experimenting with CBTextView2.',
-            'title' => 'CBTextView2 Test Page',
-        ]);
+        CBModel::merge(
+            $spec,
+            (object)[
+                'ID' => CBTextView2TestPage::ID(),
+                'description' => (
+                    'A page for testing and experimenting with CBTextView2.'
+                ),
+                'title' => 'CBTextView2 Test Page',
+            ]
+        );
 
         include __DIR__ . '/sections.php';
 
-        CBDB::transaction(function () use ($spec) {
-            CBModels::save($spec);
-        });
+        CBDB::transaction(
+            function () use (
+                $spec
+            ) {
+                CBModels::save(
+                    $spec
+                );
+            }
+        );
     }
+    /* CBInstall_configure() */
+
+
+
+    /* -- functions -- */
+
+
 
     /**
-     * @return hex160
+     * @return CBID
      */
-    static function ID(): string {
+    static function
+    ID(
+    ): string {
         return 'ab9a674ce554ce49b7a1d1415f219f8fcf8a3e1f';
     }
+    /* ID() */
+
 }
