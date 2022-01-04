0ac65b0b26076786c3b2055657bd094d1ee29cd5
Matt Calkins (wolf.mattcalkins.corp.mattifesto.com) <matt@mattifesto.com>
2022-01-03 18:59:55 -0800

    Fix database transactions in CBModelCacheTests

--- classes/CBModelCacheTests/CBModelCacheTests.php
+++ classes/CBModelCacheTests/CBModelCacheTests.php
@@ -1,6 +1,7 @@
 <?php
 
-final class CBModelCacheTests {
+final class
+CBModelCacheTests {
 
     /* -- CBTest interfaces -- -- -- -- -- */
 
@@ -130,86 +131,164 @@ final class CBModelCacheTests {
             'succeeded' => true,
         ];
     }
+    /* CBTest_fetchModelsByID() */
+
+
 
     /**
      * @return object
      */
-    static function CBTest_general(): stdClass {
-        Colby::query('START TRANSACTION');
+    static function
+    general(
+    ): stdClass {
+        CBDB::transaction(
+            function () {
+                $ID = CBID::generateRandomCBID();
 
-        $ID = CBID::generateRandomCBID();
-        $model = CBModelCache::modelByID($ID);
+                $model = CBModelCache::modelByID(
+                    $ID
+                );
 
-        if (!empty($model)) {
-            throw new Exception('Model appears to be cached when it should not be.');
-        }
+                if (
+                    !empty($model)
+                ) {
+                    throw new Exception(
+                        'Model appears to be cached when it should not be.'
+                    );
+                }
 
-        $model = CBModelCache::fetchModelByID($ID);
+                $model = CBModelCache::fetchModelByID(
+                    $ID
+                );
 
-        if (!empty($model)) {
-            throw new Exception('Model appears to exist when it should not.');
-        }
+                if (
+                    !empty($model)
+                ) {
+                    throw new Exception(
+                        'Model appears to exist when it should not.'
+                    );
+                }
 
-        $spec = (object)[
-            'className' => 'CBTextView2',
-            'ID' => $ID,
-        ];
+                $spec = (object)[
+                    'className' => 'CBTextView2',
+                    'ID' => $ID,
+                ];
 
-        CBModels::save([$spec]);
+                CBModels::save(
+                    $spec
+                );
 
-        $model = CBModelCache::fetchModelByID($ID);
+                $model = CBModelCache::fetchModelByID(
+                    $ID
+                );
 
-        if (!isset($model->className) || $model->className !== 'CBTextView2') {
-            throw new Exception('The model does not appear to have been fetched properly.');
-        }
+                if (
+                    !isset($model->className) ||
+                    $model->className !== 'CBTextView2'
+                ) {
+                    throw new Exception(
+                        'The model does not appear to have been fetched properly.'
+                    );
+                }
 
-        $model = CBModelCache::modelByID($ID);
+                $model = CBModelCache::modelByID(
+                    $ID
+                );
 
-        if (!isset($model->className) || $model->className !== 'CBTextView2') {
-            throw new Exception('The model does not appear to have been cached properly.');
-        }
+                if (
+                    !isset($model->className) ||
+                    $model->className !== 'CBTextView2'
+                ) {
+                    throw new Exception(
+                        'The model does not appear to have been cached properly.'
+                    );
+                }
 
-        CBModelCache::uncacheByID([$ID]);
+                CBModelCache::uncacheByID(
+                    [$ID]
+                );
 
-        $model = CBModelCache::modelByID($ID);
+                $model = CBModelCache::modelByID(
+                    $ID
+                );
 
-        if (!empty($model)) {
-            throw new Exception('The model does not appear to have been uncached properly.');
-        }
+                if (
+                    !empty($model)
+                ) {
+                    throw new Exception(
+                        'The model does not appear to have been uncached properly.'
+                    );
+                }
 
-        CBModelCache::fetchModelLazilyByID($ID);
+                CBModelCache::fetchModelLazilyByID(
+                    $ID
+                );
 
-        $model = CBModelCache::fetchModelByID(CBID::generateRandomCBID());
+                $model = CBModelCache::fetchModelByID(
+                    CBID::generateRandomCBID()
+                );
 
-        if (!empty($model)) {
-            throw new Exception('The false model appears to exist.');
-        }
+                if (
+                    !empty($model)
+                ) {
+                    throw new Exception(
+                        'The false model appears to exist.'
+                    );
+                }
 
-        $model = CBModelCache::modelByID($ID);
+                $model = CBModelCache::modelByID(
+                    $ID
+                );
 
-        if (!isset($model->className) || $model->className !== 'CBTextView2') {
-            throw new Exception('The model does not appear to have been cached as a needed model properly.');
-        }
+                if (
+                    !isset($model->className) ||
+                    $model->className !== 'CBTextView2'
+                ) {
+                    throw new Exception(
+                        'The model does not appear to have been cached as a needed model properly.'
+                    );
+                }
 
-        Colby::query('ROLLBACK');
+                return 'CBDB_transaction_rollback';
+            }
+        );
 
         return (object)[
             'succeeded' => true,
         ];
     }
+    /* general() */
+
+
 
     /**
      * @return object
      */
-    static function CBTest_save(): stdClass {
+    static function
+    save(
+    ): stdClass {
         $ID = '275097249aeb4eb28b1f6bfa3280485346eb5c94';
 
-        CBModels::deleteByID($ID);
+        CBDB::transaction(
+            function () use (
+                $ID
+            ) {
+                CBModels::deleteByID(
+                    $ID
+                );
+            }
+        );
 
-        $model = CBModelCache::modelByID($ID);
+        $model = CBModelCache::modelByID(
+            $ID
+        );
 
-        if (!empty($model)) {
-            throw new Exception('Subtest 1: The model should not be cached.');
+        if (
+            !empty($model)
+        ) {
+            throw new Exception(
+                'Subtest 1: The model should not be cached.'
+            );
         }
 
         $spec = (object)[
@@ -217,27 +296,53 @@ final class CBModelCacheTests {
             'ID' => $ID,
         ];
 
-        CBModels::save($spec);
+        CBDB::transaction(
+            function () use (
+                $spec
+            ) {
+                CBModels::save(
+                    $spec
+                );
+            }
+        );
 
 
         /* get model from cache */
 
-        $model = CBModelCache::modelByID($ID);
+        $model = CBModelCache::modelByID(
+            $ID
+        );
 
-        if (!empty($model)) {
-            throw new Exception('Subtest 2: The model should not be cached.');
+        if (
+            !empty($model)
+        ) {
+            throw new Exception(
+                'Subtest 2: The model should not be cached.'
+            );
         }
 
-        $model = CBModelCache::fetchModelByID($ID);
+        $model = CBModelCache::fetchModelByID(
+            $ID
+        );
 
-        if ($model->ID !== $ID) {
-            throw new Exception('Subtest 3: The model was not fetched.');
+        if (
+            $model->ID !== $ID
+        ) {
+            throw new Exception(
+                'Subtest 3: The model was not fetched.'
+            );
         }
 
-        $model = CBModelCache::modelByID($ID);
+        $model = CBModelCache::modelByID(
+            $ID
+        );
 
-        if (empty($model)) {
-            throw new Exception('Subtest 4: The model was not cached.');
+        if (
+            empty($model)
+        ) {
+            throw new Exception(
+                'Subtest 4: The model was not cached.'
+            );
         }
 
         /* update version after first save */
@@ -247,36 +352,88 @@ final class CBModelCacheTests {
 
         /* save again */
 
-        CBModels::save($spec);
+        CBDB::transaction(
+            function () use (
+                $spec
+            ) {
+                CBModels::save(
+                    $spec
+                );
+            }
+        );
 
-        $model = CBModelCache::modelByID($ID);
+        $model = CBModelCache::modelByID(
+            $ID
+        );
 
-        if (!empty($model)) {
-            throw new Exception('Subtest 5: The cached model was not uncached when the model was saved.');
+        if (
+            !empty($model)
+        ) {
+            throw new Exception(
+                CBConvert::stringToCleanLine(<<<EOT
+
+                    Subtest 5: The cached model was not uncached when the model
+                    was saved.
+
+                EOT)
+            );
         }
 
-        $model = CBModelCache::fetchModelByID($ID);
+        $model = CBModelCache::fetchModelByID(
+            $ID
+        );
 
-        if ($model->ID !== $ID) {
-            throw new Exception('Subtest 6: The model was not fetched.');
+        if (
+            $model->ID !== $ID
+        ) {
+            throw new Exception(
+                'Subtest 6: The model was not fetched.'
+            );
         }
 
-        $model = CBModelCache::modelByID($ID);
+        $model = CBModelCache::modelByID(
+            $ID
+        );
 
-        if (empty($model)) {
-            throw new Exception('Subtest 7: The model was not cached.');
+        if (
+            empty($model)
+        ) {
+            throw new Exception(
+                'Subtest 7: The model was not cached.'
+            );
         }
 
-        CBModels::deleteByID($ID);
+        CBDB::transaction(
+            function () use (
+                $ID
+            ) {
+                CBModels::deleteByID(
+                    $ID
+                );
+            }
+        );
 
-        $model = CBModelCache::modelByID($ID);
+        $model = CBModelCache::modelByID(
+            $ID
+        );
 
-        if (!empty($model)) {
-            throw new Exception('Subtest 8: The cached model was not uncached when the model was deleted.');
+        if (
+            !empty($model)
+        ) {
+            throw new Exception(
+                CDConvert::stringToCleanLine(<<<EOT
+
+                    Subtest 8: The cached model was not uncached when the model
+                    was deleted.
+
+                EOT)
+            );
         }
 
         return (object)[
             'succeeded' => true,
         ];
     }
+    /* save() */
+
 }
