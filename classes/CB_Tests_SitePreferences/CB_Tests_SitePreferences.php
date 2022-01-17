7d65c5ddca98cc70c1d9e2c9084744ff64fe040d
Matt Calkins (wolf.mattcalkins.corp.mattifesto.com) <matt@mattifesto.com>
2022-01-16 20:21:30 -0800

    Add more tests in CB_Tests_SitePreferences::setFrontPageID()

--- classes/CB_Tests_SitePreferences/CB_Tests_SitePreferences.php
+++ classes/CB_Tests_SitePreferences/CB_Tests_SitePreferences.php
@@ -97,9 +97,6 @@ CB_Tests_SitePreferences {
             'e46db04c351e35e5b86be13aa8f5858ddb84ab43'
         );
 
-        /* no page actually exists for this CBID */
-        $testFrontPageCBID = '5d7f6ceb893ff208d50e4abf548fec0f8d7feaf0';
-
         CBDB::transaction(
             function () use (
                 $testSitePreferencesModelCBID
@@ -125,10 +122,96 @@ CB_Tests_SitePreferences {
             }
         );
 
+        /* --- */
+
+        $testName = 'setFrontPageModelCBID with an invalid CBID';
+
+        $expectedResult = '2ca8a3dfa005d9247df9178ad21244ebbcf237d2';
+        $actualResult = 'no exception thrown';
+
+        try {
+            CBSitePreferences::setFrontPageModelCBID(
+                $testSitePreferencesSpec,
+                'bad cbid'
+            );
+        } catch (
+            Throwable $throwable
+        ) {
+            $actualResult = CBException::throwableToSourceCBID(
+                $throwable
+            );
+        }
+
+        if (
+            $actualResult !== $expectedResult
+        ) {
+            return CBTest::resultMismatchFailure(
+                $testName,
+                $actualResult,
+                $expectedResult
+            );
+        }
+
+        /* --- */
+
+        $testName = 'setFrontPageModelCBID with an valid CBID';
+
+        $validCBID = '072d10f57cb62cd6c0bcb6de8ea43b0a4e3b6a27';
+
+        CBSitePreferences::setFrontPageModelCBID(
+            $testSitePreferencesSpec,
+            $validCBID
+        );
+
+        $expectedResult = $validCBID;
+
+        $actualResult = CBSitePreferences::getFrontPageModelCBID(
+            $testSitePreferencesSpec
+        );
+
+        if (
+            $actualResult !== $expectedResult
+        ) {
+            return CBTest::resultMismatchFailure(
+                $testName,
+                $actualResult,
+                $expectedResult
+            );
+        }
+
+
+        /* --- */
+
+        $testName = 'Use CBSitePreferences::setFrontPageID()';
+
+        /* no page actually exists for this CBID */
+        $testFrontPageCBID = '5d7f6ceb893ff208d50e4abf548fec0f8d7feaf0';
+
         CBSitePreferences::setFrontPageID(
             $testFrontPageCBID,
             $testSitePreferencesModelCBID
         );
+
+        $testSitePreferencesModel = CBModels::fetchModelByCBID(
+            $testSitePreferencesModelCBID
+        );
+
+        $expectedResult = $testFrontPageCBID;
+
+        $actualResult = CBSitePreferences::getFrontPageModelCBID(
+            $testSitePreferencesModel
+        );
+
+        if (
+            $actualResult !== $expectedResult
+        ) {
+            return CBTest::resultMismatchFailure(
+                $testName,
+                $actualResult,
+                $expectedResult
+            );
+        }
+
 
         /* done */
 
