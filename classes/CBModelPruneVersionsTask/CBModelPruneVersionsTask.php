71952dea5ad1bc6ebdac9f65ddb9a39f9b9fc824
Matt Calkins (wolf.mattcalkins.corp.mattifesto.com) <matt@mattifesto.com>
2022-01-16 16:23:09 -0800

    Garden and improve exceptions in CBModelPruneVersionsTask

--- classes/CBModelPruneVersionsTask/CBModelPruneVersionsTask.php
+++ classes/CBModelPruneVersionsTask/CBModelPruneVersionsTask.php
@@ -9,102 +9,173 @@
  * that doing it with a set of SQL queries would lead to very complex and
  * difficult to understand queries.
  */
-final class CBModelPruneVersionsTask {
+final class
+CBModelPruneVersionsTask {
 
     const standardTaskPriority = 200;
 
     /**
      * @return void
      */
-    static function CBInstall_install(): void {
-        $IDs = CBDB::SQLToArray('SELECT LOWER(HEX(ID)) FROM CBModels');
+    static function
+    CBInstall_install(
+    ): void {
+        $IDs = CBDB::SQLToArray(<<<EOT
+
+            SELECT
+            LOWER(HEX(ID))
+            FROM
+            CBModels
+
+        EOT);
+
         $priority = CBModelPruneVersionsTask::standardTaskPriority;
 
-        CBTasks2::restart('CBModelPruneVersionsTask', $IDs, $priority);
+        CBTasks2::restart(
+            'CBModelPruneVersionsTask',
+            $IDs,
+            $priority
+        );
     }
+    /* CBInstall_install() */
+
+
 
     /**
      * @return [string]
      */
-    static function CBInstall_requiredClassNames(): array {
+    static function
+    CBInstall_requiredClassNames(
+    ): array {
         return [
             'CBTasks2',
         ];
     }
+    /* CBInstall_requiredClassNames() */
+
+
 
     /**
      * @param ID $ID
      *
      * @return ?object
      */
-    static function CBTasks2_run(string $ID): ?stdClass {
-        $IDAsSQL = CBID::toSQL($ID);
-        $SQL = <<<EOT
+    static function
+    CBTasks2_run(
+        string $targetModelCBID
+    ): ?stdClass {
+        try {
+            $targetModelCBIDAsSQL = CBID::toSQL(
+                $targetModelCBID
+            );
 
-            SELECT      version,
-                        timestamp,
-                        replaced
-            FROM        CBModelVersions
-            WHERE       ID = {$IDAsSQL}
-            ORDER BY    version DESC
+            $SQL = <<<EOT
 
-EOT;
+                SELECT      version,
+                            timestamp,
+                            replaced
+                FROM        CBModelVersions
+                WHERE       ID = {$targetModelCBIDAsSQL}
+                ORDER BY    version DESC
 
-        $versions = CBDB::SQLToObjects($SQL);
+            EOT;
 
-        /**
-         * If the model has been deleted or only has one version there's no need
-         * to take any further action or reschedule this task.
-         */
-        if (count($versions) < 2) {
-            return null;
-        }
+            $versions = CBDB::SQLToObjects($SQL);
 
-        CBModelPruneVersionsTask::assignActions($versions);
+            /**
+             * If the model has been deleted or only has one version there's no need
+             * to take any further action or reschedule this task.
+             */
+            if (
+                count($versions) < 2
+            ) {
+                return null;
+            }
 
-        $versionsToPrune = [];
+            CBModelPruneVersionsTask::assignActions($versions);
 
-        foreach ($versions as $version) {
-            if ($version->action === 'prune') {
-                array_push($versionsToPrune, $version->version);
-            } else if ($version->action === 'update') {
-                $replacedAsSQL = CBConvert::valueAsInt($version->replaced);
-                $versionAsSQL = CBConvert::valueAsInt($version->version);
+            $versionsToPrune = [];
+
+            foreach (
+                $versions as $version
+            ) {
+                if (
+                    $version->action === 'prune'
+                ) {
+                    array_push(
+                        $versionsToPrune,
+                        $version->version
+                    );
+                }
+
+                else if (
+                    $version->action === 'update'
+                ) {
+                    $replacedAsSQL = CBConvert::valueAsInt(
+                        $version->replaced
+                    );
+
+                    $versionAsSQL = CBConvert::valueAsInt(
+                        $version->version
+                    );
+
+                    $SQL = <<<EOT
+
+                        UPDATE  CBModelVersions
+                        SET     replaced = {$replacedAsSQL}
+                        WHERE   ID = {$targetModelCBIDAsSQL} AND
+                                version = {$versionAsSQL}
+
+                    EOT;
+
+                    Colby::query($SQL);
+                }
+            }
+
+            if (
+                !empty($versionsToPrune)
+            ) {
+                $versionsToPruneAsSQL = implode(
+                    ',',
+                    $versionsToPrune
+                );
+
                 $SQL = <<<EOT
 
-                    UPDATE  CBModelVersions
-                    SET     replaced = {$replacedAsSQL}
-                    WHERE   ID = {$IDAsSQL} AND
-                            version = {$versionAsSQL}
+                    DELETE FROM CBModelVersions
+                    WHERE       ID = {$targetModelCBIDAsSQL} AND
+                                version in ($versionsToPruneAsSQL)
 
-EOT;
+                EOT;
 
                 Colby::query($SQL);
             }
+
+            /**
+             * Reschedule this task to run again in 5 days to further prune the
+             * model versions.
+             */
+            return (object)[
+                'scheduled' => time() + (60 * 60 * 24 * 5),
+                'priority' => CBModelPruneVersionsTask::standardTaskPriority,
+            ];
+        } catch (
+            Throwable $throwable
+        ) {
+            throw new CBExceptionWithValue(
+                'An exception was thrown during CBTasks2_run()',
+                (object)[
+                    'targetModelCBID' => $targetModelCBID,
+                ],
+                'e657df4ab982bca27be8b17bd39578db731d68c5',
+                0,
+                $throwable
+            );
         }
-
-        if (!empty($versionsToPrune)) {
-            $versionsToPruneAsSQL = implode(',', $versionsToPrune);
-            $SQL = <<<EOT
-
-                DELETE FROM CBModelVersions
-                WHERE       ID = {$IDAsSQL} AND
-                            version in ($versionsToPruneAsSQL)
-
-EOT;
-
-            Colby::query($SQL);
-        }
-
-        /**
-         * Reschedule this task to run again in 5 days to further prune the
-         * model versions.
-         */
-        return (object)[
-            'scheduled' => time() + (60 * 60 * 24 * 5),
-            'priority' => CBModelPruneVersionsTask::standardTaskPriority,
-        ];
     }
+    /* CBTasks2_run() */
+
+
 
     /**
      * This function takes an array of version records and:
@@ -124,8 +195,13 @@ EOT;
      *      that. So instead this function just modifies the $versions array in
      *      place and returns void.
      */
-    static function assignActions(array $versions): void {
-        if (empty($versions)) {
+    static function
+    assignActions(
+        array $versions
+    ): void {
+        if (
+            empty($versions)
+        ) {
             return;
         }
 
@@ -157,10 +233,16 @@ EOT;
          * first record that could potentially be pruned.
          */
 
-        for ($index = 1; $index < $count; $index++) {
+        for (
+            $index = 1;
+            $index < $count;
+            $index++
+        ) {
             $version = $versions[$index];
 
-            if (empty($version->replaced)) {
+            if (
+                empty($version->replaced)
+            ) {
                 $version->replaced = $nextVersion->timestamp;
                 $version->action = 'update';
             }
@@ -168,42 +250,80 @@ EOT;
             // lifespan = the duration the version was the current version
             $lifespan = $version->replaced - $version->timestamp;
 
-            if ($lifespan < 0) {
-                throw new Exception('The lifespan of a version is negative.');
+            if (
+                $lifespan < 0
+            ) {
+                throw new CBExceptionWithValue(
+                    'The lifespan of a version is negative.',
+                    $versions,
+                    '0eff9c8cb5df64adfed9d0a2b6811f7944eb0efb'
+                );
             }
 
             // deathspan = the time since the version was replaced
             $deathspan = $now - $version->replaced;
 
             if ($deathspan < 0) {
-                throw new Exception('The deathspan of a version is negative.');
+                throw new CBExceptionWithValue(
+                    'The deathspan of a version is negative.',
+                    $versions,
+                    '72a78268a09c6db25c26a6f19ba7af9e91c04289'
+                );
             }
 
-            if ($deathspan < 1000) { /* 0 to 16.6 minutes: 10 seconds */
-                if ($lifespan < 10) {
+            if (
+                $deathspan < 1000
+            ) { /* 0 to 16.6 minutes: 10 seconds */
+                if (
+                    $lifespan < 10
+                ) {
                     $version->action = 'prune';
                 }
-            } else if ($deathspan < 100000) { /* 16.6 minutes to 1.1 days: 1.6 minutes */
-                if ($lifespan < 100) {
+            }
+
+            else if (
+                $deathspan < 100000
+            ) { /* 16.6 minutes to 1.1 days: 1.6 minutes */
+                if (
+                    $lifespan < 100
+                ) {
                     $version->action = 'prune';
                 }
-            } else if ($deathspan < 1000000) { /* 1.1 days to 11.5 days: 16.6 minutes */
-                if ($lifespan < 1000) {
+            }
+
+            else if (
+                $deathspan < 1000000
+            ) { /* 1.1 days to 11.5 days: 16.6 minutes */
+                if (
+                    $lifespan < 1000
+                ) {
                     $version->action = 'prune';
                 }
-            } else if ($deathspan < 10000000) { /* 11.5 days to 3.8 months: 2.7 hours */
-                if ($lifespan < 10000) {
+            }
+
+            else if (
+                $deathspan < 10000000
+            ) { /* 11.5 days to 3.8 months: 2.7 hours */
+                if (
+                    $lifespan < 10000
+                ) {
                     $version->action = 'prune';
                 }
-            } else { /* greater than 3.8 months: prune */
+            }
+
+            else { /* greater than 3.8 months: prune */
                 $version->action = 'prune';
             }
 
-            if (empty($version->action)) {
+            if (
+                empty($version->action)
+            ) {
                 $version->action = 'keep';
             }
 
             $nextVersion = $version;
         }
     }
+    /* assignActions() */
+
 }
