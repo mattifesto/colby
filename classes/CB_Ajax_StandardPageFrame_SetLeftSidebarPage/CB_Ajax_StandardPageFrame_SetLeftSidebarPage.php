48cd3af3eb6ba58f7f388130e829261eb65572d2
Matt Calkins (wolf.mattcalkins.corp.mattifesto.com) <matt@mattifesto.com>
2022-02-12 12:38:32 -0800

    Create CB_Ajax_StandardPageFrame_SetLeftSidebarPage.php

---
+++ classes/CB_Ajax_StandardPageFrame_SetLeftSidebarPage/CB_Ajax_StandardPageFrame_SetLeftSidebarPage.php
@@ -0,0 +1,67 @@
+<?php
+
+final class
+CB_Ajax_StandardPageFrame_SetLeftSidebarPage {
+
+    /* -- CBAjax interfaces -- */
+
+
+
+    /**
+     * @param object $executorArguments
+     *
+     *      {
+     *          CB_Ajax_StandardPageFrame_SetLeftSidebarPage_pageModelCBID: string,
+     *      }
+     *
+     * @param CBID|null $callingUserModelCBID
+     *
+     * @return void
+     */
+    static function
+    CBAjax_execute(
+        stdClass $executorArguments,
+        ?string $callingUserModelCBID = null
+    ): void {
+        $pageModelCBID = CBModel::valueAsCBID(
+            $executorArguments,
+            'CB_Ajax_StandardPageFrame_SetLeftSidebarPage_pageModelCBID'
+        );
+
+        if (
+            $pageModelCBID === null
+        ) {
+            // TODO throw exception
+        }
+
+        CB_StandardPageFrame::setLeftSidebarPageModelCBID(
+            $pageModelCBID
+        );
+    }
+    /* CBAjax_execute() */
+
+
+
+    /**
+     * @param CBID userModelCBID
+     *
+     * @return bool
+     */
+    static function
+    CBAjax_userModelCBIDCanExecute(
+        ?string $callingUserModelCBID = null
+    ): bool {
+
+        $userIsAnAdministrator = (
+            $callingUserModelCBID !== null &&
+            CBUserGroup::userIsMemberOfUserGroup(
+                $callingUserModelCBID,
+                'CBAdministratorsUserGroup'
+            )
+        );
+
+        return $userIsAnAdministrator;
+    }
+    /* CBAjax_userModelCBIDCanExecute() */
+
+}
