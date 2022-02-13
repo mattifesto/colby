5bcc8ec5d2edb801d84102435dba43a02fa44663
Matt Calkins (wolf.mattcalkins.corp.mattifesto.com) <matt@mattifesto.com>
2022-02-13 15:37:36 -0800

    Create CB_Ajax_YouTube_GetStatistics.php

---
+++ classes/CB_Ajax_YouTube_GetStatistics/CB_Ajax_YouTube_GetStatistics.php
@@ -0,0 +1,77 @@
+<?php
+
+final class
+CB_Ajax_YouTube_GetStatistics {
+
+    /* -- CBAjax interfaces -- */
+
+
+
+    /**
+     * @param object $executorArguments
+     * @param string|null $callingUserModelCBID
+     *
+     * @return object|null
+     */
+    static function
+    CBAjax_execute(
+        stdClass $executorArguments,
+        ?string $callingUserModelCBID = null
+    ): ?stdClass {
+        $youtubeChannelID = CBSitePreferences::getYouTubeChannelID(
+            CBSitePreferences::model()
+        );
+
+        if (
+            $youtubeChannelID === ''
+        ) {
+            return null;
+        }
+
+        $youtubeAPIKey = CBSitePreferences::getYouTubeAPIKey(
+            CBSitePreferences::model()
+        );
+
+        if (
+            $youtubeAPIKey === ''
+        ) {
+            return null;
+        }
+
+        $returnValue = CB_YouTube::call(
+            'channels',
+            (object)[
+                'id' => $youtubeChannelID,
+                'key' => $youtubeAPIKey,
+                'part' => 'statistics'
+            ]
+        );
+
+        return $returnValue;
+    }
+    /* CBAjax_execute() */
+
+
+
+    /**
+     * @param CBID callingUserModelCBID
+     *
+     * @return bool
+     */
+    static function
+    CBAjax_userModelCBIDCanExecute(
+        ?string $callingUserModelCBID = null
+    ): bool {
+        $userIsAnAdministrator = (
+            $callingUserModelCBID !== null &&
+            CBUserGroup::userIsMemberOfUserGroup(
+                $callingUserModelCBID,
+                'CBAdministratorsUserGroup'
+            )
+        );
+
+        return $userIsAnAdministrator;
+    }
+    /* CBAjax_userModelCBIDCanExecute() */
+
+}
