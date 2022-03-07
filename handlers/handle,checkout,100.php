7cec72e66df378dd2efb23333b2c4c2fbdf1b629
Matt Calkins (wolf.mattcalkins.corp.mattifesto.com) <matt@mattifesto.com>
2022-03-06 21:04:48 -0800

    Gareden handlers/handle,checkout,100.php

--- handlers/handle,checkout,100.php
+++ handlers/handle,checkout,100.php
@@ -3,11 +3,17 @@
 CBPage::renderSpec(
     CBModelTemplateCatalog::fetchLivePageTemplate(
         (object)[
-            'title' => 'Shipping Address',
-            'description' => 'Enter the address to which you would like your order shipped.',
-            'sections' => [
+            'title' =>
+            'Shipping Address',
+
+            'description' =>
+            'Enter the address to which you would like your order shipped.',
+
+            'sections' =>
+            [
                 (object)[
-                    'className' => 'SCShippingAddressEditorView',
+                    'className' =>
+                    'SCShippingAddressEditorView',
                 ],
             ],
         ]
