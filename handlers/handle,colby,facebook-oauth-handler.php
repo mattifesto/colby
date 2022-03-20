7cf3f376935c377daa086d51e1e41a7e47e29984
Matt Calkins (wolf.mattcalkins.corp.mattifesto.com) <matt@mattifesto.com>
2022-03-20 14:59:13 -0700

    Update handle,colby,facebook-oauth-handler.php
    Allow users with no Facebook name to log in.

--- handlers/handle,colby,facebook-oauth-handler.php
+++ handlers/handle,colby,facebook-oauth-handler.php
@@ -36,20 +36,60 @@ try {
  * denied.
  */
 
-if (isset($_GET['error'])) {
+if (
+    isset($_GET['error'])
+) {
     goto done;
 }
 
-$accessTokenObject = CBFacebook::fetchAccessTokenObject(
+$accessTokenObject =
+CBFacebook::fetchAccessTokenObject(
     $_GET['code'],
     $destinationURL
 );
 
-$facebookAccessToken = $accessTokenObject->access_token;
+$facebookAccessToken =
+$accessTokenObject->access_token;
 
-$facebookUserProperties = CBFacebook::fetchUserProperties(
+
+
+$facebookUserProperties =
+CBFacebook::fetchUserProperties(
     $facebookAccessToken
 );
+
+$facebookError =
+CBModel::valueAsObject(
+    $facebookUserProperties,
+    'error'
+);
+
+if (
+    $facebookError !== null
+) {
+    CBErrorHandler::report(
+        new CBExceptionWithValue(
+            CBConvert::stringToCleanLine(<<<EOT
+
+                The value returned from a call to fetch Facebook user properties
+                indicates that an error occurred.
+
+            EOT),
+            $facebookUserProperties,
+            '0cd100396d18336a94174b8362f28324def8d283'
+        )
+    );
+
+    throw new CBException(
+        CBConvert::stringToCleanLine(<<<EOT
+
+            An error occurred while attempting to log a user in via Facebook.
+
+        EOT),
+        '',
+        '8f4ec53c4f59d779c996662dee1000c142b82107'
+    );
+}
 
 $facebookUserID = CBModel::valueAsInt(
     $facebookUserProperties,
@@ -74,15 +114,30 @@ $facebookName = trim(
  *
  *      In the future give the user a more helpful message and log the current
  *      exception message for the admin to see.
+ *
+ * @NOTE 2022_03_20
+ *
+ *      I don't there there is any problem with loggin in with a Facebook
+ *      account that doesn't have a name for use, but Facebook complains if the
+ *      website doesn't work in that case.
+ *
+ *      We may want to look at getting the name a different way, but more likely
+ *      into removing Facebook login entirely becuase they make it so difficult.
  */
-if ($facebookName === '') {
-    throw CBException::createWithValue(
-        (
-            'The user properties provided by Facebook do not contain ' .
-            'a "name" property.'
-        ),
-        $facebookUserProperties,
-        'e1a1bd35ad3811df94ac1b3dbaa8c088552bc089'
+if (
+    $facebookName === ''
+) {
+    CBErrorHandler::report(
+        CBException::createWithValue(
+            CBConvert::stringToCleanLine(<<<EOT
+
+                The user properties provided by Facebook do not contain
+                a "name" property. An empty name has been used.
+
+            EOT),
+            $facebookUserProperties,
+            'e1a1bd35ad3811df94ac1b3dbaa8c088552bc089'
+        )
     );
 }
 
