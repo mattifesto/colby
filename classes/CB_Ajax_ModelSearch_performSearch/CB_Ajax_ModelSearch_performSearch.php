7f6437c4470310b593dd392776644dd904a9d374
Matt Calkins (wolf.mattcalkins.corp.mattifesto.com) <matt@mattifesto.com>
2022-05-26 15:28:30 -0700

    Create CB_Ajax_ModelSearch_performSearch.php

---
+++ classes/CB_Ajax_ModelSearch_performSearch/CB_Ajax_ModelSearch_performSearch.php
@@ -0,0 +1,76 @@
+<?php
+
+final class
+CB_Ajax_ModelSearch_performSearch
+{
+    // -- CBAjax interfaces
+
+
+
+    /**
+     * @param object $executorArguments
+     *
+     *      {
+     *          CB_Ajax_ModelSearch_performSearch_modelClassName: string,
+     *          CB_Ajax_ModelSearch_performSearch_searchQuery: string,
+     *      }
+     *
+     * @param CBID|null $callingUserModelCBID
+     *
+     * @return [object]
+     */
+    static function
+    CBAjax_execute(
+        stdClass $executorArguments,
+        ?string $callingUserModelCBID = null
+    ): array
+    {
+        $modelClassName =
+        CBModel::valueToString(
+            $executorArguments,
+            'CB_Ajax_ModelSearch_performSearch_modelClassName'
+        );
+
+        $searchQuery =
+        CBModel::valueToString(
+            $executorArguments,
+            'CB_Ajax_ModelSearch_performSearch_searchQuery'
+        );
+
+        $foundModelCBIDs =
+        CBModels::fetchModelCBIDsBySearch(
+            $searchQuery,
+            $modelClassName,
+        );
+
+        $searchResults =
+        CBModels::fetchAdministrativeSearchResults(
+            $foundModelCBIDs
+        );
+
+        return
+        $searchResults;
+    }
+    // CBAjax_execute()
+
+
+
+    /**
+     * @param CBID callingUserModelCBID
+     *
+     * @return bool
+     */
+    static function
+    CBAjax_userModelCBIDCanExecute(
+        ?string $callingUserModelCBID = null
+    ): bool
+    {
+        return
+        CBUserGroup::userIsMemberOfUserGroup(
+            $callingUserModelCBID,
+            'CBAdministratorsUserGroup'
+        );
+    }
+    /* CBAjax_userModelCBIDCanExecute() */
+
+}
