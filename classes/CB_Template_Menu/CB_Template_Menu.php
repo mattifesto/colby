d6743ecf86d8640ff57ce72ce9259dd31937c522
Matt Calkins (wolf.mattcalkins.corp.mattifesto.com) <matt@mattifesto.com>
2022-06-01 15:51:50 -0700

    Create CB_Template_Menu.php

---
+++ classes/CB_Template_Menu/CB_Template_Menu.php
@@ -0,0 +1,61 @@
+<?php
+
+final class
+CB_Template_Menu
+{
+    // -- CBInstall interfaces
+
+
+
+    /**
+     * @return void
+     */
+    static function
+    CBInstall_configure(
+    ): void
+    {
+        CBModelTemplateCatalog::install(
+            'CB_Template_Menu'
+        );
+
+        error_log(__CLASS__);
+    }
+
+
+
+    // -- CBModelTemplate interfaces
+
+
+
+    /**
+     * @return object
+     */
+    static function
+    CBModelTemplate_spec(
+    ): stdClass
+    {
+        $menuSpec =
+        CBModel::createSpec(
+            'CBMenu'
+        );
+
+        return
+        $menuSpec;
+    }
+    // CBModelTemplate_spec()
+
+
+
+    /**
+     * @return string
+     */
+    static function
+    CBModelTemplate_title(
+    ): string
+    {
+        return
+        'Menu';
+    }
+    // CBModelTemplate_title()
+
+}
