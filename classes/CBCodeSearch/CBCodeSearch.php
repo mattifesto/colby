d685601feddf841c761452192b8c9662edee8057
Matt Calkins (wolf.mattcalkins.corp.mattifesto.com) <matt@mattifesto.com>
2022-06-03 15:23:30 -0700

    Create CBCodeSearch.php

---
+++ classes/CBCodeSearch/CBCodeSearch.php
@@ -0,0 +1,22 @@
+<?php
+
+final class
+CBCodeSearch
+{
+    /**
+     * @NOTE 2022_06_03
+     *
+     *      Properties:
+     *
+     *      CBCodeSearch_CBID
+     *      CBCodeSearch_warningVersion_property
+     *
+     *      args
+     *      cbmessage
+     *      noticeStartDate
+     *      noticeVersion
+     *      regex
+     *      severity
+     *      title
+     */
+}
