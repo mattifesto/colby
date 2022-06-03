0e713cb49896860299df258d262368abdc6747fa
Matt Calkins (wolf.mattcalkins.corp.mattifesto.com) <matt@mattifesto.com>
2022-06-03 08:16:26 -0700

    Update CBSetupView.php
    Officially deprecate CBSetupView.

--- classes/CBSetupView/CBSetupView.php
+++ classes/CBSetupView/CBSetupView.php
@@ -1,6 +1,56 @@
 <?php
 
-final class CBSetupView {
+/**
+ * @deprecated 2022_01_29
+ *
+ *      Websites are no longer set up using this class, they are set up
+ *      fully using the cbt command in terminal. This code should be
+ *      removed during a larger task to remove the old setup code.
+ */
+final class
+CBSetupView
+{
+
+    // -- CBCodeAdmin interfaces
+
+
+
+    /**
+     * @return [object]
+     */
+    static function
+    CBCodeAdmin_searches(
+    ): array
+    {
+        return
+        [
+            (object)[
+                'CBCodeSearch_CBID' =>
+                '1b70fd5c75bcaa7c0a0339bd9d94c0ae61ec01d7',
+
+                'cbmessage' => <<<EOT
+
+                    Websites are no longer set up using this method, they are
+                    set up fully using the cbt command in terminal
+
+                EOT,
+
+                'regex' =>
+                '\bCBSetupView\b',
+
+                'severity' =>
+                5,
+
+                'title' =>
+                'CBSetupView',
+
+                'noticeVersion' => '2022_06_03_1654267756',
+            ],
+        ];
+    }
+    /* CBCodeAdmin_searches() */
+
+
 
     /* -- CBHTMLOutput interfaces -- -- -- -- -- */
 
