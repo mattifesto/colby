c7567a75309b746a8dd52ec2535da5cb96a631fc
Matt Calkins (wolf.mattcalkins.corp.mattifesto.com) <matt@mattifesto.com>
2022-06-20 08:43:39 -0700

    Update CBSetupView.php
    The class was removed except for the code search.

--- classes/CBSetupView/CBSetupView.php
+++ classes/CBSetupView/CBSetupView.php
@@ -3,14 +3,11 @@
 /**
  * @deprecated 2022_01_29
  *
- *      Websites are no longer set up using this class, they are set up
- *      fully using the cbt command in terminal. This code should be
- *      removed during a larger task to remove the old setup code.
+ *      This class has been effectively removed.
  */
 final class
 CBSetupView
 {
-
     // -- CBCodeAdmin interfaces
 
 
@@ -20,121 +17,51 @@ CBSetupView
      */
     static function
     CBCodeAdmin_searches(
-    ): array
+    ): stdClass
     {
-        return
-        [
-            (object)[
-                'CBCodeSearch_CBID' =>
-                '1b70fd5c75bcaa7c0a0339bd9d94c0ae61ec01d7',
+        $codeSearchSpec =
+        CBModel::createSpec(
+            'CBCodeSearch',
+            '1b70fd5c75bcaa7c0a0339bd9d94c0ae61ec01d7'
+        );
 
-                'cbmessage' => <<<EOT
+        CBCodeSearch::setErrorVersion(
+            $codeSearchSpec,
+            '2022_06_20_1655739482'
+        );
 
-                    Websites are no longer set up using this method, they are
-                    set up fully using the cbt command in terminal
+        CBCodeSearch::setNoticeVersion(
+            $codeSearchSpec,
+            '2022_06_03_1654267756'
+        );
 
-                EOT,
+        CBCodeSearch::setWarningVersion(
+            $codeSearchSpec,
+            '2022_06_20_1655739481'
+        );
 
-                'regex' =>
-                '\bCBSetupView\b',
+        $codeSearchSpec->args =
+        '--ignore-file=is:CBSetupView.php';
 
-                'severity' =>
-                5,
+        $codeSearchSpec->cbmessage =
+        <<<EOT
 
-                'title' =>
-                'CBSetupView',
+            Websites are no longer set up using this method, they are
+            set up fully using the cbt command in terminal
 
-                'noticeVersion' => '2022_06_03_1654267756',
-            ],
-        ];
+        EOT;
+
+        $codeSearchSpec->regex =
+        '\bCBSetupView\b';
+
+        $codeSearchSpec->severity =
+        3;
+
+        $codeSearchSpec->title =
+        'CBSetupView';
+
+        return $codeSearchSpec;
     }
     /* CBCodeAdmin_searches() */
-
-
-
-    /* -- CBHTMLOutput interfaces -- -- -- -- -- */
-
-
-
-    /**
-     * @return [string]
-     */
-    static function CBHTMLOutput_JavaScriptURLs(): array {
-        return [
-            Colby::flexpath(__CLASS__, 'v636.js', cbsysurl()),
-        ];
-    }
-
-
-
-    /**
-     * @return [string]
-     */
-    static function CBHTMLOutput_requiredClassNames(): array {
-        return [
-            'CBAjax',
-            'CBUI',
-            'CBUIPanel',
-            'CBUIStringEditor',
-            'Colby',
-        ];
-    }
-    /* CBHTMLOutput_requiredClassNames() */
-
-
-
-    /**
-     * @return [[<name>, <value>]]
-     */
-    static function CBHTMLOutput_JavaScriptVariables(): array {
-        return [
-            [
-                'CBSetupView_suggestedWebsiteHostname',
-                $_SERVER['HTTP_HOST'],
-            ],
-        ];
-    }
-    /* CBHTMLOutput_JavaScriptVariables() */
-
-
-
-    /* -- CBModel interfaces -- -- -- -- -- */
-
-
-
-    /**
-     * @param object $viewSpec
-     *
-     * @return object
-     */
-    static function CBModel_build(
-        stdClass $viewSpec
-    ): stdClass {
-        return (object)[];
-    }
-    /* CBModel_build() */
-
-
-
-    /* -- CBView interfaces -- -- -- -- -- */
-
-
-
-    /**
-     * @param object $viewModel
-     *
-     * @return void
-     */
-    static function CBView_render(
-        stdClass $viewModel
-    ): void {
-        ?>
-
-        <div class="CBSetupView">
-        </div>
-
-        <?php
-    }
-    /* CBView_render() */
 
 }
