4e08b79653940940fdaf93a3d286a2b6ae3246ea
Matt Calkins (wolf.mattcalkins.corp.mattifesto.com) <matt@mattifesto.com>
2022-06-20 21:21:08 -0700

    Update CBEqualizePageSettingsPart.php
    Implement CBCodeAdmin_searches() returning searches for CBBackgroundColor and CBTextColor
    Update CSS version number

--- classes/CBEqualizePageSettingsPart/CBEqualizePageSettingsPart.php
+++ classes/CBEqualizePageSettingsPart/CBEqualizePageSettingsPart.php
@@ -3,11 +3,120 @@
 final class
 CBEqualizePageSettingsPart
 {
+    // -- CBCodeAdmin interfaces
+
+
+
+    /**
+     * @return [object]
+     */
+    static function
+    CBCodeAdmin_searches(
+    ): array
+    {
+        $searches =
+        [];
+
+
+
+        $codeSearchSpec =
+        CBModel::createSpec(
+            'CBCodeSearch',
+            '47575ebc7532925602cc4cd2e0bf1b705e73bc0c'
+        );
+
+        CBCodeSearch::setNoticeVersion(
+            $codeSearchSpec,
+            '2021_09_02_1655782594'
+        );
+
+        CBCodeSearch::setWarningVersion(
+            $codeSearchSpec,
+            '2022_06_21_1655783579'
+        );
+
+        $codeSearchSpec->cbmessage =
+        <<<EOT
+
+            Use CBBackgroundColor1
+
+        EOT;
+
+        $codeSearchSpec->regex =
+        '\bCBBackgroundColor\b';
+
+        $codeSearchSpec->severity =
+        4;
+
+        $codeSearchSpec->title =
+        'CBBackgroundColor';
+
+        array_push(
+            $searches,
+            $codeSearchSpec
+        );
+
+
+
+        $codeSearchSpec =
+        CBModel::createSpec(
+            'CBCodeSearch',
+            '37194ab3f05b8295cd76f12da1b65995498abcfe'
+        );
+
+        CBCodeSearch::setNoticeVersion(
+            $codeSearchSpec,
+            '2022_06_21_1655783117'
+        );
+
+        CBCodeSearch::setWarningVersion(
+            $codeSearchSpec,
+            '2022_06_21_1655783609'
+        );
+
+        $codeSearchSpec->cbmessage =
+        <<<EOT
+
+            Use CBTextColor1
+
+        EOT;
+
+        $codeSearchSpec->regex =
+        '\bCBTextColor\b';
+
+        $codeSearchSpec->severity =
+        4;
+
+        $codeSearchSpec->title =
+        'CBTextColor';
+
+        array_push(
+            $searches,
+            $codeSearchSpec
+        );
+
+
+
+        return $searches;
+    }
+    /* CBCodeAdmin_searches() */
+
+
+
     /* -- CBPageSettings interfaces -- */
 
 
 
     /**
+     * @NOTE 2022_06_20
+     *
+     *      Implementing this function instead of CBHTMLOutput_CSSURLs() ensures
+     *      that the CSS file is included in the head element before the files
+     *      included because thare returned by CBHTMLOutput_CSSURLs().
+     *
+     *      I'm not 100% sure this is completely necesary. Document discovered
+     *      facts here.
+     *
      * @return void
      */
     static function
@@ -17,7 +126,7 @@ CBEqualizePageSettingsPart
         $CSSURL =
         Colby::flexpath(
             __CLASS__,
-            'v675.61.1.css',
+            '2022_06_21_1655784419.css',
             cbsysurl()
         );
 
