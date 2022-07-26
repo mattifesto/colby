cffda612b9a0f1ec30212d465c95ef7797746445
Matt Calkins (wolf.mattcalkins.corp.mattifesto.com) <matt@mattifesto.com>
2022-07-26 13:12:58 -0700

    Update SCCountry.php
    Garden

--- classes/SCCountry/SCCountry.php
+++ classes/SCCountry/SCCountry.php
@@ -1,67 +1,173 @@
 <?php
 
-final class SCCountry {
+final class
+SCCountry
+{
+    // CBModel interfaces
+
+
 
     /**
      * @param object $spec
      *
      * @return ?object
      */
-    static function CBModel_build(stdClass $spec): ?stdClass {
-        return (object)[
-            'isActive' => CBModel::valueToBool($spec, 'isActive'),
-            'isDefault' => CBModel::valueToBool($spec, 'isDefault'),
-            'moniker' => CBModel::valueAsMoniker($spec, 'moniker'),
-            'title' => trim(CBModel::valueToString($spec, 'title')),
+    static function
+    CBModel_build(
+        stdClass $spec
+    ): stdClass
+    {
+        $countryModel =
+        (object)
+        [
+            'isActive' =>
+            CBModel::valueToBool(
+                $spec,
+                'isActive'
+            ),
+
+            'isDefault' =>
+            CBModel::valueToBool(
+                $spec,
+                'isDefault'
+            ),
+
+            'moniker' =>
+            CBModel::valueAsMoniker(
+                $spec,
+                'moniker'
+            ),
+
+            'title' =>
+            trim(
+                CBModel::valueToString(
+                    $spec,
+                    'title'
+                )
+            ),
         ];
+
+        return $countryModel;
     }
+    // CBModel_build()
+
+
 
     /**
      * @param object $spec
      *
-     * @return ?ID
+     * @return CBID|null
      */
-    static function CBModel_toID(stdClass $spec): ?string {
-        $moniker = CBModel::valueAsMoniker($spec, 'moniker');
+    static function
+    CBModel_toID(
+        stdClass $spec
+    ): ?string
+    {
+        $moniker =
+        CBModel::valueAsMoniker(
+            $spec,
+            'moniker'
+        );
 
-        if ($moniker === null) {
+        if (
+            $moniker === null
+        ) {
             return null;
-        } else {
-            return SCCountry::monikerToID($moniker);
+        }
+
+        else
+        {
+            $CBID =
+            SCCountry::monikerToID(
+                $moniker
+            );
+
+            return $CBID;
         }
     }
+    // CBModel_toID()
+
+
+
+    // CBModels interfaces
+
+
 
     /**
-     * @param [ID] $modelIDs
+     * @param [string] $modelCBIDs
      *
      * @return void
      */
-    static function CBModels_willDelete(array $modelIDs): void {
-        CBTasks2::restart('SCCountryUpdateTask', $modelIDs);
+    static function
+    CBModels_willDelete(
+        array $modelCBIDs
+    ): void
+    {
+        CBTasks2::restart(
+            'SCCountryUpdateTask',
+            $modelCBIDs
+        );
     }
+    // CBModels_willDelete()
+
+
 
     /**
-     * @param [model] $models
+     * @param [object] $models
      *
      * @return void
      */
-    static function CBModels_willSave(array $models): void {
-        $modelIDs = array_map(
-            function ($model) {
-                return $model->ID;
+    static function
+    CBModels_willSave(
+        array $models
+    ): void
+    {
+        $modelCBIDs =
+        array_map(
+            function (
+                $model
+            ): string
+            {
+                $CBID =
+                CBModel::getCBID(
+                    $model
+                );
+
+                return $CBID;
             },
             $models
         );
 
-        CBTasks2::restart('SCCountryUpdateTask', $modelIDs);
+        CBTasks2::restart(
+            'SCCountryUpdateTask',
+            $modelCBIDs
+        );
     }
+    // CBModels_willSave()
+
+
+
+    // functions
+
+
 
     /**
      * @param string $moniker
      *
-     * @return ID
+     * @return CBID
      */
-    static function monikerToID(string $moniker): string {
-        return sha1("96e6dabb16069a8d53fd66f58430c6ac8e0f7a40 {$moniker}");
+    static function
+    monikerToID(
+        string $moniker
+    ): string
+    {
+        $CBID =
+        sha1(
+            "96e6dabb16069a8d53fd66f58430c6ac8e0f7a40 {$moniker}"
+        );
+
+        return $CBID;
     }
+    // monikerToID()
+
 }
