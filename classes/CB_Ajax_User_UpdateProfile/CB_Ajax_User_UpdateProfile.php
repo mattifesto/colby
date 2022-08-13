19c06fd373153ee6d3a27ce0a0080aebae974491
Matt Calkins (wolf.mattcalkins.corp.mattifesto.com) <matt@mattifesto.com>
2022-08-13 08:03:40 -0700

    Create CB_Ajax_User_UpdateProfile.php

---
+++ classes/CB_Ajax_User_UpdateProfile/CB_Ajax_User_UpdateProfile.php
@@ -0,0 +1,130 @@
+<?php
+
+final class
+CB_Ajax_User_UpdateProfile
+{
+    // -- CBAjax interfaces
+
+
+
+    /**
+     * @param object $executorArguments
+     *
+     *      {
+     *          CB_Ajax_User_UpdateProfile_targetUserModelCBID_argument:
+     *          <CBID>,
+     *
+     *          CB_Ajax_User_UpdateProfile_targetUserFullName_argument:
+     *          <string>,
+     *      }
+     *
+     * @param CBID|null $callingUserModelCBID
+     *
+     * @return void
+     */
+    static function
+    CBAjax_execute(
+        stdClass $executorArguments,
+        ?string $callingUserModelCBID = null
+    ): void
+    {
+        $targetUserModelCBID =
+        CBModel::valueAsCBID(
+            $executorArguments,
+            'CB_Ajax_User_UpdateProfile_targetUserModelCBID_argument'
+        );
+
+        $targetUserModelUpdater =
+        new CBModelUpdater(
+            $targetUserModelCBID
+        );
+
+        $targetUserSpec =
+        $targetUserModelUpdater->getSpec();
+
+
+
+        // full name
+
+        $updatedTargetUserFullName =
+        CBModel::valueToString(
+            $executorArguments,
+            'CB_Ajax_User_UpdateProfile_targetUserFullName_argument'
+        );
+
+        CBUser::setName(
+            $targetUserSpec,
+            $updatedTargetUserFullName
+        );
+
+
+
+        // save
+
+        CBDB::transaction(
+            function () use (
+                $targetUserModelUpdater
+            ): void
+            {
+                $targetUserModelUpdater->save2();
+            }
+        );
+    }
+    // CBAjax_execute()
+
+
+
+    /**
+     * @param CBID callingUserModelCBID
+     *
+     * @return bool
+     */
+    static function
+    CBAjax_userModelCBIDCanExecute(
+        ?string $callingUserModelCBID = null
+    ): bool
+    {
+        /**
+         * Users who are not signed in can't update any user's profile.
+         */
+
+        if (
+            $callingUserModelCBID ===
+            null
+        ) {
+            return false;
+        }
+
+
+
+        /**
+         * A user can update their own user profile.
+         */
+
+        $currentUseModelCBID =
+        ColbyUser::getCurrentUserCBID();
+
+        if (
+            $callingUserModelCBID ===
+            $currentUseModelCBID
+        ) {
+            return true;
+        }
+
+
+
+        /**
+         * An administrator can update any user's profile.
+         */
+
+        $currentUserIsAnAdministrator =
+        CBUserGroup::userIsMemberOfUserGroup(
+            $currentUseModelCBID,
+            'CBAdministratorsUserGroup'
+        );
+
+        return $currentUserIsAnAdministrator;
+    }
+    // CBAjax_userModelCBIDCanExecute()
+
+}
