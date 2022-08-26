c4c356c630d32b65d4113c2a7f4a6616fd9d23b9
Matt Calkins (wolf.mattcalkins.corp.mattifesto.com) <matt@mattifesto.com>
2022-08-26 10:36:08 -0700

    Update CB_CBView_MomentCreator.js
    Garden

--- classes/CB_CBView_MomentCreator/CB_CBView_MomentCreator.js
+++ classes/CB_CBView_MomentCreator/CB_CBView_MomentCreator.js
@@ -11,25 +11,36 @@
 */
 
 
-(function () {
+(function ()
+{
     "use strict";
 
-    window.CB_CBView_MomentCreator = {
+    let CB_CBView_MomentCreator =
+    {
         create: CB_CBView_MomentCreator_create,
     };
+
+    window.CB_CBView_MomentCreator =
+    CB_CBView_MomentCreator;
 
 
 
     Colby.afterDOMContentLoaded(
-        function () {
-            let elements = Array.from(
+        function (
+        ) // -> undefined
+        {
+            let elements =
+            Array.from(
                 document.getElementsByClassName(
                     "CB_CBView_MomentCreator"
                 )
             );
 
             elements.forEach(
-                function (element) {
+                function (
+                    element
+                ) // -> undefined
+                {
                     CB_CBView_MomentCreator_initializeElement(
                         element
                     );
@@ -49,9 +60,11 @@
      */
     function
     CB_CBView_MomentCreator_create(
-    ) {
+    ) // -> object
+    {
         if (
-            CB_Moment_showMomentEditor_jsvariable !== true
+            CB_Moment_showMomentEditor_jsvariable !==
+            true
         ) {
             return;
         }
@@ -66,13 +79,15 @@
         momentCreatorRootElement.className =
         "CB_CBView_MomentCreator";
 
-        let stringEditor = CB_UI_StringEditor.create();
+        let stringEditor =
+        CB_UI_StringEditor.create();
 
         stringEditor.CB_UI_StringEditor_setPlaceholderText(
             "Share a Moment"
         );
 
-        let stringEditorElement = stringEditor.CB_UI_StringEditor_getElement();
+        let stringEditorElement =
+        stringEditor.CB_UI_StringEditor_getElement();
 
         stringEditorElement.classList.add(
             "CB_UI_StringEditor_tall"
@@ -91,28 +106,39 @@
         let imageFileInputElement;
         let imageModel;
 
-        let imageContainerElement = document.createElement(
+        let imageContainerElement =
+        document.createElement(
             "div"
         );
 
-        imageContainerElement.className = (
-            "CB_CBView_MomentCreator_imageContainer_element"
-        );
+        imageContainerElement.className =
+        "CB_CBView_MomentCreator_imageContainer_element";
 
-        imageElement = document.createElement(
+        imageElement =
+        document.createElement(
             "img"
         );
 
-        imageElement.className = "CB_CBView_MomentCreator_image_element";
+        imageElement.className =
+        "CB_CBView_MomentCreator_image_element";
 
         imageContainerElement.append(
             imageElement
         );
 
-        imageFileInputElement = document.createElement("input");
-        imageFileInputElement.type = "file";
-        imageFileInputElement.style.display = "none";
-        imageFileInputElement.accept="image/jpeg, image/png, image/gif";
+        imageFileInputElement =
+        document.createElement(
+            "input"
+        );
+
+        imageFileInputElement.type =
+        "file";
+
+        imageFileInputElement.style.display =
+        "none";
+
+        imageFileInputElement.accept =
+        "image/jpeg, image/png, image/gif";
 
         momentCreatorRootElement.append(
             imageFileInputElement
@@ -180,14 +206,17 @@
             }
         );
 
-        addImageButton = CBUIButton.create();
+        addImageButton =
+        CBUIButton.create();
 
         addImageButton.CBUIButton_setTextContent(
             "Add Image"
         );
 
         addImageButton.CBUIButton_addClickEventListener(
-            function () {
+            function (
+            ) // -> undefined
+            {
                 imageFileInputElement.click();
             }
         );
@@ -196,14 +225,17 @@
 
         // share button
 
-        let shareButton = CBUIButton.create();
+        let shareButton =
+        CBUIButton.create();
 
         shareButton.CBUIButton_setTextContent(
             "Share"
         );
 
         shareButton.CBUIButton_addClickEventListener(
-            function () {
+            function (
+            ) // -> undefined
+            {
                 createMoment();
             }
         );
@@ -211,19 +243,21 @@
 
         /* buttons */
         {
-            let buttonBarContainerElement = document.createElement(
+            let buttonBarContainerElement =
+            document.createElement(
                 "div"
             );
 
-            buttonBarContainerElement.className = (
-                "CB_CBView_MomentCreator_buttonBarContainer"
-            );
+            buttonBarContainerElement.className =
+            "CB_CBView_MomentCreator_buttonBarContainer";
 
-            let buttonBarElement = document.createElement(
+            let buttonBarElement =
+            document.createElement(
                 "div"
             );
 
-            buttonBarElement.className = "CB_CBView_MomentCreator_buttonBar";
+            buttonBarElement.className =
+            "CB_CBView_MomentCreator_buttonBar";
 
             buttonBarElement.append(
                 addImageButton.CBUIButton_getElement()
@@ -250,26 +284,33 @@
          */
         async function
         createMoment(
-        ) {
-            try {
+        ) // -> undefined
+        {
+            try
+            {
                 shareButton.CBUIButton_setIsDisabled(
                     true
                 );
 
-                let response = await CBAjax.call(
+                let executorArguments =
+                {
+                    CB_Moment_create_imageModel_parameter:
+                    imageModel,
+
+                    CB_Moment_create_text_parameter:
+                    stringEditor.CB_UI_StringEditor_getValue(),
+                };
+
+                let response =
+                await CBAjax.call(
                     "CB_Moment",
                     "create",
-                    {
-                        CB_Moment_create_imageModel_parameter: imageModel,
-
-                        CB_Moment_create_text_parameter: (
-                            stringEditor.CB_UI_StringEditor_getValue()
-                        ),
-                    }
+                    executorArguments
                 );
 
                 if (
-                    response.CB_Moment_create_userErrorMessage !== null
+                    response.CB_Moment_create_userErrorMessage !==
+                    null
                 ) {
                     CBUIPanel.displayText2(
                         response.CB_Moment_create_userErrorMessage
@@ -308,7 +349,9 @@
                         response.CB_Moment_create_momentModel
                     );
                 }
-            } catch (
+            }
+
+            catch (
                 error
             ) {
                 CBErrorHandler.report(
@@ -320,7 +363,10 @@
                     An error occured. Site administrators have been notified.
 
                 `);
-            } finally {
+            }
+
+            finally
+            {
                 shareButton.CBUIButton_setIsDisabled(
                     false
                 );
@@ -335,7 +381,8 @@
          */
         function
         CB_CBView_MomentCreator_getElement(
-        ) {
+        ) // -> Element
+        {
             return momentCreatorRootElement;
         }
         /* CB_CBView_MomentCreator_getElement() */
@@ -350,25 +397,33 @@
         function
         CB_CBView_MomentCreator_setNewMomentCallback(
             potentialCallback
-        ) {
+        ) // -> undefined
+        {
             if (
-                potentialCallback !== undefined &&
-
-                typeof potentialCallback !== "function"
+                potentialCallback !==
+                undefined &&
+                typeof potentialCallback !==
+                "function"
             ) {
-                throw new Error("potentialCallback");
+                throw new Error(
+                    "potentialCallback"
+                );
             }
 
-            newMomentCallback = potentialCallback;
+            newMomentCallback =
+            potentialCallback;
         }
         /* CB_CBView_MomentCreator_setNewMomentCallback() */
 
 
 
-        return {
+        let api =
+        {
             CB_CBView_MomentCreator_getElement,
             CB_CBView_MomentCreator_setNewMomentCallback,
         };
+
+        return api;
     }
     /* CB_CBView_MomentCreator_create() */
 
@@ -382,15 +437,17 @@
     function
     CB_CBView_MomentCreator_initializeElement(
         element
-    ) {
-        let momentCreator = CB_CBView_MomentCreator_create();
+    ) // -> undefined
+    {
+        let momentCreator =
+        CB_CBView_MomentCreator_create();
 
-        let momentCreatorElement = (
-            momentCreator.CB_CBView_MomentCreator_getElement()
-        );
+        let momentCreatorElement =
+        momentCreator.CB_CBView_MomentCreator_getElement();
 
         if (
-            momentCreatorElement !== undefined
+            momentCreatorElement !==
+            undefined
         ) {
             element.append(
                 momentCreatorElement
@@ -399,4 +456,5 @@
     }
     /* CB_CBView_MomentCreator_initializeElement() */
 
-})();
+}
+)();
