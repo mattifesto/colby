43af252fe943bb46de9a09eb940a12786bdab690
Matt Calkins (wolf.mattcalkins.corp.mattifesto.com) <matt@mattifesto.com>
2022-09-14 18:17:27 -0700

    Create CB_View_SVGBarChart1.js

---
+++ classes/CB_View_SVGBarChart1/CB_View_SVGBarChart1.js
@@ -0,0 +1,70 @@
+/* global
+    CBJavaScript,
+*/
+
+(function ()
+{
+    "use strict";
+
+
+
+    CBJavaScript.afterDOMContentLoaded(
+        function (
+        ) // -> undefined
+        {
+            let rootElements =
+            Array.from(
+                document.getElementsByClassName(
+                    "CB_View_SVGBarChart1_root_element"
+                )
+            );
+
+
+
+            rootElements.forEach(
+                function (
+                    rootElement
+                ) // -> undefined
+                {
+                    let currentValueElement =
+                    rootElement.getElementsByClassName(
+                        "CB_View_SVGBarChart1_currentValue_element"
+                    )[0];
+
+                    let barElements =
+                    Array.from(
+                        rootElement.getElementsByClassName(
+                            "CB_View_SVGBarChart1_bar_element"
+                        )
+                    );
+
+
+
+                    barElements.forEach(
+                        function (
+                            barElement
+                        ) // -> undefined
+                        {
+                            barElement.addEventListener(
+                                "mouseover",
+                                function (
+                                ) // -> undefined
+                                {
+                                    currentValueElement.textContent =
+                                    barElement.dataset.value;
+                                }
+                            );
+                        }
+                    );
+                    // barElements.forEach()
+
+                }
+            );
+            // rootElements.forEach()
+
+        }
+    );
+    // CBJavaScript.afterDOMContentLoaded()
+
+}
+)();
