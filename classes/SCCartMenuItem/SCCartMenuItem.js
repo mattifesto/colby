c5bc65ab32fff34031e4c5c435adc4fac6d61917
Matt Calkins (wolf.mattcalkins.corp.mattifesto.com) <matt@mattifesto.com>
2022-10-25 11:03:45 -0700

    Update SCCartMenuItem.js
    Wrap in an IIFE.

--- classes/SCCartMenuItem/SCCartMenuItem.js
+++ classes/SCCartMenuItem/SCCartMenuItem.js
@@ -1,21 +1,24 @@
-"use strict";
-/* jshint strict: global */
-/* jshint esversion: 6 */
-/* exported SCCartMenuItem */
 /* global
     Colby,
     SCCartItem,
     SCShoppingCart,
 */
 
-var SCCartMenuItem = {
+(function()
+{
+    "use strict";
+
 
     /**
      * @param Element element
      *
      * @return undefined
      */
-    activateElement: function (element) {
+    function
+    SCCartMenuItem_activateElement(
+        element
+    ) // -> undefined
+    {
         let anchorElement = document.createElement("a");
 
         element.appendChild(anchorElement);
@@ -54,20 +57,20 @@ var SCCartMenuItem = {
             }
         }
         /* activateElement_updateQuantity() */
-    },
-    /* activateElement() */
-};
-/* SCCartMenuItem */
-
-
-Colby.afterDOMContentLoaded(
-    function SCCartMenuItem_afterDOMContentLoaded() {
-        let elements = document.getElementsByClassName("SCCartMenuItem");
-
-        for (let index = 0; index < elements.length; index += 1) {
-            SCCartMenuItem.activateElement(
-                elements.item(index)
-            );
-        }
     }
-);
+    /* SCCartMenuItem_activateElement() */
+
+
+    Colby.afterDOMContentLoaded(
+        function SCCartMenuItem_afterDOMContentLoaded() {
+            let elements = document.getElementsByClassName("SCCartMenuItem");
+
+            for (let index = 0; index < elements.length; index += 1) {
+                SCCartMenuItem_activateElement(
+                    elements.item(index)
+                );
+            }
+        }
+    );
+
+})();
