612aec190a3e40b6fc0a190bc5b3262a101dfa84
Matt Calkins (wolf.mattcalkins.corp.mattifesto.com) <matt@mattifesto.com>
2022-10-25 17:09:40 -0700

    Create CB_CBView_MostRecentUserMoment.js

---
+++ classes/CB_CBView_MostRecentUserMoment/CB_CBView_MostRecentUserMoment.js
@@ -0,0 +1,36 @@
+(function ()
+{
+    "use strict";
+
+
+
+    let CB_CBView_MostRecentUserMoment =
+    {
+        setUserModelCBID:
+        CB_CBView_MostRecentUserMoment_setUserModelCBID,
+    };
+
+    window.CB_CBView_MostRecentUserMoment =
+    CB_CBView_MostRecentUserMoment;
+
+
+
+    /**
+     * @param object viewModelArgument
+     * @param string userModelCBIDArgument
+     *
+     * @return undefined
+     */
+    function
+    CB_CBView_MostRecentUserMoment_setUserModelCBID(
+        viewModelArgument,
+        userModelCBIDArgument
+    ) // -> undefined
+    {
+        viewModelArgument.CB_CBView_MostRecentUserMoment_userModelCBID =
+        userModelCBIDArgument;
+    }
+    // CB_CBView_MostRecentUserMoment_setUserModelCBID()
+
+}
+)();
