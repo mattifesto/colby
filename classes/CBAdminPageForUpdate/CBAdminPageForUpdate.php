0a8a9a499dc55c98179a2f236c7669987fcb7cf7
Matt Calkins (wolf.mattcalkins.corp.mattifesto.com) <matt@mattifesto.com>
2022-10-30 18:40:23 -0700

    CBAdminPageForUpdate.php : Update JS and CSS file version numbers

--- classes/CBAdminPageForUpdate/CBAdminPageForUpdate.php
+++ classes/CBAdminPageForUpdate/CBAdminPageForUpdate.php
@@ -195,7 +195,7 @@ CBAdminPageForUpdate
         [
             CBLibrary::buildLibraryClassFilePath(
                 __CLASS__,
-                '2022_10_29_1667059611',
+                '2022_10_30_1667167671',
                 'css',
                 cbsysurl()
             ),
@@ -218,7 +218,7 @@ CBAdminPageForUpdate
         [
             CBLibrary::buildLibraryClassFilePath(
                 __CLASS__,
-                '2022_10_29_1667058389',
+                '2022_10_30_1667148515',
                 'js',
                 cbsysurl()
             ),
