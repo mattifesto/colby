89ebe40ecdbd3efb6546f8e66cade708d3d1a93a
Matt Calkins (wolf.mattcalkins.corp.mattifesto.com) <matt@mattifesto.com>
2022-11-13 11:01:46 -0800

    Garden Documentation_CBTest.php

--- classes/Documentation_CBTest/Documentation_CBTest.php
+++ classes/Documentation_CBTest/Documentation_CBTest.php
@@ -1,80 +1,151 @@
 <?php
 
-final class Documentation_CBTest {
+final class
+Documentation_CBTest
+{
+    // -- CBAdmin interfaces
+
+
 
     /**
      * @return string
      */
-    static function CBAdmin_getUserGroupClassName(): string {
+    static function
+    CBAdmin_getUserGroupClassName(
+    ): string
+    {
         return 'CBDevelopersUserGroup';
     }
+
+
 
     /**
      * @return [string]
      */
-    static function CBAdmin_menuNamePath(): array {
+    static function
+    CBAdmin_menuNamePath(
+    ): array
+    {
         return [
             'help',
         ];
     }
 
+
+
     /**
      * @return void
      */
-    static function CBAdmin_render(): void {
-        CBHTMLOutput::pageInformation()->title = 'CBTest Documentation';
+    static function
+    CBAdmin_render(
+    ): void
+    {
+        CBHTMLOutput::pageInformation()->title =
+        'CBTest Documentation';
 
         $CSS = <<<EOT
 
-            main.CBUIRoot {
-                background-color: var(--CBBackgroundColor);
+            main.CBUIRoot
+            {
+                background-color:
+                var(--CBBackgroundColor);
             }
 
-EOT;
+            EOT;
 
-        CBHTMLOutput::addCSS($CSS);
+        CBHTMLOutput::addCSS(
+            $CSS
+        );
 
-        CBView::renderSpec((object)[
-            'className' => 'CBPageTitleAndDescriptionView',
-        ]);
+        CBView::renderSpec(
+            (object)
+            [
+                'className' =>
+                'CBPageTitleAndDescriptionView',
+            ]
+        );
 
-        CBView::renderSpec((object)[
-            'className' => 'CBMessageView',
-            'markup' => file_get_contents(
-                Colby::flexpath(__CLASS__, 'mmk', cbsysdir())
-            ),
-        ]);
+        CBView::renderSpec(
+            (object)
+            [
+                'className' =>
+                'CBMessageView',
+
+                'markup' =>
+                file_get_contents(
+                    Colby::flexpath(
+                        __CLASS__,
+                        'mmk',
+                        cbsysdir()
+                    )
+                ),
+            ]
+        );
     }
+    // CBAdmin_render()
+
+
+
+    // -- CBInstall interfaces
+
+
 
     /**
      * @return void
      */
-    static function CBInstall_install(): void {
-        $updater = CBModelUpdater::fetch((object)[
-            'ID' => CBHelpAdminMenu::ID(),
-        ]);
-
-        $menuSpec = $updater->working;
-
-        CBMenu::addOrReplaceItem(
-            $menuSpec,
-            (object)[
-                'className' => 'CBMenuItem',
-                'name' => 'CBTest',
-                'text' => 'CBTest',
-                'URL' => '/admin/?c=Documentation_CBTest',
+    static function
+    CBInstall_install(
+    ): void
+    {
+        $updater =
+        CBModelUpdater::fetch(
+            (object)
+            [
+                'ID' =>
+                CBHelpAdminMenu::ID(),
             ]
         );
 
-        CBModelUpdater::save($updater);
+        $menuSpec =
+        $updater->working;
+
+        CBMenu::addOrReplaceItem(
+            $menuSpec,
+            (object)
+            [
+                'className' =>
+                'CBMenuItem',
+
+                'name' =>
+                'CBTest',
+
+                'text' =>
+                'CBTest',
+
+                'URL' =>
+                '/admin/?c=Documentation_CBTest',
+            ]
+        );
+
+        CBModelUpdater::save(
+            $updater
+        );
     }
+    // CBInstall_install()
+
+
 
     /**
      * @return [string]
      */
-    static function CBInstall_requiredClassNames(): array {
+    static function
+    CBInstall_requiredClassNames(
+    ): array
+    {
         return [
             'CBHelpAdminMenu'
         ];
     }
+    // CBInstall_requiredClassNames()
+
 }
