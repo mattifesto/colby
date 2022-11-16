1313d14094bd0ff4a8bc4b9480ddc64e2b3f8e21
Matt Calkins (wolf.mattcalkins.corp.mattifesto.com) <matt@mattifesto.com>
2022-11-16 09:43:57 -0800

    Garden CBContentStyleSheet.php

--- classes/CBContentStyleSheet/CBContentStyleSheet.php
+++ classes/CBContentStyleSheet/CBContentStyleSheet.php
@@ -1,24 +1,30 @@
 <?php
 
-final class CBContentStyleSheet {
-
-    /* -- CBHTMLOutput interfaces -- */
+final class
+CBContentStyleSheet
+{
+    // -- CBHTMLOutput interfaces
 
 
 
     /**
      * @return [string]
      */
-    static function CBHTMLOutput_CSSURLs(
-    ) {
-        return [
+    static function
+    CBHTMLOutput_CSSURLs(
+    ): array
+    {
+        $arrayOfCSSURLs =
+        [
             Colby::flexpath(
                 __CLASS__,
                 'v648.css',
                 cbsysurl()
             ),
         ];
+
+        return $arrayOfCSSURLs;
     }
-    /* CBHTMLOutput_CSSURLs() */
+    // CBHTMLOutput_CSSURLs()
 
 }
