2ed4b8717420ac6d317bfbb1742188afb340d6e0
Matt Calkins (wolf.mattcalkins.corp.mattifesto.com) <matt@mattifesto.com>
2022-12-04 10:22:06 -0800

    CBUISpecClipboard.php : deprecate "specs" JS property

--- classes/CBUISpecClipboard/CBUISpecClipboard.php
+++ classes/CBUISpecClipboard/CBUISpecClipboard.php
@@ -3,6 +3,82 @@
 final class
 CBUISpecClipboard
 {
+    // -- CBCodeAdmin interfaces
+
+
+
+    /**
+     * @return [object]
+     */
+    static function
+    CB_CBAdmin_Code_getDeprecatedCodeSearches(
+    ): array
+    {
+        $searches =
+        [];
+
+
+
+        // CBUISpecClipboard.specs
+
+        $codeSearchSpec =
+        CBModel::createSpec(
+            'CBCodeSearch',
+            '2b59f53f576404075b95249cff87477144b3041c'
+        );
+
+        CBCodeSearch::setNoticeVersion(
+            $codeSearchSpec,
+            '2022_12_04_1670170445'
+        );
+
+        CBCodeSearch::setWarningVersion(
+            $codeSearchSpec,
+            '2022_12_04_1670170446'
+        );
+
+        CBCodeSearch::setErrorVersion(
+            $codeSearchSpec,
+            '2022_12_04_1670170447'
+        );
+
+        CBCodeSearch::setCBMessage(
+            $codeSearchSpec,
+            <<<EOT
+
+                Use CBUISpecClipboard.getSpecs() or
+                CBUISpecClipboard.setSpecs().
+
+            EOT
+        );
+
+        CBCodeSearch::setFileType(
+            $codeSearchSpec,
+            'js'
+        );
+
+        $codeSearchSpec->regex =
+        '\bCBUISpecClipboard\.specs\b';
+
+        $codeSearchSpec->severity =
+        3;
+
+        $codeSearchSpec->title =
+        'CBUISpecClipboard.specs';
+
+        array_push(
+            $searches,
+            $codeSearchSpec
+        );
+
+
+
+        return $searches;
+    }
+    /* CBCodeAdmin_searches() */
+
+
+
     // -- CBHTMLOutput interfaces
 
 
