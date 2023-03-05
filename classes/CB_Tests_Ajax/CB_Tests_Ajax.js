a8e8fcd48dce224d36035b69bdf6c0d9a31f0afb
Matt Calkins (wolf.mattcalkins.corp.mattifesto.com) <matt@mattifesto.com>
2023-03-05 08:28:43 -0800

    CB_Tests_Ajax.js : create CB_Tests_Ajax_checkErrorAjaxResponseProperties()

--- classes/CB_Tests_Ajax/CB_Tests_Ajax.js
+++ classes/CB_Tests_Ajax/CB_Tests_Ajax.js
@@ -1,16 +1,91 @@
 /* global
     CBAjax,
+    CBAjaxResponse,
+    CBConvert,
     CBException,
+    CBTest,
 */
 
 
 (function ()
 {
+    "use strict";
+
+
+
     window.CB_Tests_Ajax =
     {
+        checkErrorAjaxResponseProperties:
+        CB_Tests_Ajax_checkErrorAjaxResponseProperties,
+
         interfaceHasNotBeenImplemented:
         CB_Tests_Ajax_interfaceHasNotBeenImplemented,
     };
+
+
+
+    /**
+     * @return Promise -> undefined
+     */
+    async function
+    CB_Tests_Ajax_checkErrorAjaxResponseProperties(
+    ) // Promise -> object|undefined
+    {
+        try
+        {
+            await
+            CBAjax.call2(
+                "CB_Ajax_TestFunctionThatAlwaysProducesAnError"
+            );
+        }
+
+        catch (
+            error
+        ) {
+            let ajaxResponse =
+            CBAjaxResponse.fromError(
+                error
+            );
+
+            if (
+                ajaxResponse === undefined
+            ) {
+                throw CBException.withError(
+                    error,
+                    "The error object does not have an ajax response.",
+                    "ec156c06bc4b2e316f3405fbe835a4531acec182"
+                );
+            }
+
+            let actualCBMessage =
+            CBAjaxResponse.getCBMessage(
+                ajaxResponse
+            );
+
+            let expectedCBMessage =
+            "(cbmessage (b)) exception message";
+
+            if (
+                actualCBMessage !==
+                expectedCBMessage
+            ) {
+                let title =
+                CBConvert.stringToCleanLine(`
+
+                    The actual ajax response cbmessage does not match the
+                    expected ajax response cbmessage.
+
+                `);
+
+                return CBTest.resultMismatchFailure(
+                    title,
+                    actualCBMessage,
+                    expectedCBMessage
+                );
+            }
+        }
+    }
+    // CB_Tests_Ajax_checkErrorAjaxResponseProperties()
 
 
 
