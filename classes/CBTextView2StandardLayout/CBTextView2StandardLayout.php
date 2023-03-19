c616ba694b67f98d7818a15c58c4c4eff68a8f68
Matt Calkins (wolf.mattcalkins.corp.mattifesto.com) <matt@mattifesto.com>
2023-03-19 12:11:27 -0700

    CBTextView2StandardLayout.php : update CSS file version number

--- classes/CBTextView2StandardLayout/CBTextView2StandardLayout.php
+++ classes/CBTextView2StandardLayout/CBTextView2StandardLayout.php
@@ -7,10 +7,29 @@
 final class
 CBTextView2StandardLayout
 {
+    // -- CBHTMLOutput interfaces
+
+
+
     /**
      * @return [string]
      */
-    static function CBHTMLOutput_CSSURLs() {
-        return [Colby::flexpath(__CLASS__, 'v374.css', cbsysurl())];
+    static function
+    CBHTMLOutput_CSSURLs(
+    ): array
+    {
+        $arrayOfCSSURLs =
+        [
+            CBLibrary::buildLibraryClassFilePath(
+                __CLASS__,
+                '2023_03_19_1679253018',
+                'css',
+                cbsysurl()
+            ),
+        ];
+
+        return $arrayOfCSSURLs;
     }
+    // CBHTMLOutput_CSSURLs()
+
 }
