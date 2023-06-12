23e6f11c73d93175bf324b55dc4d4579d8f3c266
Matt Calkins (wolf.mattcalkins.corp.mattifesto.com) <matt@mattifesto.com>
2023-06-11 18:20:06 -0700

    CB_TestingDockerComposeFileCreator : create

---
+++ classes/CB_TestingDockerComposeFileCreator/CB_TestingDockerComposeFileCreator.php
@@ -0,0 +1,56 @@
+<?php
+
+final class
+CB_TestingDockerComposeFileCreator
+{
+    // -- functions
+
+
+
+    /**
+     * @return string
+     */
+    static function
+    create(
+    ): string
+    {
+        $mysqlRootPassword =
+        CBDB::generateDatabasePassword();
+
+        $mysqlUserUsername =
+        CBDB::generateDatabaseUsername();
+
+        $mysqlUserPassword =
+        CBDB::generateDatabasePassword();
+
+        $my =
+        <<<EOT
+
+        services:
+
+
+            db:
+                environment:
+                    MYSQL_ROOT_PASSWORD: $mysqlRootPassword
+                    MYSQL_USER: $mysqlUserUsername
+                    MYSQL_PASSWORD: $mysqlUserPassword
+                    MYSQL_DATABASE: thedatabase
+                image: mysql
+                ports:
+                    - "51635:3306"
+                restart: always
+
+
+            web:
+                container_name: web_container_23
+                image: php:8.0-apache
+                ports:
+                    - "8082:80"
+
+        EOT;
+
+        return $my;
+    }
+    // go()
+
+}
