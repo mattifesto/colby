79b2a559fd70dfe30dbeb926e40407055a0de414
Matt Calkins (wolf.mattcalkins.corp.mattifesto.com) <matt@mattifesto.com>
2023-07-02 13:32:58 -0700

    classes/CB_TestingDockerComposeFileCreator/CB_TestingDockerComposeFileCreator.php : add to Dockerfile

--- classes/CB_TestingDockerComposeFileCreator/CB_TestingDockerComposeFileCreator.php
+++ classes/CB_TestingDockerComposeFileCreator/CB_TestingDockerComposeFileCreator.php
@@ -49,8 +49,33 @@ CB_TestingDockerComposeFileCreator
                 build:
                     context: .
                     dockerfile_inline: |
+
                         FROM php:8.0-apache
+
                         COPY . /var/www/html
+
+                        ENV CB_DISABLE_SSL true
+
+                        RUN apt-get update
+
+                        RUN apt-get install -y \
+                            libfreetype6-dev \
+                            libjpeg62-turbo-dev \
+                            libpng-dev \
+                            libwebp-dev \
+                            zlib1g-dev
+
+                        RUN docker-php-ext-configure gd \
+                            --with-freetype \
+                            --with-jpeg \
+                            --with-webp
+
+                        RUN docker-php-ext-install \
+                            gd \
+                            mysqli
+
+                        RUN a2enmod rewrite
+
                 ports:
                     - "$websitePort:80"
                 restart: always
