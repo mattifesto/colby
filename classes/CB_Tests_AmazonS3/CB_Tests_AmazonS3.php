c6b801ccdd42f65e0af281f21bfa7f3aedebf241
Matt Calkins <matt@mattifesto.com>
2023-11-13 03:31:15 +0000

    fix #59

--- classes/CB_Tests_AmazonS3/CB_Tests_AmazonS3.php
+++ classes/CB_Tests_AmazonS3/CB_Tests_AmazonS3.php
@@ -5,6 +5,18 @@ use Aws\Exception\AwsException;
 
 
 
+/**
+ * Matt Calkins
+ * 2023-11-12
+ *
+ *      To run this test set the following environment variables before starting
+ *      Apache in dev or define these variables in your composer file used to
+ *      run a container.
+ *
+ *      export AWS_ACCESS_KEY_ID=<value>
+ *      export AWS_SECRET_ACCESS_KEY=<value>
+ *      export AWS_DEFAULT_REGION=<value>
+ */
 final class
 CB_Tests_AmazonS3
 {
@@ -41,14 +53,27 @@ CB_Tests_AmazonS3
     test1()
     : void
     {
+        /**
+         * 2023-11-12
+         * Matt Calkins
+         *
+         *      This API will not read the AWS_DEFAULT_REGION environment
+         *      variable because that variable is only used by the CLI according
+         *      to the following:
+         *
+         *          https://github.com/aws/aws-sdk-php/issues/2658
+         *
+         *      So we read the environment variable and use it.
+         */
+
+        $awsDefaultRegion =
+        getenv('AWS_DEFAULT_REGION');
+
         $s3Client =
         new S3Client(
             [
-                'profile' =>
-                'colby-dev',
-
                 'region' =>
-                'us-west-2',
+                $awsDefaultRegion,
 
                 'version' =>
                 '2006-03-01',
